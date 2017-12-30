c82439e7b1639e666495d2f62a08af9471cb841b
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2017-12-30 11:20:35 -0600

    removed #define int long long, causing RTE

--- header.cpp
+++ header.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 #include <unordered_map>
 using namespace std;
-#define int long long
 #define ll long long
 #define ld long double
 #define pb push_back
@@ -10,13 +9,13 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define uni(x) (x).erase(unique(all(x)), (x).end())
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define rep1(i, n) for (int i = 1; i <= (int)(n); ++i)
-#define infll 0x7fffffffffffffff
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define rep1(i, n) for (ll i = 1; i <= (ll)(n); ++i)
 const ld pi = 4.0*atanl(1.0);
+const ll infll = (ll)(1e18) + 10;
 const ll mod = powl(10, 9) + 7;
 
-int32_t main() {ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+int main() {ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
     //freopen("e.in", "r", stdin);
     //freopen("e.out", "w", stdout);
 
