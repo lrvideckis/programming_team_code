78f8430010a5159d7ec917117f0d289959a0d0c6
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2018-01-18 16:11:24 -0700

    reimplemented with better performance and error checking

--- math/choose.cpp
+++ math/choose.cpp
@@ -5,18 +5,23 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
-ll choose(ll n, ll k, unordered_map<string, ll> &table) {
+ll choose(int n, int k) {
+    if(k < 0 || k > n || n < 0) return 0;
     if(k == 0 || k == n) {
         return 1;
     }
-    string index = to_string(n)+'$'+to_string(k);
-    if(table.find(index) != table.end()) return table[index];
-    table[index] = choose(n-1, k-1, table)+choose(n-1, k, table);
-    return table[index];
+    static map<pair<int, int> , ll> table;
+    if(table.count({n,k})) return table[{n,k}];
+    return table[{n,k}] = choose(n-1, k-1)+choose(n-1, k);
 }
 
 int main() {
-    
+    cout << "n,k,choose(n,k):\n";
+    for(int n = -1; n < 5; n++) {
+        for(int k = -1; k < 5; ++k) {
+            cout << n << '\t' << k << '\t' << choose(n, k) << '\n';
+        }
+    }
     return 0;
 }
 
