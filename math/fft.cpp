159b80ccf1f6f262b9873009f162b6437ad4f47c
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2018-06-30 22:26:25 -0500

    lowered implementation constants

--- math/fft.cpp
+++ math/fft.cpp
@@ -1,157 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-
-//maxDist is the longest distince we can hit
-//maxLength is the maximum length of a hole
-int maxDist = 0, maxLength = 0;
-
-void mult(vector<int> &a, vector<int> &b, vector<int> &c) {
-    c.clear();
-    for(int degree = 0; degree <= maxLength; ++degree) {
-        int sum = 0;
-        for(int i = 0; i <= degree; ++i) {
-            sum += a[degree-i] * b[i];
-        }
-        c.push_back(sum);
-    }
-}
-
 typedef complex<double> cd;
-typedef vector<cd> vcd;
 
-vcd fft(const vcd &as) {
-    int n = as.size();
-    int k = 0;
-    while ((1 << k) < n) k++;
-    vector<int> rev(n);
-    rev[0] = 0;
-    int high1 = -1;
-    for (int i = 1; i < n; i++) {
-        if ((i & (i - 1)) == 0)
-            high1++;
-        rev[i] = rev[i ^ (1 << high1)];
-        rev[i] |= (1 << (k - high1 - 1));
+const int Log = 19, Max = 1 << Log;
+const double pi = 4*atan(1.0);
+cd root[Max], arrA[Max], arrB[Max];
+int perm[Max];
+
+void fft(cd* arr) {
+    for(int i = 0; i < Max; i++) {
+        if(i < perm[i]) {
+            swap(arr[i], arr[perm[i]]);
+        }
     }
-    vcd roots(n);
-    for (int i = 0; i < n; i++) {
-        double alpha = 2 * M_PI * i / n;
-        roots[i] = cd(cos(alpha), sin(alpha));
-    }
-
-    vcd cur(n);
-    for (int i = 0; i < n; i++)
-        cur[i] = as[rev[i]];
-
-    for (int len = 1; len < n; len <<= 1) {
-        vcd ncur(n);
-        int rstep = roots.size() / (len * 2);
-        for (int pdest = 0; pdest < n;) {
-            int p1 = pdest;
-            for (int i = 0; i < len; i++) {
-                cd val = roots[i * rstep] * cur[p1 + len];
-                ncur[pdest] = cur[p1] + val;
-                ncur[pdest + len] = cur[p1] - val;
-                pdest++, p1++;
+    for(int len = 1; len < Max; len *= 2) {
+        for(int pos = 0; pos < Max; pos += 2 * len) {
+            for(int i = 0; i < len; i++) {
+                cd x = arr[pos + i], y = arr[pos + i + len] * root[Max / len / 2 * i];
+                arr[pos + i] = x + y;
+                arr[pos + i + len] = x - y;
             }
-            pdest += len;
-        }
-        cur.swap(ncur);
-    }
-    return cur;
-}
-
-void reverseFFT(int n, vector<complex<double> > &coef, vector<complex<double> > &roots) {
-    roots.clear();
-    roots = fft(coef);
-    reverse(roots.begin()+1,roots.end());
-    for(complex<double> &r : roots) r = r.real() / n;
-}
-
-void multFFT(vector<int> &a, vector<int> &b, vector<int> &c) {
-    int n = a.size() + b.size() - 1;
-    n = (ll)(ceil(log2(n)));
-    n = (ll)pow(2, n);
-    vector<complex<double> > coefA, coefB, rootsA, rootsB;
-    for(int i = 0; i < n; ++i) {
-        if(i < a.size()) {
-            coefA.push_back(complex<double>(a[i],0));
-        } else {
-            coefA.push_back(complex<double>(0,0));
-        }
-        if(i < b.size()) {
-            coefB.push_back(complex<double>(b[i],0));
-        } else {
-            coefB.push_back(complex<double>(0,0));
         }
     }
-    rootsA = fft(coefA);
-    rootsB = fft(coefB);
-
-    vector<complex<double> > prod(n);
-    for(int i = 0; i < n; ++i) {
-        prod.at(i) = rootsA[i] * rootsB[i];
-    }
-    vector<complex<double> > answer;
-       
-    reverseFFT(n,prod,answer);
-       
-    //c.clear();
-    for(auto &x : answer) {
-        c.push_back(round(x.real()));
-    }
-    c.resize(a.size() + b.size() - 1);
 }
 
-int32_t main() {
-    ios::sync_with_stdio(false);
+void mult(int* a, int* b, ll* ans) {
+    for(int i = 0; i < Max; i++) {
+        arrA[i] = cd(a[i], 0);
+        arrB[i] = cd(b[i], 0);
+    }
+    fft(arrA);
+    fft(arrB);
+    for(int i = 0; i < Max; i++) {
+        arrA[i] *= arrB[i];
+    }
+    fft(arrA);
+    reverse(arrA + 1, arrA + Max);
+    for(int i = 0; i < Max; i++) {
+        arrA[i] /= Max;
+        ans[i] = (ll)(round(arrA[i].real()));
+    }
+}
+
+void pre_proc() {
+    int hb = -1;
+    root[0] = 1;
+    double angle = 2 * pi / Max;
+    for(int i = 1; i < Max; i++) {
+        if((i & (i - 1)) == 0) hb++;
+        root[i] = cd(cos(angle * i), sin(angle * i));
+        perm[i] = perm[i ^ (1 << hb)] + (1 << (Log - hb - 1));
+    }
+}
+
+int a[Max] = {0}, b[Max] = {0};
+ll prod[Max];
+
+int main() {
+    ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int n;
-    cin >> n;
-    vector<int> dist(n);
-    for(int &x : dist) {
-        cin >> x;
-        maxDist = max(maxDist, x);
+    pre_proc();
+    int sizeA;
+    cin >> sizeA;
+    cin >> sizeA;
+    for(int i = 0; i <= sizeA; ++i) {
+        cin >> a[i];
     }
-    int m;
-    cin >> m;
-    vector<int> holeLengths(m);
-    for(int &x : holeLengths) {
-        cin >> x;
-        maxLength = max(maxLength, x);
+    int sizeB;
+    cin >> sizeB;
+    for(int i = 0; i <= sizeB; ++i) {
+        cin >> b[i];
     }
-    vector<int> a(maxLength+1,0),b(maxLength+1,0),c;
-    a[0] = 1;
-    for(int x : dist) a[x] = 1;
-    for(int x : holeLengths) b[x] = 1;
-    multFFT(a, a,c);
-    int holes = 0;
-    for(int i = 0; i <= maxLength; ++i) 
-        if(b[i] == 1 && c[i] >= 1) {
-            holes++;
-        }
-    
-    cout << holes << '\n';
+    mult(a, b, prod);
+    cout << sizeA+sizeB << '\n';
+    for(int i = 0; i <= sizeA+sizeB; ++i) cout << prod[i] << ' ';cout << '\n';
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
