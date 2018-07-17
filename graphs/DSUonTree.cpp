e4548f14ce4d3c1f1962929d5020759914152ee5
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2018-07-17 18:11:29 -0500

    removed specific input

--- graphs/DSUonTree.cpp
+++ graphs/DSUonTree.cpp
@@ -55,18 +55,6 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int n;
-    cin >> n;
-    for(int i = 1; i <= n; ++i) {
-        cin >> color[i];
-        cnt[i] = 0;
-    }
-    int u, v;
-    for(int i = 0; i < n-1; ++i) {
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
     dfs(1, 1);
     dfs1(1, 1);
     return 0;
