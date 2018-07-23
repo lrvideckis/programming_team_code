83a71571d72560a85d63669f7435bf3feb33cead
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2018-07-22 20:09:06 -0500

    longest increasing subsequence

---
+++ Data Structures & Algos/LIS.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> LIS(vector<int> &arr) {//longest non-decreasing sequence
+    vector<int> longest(arr.size(),0);
+    multiset<int> seq;
+    for(int i = 0; i < arr.size(); ++i) {
+        seq.insert(arr[i]);
+        auto it = seq.upper_bound(arr[i]);
+        if(it != seq.end()) seq.erase(it);
+        longest[i] = seq.size();
+    }
+    return longest;
+}
+
+vector<int> LSIS(vector<int> &arr) {//longest strictly increasing sequence
+    vector<int> longest(arr.size(),0);
+    multiset<int> seq;
+    for(int i = 0; i < arr.size(); ++i) {
+        seq.insert(arr[i]);
+        auto it = seq.lower_bound(arr[i]);
+        it++;
+        if(it != seq.end()) seq.erase(it);
+        longest[i] = seq.size();
+    }
+    return longest;
+}
+
+int main() {
+    return 0;
+}
