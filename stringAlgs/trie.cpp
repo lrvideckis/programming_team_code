59c9151da5c7e3fbc028b48c59ccbea314b0511b
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-02-22 01:07:11 -0700

    better implementation of trie

--- stringAlgs/trie.cpp
+++ stringAlgs/trie.cpp
@@ -1,140 +1,96 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-struct trie {//only lowercase letters for all words
-private:
-    struct node {
-        bool isWord;
-        vector<node*> children;
-        node() {
-            isWord=false;
-            children.resize(26,nullptr);
-        }
-    };
-    node *root;
-    //for easy memory cleanup
-    vector<node*> allNodes;
-public:
-    trie() {
-        root = new node();
-        allNodes.push_back(root);
-    }
-    trie(vector<string> &arr) {//initialize a trie and insert all words in arr
-        root = new node();
-        allNodes.push_back(root);
-        for(string x : arr) insert(x);
-    }
-    ~trie() {
-        for(node* x : allNodes) delete x;
-    }
-    void insert(string word) {
-        node *curr = root;
-        for(char x : word) {
-            if(curr->children[x-'a'] == nullptr) {
-                curr->children[x-'a'] = new node();
-                allNodes.push_back(curr->children[x-'a']);
-            }
-            curr = curr->children[x-'a'];
-        }
-        curr->isWord = true;
-    }
-    bool find(string word) {
-        node* curr = root;
-        for(char x : word) {
-            if(curr->children[x-'a'] == nullptr) return false;
-            curr = curr->children[x-'a'];
-        }
-        return curr->isWord;
-    }
-    void erase(string word) {
-        node* curr = root;
-        for(char x : word) {
-            if(curr->children[x-'a'] == nullptr) return;
-            curr = curr->children[x-'a'];
-        }
-        curr->isWord = false;
-    }
-    void preorder(node* curr, vector<string> &arr, string &word) {
-        if(curr->isWord) arr.push_back(word);
-        for(int i = 0; i < 26; ++i) {
-            if(curr->children[i] != nullptr) {
-                word += i+'a';
-                preorder(curr->children[i], arr, word);
-                word.pop_back();
-            }
-        }
-    }
-    vector<string> sort() {//Pre-order traversal to get all words in sorted order
-        vector<string> arr;
-        string word = "";
-        preorder(root, arr, word);
-        return arr;
+const int K = 26;//character size
+
+struct node {
+    int next[K];
+    bool leaf = 0;
+    char pch;
+    int p = -1;
+    int id;
+    node(int p = -1,char ch = '#'):p(p),pch(ch) {
+        fill(next,next+K,-1);
     }
 };
 
+vector<node> t(1);//adj list
 
-const int Max = 1e5+100;
-const int root = 1;
-set<int> adj[Max];
-vector<int> children[Max];
-int cnt[Max];
+void add_string(string s, int id) {
+    int c = 0;
+    for(char ch: s){
+        int v = ch-'a';
+        if(t[c].next[v] == -1) {
+            t[c].next[v] = t.size();
+            t.emplace_back(c,ch);
+        }
+        c = t[c].next[v];
+    }
+    t[c].leaf = 1;
+    t[c].id = id;
+}
+
+void remove_string(string s) {
+    int c = 0;
+    for(char ch: s){
+        int v = ch-'a';
+        if(t[c].next[v] == -1) {
+            return;
+        }
+        c = t[c].next[v];
+    }
+    t[c].leaf = 0;
+}
+
+int find_string(string s) {
+    int c = 0;
+    for(char ch: s){
+        int v = ch-'a';
+        if(t[c].next[v] == -1) {
+            return -1;
+        }
+        c = t[c].next[v];
+    }
+    if(!t[c].leaf) return -1;
+    return t[c].id;
+}
 
 int32_t main() {
-    int n;
-    cin >> n;
-    string s;
-    int nextID = 2;
-    for(int i = 0; i < n; ++i) {
-        cin >> s;
-        int curr = root;
-        for(int j = 0; j < (int)s.size(); ++j) {
-            if(children[curr].empty()) children[curr].resize(26,-1);
-            if(children[curr][s[j]-'a'] == -1) {
-                children[curr][s[j]-'a'] = nextID++;
-            }
-            int next = children[curr][s[j]-'a'];
-            adj[curr].insert(next);
-            adj[next].insert(curr);
-            if(j+1 == (int)s.size()) {
-                cnt[next]++;
-            }
-            curr = next;
-        }
-    }
-
-
-
 
     vector<string> words = {"mom", "dad", "aj", "luke", "luck"};
-    trie t(words);
+    for(int i = 0; i < words.size(); ++i) {
+        add_string(words[i], i);
+    }
     
     cout<<"searching for words in/not in trie:\n";
-    cout << "mom: " << t.find("mom") << '\n';
-    cout << "mo: " << t.find("mo") << '\n';
-    cout << "momo: " << t.find("momo") << '\n';
-    cout << "lucky: " << t.find("lucky") << '\n';
-    cout << "luck: " << t.find("luck") << '\n';
-    cout << "luck: " << t.find("luck") << '\n';
-    cout << "luke: " << t.find("luke") << "\n\n";
+    cout << "mom: " << find_string("mom") << '\n';
+    cout << "mo: " << find_string("mo") << '\n';
+    cout << "momo: " << find_string("momo") << '\n';
+    cout << "lucky: " << find_string("lucky") << '\n';
+    cout << "luck: " << find_string("luck") << '\n';
+    cout << "luke: " << find_string("luke") << "\n\n";
     
     cout<<"erasing luke\n";
-    t.erase("luke");
-    cout << "luke: " << t.find("luke") << "\n\n";
+    remove_string("luke");
+    cout << "luke: " << find_string("luke") << "\n\n";
     
     cout<<"inserting luke back\n";
-    t.insert("luke");
-    cout << "luke: " << t.find("luke") << '\n';
-    cout << "lukey: " << t.find("lukey") << "\n\n";
+    add_string("luke", 123);
+    cout << "luke: " << find_string("luke") << '\n';
+    cout << "lukey: " << find_string("lukey") << "\n\n";
      
     cout<<"trying to erase word not in list:\n";
-    t.erase("lukey");
-    cout << "lukey: " << t.find("lukey") << "\n\n";
+    remove_string("lukey");
+    cout << "lukey: " << find_string("lukey") << "\n\n";
     
+    /*
     cout<<"printing all names in sorted order:\n";
     
     vector<string> temp = t.sort();
     for(string x : temp)cout<<x<<'\n';
     
+    */
     return 0;
 }
 
