c7b78acfa309eb0179d069e75e5d9b1d1151c540
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-18 11:08:03 -0600

    removed template

--- math/fib.cpp
+++ math/fib.cpp
@@ -1,20 +1,3 @@
-#define _USE_MATH_DEFINES//M_PI
-#include <bits/stdc++.h>
-#include <unordered_map>
-using namespace std;
-#define int long long
-#define ll long long
-#define ld long double
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define uni(x) (x).erase(unique(all(x)), (x).end());
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define rep1(i, n) for (int i = 1; i <= (int)(n); ++i)
-const ll infll = powl(2, 62)-1;
-const int mod = pow(10, 9) + 7;
-
 //**O(log(n))**
 unordered_map<ll,ll> table;
 int fib(int n) {
@@ -23,28 +6,3 @@ int fib(int n) {
     table[n] = (fib((n+1) / 2)*fib(n/2) + fib((n-1) / 2)*fib((n-2) / 2)) % mod;
     return table[n];
 }
-
-int32_t main() {
-    rep(i, 10) cout << i << ' ' << fib(i) << endl;
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
