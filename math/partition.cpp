93b47fb82c6ba8217f80d90cbec6670e2d8bbfb6
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-18 11:20:04 -0600

    cleaned partition

--- math/partition.cpp
+++ math/partition.cpp
@@ -1,53 +1,23 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-struct partitionFunction
-{
-    map<ll, ll> remember;
-    
-    
+struct partitionFunction {
+    vector<ll> remember;
     //The number of ways you can add to a number
-    ll getPartitionsModM(ll n, ll m)
-    {
-        if(n < 0)
-            return 0;
-        if(n == 0)
-            return 1;
-        if(remember.find(n) != remember.end())
-            return remember[n];
+    ll getPartitionsModM(ll n, ll m) {
+        if(n < 0) return 0;
+        if(n == 0) return 1;
+        if(remember.size() <= n) remember.resize(n+1,-1);
+        if(remember[n] != -1) return remember[n];
         ll sum = 0;
         ll val = 1;
-        for(ll i = 1;  val <= n; i++)
-        {
+        for(ll i = 1; val <= n; i++) {
             ll multiply = 1;
-            if(i % 2 == 0)
-                multiply = -1;
-            ll three = 3;
-            val = ((three*i*i) + i) / 2;
-            sum += getPartitionsModM(n - val, m) * multiply;
-            val = ((three*i*i) - i) / 2;
-            sum += getPartitionsModM(n - val, m) * multiply;
+            if(i % 2 == 0) multiply = -1;
+            val = ((3*i*i) + i) / 2;
+            sum += getPartitionsModM(n - val, m) * multiply % m;
+            val = ((3*i*i) - i) / 2;
+            sum += getPartitionsModM(n - val, m) * multiply % m;
+            sum %= m;
+            if(sum < 0) sum += m;
         }
-        remember[n] = sum % m;
-        return sum % m;
+        return remember[n] = sum % m;
     }
 };
-
-int main() {
-    partitionFunction p;
-    cout << p.getPartitionsModM(4ll,1000ll) << '\n';
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
