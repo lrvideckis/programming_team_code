dd9a6b7e39f1a8910e2eb7ccee89b6b7ea649d00
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-18 11:05:41 -0600

    iterative fast pow

--- math/fastPow.cpp
+++ math/fastPow.cpp
@@ -1,47 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-const int mod = (int)1e9 + 7;
-
-ll power(ll a, ll b, ll c = mod) {//returns a^b mod c
-    if (b <= 0) return 1;
-    ll temp = power(a, b / 2, c) % c;
-    if (b % 2 == 0) {
-        return (temp * temp) % c;
-    }
-    return (((a * temp) % c) * temp) % c;
+ll fastPow(ll a, ll pw, ll mod) {
+	ll res = 1;
+	while(pw) {
+		if(pw & 1) res = (res*a)%mod; 
+		a = (a*a)%mod;
+		pw >>= 1;
+	}
+	return res;
 }
-
-//this function returns base^exp in log(exp) time
-ll fastpow(ll base, ll exp) {
-    if(exp == 0) {
-        return 1;
-    }
-    ll temp;
-    if(exp % 2 == 0) {
-        temp = fastpow(base, exp / 2);
-        temp *= temp;
-    } else {
-        temp = fastpow(base, exp - 1) * base;
-    }
-    return temp;
-}
-
-int main() {
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
