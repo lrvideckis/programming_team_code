c53373fa3c1c35323ec870faf3ca60c7a0eef676
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-18 13:43:11 -0600

    cleaned

--- math/combinatorics.cpp
+++ math/combinatorics.cpp
@@ -22,6 +22,7 @@ ll fastPow(ll a, ll pw, ll mod) {
 struct NchooseK {
     int range, mod;
     vector<ll> fact,ifact;
+    
     NchooseK(int currMod) {
         this->mod = currMod;
         range = (int)1e6+3;
@@ -29,6 +30,7 @@ struct NchooseK {
         ifact.resize(range+1);
         calcFacts();
     }
+    
     void calcFacts() {
         fact[0] = 1;
         for(ll i = 1; i <= range; ++i) {
@@ -39,6 +41,8 @@ struct NchooseK {
             ifact[i] = (1LL*ifact[i+1]*(i+1))%this->mod;
         }
     }
+    
+    //helper function for calcChoose
     ll modFact(ll n, ll &e) const {
         if(n <= 1) return 1;
         ll res = modFact(n/this->mod, e);
@@ -46,6 +50,9 @@ struct NchooseK {
         if ((n/this->mod)%2 == 1) return res*(fact[n%this->mod]*(this->mod-1)%this->mod)%this->mod;
         return res*fact[n%this->mod]%this->mod;
     }
+    
+    //n choose k with n >= mod
+    //***use prime moduli***
     ll calcChoose(ll n, ll k) const {
         ll e1 = 0, e2 = 0, e3 = 0;
         ll a1 = modFact(n, e1);
@@ -54,7 +61,9 @@ struct NchooseK {
         if (e1-e2-e3 > 0) return 0;
         return (a1*fastPow (a2*a3%this->mod, this->mod-2, this->mod)%this->mod);
     }
+    
     //classic n choose k
+    //***use prime moduli***
     ll choose(int n, int k) const {
         if(k < 0 || k > n || n < 0) return 0;
         return ((1LL*fact[n]*ifact[k])%this->mod * 1LL*ifact[n-k])%this->mod;
@@ -63,17 +72,14 @@ struct NchooseK {
     //lucas theorem to calculate n choose k in O(log(k))
     //***use prime moduli***
     //***can only use with: prime moduli < 1e6***
-    
-    //n choose k, using the lucas theorem
     ll chooseLucas(ll n, ll k) const {
         if(k < 0 || k > n || n < 0) return 0;
-        if(k == 0) {
-            return 1;
-        }
+        if(k == 0) return 1;
         int ni = n % this->mod;
         int ki = k % this->mod;
         return (this->chooseLucas(n/this->mod, k/this->mod) * this->choose(ni,ki)) % this->mod;
     }
+    
     //bars and stars problem: given n objects, each with an endless supply,
     //this returns the number of ways to choose k of them.
     ll multiChoose(ll n, ll k) const {
