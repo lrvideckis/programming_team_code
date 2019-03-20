6cd493f7310bb6a2434511f2f47f6ce430169c23
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-20 12:12:52 -0600

    less mods

--- math/sumOfKthPowers.cpp
+++ math/sumOfKthPowers.cpp
@@ -14,27 +14,27 @@ ll fastPow(ll a, ll pw, ll mod) {
 
 //Faulhaber'the sum of the k-th powers of the first n positive integers
 //1^k + 2^k + 3^k + 4^k + ... + n^k
-//O(k)
+//O(k*log(k))
 int faulhaber(long long n, int k){
     vector<ll> sum(k+5, 0);
-	for(ll i=1;i<=k+2;i++)
-		sum[i]=(sum[i-1]+fastPow(i,k, mod))%mod;
-	if(n<=k+2)
-		return sum[n];
-	ll t = 1;
+    for(ll i=1;i<=k+2;i++) {
+        sum[i]=sum[i-1]+fastPow(i,k, mod);
+        if(sum[i] >= mod) sum[i] -= mod;
+    }
+    if(n<=k+2) return sum[n];
+    ll t = 1;
     vector<ll> fac(k+5, 1);
-	for(ll i=1;i<=k+2;i++)
-	{
-		t=(n-i)*t%mod;
-		fac[i]=fac[i-1]*i%mod;
-	}
-	ll ans = 0;
-	for(ll i=1;i<=k+2;i++)
-	{
-		ll t1=fastPow(fac[i-1]*fac[k-i+2]%mod,mod-2, mod);
-		ll t2=fastPow(n-i,mod-2, mod);
-		if((k-i+2)&1)t1=-t1;
-		ans=(ans+sum[i]*t%mod*t1%mod*t2%mod)%mod;
-	}
-	return (ans+mod)%mod;
+    for(ll i=1;i<=k+2;i++) {
+        t=(n-i)*t%mod;
+        fac[i]=fac[i-1]*i%mod;
+    }
+    ll ans = 0;
+    for(ll i=1;i<=k+2;i++) {
+        ll t1=fastPow(fac[i-1]*fac[k-i+2]%mod,mod-2, mod);
+        ll t2=fastPow(n-i,mod-2, mod);
+        if((k-i+2)&1)t1=-t1;
+        ans+=sum[i]*t%mod*t1%mod*t2%mod;
+        if(ans >= mod) ans -= mod;
+    }
+    return (ans+mod)%mod;
 }
