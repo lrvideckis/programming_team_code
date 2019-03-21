8d49e750f0fbad100006f3810d04d5e2b3ce41ae
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-20 22:17:40 -0600

    moved out into struct

--- ds/cntRectangles.cpp
+++ ds/cntRectangles.cpp
@@ -1,38 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-bool table[2002][2002] = {0};
-int arr[2002][2002] = {0}, cnt[2002][2002] = {0};
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int rows, cols, n, d;
-    cin >> rows >> cols >> n >> d;
-    int i1, i2, j1, j2;
-    while(n--) {
-        cin >> i1 >> i2 >> j1 >> j2;
-        for(int i = i1+1; i <= i2; ++i) {
-            for(int j = j1+1; j <= j2; ++j) {
-                table[i][j] = true;
-            }
-        }
-    }
+//given a 2D boolean matrix, calculate cnt[i][j]
+//cnt[i][j] = the number of times an (i * j) rectangle appears in the matrix
+//such that all cells in the rectangle are true
+//O(R*C)
+vector<vector<int>> getNumRectangles(vector<vector<bool>> &grid) {
+    vector<vector<int>> cnt;
+    const int rows = grid.size();
+    if(rows == 0) return cnt;
+    const int cols = grid[0].size();
+    if(cols == 0) return cnt;
+    cnt.resize(rows+1, vector<int>(cols+1, 0));
+    vector<vector<int>> arr(rows+2, vector<int>(cols+1, 0));
     for(int i = 1; i <= rows; ++i) {
         for(int j = 1; j <= cols; ++j) {
             arr[i][j] = 1 + arr[i][j-1];
-            if(table[i][j]) arr[i][j] = 0;
+            if(grid[i-1][j-1]) arr[i][j] = 0;
         }
     }
-    int tempRows;
-    bool debug = true;
     for(int j = 1; j <= cols; ++j) {
         arr[rows+1][j] = 0;
         stack<pair<int, int> > st;
         st.push({0,0});
-        int prevZero = 0;
         for(int i = 1; i <= rows+1; ++i) {
             pair<int, int> curr = {i, arr[i][j]};
             while(arr[i][j] < st.top().second) {
@@ -57,11 +44,5 @@ int main() {
             cnt[i][j] += cnt[i][j+1];
         }
     }
-    int i, j;
-    while(d--) {
-        cin >> i >> j;
-        //cout << naive(i, j, rows, cols) << '\n';
-        cout << cnt[i][j] << '\n';
-    }
-    return 0;
+    return cnt;
 }
