77c11617d1bcddf21b1a0aa11f7aa9232da70b9d
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-20 23:12:50 -0600

    cleaned into a struct

--- rangeDS/sparseTable2D.cpp
+++ rangeDS/sparseTable2D.cpp
@@ -1,33 +1,33 @@
-int M[1002][1002][11][11],prelog[1002];
-void calcprelog()
-{
-    REP(i,1,1001)prelog[i]=log2(i);
-}
-
-void precompute_max(){
-    for (int i = 0 ; (1<<i) <= n; i += 1){
-        for(int j = 0 ; (1<<j) <= m ; j += 1){
-            for (int x = 1 ; x + (1<<i) -1 <= n; x+= 1){
-                for (int y = 1 ;  y + (1<<j) -1 <= m; y+= 1){
-                    if (i == 0 and j == 0)
-                        M[x][y][i][j] = arr[x][y]; // store x, y
-                    else if (i == 0)
-                        M[x][y][i][j] = max(M[x][y][i][j-1], M[x][y+(1<<(j-1))][i][j-1]);
-                    else if (j == 0)
-                        M[x][y][i][j] = max(M[x][y][i-1][j], M[x+ (1<<(i-1))][y][i-1][j]);
-                    else
-                        M[x][y][i][j] = max(max(M[x][y][i-1][j-1], M[x + (1<<(i-1))][y][i-1][j-1]), max(M[x][y+(1<<(j-1))][i-1][j-1], M[x + (1<<(i-1))][y+(1<<(j-1))][i-1][j-1]));
-                    // cout << "from i="<<x<<" j="<<y<<" of length="<<(1<<i)<<" and length="<<(1<<j) <<"max is: " << M[x][y][i][j] << endl;
-                }
-            }
+//***zero based indexing
+int table[11][1010][11][1010],logTwo[1010];
+struct sparseTable2D {
+    int n, m;
+    //vector<vector<vector<vector<int>>>> table;
+    sparseTable2D(const vector<vector<int>> &Matrix) {
+        n = Matrix.size();
+        m = Matrix[0].size();
+        logTwo[0] = -1;
+        for(int i = 1; i <= max(n,m); ++i) {
+            logTwo[i] = 1 + logTwo[i/2];
         }
+        for(int ir=0; ir<n; ++ir) {
+            for(int ic=0; ic<m; ++ic)
+                table[0][ir][0][ic] = Matrix[ir][ic];
+            for(int jc=1; jc<=logTwo[m]; ++jc)
+                for(int ic=0; ic+(1<<(jc-1))<m; ++ic)
+                    table[0][ir][jc][ic] = min(table[0][ir][jc-1][ic],table[0][ir][jc-1][ic+(1<<(jc-1))]);
+        }
+        for(int jr=1; jr<=logTwo[n]; ++jr)
+            for(int ir=0; ir<n; ++ir)
+                for(int jc=0; jc<=logTwo[m]; ++jc)
+                    for(int ic=0; ic<m; ++ic)
+                        table[jr][ir][jc][ic] = min(table[jr-1][ir][jc][ic],table[jr-1][ir+(1<<(jr-1))][jc][ic]);
     }
-}
-
-int compute_max(int x, int y, int x1, int y1){
-    int k = prelog[x1 - x + 1];
-    int l = prelog[y1 - y + 1];
-    // cout << "Value of k="<<k<<" l="<<l<<endl;
-    int ans = max(max(M[x][y][k][l], M[x1 - (1<<k) + 1][y][k][l]), max(M[x][y1 - (1<<l) + 1][k][l], M[x1 - (1<<k) + 1][y1 - (1<<l) + 1][k][l]));
-    return ans;
-}
+    int queryMin(int x1, int y1, int x2, int y2){
+        int kx = logTwo[x2-x1+1];
+        int ky = logTwo[y2-y1+1];
+        int min_R1 = min(table[kx][x1][ky][y1], table[kx][x1][ky][y2+1-(1<<ky)]);
+        int min_R2 = min(table[kx][x2+1-(1<<kx)][ky][y1], table[kx][x2+1-(1<<kx)][ky][y2+1-(1<<ky)]);
+        return min(min_R1, min_R2);
+    }
+};
