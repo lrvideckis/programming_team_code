f78e1a2fa440f7fb16e93b18c43b95701a2fca8c
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-24 17:24:51 -0600

    asdndcjnwnonxlsand

--- geometry/geometry.cpp
+++ geometry/geometry.cpp
@@ -217,17 +217,3 @@ PT ComputeCentroid(const vector<PT> &p) {
   }
   return c / scale;
 }
-
-// tests whether or not a given polygon (in CW or CCW order) is simple
-bool IsSimple(const vector<PT> &p) {
-  for (int i = 0; i < p.size(); i++) {
-    for (int k = i+1; k < p.size(); k++) {
-      int j = (i+1) % p.size();
-      int l = (k+1) % p.size();
-      if (i == l || j == k) continue;
-      if (SegmentsIntersect(p[i], p[j], p[k], p[l]))
-        return false;
-    }
-  }
-  return true;
-}
