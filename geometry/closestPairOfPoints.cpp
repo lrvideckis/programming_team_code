02211d1fed1015c14725c5ca510e78c2ebc7e2bf
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-24 14:33:48 -0600

    closest pop

--- geometry/closestPairOfPoints.cpp
+++ geometry/closestPairOfPoints.cpp
@@ -1,81 +1,31 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long
 typedef long long ll;
-
-const int inf = 1e18;
-
-int dist(const pair<int, int> &a, const pair<int, int> &b) {
-    return (a.first-b.first)*(a.first-b.first) + (a.second-b.second)*(a.second-b.second);
-}
-
-bool cmpY(const pair<int, int> &a, const pair<int, int> &b) {
-    return a.second < b.second;
-}
-
-int closestPair(vector<pair<int, int>> &points, int l, int r) {//inclusive
-    if(r-l+1 <= 3) {
-        int least = inf;
-        for(int i = l; i <= r; ++i) {
-            for(int j = i+1; j <= r; ++j) {
-                least = min(least, dist(points[i], points[j]));
+pair<int, int> closestPair(vector<pair<ll, ll>> &pts) {
+    int n = pts.size();
+    sort(pts.begin(), pts.end());
+    set<pair<pair<ll, ll>, int>> s;
+    ll best_dist = 1e18;
+    int j = 0;
+    pair<int, int> pos;
+    for (int i = 0; i < n; ++i) {
+        ll d = ceil(sqrt(best_dist));
+        while (pts[i].first - pts[j].first > best_dist) {
+            s.erase({{pts[j].second, pts[j].first}, j});
+            j += 1;
+        }
+        auto it1 = s.lower_bound({{pts[i].second - d, pts[i].first}, -1});
+        auto it2 = s.upper_bound({{pts[i].second + d, pts[i].first}, i});
+        for (auto it = it1; it != it2; ++it) {
+            ll dx = pts[i].first - it->first.second;
+            ll dy = pts[i].second - it->first.first;
+            ll dist = dx * dx + dy * dy;
+            if(best_dist > dist) {
+                best_dist = dist;
+                pos.first = it->second;
+                pos.second = i;
             }
-        }
-        return least;
+        } 
+        s.insert({{pts[i].second, pts[i].first}, i}); 
     }
-    int mid = (l+r)/2;
-    int least = closestPair(points, l, mid);
-    least = min(least, closestPair(points, mid+1, r));
-    vector<pair<int, int>> strip;
-    for(int i = l; i <= r; ++i) {
-        int distToStrip = (points[i].first-points[mid].first)*(points[i].first-points[mid].first);
-        if(distToStrip <= least) {
-            strip.push_back(points[i]);
-        }
-    }
-    sort(strip.begin(), strip.end(), cmpY);
-    for(int i = 0; i < strip.size(); ++i) {
-        for(int j = i+1; j < strip.size(); ++j) {
-            int currDist = dist(strip[i], strip[j]);
-            if(currDist > least) break;
-            least = min(least, currDist);
-        }
-    }
-    return least;
+    return pos;
 }
-
-int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    vector<pair<int, int>> points;
-    cout << closestPair(points, 0, (int)points.size()-1) << '\n';
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
