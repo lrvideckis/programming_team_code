05446392a08b30f6e0fd5b4e44bbb8cd891eb14f
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-03-24 17:22:04 -0600

    asdndcjnwnonxlsand

--- geometry/geometry.cpp
+++ geometry/geometry.cpp
@@ -1,98 +1,87 @@
-#include <iostream>
-#include <vector>
-#include <iomanip>
-#include <cmath>
-#include <algorithm>
-#include <map>
-#include <set>
-#include <limits>
-#include <stack>
-#include <random>
+// C++ routines for computational geometry.
 
-using namespace std;
+double INF = 1e100;
+double EPS = 1e-12;
 
-//Constant values to be returned
-constexpr int Colinear = -1, NoIntersect = 0, Intersect = 1;
-constexpr int CW = 2, CCW = 3;
-constexpr int Inside = 4, Outside = 5, OnEdge = 6;
-
-//Epsilon for all double comparisons
-const double EPSILON = 0.000001;
-
-struct point
-{
-    long double x, y;
-
-    point(long double x_=0, long double y_=0) : x(x_), y(y_){}
-
-    // Only < operator is unusual behavior
-    bool operator <(const point& other) const
-    {
-        return (x < other.x ? true : (x == other.x && y < other.y));
-    }
-
-    bool operator == (const point& other) const
-    {
-        return abs(other.x - x) < EPSILON && abs(other.y - y) < EPSILON;
-    }
-
-    //Add other operators as needed
-    point operator + (const point& other) const
-    {
-        return point(this->x + other.x, this->y + other.y);
-    }
-
-    point operator - (const point& other) const
-    {
-        return point(this->x - other.x, this->y - other.y);
-    }
-
-    point operator * (long double other) const
-    {
-        return point(this->x * other, this->y * other);
-    }
-
-    point operator / (long double other) const
-    {
-        return point(this->x / other, this->y / other);
-    }
+struct PT {
+  double x, y;
+  PT() {}
+  PT(double x, double y) : x(x), y(y) {}
+  PT(const PT &p) : x(p.x), y(p.y)    {}
+  PT operator + (const PT &p)  const { return PT(x+p.x, y+p.y); }
+  PT operator - (const PT &p)  const { return PT(x-p.x, y-p.y); }
+  PT operator * (double c)     const { return PT(x*c,   y*c  ); }
+  PT operator / (double c)     const { return PT(x/c,   y/c  ); }
+bool operator <(const PT &p)   const { return (x < p.x ? true : (x == p.x && y < p.y)); }
+bool operator ==(const PT &p)  const { return fabs(x-p.x) < EPS && fabs(y-p.y) < EPS; }
+bool operator !=(const PT &p)  const { return !((*this) == p); }
 };
-
-//Container for line segment
-struct segment { point p1, p2; };
-
-//Dot product ab.bc
-double dot(const point& a, const point& b, const point& c)
-{
-    point AB = b - a;
-    point BC = c - b;
-    return AB.x*BC.x + AB.y*BC.y;
+double dot(PT p, PT q)     { return p.x*q.x+p.y*q.y; }
+double dist2(PT p, PT q)   { return dot(p-q,p-q); }
+double cross(PT p, PT q)   { return p.x*q.y-p.y*q.x; }
+ostream &operator<<(ostream &os, const PT &p) {
+  return os << "(" << p.x << "," << p.y << ")";
 }
 
-//Cross product
-//AB X AC
-double cross(const point& A, const point& B, const point& C)
-{
-    point AB = B - A, AC = C - A;
-    return(AB.x * AC.y - AB.y * AC.x);
+// rotate a point CCW or CW around the origin
+PT RotateCCW90(PT p)   { return PT(-p.y,p.x); }
+PT RotateCW90(PT p)    { return PT(p.y,-p.x); }
+PT RotateCCW(PT p, double t) {
+  return PT(p.x*cos(t)-p.y*sin(t), p.x*sin(t)+p.y*cos(t));
 }
 
-//Finds orientation of triplet of points p, q, r
-//Returns Colinear, CW, or CCW
-int orientation(const point& p, const point& q, const point& r)
-{
-    double val = cross(p, q, r);
-    if(abs(val) < EPSILON) return Colinear;
-    return (val > 0) ? CW : CCW;
+// project point c onto line through a and b
+// assuming a != b
+PT ProjectPointLine(PT a, PT b, PT c) {
+  return a + (b-a)*dot(c-a, b-a)/dot(b-a, b-a);
 }
 
-//Checks if point p is possibly on the segment s
-//but doesn't guarantee it is
-bool onSegment(const point& p, const segment& s)
+// project point c onto line segment through a and b
+PT ProjectPointSegment(PT a, PT b, PT c) {
+  double r = dot(b-a,b-a);
+  if (fabs(r) < EPS) return a;
+  r = dot(c-a, b-a)/r;
+  if (r < 0) return a;
+  if (r > 1) return b;
+  return a + (b-a)*r;
+}
+
+// compute distance from c to segment between a and b
+double DistancePointSegment(PT a, PT b, PT c) {
+  return sqrt(dist2(c, ProjectPointSegment(a, b, c)));
+}
+
+// compute distance between point (x,y,z) and plane ax+by+cz=d
+double DistancePointPlane(double x, double y, double z,
+                          double a, double b, double c, double d)
 {
-    bool x = (abs(s.p1.x - s.p2.x) < EPSILON && abs(p.x - s.p2.x) < EPSILON) || (p.x <= max(s.p1.x, s.p2.x) && p.x >= min(s.p1.x, s.p2.x));
-    bool y = (abs(s.p1.y - s.p2.y) < EPSILON && abs(p.y - s.p2.y) < EPSILON) || (p.y <= max(s.p1.y, s.p2.y) && p.y >= min(s.p1.y, s.p2.y));
-    return x && y;
+  return fabs(a*x+b*y+c*z-d)/sqrt(a*a+b*b+c*c);
+}
+
+// determine if lines from a to b and c to d are parallel or collinear
+bool LinesParallel(PT a, PT b, PT c, PT d) {
+  return fabs(cross(b-a, c-d)) < EPS;
+}
+
+bool LinesCollinear(PT a, PT b, PT c, PT d) {
+  return LinesParallel(a, b, c, d)
+      && fabs(cross(a-b, a-c)) < EPS
+      && fabs(cross(c-d, c-a)) < EPS;
+}
+
+// determine if line segment from a to b intersects with
+// line segment from c to d
+bool SegmentsIntersect(PT a, PT b, PT c, PT d) {
+  if (LinesCollinear(a, b, c, d)) {
+    if (dist2(a, c) < EPS || dist2(a, d) < EPS ||
+      dist2(b, c) < EPS || dist2(b, d) < EPS) return true;
+    if (dot(c-a, c-b) > 0 && dot(d-a, d-b) > 0 && dot(c-b, d-b) > 0)
+      return false;
+    return true;
+  }
+  if (cross(d-a, b-a) * cross(c-a, b-a) > 0) return false;
+  if (cross(a-c, d-c) * cross(b-c, d-c) > 0) return false;
+  return true;
 }
 
 //Returns of list of intersection points between segments s1, and s2
@@ -100,207 +89,145 @@ bool onSegment(const point& p, const segment& s)
 //If they intersect at exactly 1 point, the result contains that point
 //If they overlap for non-0 distance, the left and right points of that intersection
 //  are returned
-vector<point> intersect(const segment& s1, const segment& s2)
-{
-/*
-    cout << "Intersect:" << endl;
-    cout << s1.p1 << " -> " << s1.p2 << endl;
-    cout << s2.p1 << " -> " << s2.p2 << endl;
-*/
-    point a = s1.p1, b = s1.p2, c = s2.p1, d = s2.p2;
-
-    if(orientation(a, b, c) == Colinear && orientation(a, b, d) == Colinear &&
-        orientation(c, d, a) == Colinear && orientation(c, d, b) == Colinear)
-    {
-        point min_s1 = min(a, b), max_s1 = max(a, b);
-        point min_s2 = min(c, d), max_s2 = max(c, d);
-/*
-        cout << "Colinear" << endl;
-        cout << min_s1 << " -> " << max_s1 << endl;
-        cout << min_s2 << " -> " << max_s2 << endl;
-*/
-        if(max_s1 < min_s2 || max_s2 < min_s1) return {};
-
-        point start = max(min_s1, min_s2), end = min(max_s1, max_s2);
-        if(start == end)
-            return {start};
-        else
-            return {min(start, end), max(start, end)};
-    }
-
-    long double a1 = b.y - a.y, a2 = d.y - c.y;
-    long double b1 = a.x - b.x, b2 = c.x - d.x;
-    long double c1 = a1*a.x + b1*a.y, c2 = a2*c.x + b2*c.y;
-    long double det = a1*b2 - a2*b1;
-    if(abs(det) > EPSILON)
-    {
-        point inter((b2*c1 - b1*c2)/det, (a1*c2 - a2*c1)/det);
-        //cout << "Checking " << inter << " vs segments" << endl;
-        //cout << onSegment(inter, s1) << " " << onSegment(inter, s2) << endl;
-        if(onSegment(inter, s1) && onSegment(inter, s2))
-            return {inter};
-    }
-    return {};
-}
-
-//Squared magnitude of point vector
-double sqmag(const point& p1)
-{
-    return p1.x*p1.x + p1.y*p1.y;
-}
-
-//Magnitude of point vector
-double mag(const point& p1)
-{
-    return sqrt(sqmag(p1));
-}
-
-//Scalar projection of vector a onto vector b
-double sproject(const point& a, const point& b)
-{
-    return dot(a, point(0, 0), b)/mag(b);
-}
-
-//Vector projection of vector a onto vector b
-point vproject(const point& a, const point& b)
-{
-    return b * sproject(a, b) / mag(b);
-}
-
-//Checks if two segments straddle each other
-bool straddle(const segment& s1, const segment& s2)
-{
-    long double cross1 = cross(s1.p1, s1.p2, s2.p1);
-    long double cross2 = cross(s1.p1, s1.p2, s2.p2);
-
-    if((cross1 > 0 && cross2 > 0) ||
-       (cross1 < 0 && cross2 < 0)) return false;
-
-    if(abs(cross1) < EPSILON && abs(cross2) < EPSILON &&
-       orientation(s1.p2, s2.p1, s2.p2) != Colinear)
-       return false;
-
-    return true;
-}
-
-//Returns distance from line (or segment) to point
-long double linePointDist(const segment& s, const point& p, bool isSegment=false)
-{
-    if(s.p1 == s.p2)
-    {
-        if(p == s.p1) return 0;
-        return mag(p - s.p1);
-    }
-
-    if(isSegment)
-    {
-        if(dot(s.p1, s.p2, p) > 0) return mag(s.p2 - p);
-        if(dot(s.p2, s.p1, p) > 0) return mag(s.p1 - p);
-    }
-    return abs(cross(s.p1, s.p2, p) / mag(s.p1 - s.p2));
-}
-
-//Returns positive area if points are counterclockwise,
-//negative area if clockwise
-long double polyArea(const vector<point>& points)
-{
-    long double result = 0;
-    for(int i=0, j=1; i<points.size(); i++, j=(j+1)%points.size())
-    {
-        result += points[i].x * points[j].y;
-        result -= points[i].y * points[j].x;
-    }
-    return result/2;
-}
-
-//Checks if point p is inside the polygon
-//Returns Inside, Outside, or OnEdge
-int pointInPoly(const vector<point>& poly, const point& p)
-{
-    //cout << "Point in poly " << p.x << " " << p.y << endl;
-    bool inside = false;
-
-    long double maxX = numeric_limits<long double>::lowest();
-    for(const point& p : poly)
-        maxX = max(maxX, p.x);
-
-    //Create point definitely outside polygon
-    point outside(maxX+1, p.y);
-
-    vector<point> intersection;
-
-    for(int i=0, j = poly.size()-1; i < poly.size(); i++, j=i-1)
-    {
-        if(p == poly[i] || p == poly[j]) return OnEdge;
-        if(orientation(p, poly[i], poly[j]) == Colinear &&
-           onSegment(p, segment{poly[i], poly[j]})) return OnEdge;
-
-        intersection = intersect(segment{p, outside}, segment{poly[i], poly[j]});
-        //cout << intersection.size() << " intersections with " << poly[i].x << ", " << poly[i].y << " -> " << poly[j].x << ", " << poly[j].y << endl;
-        if(intersection.size() == 1)
-        {
-            if(poly[i] == intersection[0] && poly[j].y <= p.y) continue;
-            if(poly[j] == intersection[0] && poly[i].y <= p.y) continue;
-            //cout << intersection[0].x << " " << intersection[0].y << endl;
-            inside = !inside;
+vector<PT> intersect(PT a, PT b, PT c, PT d) {
+    vector<PT> res;
+    if(fabs(cross(b-a, c-b)) < EPS && fabs(cross(b-a, d-b)) < EPS &&
+        fabs(cross(d-c, a-d)) < EPS && fabs(cross(d-c, b-d)) < EPS) {
+        PT min_s1 = min(a, b), max_s1 = max(a, b);
+        PT min_s2 = min(c, d), max_s2 = max(c, d);
+        if(min_s1 < min_s2) {
+            if(max_s1 < min_s2) return res;//return false;
         }
-    }
-
-    //cout << "Is inside? " << inside << endl;
-    return (inside ? Inside : Outside);
-}
-
-//Computes the convex hull of a set of points
-//Using the graham scan algorithm
-vector<point> convexHull(vector<point> points) {
-    if(points.size() < 4) return points;
-    point lowestPoint = points[0];
-
-    //Don't just use point < operator because that checks
-    //x then y; we need y then x
-    for(int i=0; i<points.size(); i++)
-        if(points[i].y < lowestPoint.y ||
-          (abs(points[i].y - lowestPoint.y) < EPSILON && points[i].x < lowestPoint.x))
-            lowestPoint = points[i];
-
-    point horiz = lowestPoint + point(1, 0);
-    sort(points.begin(), points.end(),
-    [=](const point& l, const point& r) {
-        if(r == lowestPoint) return false;
-        if(l == lowestPoint) return true;
-
-        long double scoreL = dot(horiz, lowestPoint, l) / (mag(horiz-lowestPoint)*mag(l-lowestPoint));
-        long double scoreR = dot(horiz, lowestPoint, r) / (mag(horiz-lowestPoint)*mag(r-lowestPoint));
-
-        return scoreL < scoreR;
-    });
-
-/*
-    cerr << "Hull sort" << endl;
-    for(point& p : points)
-        cerr << p << " ";
-    cerr << endl;
-*/
-
-    points.insert(points.begin(), points.back());
-
-    uint32_t m = 1;
-    for(int i=2; i<points.size(); i++) {
-        while(cross(points[m-1], points[m], points[i]) <= -EPSILON) {
-            if(m > 1) { m--; continue; }
-            else if(i == points.size()-1) break;
-            else i++;
+        else if(min_s2 < min_s1 && max_s2 < min_s1) return res;//return false;
+        PT start = max(min_s1, min_s2), end = min(max_s1, max_s2);
+        if(start==end) res.push_back(start);//overlap is one point
+        else {
+            res.push_back(min(start, end));
+            res.push_back(max(start, end));
         }
-
-        m++;
-        std::swap(points[m], points[i]);
+        return res;//return true; //remove overlap code block
     }
-    points.resize(m);
-    if(points.front() == points.back()) points.pop_back();
-
-    return points;
+    double x1 = (b.x-a.x), y1 = (b.y-a.y), x2 = (d.x - c.x), y2 = (d.y - c.y);
+    double u1 = (-y1*(a.x - c.x) + x1 * (a.y - c.y))/(-x2 * y1 + x1 * y2);
+    double u2 = (x2 * (a.y - c.y) - y2 * (a.x - c.x)) / (-x2 * y1 + x1 * y2);
+    if(u1 >= 0 && u1 <= 1 && u2 >= 0 && u2 <= 1)
+        res.push_back(PT((a.x + u2*x1),(a.y+u2*y1))); //return true;
+    return res;//return false;
 }
 
-int main() {
-    return 0;
+// compute intersection of line passing through a and b
+// with line passing through c and d, assuming that unique
+// intersection exists; for segment intersection, check if
+// segments intersect first
+PT ComputeLineIntersection(PT a, PT b, PT c, PT d) {
+  b=b-a; d=c-d; c=c-a;
+  assert(dot(b, b) > EPS && dot(d, d) > EPS);
+  return a + b*cross(c, d)/cross(b, d);
+}
+
+// compute center of circle given three points
+PT ComputeCircleCenter(PT a, PT b, PT c) {
+  b=(a+b)/2;
+  c=(a+c)/2;
+  return ComputeLineIntersection(b, b+RotateCW90(a-b), c, c+RotateCW90(a-c));
+}
+
+// determine if point is in a possibly non-convex polygon (by William
+// Randolph Franklin); returns 1 for strictly interior points, 0 for
+// strictly exterior points, and 0 or 1 for the remaining points.
+// Note that it is possible to convert this into an *exact* test using
+// integer arithmetic by taking care of the division appropriately
+// (making sure to deal with signs properly) and then by writing exact
+// tests for checking point on polygon boundary
+bool PointInPolygon(const vector<PT> &p, PT q) {
+  bool c = 0;
+  for (int i = 0; i < p.size(); i++){
+    int j = (i+1)%p.size();
+    if ((p[i].y <= q.y && q.y < p[j].y ||
+      p[j].y <= q.y && q.y < p[i].y) &&
+      q.x < p[i].x + (p[j].x - p[i].x) * (q.y - p[i].y) / (p[j].y - p[i].y))
+      c = !c;
+  }
+  return c;
+}
+
+// determine if point is on the boundary of a polygon
+bool PointOnPolygon(const vector<PT> &p, PT q) {
+  for (int i = 0; i < p.size(); i++)
+    if (dist2(ProjectPointSegment(p[i], p[(i+1)%p.size()], q), q) < EPS)
+      return true;
+    return false;
+}
+
+// compute intersection of line through points a and b with
+// circle centered at c with radius r > 0
+vector<PT> CircleLineIntersection(PT a, PT b, PT c, double r) {
+  vector<PT> ret;
+  b = b-a;
+  a = a-c;
+  double A = dot(b, b);
+  double B = dot(a, b);
+  double C = dot(a, a) - r*r;
+  double D = B*B - A*C;
+  if (D < -EPS) return ret;
+  ret.push_back(c+a+b*(-B+sqrt(D+EPS))/A);
+  if (D > EPS)
+    ret.push_back(c+a+b*(-B-sqrt(D))/A);
+  return ret;
+}
+
+// compute intersection of circle centered at a with radius r
+// with circle centered at b with radius R
+vector<PT> CircleCircleIntersection(PT a, PT b, double r, double R) {
+  vector<PT> ret;
+  double d = sqrt(dist2(a, b));
+  if (d > r+R || d+min(r, R) < max(r, R)) return ret;
+  double x = (d*d-R*R+r*r)/(2*d);
+  double y = sqrt(r*r-x*x);
+  PT v = (b-a)/d;
+  ret.push_back(a+v*x + RotateCCW90(v)*y);
+  if (y > 0)
+    ret.push_back(a+v*x - RotateCCW90(v)*y);
+  return ret;
+}
+
+// This code computes the area or centroid of a (possibly nonconvex)
+// polygon, assuming that the coordinates are listed in a clockwise or
+// counterclockwise fashion.  Note that the centroid is often known as
+// the "center of gravity" or "center of mass".
+double ComputeSignedArea(const vector<PT> &p) {
+  double area = 0;
+  for(int i = 0; i < p.size(); i++) {
+    int j = (i+1) % p.size();
+    area += p[i].x*p[j].y - p[j].x*p[i].y;
+  }
+  return area / 2.0;
+}
+
+double ComputeArea(const vector<PT> &p) {
+  return fabs(ComputeSignedArea(p));
+}
+
+PT ComputeCentroid(const vector<PT> &p) {
+  PT c(0,0);
+  double scale = 6.0 * ComputeSignedArea(p);
+  for (int i = 0; i < p.size(); i++){
+    int j = (i+1) % p.size();
+    c = c + (p[i]+p[j])*(p[i].x*p[j].y - p[j].x*p[i].y);
+  }
+  return c / scale;
+}
+
+// tests whether or not a given polygon (in CW or CCW order) is simple
+bool IsSimple(const vector<PT> &p) {
+  for (int i = 0; i < p.size(); i++) {
+    for (int k = i+1; k < p.size(); k++) {
+      int j = (i+1) % p.size();
+      int l = (k+1) % p.size();
+      if (i == l || j == k) continue;
+      if (SegmentsIntersect(p[i], p[j], p[k], p[l]))
+        return false;
+    }
+  }
+  return true;
 }
