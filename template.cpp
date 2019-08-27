267d39d2540ccc99a422b882c199d4cc799feba3
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-08-26 21:18:30 -0600

    adding template

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+typedef long long ll;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
