7efe8ae1e989a1674a2722fd0ce77bc61f389117
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-10-28 11:10:59 -0600

    fixed bug

--- math/gaussElimination.cpp
+++ math/gaussElimination.cpp
@@ -1,6 +1,7 @@
+const double eps = 0.00000001;
 int Rank;
-Fraction Det;
-void gauss(vector<vector<Fraction>> &a) {
+double Det;
+void gauss(vector<vector<double>> &a) {
     int n = a.size();
     int m = a[0].size();//possible RTE here
     Det = 1.0, Rank = 0;
@@ -9,16 +10,16 @@ void gauss(vector<vector<Fraction>> &a) {
         int sel = row;
         for(int i = row+1; i < n; ++i)
             if(abs(a[i][col]) > abs(a[sel][col])) sel = i;
-        if(abs(a[sel][col]) == 0) { Det = 0.0; continue; }
-        for(int j = 0; j <= m; ++j) swap(a[sel][j], a[row][j]);
-        if(row != sel) Det = Fraction(0,1)-Det;
+        if(abs(a[sel][col]) < eps) { Det = 0.0; continue; }
+        for(int j = 0; j < m; ++j) swap(a[sel][j], a[row][j]);
+        if(row != sel) Det = -Det;
         Det = Det * a[row][col];
         where[col] = row;
-        Fraction s = (Fraction(1,1) / a[row][col]);
-        for(int j = 0; j <= m; ++j) a[row][j] = a[row][j] * s;
-        for(int i = 0; i < n; ++i) if (i != row && abs(a[i][col]) != 0) {
-            Fraction t = a[i][col];
-            for(int j = 0; j <= m; ++j) a[i][j] = a[i][j] - (a[row][j] * t);
+        double s = (1.0 / a[row][col]);
+        for(int j = 0; j < m; ++j) a[row][j] = a[row][j] * s;
+        for(int i = 0; i < n; ++i) if (i != row && abs(a[i][col]) > eps) {
+            double t = a[i][col];
+            for(int j = 0; j < m; ++j) a[i][j] = a[i][j] - (a[row][j] * t);
         }
         ++row, ++Rank;
     }
