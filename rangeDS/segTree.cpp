2ae6eb02f2104001706e49e09cf25c1a49cecab3
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-11-18 18:15:34 -0700

    seg tree min and made it easier to remove unwanted stuff

--- rangeDS/segTree.cpp
+++ rangeDS/segTree.cpp
@@ -1,11 +1,15 @@
 struct SegmentTree {
-    vector<ll> treeSum, treeMax, lazy;
+    vector<ll> treeSum;
+    vector<ll> treeMax;
+    vector<ll> treeMin;
+    vector<ll> lazy;
     ll n, root, size;
     SegmentTree(int currSize) : n(currSize), root(1) {
         ll x = (ll)(ceil(log2(currSize)));
         size = 2*(ll)pow(2, x);
         treeSum.resize(size, 0);
         treeMax.resize(size, 0);
+        treeMin.resize(size, 0);
         lazy.resize(size, 0);
     }
     SegmentTree(const vector<ll> &arr) : n(arr.size()), root(1) {
@@ -13,23 +17,29 @@ struct SegmentTree {
         size = 2*(ll)pow(2, x);
         treeSum.resize(size);
         treeMax.resize(size);
+        treeMin.resize(size);
         lazy.resize(size, 0);
         build(arr, root, 0, n-1);
     }
     void build(const vector<ll> &arr, int node, int start, int end) {
-        if(start == end) treeMax[node] = treeSum[node] = arr[start];
-        else {
+        if(start == end) {
+            treeMin[node] = arr[start];
+            treeMax[node] = arr[start];
+            treeSum[node] = arr[start];
+        } else {
             ll mid = (start+end)/2;
             build(arr, 2*node, start, mid);
             build(arr, 2*node+1, mid+1, end);
             treeSum[node] = treeSum[2*node] + treeSum[2*node+1];
             treeMax[node] = max(treeMax[2*node], treeMax[2*node+1]);
+            treeMin[node] = min(treeMin[2*node], treeMin[2*node+1]);
         }
     }
     void pendingUpdate(int node, int start, int end) {
         if(lazy[node]) {
             treeSum[node] += (end-start+1) * lazy[node];
             treeMax[node] += lazy[node];
+            treeMin[node] += lazy[node];
             if(start != end) {
                 lazy[2*node] += lazy[node];
                 lazy[2*node+1] += lazy[node];
@@ -44,6 +54,7 @@ struct SegmentTree {
         if(start >= l && end <= r) {
             treeSum[node] += (end-start+1) * diff;
             treeMax[node] += diff;
+            treeMin[node] += diff;
             if(start != end) {
                 lazy[2*node] += diff;
                 lazy[2*node+1] += diff;
@@ -55,6 +66,7 @@ struct SegmentTree {
         updateRange(2*node+1, mid+1, end, l, r, diff);
         treeSum[node] = treeSum[2*node] + treeSum[2*node+1];
         treeMax[node] = max(treeMax[2*node], treeMax[2*node+1]);
+        treeMin[node] = min(treeMin[2*node], treeMin[2*node+1]);
     }
     ll querySum(int l, int r) {return querySum(root, 0, n-1, l, r);}
     ll querySum(int node, int start, int end, int l, int r) {
@@ -71,5 +83,13 @@ struct SegmentTree {
         if(l <= start && end <= r) return treeMax[node];
         ll mid = (start+end)/2;
         return max(queryMax(2*node, start, mid, l, r), queryMax(2*node+1, mid+1, end, l, r));
+    }
+    ll queryMin(int l, int r) {return queryMin(root, 0, n-1, l, r);}
+    ll queryMin(int node, int start, int end, int l, int r) {
+        if(r < start || end < l) return 1e18;
+        pendingUpdate(node, start, end);
+        if(l <= start && end <= r) return treeMin[node];
+        ll mid = (start+end)/2;
+        return min(queryMin(2*node, start, mid, l, r), queryMin(2*node+1, mid+1, end, l, r));
     }
 };
