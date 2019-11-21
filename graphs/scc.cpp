fec1a66f889d20b84860f8e76bfbd92812debf0a
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2019-11-21 15:01:02 -0700

    made scc a struct

--- graphs/scc.cpp
+++ graphs/scc.cpp
@@ -1,45 +1,55 @@
-int n,m;
-vector<vector<int> > adj, adjInv;
-vector<int> scc;
-int sccID;
-vector<bool> visited;
+struct scc {
+    int n;
+    vector<vector<int> > adj, adjInv;
+    vector<int> sccId;
+    int currScc=0;
+    vector<bool> visited;
 
-void dfs1(int curr, stack<int> &seen) {
-    visited[curr] = true;
-    for(int x : adj[curr]) {
-        if(!visited[x]) {
-            dfs1(x, seen);
+    void dfs1(int curr, stack<int> &seen) {
+        visited[curr] = true;
+        for(int x : adj[curr]) {
+            if(!visited[x]) {
+                dfs1(x, seen);
+            }
+        }
+        seen.push(curr);
+    }
+
+    void dfs2(int curr) {
+        visited[curr] = true;
+        sccId[curr] = currScc;
+        for(int x : adjInv[curr]) {
+            if(!visited[x]) {
+                dfs2(x);
+            }
         }
     }
-    seen.push(curr);
-}
 
-void dfs2(int curr) {
-    visited[curr] = true;
-    scc[curr] = sccID;
-    for(int x : adjInv[curr]) {
-        if(!visited[x]) {
-            dfs2(x);
+    scc(const vector<vector<int>> &graph) {
+        adj = graph;
+        n = adj.size();
+        adjInv.resize(n);
+        for(int i = 0; i < n; ++i) {
+            for(int to : graph[i]) {
+                adjInv[to].push_back(i);
+            }
         }
-    }
-}
-
-void calcSCC() {
-    visited.resize(n+1,false);
-    stack<int> seen;
-    for(int i = 1; i <= n; ++i) {
-        if(!visited[i]) {
-            dfs1(i, seen);
+        visited.resize(n,false);
+        stack<int> seen;
+        for(int i = 0; i < n; ++i) {
+            if(!visited[i]) {
+                dfs1(i, seen);
+            }
         }
-    }
-    visited.clear();
-    visited.resize(n+1,false);
-    sccID = 0;
-    while(!seen.empty()) {
-        while(!seen.empty() && visited[seen.top()]) seen.pop();
-        if(!seen.empty()) {
+        visited.assign(n,false);
+        sccId.resize(n);
+        while(!seen.empty()) {
+            if(visited[seen.top()]) {
+                seen.pop();
+                continue;
+            }
             dfs2(seen.top());
-            sccID++;
+            currScc++;
         }
     }
-}
+};
