b307973365483337416250343a6f68ad96ae6a27
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2020-01-10 13:43:04 -0700

    reaasdf

--- template.cpp
+++ template.cpp
@@ -1,8 +1,3 @@
- #pragma comment(linker, "/stack:200000000")
- #pragma GCC optimize("Ofast")
- #pragma GCC optimize("unroll-loops")
- #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
-
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
