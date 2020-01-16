30adfab18cdacd2b9d0022ea7ecae094f57d6eda
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2020-01-16 16:11:32 -0700

    faster compilation

--- template.cpp
+++ template.cpp
@@ -1,4 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
 using namespace std;
 #define int long long
 #define endl '\n'
