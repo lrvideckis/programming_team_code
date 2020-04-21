28b27213b78a0040e4ded91fe206e712b65f9fe8
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2020-04-20 20:05:17 -0500

    fixed yet another bug

--- rangeDS/segTree.cpp
+++ rangeDS/segTree.cpp
@@ -64,8 +64,9 @@ struct SegmentTree {
 			currLazy = 0;
 		}
 	}
-	void update(int L, int R, ll Diff) {l=L,r=R,diff=Diff;update(1,0,n-1);}
-	void update(int node, int start, int end) {
+	void update(int L, int R, ll Diff) {l=L,r=R,diff=Diff;update2(1,0,n-1);}
+	Node query(int L, int R) {l=L,r=R;return query2(1, 0, n-1);}
+	void update2(int node, int start, int end) {
 		push(node, start, end);
 		if(start > end || start > r || end < l) return;
 		if(start >= l && end <= r) {
@@ -73,16 +74,15 @@ struct SegmentTree {
 			return;
 		}
 		int mid = (start + end) / 2;
-		update(2*node,start,mid);
-		update(2*node+1,mid+1,end);
+		update2(2*node,start,mid);
+		update2(2*node+1,mid+1,end);
 		tree[node] = combine(tree[2*node], tree[2*node+1]);
 	}
-	Node query(int L, int R) {l=L,r=R;return query(1, 0, n-1);}
-	Node query(int node, int start, int end) {
+	Node query2(int node, int start, int end) {
 		if(r < start || end < l) return zero;
 		push(node, start, end);
 		if(l <= start && end <= r) return tree[node];
 		int mid = (start+end)/2;
-		return combine(query(2*node,start,mid), query(2*node+1,mid+1,end));
+		return combine(query2(2*node,start,mid), query2(2*node+1,mid+1,end));
 	}
 };
