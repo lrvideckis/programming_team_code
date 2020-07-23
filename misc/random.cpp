cbf1aafc6f2f349cda30a9c882cb11b57c7d813e
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2020-07-23 02:14:54 -0500

    removeing includes and source

--- misc/random.cpp
+++ misc/random.cpp
@@ -1,7 +1,3 @@
-#include <random>
-#include <chrono>
-
-//Source: http://codeforces.com/blog/entry/61587
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 inline int getRand(int l, int r) {
 	uniform_int_distribution<int> uid(l, r);
