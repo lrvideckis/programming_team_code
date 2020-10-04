7056812347ef062cf2d562a68bae112de5fbaa94
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2020-10-04 16:20:39 -0600

    easier to understand compiler flag

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 for((i = 1; ; ++i)); do
     echo $i
     ./test.out $i > int
-    diff -w <(./a.out < int) <(./brute.out < int) || break
+    diff --ignore-all-space <(./a.out < int) <(./brute.out < int) || break
 done
