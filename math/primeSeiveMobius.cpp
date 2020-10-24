c580b0192404e13f9a18d8e430621534ad983694
Luke Videckis <lrvideckis@fb.com>
2020-10-24 12:41:23 -0600

    changing implementation of seive and mobius

--- math/primeSeiveMobius.cpp
+++ math/primeSeiveMobius.cpp
@@ -1,38 +1,28 @@
-const int Max = 1e6;
-int seive[Max], mob[Max];
-
-//returns 0 iff there exists a prime p s.t. n%(p^2)=0
-//returns -1 iff n has an odd number of distinct prime factors
-//returns 1 iff n has an even number of distinct prime factors
-int mobius(int n) {
-	int &temp = mob[n];
-	if(temp != -2) return temp;
-	int factors = 0, counter = 1, prev = 0;
-	while(n > 1) {
-		if(prev == seive[n]) counter++;
-		else counter = 1;
-		if(counter == 2) {
-			return temp = 0;
+//mobius[i] = 0 iff there exists a prime p s.t. i%(p^2)=0
+//mobius[i] = -1 iff i has an odd number of distinct prime factors
+//mobius[i] = 1 iff i has an even number of distinct prime factors
+const int N = 2e6+10;
+int mobius[N];
+void calcMobius() {
+	mobius[1] = 1;
+	for(int i = 1; i < N; ++i) {
+		for(int j = i+i; j < N; j += i) {
+			mobius[j] -= mobius[i];
 		}
-		prev = seive[n];
-		n /= seive[n];
-		factors++;
 	}
-	if(counter == 2) {
-		return temp = 0;
-	}
-	return temp = (factors%2==0?1:-1);
 }
 
-void doSeive() {
-	for (int i = 0; i < Max; ++i) {
-		mob[i] = -2;
-		seive[i] = i;
+int minPrime[N], maxPrime[N];
+void calcSeive() {
+	fill(minPrime, minPrime+N, N);
+	for (int i = N-1; i >= 2; --i) {
+		for(int j = i; j < N; j += i) {
+			minPrime[j] = i;
+		}
 	}
-	seive[0] = seive[1] = -1;
-	for (int i = 2; i*i < Max; ++i) {
-		if (seive[i] == i) {
-			for (int j = i * i; j < Max; j += i) seive[j] = min(seive[j], i);
+	for (int i = 2; i < N; ++i) {
+		for(int j = i; j < N; j += i) {
+			minPrime[j] = i;
 		}
 	}
 }
