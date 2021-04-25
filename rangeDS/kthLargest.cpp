9af47cfe03577ebd22ffa8df58cd64f785351df0
Luke Videckis <lrvideckis@fb.com>
2021-04-25 13:41:59 -0600

    kth largest persistent seg tree no updates

---
+++ rangeDS/kthLargest.cpp
@@ -0,0 +1,60 @@
+struct persistentSegTree {
+	public:
+		persistentSegTree(const vector<int> &arr) {
+			doCompression(arr);
+			tl = 0, tr = (int)arr.size()+1;
+			roots.push_back(build(tl, tr));
+			for (int i = 0; i < (int)arr.size(); i++) {
+				roots.push_back(update(roots.back(), tl, tr, compress[i]));
+			}
+		}
+		int find_kth(int L, int R, int k) {
+			return sorted[find_kth(roots[L], roots[R+1], tl, tr, k)];
+		}
+	private:
+		vector<int> compress, sorted;
+		void doCompression(const vector<int> &arr) {
+			int n = arr.size();
+			sorted = arr;
+			sort(sorted.begin(), sorted.end());
+			sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
+			compress.resize(n);
+			for(int i = 0; i < n; ++i) {
+				compress[i] = lower_bound(sorted.begin(), sorted.end(), arr[i]) - sorted.begin();
+			}
+		}
+		struct Vertex {
+			Vertex *l, *r;
+			int sum;
+			Vertex(int val) : l(nullptr), r(nullptr), sum(val) {}
+			Vertex(Vertex *l, Vertex *r) : l(l), r(r), sum(0) {
+				if (l) sum += l->sum;
+				if (r) sum += r->sum;
+			}
+		};
+		vector<Vertex*> roots;
+		int tl,tr;
+		Vertex* build(int l, int r) {
+			if (l == r)
+				return new Vertex(0);
+			int m = (l + r) / 2;
+			return new Vertex(build(l, m), build(m+1, r));
+		}
+		Vertex* update(Vertex* v, int l, int r, int pos) {
+			if (l == r)
+				return new Vertex(v->sum+1);
+			int m = (l + r) / 2;
+			if (pos <= m)
+				return new Vertex(update(v->l, l, m, pos), v->r);
+			else
+				return new Vertex(v->l, update(v->r, m+1, r, pos));
+		}
+		int find_kth(Vertex* vl, Vertex *vr, int l, int r, int k) {
+			if (l == r)
+				return l;
+			int m = (l + r) / 2, left_count = vr->l->sum - vl->l->sum;
+			if (left_count >= k)
+				return find_kth(vl->l, vr->l, l, m, k);
+			return find_kth(vl->r, vr->r, m+1, r, k-left_count);
+		}
+};
