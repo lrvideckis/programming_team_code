deae560563142f911d510d3d05708b455874811d
Luke Videckis <lrvideckis@fb.com>
2021-04-25 14:55:28 -0600

    fixed compile warnings

--- stringAlgs/suffixArray.cpp
+++ stringAlgs/suffixArray.cpp
@@ -6,7 +6,7 @@ namespace SAIS {
 	void pre(const vector<int> &s, int sigma) {
 		fill(bkt, bkt + s.size(), -1);
 		fill(cnt, cnt + sigma, 0);
-		for (int i = 0; i < s.size(); ++i) ++cnt[s[i]];
+		for (int i = 0; i < (int)s.size(); ++i) ++cnt[s[i]];
 		int last = 0;
 		for (int i = 0; i < sigma; ++i) {
 			lptr[i] = last;
@@ -15,10 +15,10 @@ namespace SAIS {
 		}
 	}
 	void induce(const vector<int> &s, const vector<type> &v) {
-		for (int i = 0; i < s.size(); ++i) if (bkt[i] > 0) {
+		for (int i = 0; i < (int)s.size(); ++i) if (bkt[i] > 0) {
 			if (v[bkt[i] - 1] == L) bkt[lptr[s[bkt[i] - 1]]++] = bkt[i] - 1;
 		}
-		for (int i = s.size() - 1; i >= 0; --i) if (bkt[i] > 0) {
+		for (int i = (int)s.size() - 1; i >= 0; --i) if (bkt[i] > 0) {
 			if (v[bkt[i] - 1] != L) bkt[rptr[s[bkt[i] - 1]]--] = bkt[i] - 1;
 		}
 	}
@@ -28,12 +28,12 @@ namespace SAIS {
 	}
 	vector<int> radix_sort(const vector<int> &lms, const vector<int> &s, const vector<type> &v, int sigma) {
 		pre(s, sigma);
-		for (int i = 0; i < lms.size(); ++i) bkt[tptr[s[lms[i]]]--] = lms[i];
+		for (int i = 0; i < (int)lms.size(); ++i) bkt[tptr[s[lms[i]]]--] = lms[i];
 		induce(s, v);
 		vector<int> rt(lms.size());
-		for (int i = 0; i < lms.size(); ++i) rev[lms[i]] = i;
+		for (int i = 0; i < (int)lms.size(); ++i) rev[lms[i]] = i;
 		int prv = -1, rnk = 0;
-		for (int i = 0; i < s.size(); ++i) {
+		for (int i = 0; i < (int)s.size(); ++i) {
 			int x = bkt[i];
 			if (v[x] != LMS) continue;
 			if (prv == -1) {
@@ -49,31 +49,31 @@ namespace SAIS {
 	}
 	vector<int> counting_sort(const vector<int> &s) {
 		vector<int> o(s.size());
-		for (int i = 0; i < s.size(); ++i) o[s[i]] = i;
+		for (int i = 0; i < (int)s.size(); ++i) o[s[i]] = i;
 		return o;
 	}
 	vector<int> reconstruct(const vector<int> &sa, const vector<int> &s, const vector<type> &v) {
 		vector<int> pos;
-		for (int i = 0; i < s.size(); ++i) if (v[i] == LMS) pos.push_back(i);
-		vector<int> rev(sa.size());
-		for (int i = 0; i < sa.size(); ++i) rev[i] = pos[sa[i]];
-		return rev;
+		for (int i = 0; i < (int)s.size(); ++i) if (v[i] == LMS) pos.push_back(i);
+		vector<int> ret(sa.size());
+		for (int i = 0; i < (int)sa.size(); ++i) ret[i] = pos[sa[i]];
+		return ret;
 	}
 	vector<int> sais(const vector<int> &s, int sigma) {
 		vector<type> v(s.size());
 		v[s.size() - 1] = S;
 		for (int i = s.size() - 2; i >= 0; --i) {
-			if (s[i] < s[i + 1] || s[i] == s[i + 1] && v[i + 1] == S) v[i] = S;
+			if (s[i] < s[i + 1] || (s[i] == s[i + 1] && v[i + 1] == S)) v[i] = S;
 			else v[i] = L;
 		}
 		for (int i = s.size() - 1; i >= 1; --i) {
 			if (v[i] == S && v[i - 1] == L) v[i] = LMS;
 		}
 		vector<int> lms;
-		for (int i = 0; i < s.size(); ++i) if (v[i] == LMS) lms.push_back(i);
+		for (int i = 0; i < (int)s.size(); ++i) if (v[i] == LMS) lms.push_back(i);
 		vector<int> r = radix_sort(lms, s, v, sigma);
 		vector<int> sa;
-		if (*max_element(r.begin(), r.end()) == r.size() - 1) sa = counting_sort(r);
+		if (*max_element(r.begin(), r.end()) == (int)r.size() - 1) sa = counting_sort(r);
 		else sa = sais(r, *max_element(r.begin(), r.end()) + 1);
 		sa = reconstruct(sa, s, v);
 		pre(s, sigma);
@@ -83,26 +83,9 @@ namespace SAIS {
 	}
 	vector<int> build(const string &s) {
 		vector<int> v(s.size() + 1);
-		for (int i = 0; i < s.size(); ++i) v[i] = s[i];
+		for (int i = 0; i < (int)s.size(); ++i) v[i] = s[i];
 		v[v.size() - 1] = 0;
 		vector<int> sa = sais(v, 256);
 		return vector<int>(sa.begin() + 1, sa.end());
 	}
-}
-
-vector<int> getLcp(const string &s, const vector<int> &sa) {
-	int n=s.size(),k=0;
-	vector<int> lcp(n,0);
-	vector<int> rank(n,0);
-	for(int i=0; i<n; i++) rank[sa[i]]=i;
-	for(int i=0; i<n; i++, k?k--:0) {
-		if(rank[i]==n-1) {
-			k=0;
-			continue;
-		}
-		int j=sa[rank[i]+1];
-		while(i+k<n && j+k<n && s[i+k]==s[j+k]) k++;
-		lcp[rank[i]]=k;
-	}
-	return lcp;
 }
