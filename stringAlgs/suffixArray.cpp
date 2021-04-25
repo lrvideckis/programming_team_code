bc7c245ab23b1b9f3caaf0ebc7ea4da006a1b6dd
Luke Videckis <lrvideckis@fb.com>
2021-04-25 14:56:48 -0600

    fixed compile warnings

--- stringAlgs/suffixArray.cpp
+++ stringAlgs/suffixArray.cpp
@@ -89,3 +89,20 @@ namespace SAIS {
 		return vector<int>(sa.begin() + 1, sa.end());
 	}
 }
+
+vector<int> getLcp(const string &s, const vector<int> &sa) {
+	int n=s.size(),k=0;
+	vector<int> lcp(n,0);
+	vector<int> rank(n,0);
+	for(int i=0; i<n; i++) rank[sa[i]]=i;
+	for(int i=0; i<n; i++, k?k--:0) {
+		if(rank[i]==n-1) {
+			k=0;
+			continue;
+		}
+		int j=sa[rank[i]+1];
+		while(i+k<n && j+k<n && s[i+k]==s[j+k]) k++;
+		lcp[rank[i]]=k;
+	}
+	return lcp;
+}
