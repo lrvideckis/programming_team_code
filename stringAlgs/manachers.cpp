0f5b4a7ab4f73173ac4655f29fba3cad4f3d4070
Luke Videckis <lrvideckis@fb.com>
2021-04-27 09:48:52 -0600

    removing compile warnings

--- stringAlgs/manachers.cpp
+++ stringAlgs/manachers.cpp
@@ -1,13 +1,13 @@
 // Returns the longest palindrome
 string cntPals(string &s) {
 	string T = "";
-	for(int i = 0; i < s.size(); ++i) {
+	for(int i = 0; i < (int)s.size(); ++i) {
 		T += "#" + s.substr(i,1);
 	}
 	T += '#';
 	vector<int> P(T.size(),0);
 	int center = 0, boundary = 0, maxLen = 0, resCenter = 0, cnt = 0;
-	for(int i = 1; i < T.size()-1; ++i) {
+	for(int i = 1; i+1 < (int)T.size(); ++i) {
 		int iMirror = 2 * center - i;
 		P[i] = (boundary > i ? min(boundary - i, P[iMirror]) : 0);
 		while(i-1-P[i] >= 0 && i+1+P[i] <= (int)T.size()-1 && T[i+1+P[i]] == T[i-1-P[i]])
