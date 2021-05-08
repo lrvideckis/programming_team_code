560bdbcf8af4811ddf6b945283523981f0156afe
lrvideckis <lukevideckis@gmail.com>
2021-05-07 21:38:39 -0600

    better interface for bit

--- rangeDS/fenwickTree.cpp
+++ rangeDS/fenwickTree.cpp
@@ -1,9 +1,9 @@
 struct fenwickTree {
 	vector<ll> bit;
-	int n;
-	fenwickTree() {
-		n = (int)1e5+3;
-		bit.assign(n,0);
+	fenwickTree(int n) : bit(n,0) {}
+	void add(int idx, ll d) {
+		for(; idx < (int)bit.size(); idx = idx | (idx+1))
+			bit[idx] += d;
 	}
 	ll sum(int r) {
 		ll ret = 0;
@@ -11,11 +11,7 @@ struct fenwickTree {
 			ret += bit[r];
 		return ret;
 	}
-	void add(int idx, ll d) {
-		for(; idx < n; idx = idx | (idx+1))
-			bit[idx] += d;
-	}
 	ll sum(int l, int r) {
 		return sum(r) - sum(l-1);
 	}
-}ft;
+};
