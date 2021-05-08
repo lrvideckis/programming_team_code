33a07b9ec2441f0d10161ad283c8d3537dbb4c6b
lrvideckis <lukevideckis@gmail.com>
2021-05-07 22:07:15 -0600

    improved API of kmp

--- stringAlgs/kmp.cpp
+++ stringAlgs/kmp.cpp
@@ -1,55 +1,52 @@
 struct KMP_Match {
-	vector<int> T;
-	string pat;
-	KMP_Match() {};
-	KMP_Match(string pattern) : pat(pattern) {this->buildTable(pat);};
-	void buildTable(string pattern) {
-		pat = pattern;
-		T.clear();
-		T.resize(pat.length()+1);
-		int i = 0, j = -1;
-		T[i] = j;
-		while(i < pat.size()) {
-			while(j >= 0 && pat[i] != pat[j]) j = T[j];
-			i++, j++;
+	public:
+		KMP_Match(string pattern) : T(pattern.size()+1), pat(pattern) {
+			int i = 0, j = -1;
 			T[i] = j;
-		}
-	}
-	vector<int> find(string txt, bool all = true) {
-		int m = 0, i = 0;
-		vector<int> matches;
-		while(m + i < txt.length()) {
-			if(pat[i] == txt[m+i]) {
-				if(i == pat.length()-1) {
-					matches.push_back(m);
-					if(!all) return matches;
-					m = m + i - T[i];
-					i = T[i];
-				}
-				i++;
-			} else {
-				if(T[i] != -1) {
-					m = m + i - T[i];
-					i = T[i];
+			while(i < (int)pat.size()) {
+				while(j >= 0 && pat[i] != pat[j]) j = T[j];
+				i++, j++;
+				T[i] = j;
+			}
+		};
+		vector<int> find(string txt, bool all = true) {
+			int m = 0, i = 0;
+			vector<int> matches;
+			while(m + i < (int)txt.size()) {
+				if(pat[i] == txt[m+i]) {
+					if(i+1 == (int)pat.size()) {
+						matches.push_back(m);
+						if(!all) return matches;
+						m = m + i - T[i];
+						i = T[i];
+					}
+					i++;
 				} else {
-					i = 0;
-					m++;
+					if(T[i] != -1) {
+						m = m + i - T[i];
+						i = T[i];
+					} else {
+						i = 0;
+						m++;
+					}
 				}
 			}
+			return matches;
 		}
-		return matches;
-	}
+	private:
+		vector<int> T;
+		string pat;
 };
 
 int fail[1000005];
 
 // Checks if two arrays are rotationally equvalent
 bool KMPints(vector<ll> a, vector<ll> b){
-	for(int i = 0; i < a.size(); i++) {
+	for(int i = 0; i < (int)a.size(); i++) {
 		b.push_back(b[i]);
 	}
 	int p = 0;
-	for(int i = 1; i < a.size(); i++) {
+	for(int i = 1; i < (int)a.size(); i++) {
 		while(p && a[i] != a[p]) p = fail[p];
 		if(a[i] == a[p]) p++;
 		fail[i + 1] = p;
@@ -58,7 +55,7 @@ bool KMPints(vector<ll> a, vector<ll> b){
 	for(auto &i : b) {
 		while(p && i != a[p]) p = fail[p];
 		if(i == a[p]) p++;
-		if(p == a.size()) return true;
+		if(p == (int)a.size()) return true;
 	}
 	return false;
 }
