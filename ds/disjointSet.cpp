ad94c9661052be6cdf2947a662d3827e237dec4d
lrvideckis <lukevideckis@gmail.com>
2021-05-07 21:29:03 -0600

    extra functionality to disjoint set

--- ds/disjointSet.cpp
+++ ds/disjointSet.cpp
@@ -1,16 +1,15 @@
 struct disjointSet {
+	int numberOfSets;
 	vector<int> parent;
-	disjointSet(int n) {
-		parent.resize(n,-1);
-	}
+	disjointSet(int n) : numberOfSets(n), parent(n,-1) {}
 	int find(int x) {return parent[x] < 0 ? x : parent[x] = find(parent[x]);}
-	void merge(int x, int y) {
-		if((x=find(x)) == (y=find(y))) return;
+	int sizeOfSet(int x) {return -parent[find(x)];}
+	bool merge(int x, int y) {
+		if((x=find(x)) == (y=find(y))) return false;
 		if(parent[y] < parent[x]) swap(x,y);
 		parent[x] += parent[y];
 		parent[y] = x;
-	}
-	int sizeOfSet(int x) {
-		return -parent[find(x)];
+		numberOfSets--;
+		return true;
 	}
 };
