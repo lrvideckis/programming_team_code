c47f62e26253a0681c7d30c6fc9d73c26ceeb6fd
Luke Videckis <lrvideckis@fb.com>
2021-07-02 01:18:46 -0400

    changed input file name to in

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 for((i = 1; ; ++i)); do
     echo $i
-    ./test.out $i > int
-    diff --ignore-all-space <(./a.out < int) <(./brute.out < int) || break
+    ./test.out $i > in
+    diff --ignore-all-space <(./a.out < in) <(./brute.out < in) || break
 done
