6ad5987107bab67818b4be4fe3ac4487af0bf703
Luke Videckis <lrvideckis@fb.com>
2021-09-08 01:33:54 -0700

    added code golfed version of RMQ

--- rangeDS/sparseTable.cpp
+++ rangeDS/sparseTable.cpp
@@ -1,9 +1,9 @@
 template <class T>
-struct sparseTable {
+struct sparseTableIdx {
 	vector<int> log2;
 	vector<vector<T>> dp;
 	vector<vector<int>> index;
-	sparseTable(const vector<T> &arr) {
+	sparseTableIdx(const vector<T> &arr) {
 		const int n = arr.size();
 		log2.resize(n+1,-1);
 		for(int i = 1; i <= n; ++i) log2[i] = 1 + log2[i/2];
@@ -32,5 +32,26 @@ struct sparseTable {
 			return index[x][r-(1<<x)+1];
 		}
 		return index[x][l];
+	}
+};
+
+struct sparseTable {
+	vector<int> log2;
+	vector<vector<ll>> dp;
+	sparseTable(const vector<ll> &arr) {
+		const int n = arr.size();
+		log2.resize(n+1,-1);
+		for(int i = 1; i <= n; ++i) log2[i] = 1 + log2[i/2];
+		dp.resize(log2[n]+1, arr);
+		for(int i = 1; i <= log2[n]; ++i) {
+			for(int j = 0; j+(1<<i)-1<n; ++j) {
+				dp[i][j] = min(dp[i-1][j], dp[i-1][j+(1<<(i-1))]);
+			}
+		}
+	}
+	//returns min of arr[l], arr[l+1], ..., arr[r]
+	ll query(int l, int r) const {
+		const int x = log2[r-l+1];
+		return min(dp[x][l], dp[x][r-(1<<x)+1]);
 	}
 };
