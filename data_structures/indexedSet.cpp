0582be7c0c1c2180c4a337714261aafa31e3c0da
Luke Videckis <lrvideckis@fb.com>
2021-09-16 01:19:16 -0500

    removed unnecessary include

--- data_structures/indexedSet.cpp
+++ data_structures/indexedSet.cpp
@@ -1,5 +1,4 @@
 #include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template<class TI>
 using indexed_set = tree<TI, null_type, less<TI>, rb_tree_tag, tree_order_statistics_node_update>;
