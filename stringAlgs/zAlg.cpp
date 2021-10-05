fe12df9956967146a21503978b752e48ab8b6c83
Luke Videckis <lrvideckis@fb.com>
2021-10-04 18:30:25 -0700

    made z alg method contract much better

--- stringAlgs/zAlg.cpp
+++ stringAlgs/zAlg.cpp
@@ -1,23 +1,17 @@
-string s;
 //z[i] is the length of the longest substring
 //starting from s[i] which is also a prefix of s
-int z[100010];//change size here
-void zAlg() {
-	int n = s.size();
-	int L = 0, R = 0;
-	for (int i = 1; i < n; i++) {
-		if (i > R) {
-			L = R = i;
-			while (R < n && s[R-L] == s[R]) R++;
-			z[i] = R-L; R--;
-		} else {
-			int k = i-L;
-			if (z[k] < R-i+1) z[i] = z[k];
-			else {
-				L = i;
-				while (R < n && s[R-L] == s[R]) R++;
-				z[i] = R-L; R--;
-			}
-		}
+//stolen from https://cp-algorithms.com/string/z-function.html
+vector<int> z_function(const string &s) {
+	int n = (int) s.length();
+	vector<int> z(n);
+	for (int i = 1, l = 0, r = 0; i < n; ++i) {
+		if (i <= r)
+			z[i] = min (r - i + 1, z[i - l]);
+		while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+			++z[i];
+		if (i + z[i] - 1 > r)
+			l = i, r = i + z[i] - 1;
 	}
+	z[0] = n;
+	return z;
 }
