fa4adf5bcf72affb2a6c92e6674e386d2eaeb0a8
Luke Videckis <lrvideckis@fb.com>
2021-10-04 19:07:45 -0700

    revamped scc yet again

--- graphs/scc.cpp
+++ graphs/scc.cpp
@@ -1,61 +1,58 @@
 struct scc {
-	public:
-		scc(const vector<vector<int>> &graph) {
-			adj = graph;
-			n = adj.size();
-			adjInv.resize(n);
+public:
+	scc(const vector<vector<int>> &adj) : sccId(adj.size()), numSccs(0) {
+		int n = adj.size();
+		stack<int> seen;
+		{
+			vector<bool> vis(n,false);
 			for(int i = 0; i < n; ++i) {
-				for(int to : graph[i]) {
-					adjInv[to].push_back(i);
-				}
-			}
-			visited.resize(n,false);
-			stack<int> seen;
-			for(int i = 0; i < n; ++i) {
-				if(!visited[i]) {
-					dfs1(i, seen);
-				}
-			}
-			visited.assign(n,false);
-			sccId.resize(n);
-			while(!seen.empty()) {
-				int node = seen.top();
-				seen.pop();
-				if(visited[node]) {
-					continue;
-				}
-				dfs2(node);
-				currScc++;
-			}
-		}
-		int numberOfSCCs() {
-			return currScc;
-		}
-		int getSccID(int node) {
-			return sccId[node];
-		}
-	private:
-		int n;
-		vector<vector<int> > adj, adjInv;
-		vector<int> sccId;
-		int currScc=0;
-		vector<bool> visited;
-		void dfs1(int curr, stack<int> &seen) {
-			visited[curr] = true;
-			for(int x : adj[curr]) {
-				if(!visited[x]) {
-					dfs1(x, seen);
-				}
-			}
-			seen.push(curr);
-		}
-		void dfs2(int curr) {
-			visited[curr] = true;
-			sccId[curr] = currScc;
-			for(int x : adjInv[curr]) {
-				if(!visited[x]) {
-					dfs2(x);
+				if(!vis[i]) {
+					dfs1(i, seen, adj, vis);
 				}
 			}
 		}
+		vector<vector<int>> adjInv(n);
+		for(int i = 0; i < n; ++i) {
+			for(int to : adj[i]) {
+				adjInv[to].push_back(i);
+			}
+		}
+		vector<bool> vis(n,false);
+		while(!seen.empty()) {
+			int node = seen.top();
+			seen.pop();
+			if(vis[node]) {
+				continue;
+			}
+			dfs2(node, adjInv, vis);
+			numSccs++;
+		}
+	}
+	int numberOfSCCs() const {
+		return numSccs;
+	}
+	int getSccID(int node) const {
+		return sccId[node];
+	}
+private:
+	vector<int> sccId;
+	int numSccs;
+	void dfs1(int curr, stack<int> &seen, const vector<vector<int>> &adj, vector<bool> &vis) {
+		vis[curr] = true;
+		for(int x : adj[curr]) {
+			if(!vis[x]) {
+				dfs1(x, seen, adj, vis);
+			}
+		}
+		seen.push(curr);
+	}
+	void dfs2(int curr, const vector<vector<int>> &adjInv, vector<bool> &vis) {
+		vis[curr] = true;
+		sccId[curr] = numSccs;
+		for(int x : adjInv[curr]) {
+			if(!vis[x]) {
+				dfs2(x, adjInv, vis);
+			}
+		}
+	}
 };
