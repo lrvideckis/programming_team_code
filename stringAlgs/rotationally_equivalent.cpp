e051ede859ba0781490acf05e2871ff314717e75
Luke Videckis <lrvideckis@fb.com>
2021-10-11 10:07:09 -0700

    removed coping one of the arrays

--- stringAlgs/rotationally_equivalent.cpp
+++ stringAlgs/rotationally_equivalent.cpp
@@ -2,20 +2,18 @@
 // uses KMP with doubling trick
 bool rot_eq(const vector<int> &a, const vector<int> &b) {
 	if(a.size() != b.size()) return false;
-	vector<int> fail(a.size()+1, 0);
-	vector<int> both(b);
-	both.insert(both.end(), b.begin(), b.end());
-	int p = 0;
-	for(int i = 1; i < (int)a.size(); i++) {
+	int n = a.size();
+	vector<int> fail(n+1, 0);
+	for(int i = 1, p = 0; i < n; i++) {
 		while(p && a[i] != a[p]) p = fail[p];
 		if(a[i] == a[p]) p++;
 		fail[i + 1] = p;
 	}
-	p = 0;
-	for(int i : both) {
-		while(p && i != a[p]) p = fail[p];
-		if(i == a[p]) p++;
-		if(p == (int)a.size()) return true;
+	for(int i = 0, p = 0; i < 2*n; i++) {
+		int val = b[i%n];
+		while(p && val != a[p]) p = fail[p];
+		if(val == a[p]) p++;
+		if(p == n) return true;
 	}
 	return false;
 }
