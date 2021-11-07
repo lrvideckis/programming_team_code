63e706c6673f7f9f8b4e3fd3d31fe1cdea60d078
lrvideckis <lukevideckis@gmail.com>
2021-11-07 12:44:21 -0800

    removed duplicate code by using lambda

--- stringAlgs/rotationally_equivalent.cpp
+++ stringAlgs/rotationally_equivalent.cpp
@@ -4,15 +4,16 @@ bool rot_eq(const vector<int> &a, const vector<int> &b) {
 	if(a.size() != b.size()) return false;
 	int n = a.size();
 	vector<int> fail(n+1, 0);
+	auto update = [&](int val, int &p) -> void {
+		while(p && val != a[p]) p = fail[p];
+		if(val == a[p]) p++;
+	};
 	for(int i = 1, p = 0; i < n; i++) {
-		while(p && a[i] != a[p]) p = fail[p];
-		if(a[i] == a[p]) p++;
+		update(a[i], p);
 		fail[i + 1] = p;
 	}
 	for(int i = 0, p = 0; i < 2*n; i++) {
-		int val = b[i%n];
-		while(p && val != a[p]) p = fail[p];
-		if(val == a[p]) p++;
+		update(b[i%n], p);
 		if(p == n) return true;
 	}
 	return false;
