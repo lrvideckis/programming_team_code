5f8df6a50ba56c24539b8500fdb6b6452032718d
lrvideckis <lukevideckis@gmail.com>
2021-11-07 23:04:19 -0800

    changed mods to have a default value

--- stringAlgs/rollingHash.cpp
+++ stringAlgs/rollingHash.cpp
@@ -4,22 +4,21 @@ struct Hash {
 	vector<vector<int>> prefix, powB;
 
 	//primes: 10^9 + {7,9,21,33,87}
-	Hash(const string &s, const vector<int> &currMods) :
+	Hash(const string &s, vector<int> currMods = {(int)1e9+7, (int)1e9+9, (int)1e9+21, (int)1e9+33, (int)1e9+87}) :
 		mods(currMods),
 		prefix(currMods.size(), vector<int>(s.size())),
 		powB(currMods.size(), vector<int>(s.size(), 1)) {
-			for(int i = 0; i < (int)mods.size(); i++) {
-				for(int j = 1; j < (int)s.size(); j++) {
-					powB[i][j] = 1LL * powB[i][j-1] * base % mods[i];
-				}
-				int sum = 0;
-				for(int j = 0; j < (int)s.size(); j++) {
-					sum = (1LL * base * sum + s[j]) % mods[i];
-					prefix[i][j] = sum;
-				}
+		for(int i = 0; i < (int)mods.size(); i++) {
+			for(int j = 1; j < (int)s.size(); j++) {
+				powB[i][j] = 1LL * powB[i][j-1] * base % mods[i];
+			}
+			int sum = 0;
+			for(int j = 0; j < (int)s.size(); j++) {
+				sum = (1LL * base * sum + s[j]) % mods[i];
+				prefix[i][j] = sum;
 			}
 		}
-
+	}
 
 	//returns hashes of substring [L,R] inclusive
 	vector<int> getHashes(int L, int R) const {
