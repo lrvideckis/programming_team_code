11e92cd70fb4741f193bccd7b1c901b127b8a64a
lrvideckis <lukevideckis@gmail.com>
2021-11-07 23:33:05 -0800

    making methods const

--- range_data_structures/fenwickTree.cpp
+++ range_data_structures/fenwickTree.cpp
@@ -2,28 +2,27 @@ struct fenwickTree {
 	vector<ll> bit;
 	fenwickTree(int n) : bit(n,0) {}
 	fenwickTree(const vector<ll> & a) : bit(a.size()) {
-		if (!a.empty()) {
-			bit[0] = a[0];
-			for (int i = 1; i < (int)a.size(); i++)
-				bit[i] = bit[i-1] + a[i];
-			for (int i = (int)a.size() - 1; i > 0; i--) {
-				int lower_i = (i & (i+1)) - 1;
-				if (lower_i >= 0)
-					bit[i] -= bit[lower_i];
-			}
+		if (a.empty()) return;
+		bit[0] = a[0];
+		for (int i = 1; i < (int)a.size(); i++)
+			bit[i] = bit[i-1] + a[i];
+		for (int i = (int)a.size() - 1; i > 0; i--) {
+			int lower_i = (i & (i+1)) - 1;
+			if (lower_i >= 0)
+				bit[i] -= bit[lower_i];
 		}
 	}
 	void update(int idx, ll d) {
 		for(; idx < (int)bit.size(); idx = idx | (idx+1))
 			bit[idx] += d;
 	}
-	ll sum(int r) {
+	ll sum(int r) const {
 		ll ret = 0;
 		for(; r >= 0; r = (r&(r+1))-1)
 			ret += bit[r];
 		return ret;
 	}
-	ll sum(int l, int r) {
+	ll sum(int l, int r) const {
 		return sum(r) - sum(l-1);
 	}
 };
