da541dc8ffd14ade252b8d611efce2c40e044a25
lrvideckis <lukevideckis@gmail.com>
2021-11-07 23:18:22 -0800

    consolidated duplicate code with updating the pointer

--- stringAlgs/kmp.cpp
+++ stringAlgs/kmp.cpp
@@ -1,10 +1,8 @@
 struct KMP_Match {
 public:
 	KMP_Match(const string &needle_) : prefixFunction(needle_.size()+1, 0), needle(needle_) {
-		int p = 0;
-		for(int i = 1; i < (int)needle.size(); i++) {
-			while(p && needle[i] != needle[p]) p = prefixFunction[p];
-			if(needle[i] == needle[p]) p++;
+		for(int i = 1, p = 0; i < (int)needle.size(); i++) {
+			update(needle[i], p);
 			prefixFunction[i + 1] = p;
 		}
 	};
@@ -25,12 +23,10 @@ public:
 	// some match:
 	//
 	// KMP_Match::find(<haystack>,false).size() > 0
-	vector<int> find(const string &haystack, bool all = true) {
-		int p = 0;
+	vector<int> find(const string &haystack, bool all = true) const {
 		vector<int> matches;
-		for(int i = 0; i < (int)haystack.size(); i++) {
-			while(p && haystack[i] != needle[p]) p = prefixFunction[p];
-			if(haystack[i] == needle[p]) p++;
+		for(int i = 0, p = 0; i < (int)haystack.size(); i++) {
+			update(haystack[i], p);
 			if(p == (int)needle.size()) {
 				matches.push_back(i - (int)needle.size() + 1);
 				if(!all) return matches;
@@ -39,6 +35,10 @@ public:
 		return matches;
 	}
 private:
+	void update(char val, int &p) const {
+		while(p && val != needle[p]) p = prefixFunction[p];
+		if(val == needle[p]) p++;
+	}
 	vector<int> prefixFunction;
 	string needle;
 };
