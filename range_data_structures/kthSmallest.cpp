951c3ff432c5413db1e6e7cf4e58c5ab6365e168
lrvideckis <lukevideckis@gmail.com>
2021-12-15 05:02:35 -0800

    cleaning up code

--- range_data_structures/kthSmallest.cpp
+++ range_data_structures/kthSmallest.cpp
@@ -5,10 +5,12 @@ public:
 	 * - no updates
 	 * O(nlogn) time and space to build tree
 	 */
-	kthSmallest(const vector<int> &arr) {
-		doCompression(arr);
+	kthSmallest(const vector<int> &arr) : sorted(arr) {
+		nodes.push_back(Node(0));//acts as nullptr
+		sort(sorted.begin(), sorted.end());
+		sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
 		tl = 0, tr = (int)sorted.size()-1, n = arr.size();
-		roots.push_back(allocateNode(Vertex(0)));
+		roots.push_back(allocateNode(Node(0)));
 		for (int val : arr) {
 			int idx = lower_bound(sorted.begin(), sorted.end(), val) - sorted.begin();
 			roots.push_back(update(roots.back(), tl, tr, idx));
@@ -24,33 +26,15 @@ public:
 		return sorted[find_kth(roots[L], roots[R+1], tl, tr, k)];
 	}
 private:
-	vector<int> sorted;
-	void doCompression(const vector<int> &arr) {
-		sorted = arr;
-		sort(sorted.begin(), sorted.end());
-		sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
-	}
-	struct Vertex {
+	struct Node {
 		int l, r;
 		int sum;
-		Vertex(int val) : l(-1), r(-1), sum(val) {}
-		Vertex(int _l, int _r, const vector<Vertex>& nodes) : l(_l), r(_r), sum(0) {
-			if (l != -1) sum += nodes[l].sum;
-			if (r != -1) sum += nodes[r].sum;
-		}
-		static int getSum(int v, const vector<Vertex>& nodes) {
-			return v != -1 ? nodes[v].sum : 0;
-		}
-		static int getL(int v, const vector<Vertex>& nodes) {
-			return v != -1 ? nodes[v].l : -1;
-		}
-		static int getR(int v, const vector<Vertex>& nodes) {
-			return v != -1 ? nodes[v].r : -1;
-		}
+		Node(int val) : l(0), r(0), sum(val) {}
+		Node(int _l, int _r, const vector<Node>& nodes) : l(_l), r(_r), sum(nodes[_l].sum + nodes[_r].sum) { }
 	};
-	vector<Vertex> nodes;
-	vector<int> roots;
-	int allocateNode(Vertex v) {
+	vector<Node> nodes;
+	vector<int> roots, sorted;
+	int allocateNode(const Node& v) {
 		int idx = nodes.size();
 		nodes.push_back(v);
 		return idx;
@@ -58,18 +42,18 @@ private:
 	int tl, tr, n;
 	int update(int v, int l, int r, int pos) {
 		if (l == r) {
-			return allocateNode(Vertex(Vertex::getSum(v, nodes)+1));
+			return allocateNode(Node(nodes[v].sum+1));
 		}
 		int m = (l + r) / 2;
 		if (pos <= m)
-			return allocateNode(Vertex(update(Vertex::getL(v, nodes), l, m, pos), Vertex::getR(v, nodes), nodes));
-		return allocateNode(Vertex(Vertex::getL(v, nodes), update(Vertex::getR(v, nodes), m+1, r, pos), nodes));
+			return allocateNode(Node(update(nodes[v].l, l, m, pos), nodes[v].r, nodes));
+		return allocateNode(Node(nodes[v].l, update(nodes[v].r, m+1, r, pos), nodes));
 	}
 	int find_kth(int vl, int vr, int l, int r, int k) const {
 		if (l == r)
 			return l;
-		int m = (l + r) / 2, left_count = Vertex::getSum(Vertex::getL(vr, nodes), nodes) - Vertex::getSum(Vertex::getL(vl, nodes), nodes);
-		if (left_count >= k) return find_kth(Vertex::getL(vl, nodes), Vertex::getL(vr, nodes), l, m, k);
-		return find_kth(Vertex::getR(vl, nodes), Vertex::getR(vr, nodes), m+1, r, k-left_count);
+		int m = (l + r) / 2, left_count = nodes[nodes[vr].l].sum - nodes[nodes[vl].l].sum;
+		if (left_count >= k) return find_kth(nodes[vl].l, nodes[vr].l, l, m, k);
+		return find_kth(nodes[vl].r, nodes[vr].r, m+1, r, k-left_count);
 	}
 };
