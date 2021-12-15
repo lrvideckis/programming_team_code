8d20d2042cda92661d38c0c1d36e0e0daba28b28
lrvideckis <lukevideckis@gmail.com>
2021-12-15 04:19:12 -0800

    starting point for compression and no build

--- range_data_structures/kthSmallest.cpp
+++ range_data_structures/kthSmallest.cpp
@@ -6,11 +6,12 @@ public:
 	 * O(nlogn) time and space to build tree
 	 */
 	kthSmallest(const vector<int> &arr) {
-		tl = *min_element(arr.begin(), arr.end());
-		tr = *max_element(arr.begin(), arr.end());
+		doCompression(arr);
+		tl = 0, tr = (int)sorted.size()-1, n = arr.size();
 		roots.push_back(shared_ptr<Vertex>(new Vertex(0)));
 		for (int val : arr) {
-			roots.push_back(update(roots.back(), tl, tr, val));
+			int idx = lower_bound(sorted.begin(), sorted.end(), val) - sorted.begin();
+			roots.push_back(update(roots.back(), tl, tr, idx));
 		}
 	}
 	/* find kth smallest number among arr[L], arr[L+1], ..., arr[R]
@@ -19,15 +20,16 @@ public:
 	 */
 	int find_kth(int L, int R, int k) const {
 		assert(1 <= k && k <= R-L+1);
-		return find_kth(roots[L], roots[R+1], tl, tr, k);
-	}
-	/* does: arr[idx] = newValue
-	 *
-	 */
-	void update(int idx, int newValue) {
-
+		assert(0 <= L && R < n);
+		return sorted[find_kth(roots[L], roots[R+1], tl, tr, k)];
 	}
 private:
+	vector<int> sorted;
+	void doCompression(const vector<int> &arr) {
+		sorted = arr;
+		sort(sorted.begin(), sorted.end());
+		sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
+	}
 	struct Vertex {
 		shared_ptr<Vertex> l, r;
 		int sum;
@@ -47,16 +49,11 @@ private:
 		}
 	};
 	vector<shared_ptr<Vertex>> roots;
-	int tl,tr;
-	int getMid(int l, int r) const {
-		int mid = l + (r - l)/2;
-		if(mid > l) mid--;
-		return mid;
-	}
+	int tl, tr, n;
 	shared_ptr<Vertex> update(shared_ptr<Vertex> v, int l, int r, int pos) {
 		if (l == r)
 			return shared_ptr<Vertex>(new Vertex(Vertex::getSum(v)+1));
-		int m = getMid(l, r);
+		int m = (l + r) / 2;
 		if (pos <= m)
 			return shared_ptr<Vertex>(new Vertex(update(Vertex::getL(v), l, m, pos), Vertex::getR(v)));
 		else
@@ -65,7 +62,7 @@ private:
 	int find_kth(shared_ptr<Vertex> vl, shared_ptr<Vertex> vr, int l, int r, int k) const {
 		if (l == r)
 			return l;
-		int m = getMid(l, r), left_count = Vertex::getSum(Vertex::getL(vr)) - Vertex::getSum(Vertex::getL(vl));
+		int m = (l + r) / 2, left_count = Vertex::getSum(Vertex::getL(vr)) - Vertex::getSum(Vertex::getL(vl));
 		if (left_count >= k) return find_kth(Vertex::getL(vl), Vertex::getL(vr), l, m, k);
 		return find_kth(Vertex::getR(vl), Vertex::getR(vr), m+1, r, k-left_count);
 	}
