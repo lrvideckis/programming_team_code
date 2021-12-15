2aeaa07c7a604603e2c44b03d1b5474b5b0f4f6f
lrvideckis <lukevideckis@gmail.com>
2021-12-15 00:23:20 -0800

    changed to smart pointers to not leak memory

--- range_data_structures/kthSmallest.cpp
+++ range_data_structures/kthSmallest.cpp
@@ -1,70 +1,69 @@
 //modified from k-th smallest section of https://cp-algorithms.com/data_structures/segment_tree.html
-//this leaks memory
-struct persistentSegTree {
-	public:
-		/* Persistent seg tree to find kth smallest number in a range
-		 * - no updates
-		 * O(nlogn) time and space to build tree
-		 */
-		persistentSegTree(const vector<int> &arr) {
-			doCompression(arr);
-			tl = 0, tr = (int)arr.size();
-			roots.push_back(build(tl, tr));
-			for (int i = 0; i < (int)arr.size(); i++) {
-				roots.push_back(update(roots.back(), tl, tr, compress[i]));
-			}
+struct kthSmallest {
+public:
+	/* Persistent seg tree to find kth smallest number in a range
+	 * - no updates
+	 * O(nlogn) time and space to build tree
+	 */
+	kthSmallest(const vector<int> &arr) {
+		doCompression(arr);
+		tl = 0, tr = (int)arr.size();
+		roots.push_back(build(tl, tr));
+		for (int i = 0; i < (int)arr.size(); i++) {
+			roots.push_back(update(roots.back(), tl, tr, compress[i]));
 		}
-		/* find kth smallest number among arr[L], arr[L+1], ..., arr[R]
-		 * k is 1-based, so find_kth(L,R,1) returns the min
-		 * O(logn)
-		 */
-		int find_kth(int L, int R, int k) const {
-			assert(1 <= k && k <= R-L+1);
-			return sorted[find_kth(roots[L], roots[R+1], tl, tr, k)];
+	}
+	/* find kth smallest number among arr[L], arr[L+1], ..., arr[R]
+	 * k is 1-based, so find_kth(L,R,1) returns the min
+	 * O(logn)
+	 */
+	int find_kth(int L, int R, int k) const {
+		assert(1 <= k && k <= R-L+1);
+		return sorted[find_kth(roots[L], roots[R+1], tl, tr, k)];
+	}
+private:
+	vector<int> compress, sorted;
+	void doCompression(const vector<int> &arr) {
+		int n = arr.size();
+		sorted = arr;
+		sort(sorted.begin(), sorted.end());
+		sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
+		compress.resize(n);
+		for(int i = 0; i < n; ++i) {
+			compress[i] = lower_bound(sorted.begin(), sorted.end(), arr[i]) - sorted.begin();
 		}
-	private:
-		vector<int> compress, sorted;
-		void doCompression(const vector<int> &arr) {
-			int n = arr.size();
-			sorted = arr;
-			sort(sorted.begin(), sorted.end());
-			sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
-			compress.resize(n);
-			for(int i = 0; i < n; ++i) {
-				compress[i] = lower_bound(sorted.begin(), sorted.end(), arr[i]) - sorted.begin();
-			}
+	}
+	struct Vertex {
+		shared_ptr<Vertex> l, r;
+		int sum;
+		Vertex(int val) : l(nullptr), r(nullptr), sum(val) {}
+		Vertex(shared_ptr<Vertex> _l, shared_ptr<Vertex> _r) : l(_l), r(_r), sum(0) {
+			if (l) sum += l->sum;
+			if (r) sum += r->sum;
 		}
-		struct Vertex {
-			Vertex *l, *r;
-			int sum;
-			Vertex(int val) : l(nullptr), r(nullptr), sum(val) {}
-			Vertex(Vertex *l, Vertex *r) : l(l), r(r), sum(0) {
-				if (l) sum += l->sum;
-				if (r) sum += r->sum;
-			}
-		};
-		vector<Vertex*> roots;
-		int tl,tr;
-		Vertex* build(int l, int r) {
-			if (l == r)
-				return new Vertex(0);
-			int m = (l + r) / 2;
-			return new Vertex(build(l, m), build(m+1, r));
-		}
-		Vertex* update(Vertex* v, int l, int r, int pos) {
-			if (l == r)
-				return new Vertex(v->sum+1);
-			int m = (l + r) / 2;
-			if (pos <= m)
-				return new Vertex(update(v->l, l, m, pos), v->r);
-			else
-				return new Vertex(v->l, update(v->r, m+1, r, pos));
-		}
-		int find_kth(Vertex* vl, Vertex *vr, int l, int r, int k) const {
-			if (l == r)
-				return l;
-			int m = (l + r) / 2, left_count = vr->l->sum - vl->l->sum;
-			if (left_count >= k) return find_kth(vl->l, vr->l, l, m, k);
-			return find_kth(vl->r, vr->r, m+1, r, k-left_count);
-		}
+	};
+	vector<shared_ptr<Vertex>> roots;
+	int tl,tr;
+	shared_ptr<Vertex> build(int l, int r) {
+		if (l == r)
+			return shared_ptr<Vertex>(new Vertex(0));
+		int m = (l + r) / 2;
+		return shared_ptr<Vertex>(new Vertex(build(l, m), build(m+1, r)));
+	}
+	shared_ptr<Vertex> update(shared_ptr<Vertex> v, int l, int r, int pos) {
+		if (l == r)
+			return shared_ptr<Vertex>(new Vertex(v->sum+1));
+		int m = (l + r) / 2;
+		if (pos <= m)
+			return shared_ptr<Vertex>(new Vertex(update(v->l, l, m, pos), v->r));
+		else
+			return shared_ptr<Vertex>(new Vertex(v->l, update(v->r, m+1, r, pos)));
+	}
+	int find_kth(shared_ptr<Vertex> vl, shared_ptr<Vertex> vr, int l, int r, int k) const {
+		if (l == r)
+			return l;
+		int m = (l + r) / 2, left_count = vr->l->sum - vl->l->sum;
+		if (left_count >= k) return find_kth(vl->l, vr->l, l, m, k);
+		return find_kth(vl->r, vr->r, m+1, r, k-left_count);
+	}
 };
