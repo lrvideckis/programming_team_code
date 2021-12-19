a4cee6e189e887cab11813ec226ba9eb17218103
lrvideckis <lukevideckis@gmail.com>
2021-12-19 12:24:28 -0800

    code golfed dijkstras

--- content/graphs/dijkstra.h
+++ content/graphs/dijkstra.h
@@ -1,18 +1,15 @@
 #pragma once
 
 vector<ll> dijkstra(const vector<vector<pair<int,ll>>> &adj, int startNode) {
-	const int n = adj.size();
-	vector<ll> length(n, 1e18);
+	vector<ll> length(adj.size(), 1e18);
 	length[startNode] = 0;
-	set<pair<ll, int> > q;//weight, node
+	set<pair<ll, int>> q;//weight, node
 	q.insert({0, startNode});
 	while(!q.empty()) {
 		auto it = q.begin();
 		const int node = it->second;
 		q.erase(it);
-		for(auto &p : adj[node]) {
-			int to = p.first;
-			ll weight = p.second;
+		for(auto [to, weight] : adj[node]) {
 			if(length[to] > weight + length[node]) {
 				q.erase({length[to], to});
 				length[to] = weight + length[node];
