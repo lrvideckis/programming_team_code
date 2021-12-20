f02065bb8d08e9a4a83133e07b059a68762a8cb4
Luke Videckis <lrvideckis@fb.com>
2021-12-20 16:04:07 -0600

    moving pragmas into comment in template to consolidate files

--- template.cpp
+++ template.cpp
@@ -1,3 +1,6 @@
+//https://codeforces.com/blog/entry/96344
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
