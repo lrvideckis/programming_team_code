cb1f6f715fd7b4ae28606217dce562fe358bf2d7
Luke Videckis <lrvideckis@fb.com>
2022-01-02 11:57:45 -0600

    templatizing sparse table

--- stress-tests/range_data_structures/sparseTable.cpp
+++ stress-tests/range_data_structures/sparseTable.cpp
@@ -8,16 +8,18 @@ int main() {
 		int n = getRand(1, 1000);
 		vector<ll> arr(n);
 		int mx = 1e9;
+		vector<pair<ll,int>> arrIdx(n);
 		for(int i = 0; i < n; i++) {
 			arr[i] = getRand(-mx, mx);
+			arrIdx[i] = make_pair(arr[i], i);
 		}
-		sparseTableIdx<ll> stIdx(arr);
-		sparseTable st(arr);
+		sparseTable<pair<ll,int>> stIdx(arrIdx);
+		sparseTable<ll> st(arr);
 		for(int i = 0; i < n; i++) {
 			int mnIdx = i;
 			for(int j = i; j < n; j++) {
 				if(arr[mnIdx] > arr[j]) mnIdx = j;
-				assert(stIdx.query(i,j) == mnIdx);
+				assert(stIdx.query(i,j).second == mnIdx);
 				assert(st.query(i,j) == arr[mnIdx]);
 			}
 		}
