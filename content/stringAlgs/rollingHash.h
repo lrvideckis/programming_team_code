68fb31386ff20563eb9d9bf2f64496a42a7d87d0
Luke Videckis <lrvideckis@fb.com>
2022-01-03 13:01:44 -0600

    code golfed hashing struct

--- content/stringAlgs/rollingHash.h
+++ content/stringAlgs/rollingHash.h
@@ -14,38 +14,30 @@ struct Hash {
 	//From C.R.T, this is the same as having a single mod = product of `mods`
 	//probability of collision is p = 1/(product of `mods`)
 	//probability of >=1 collision after n compares is 1-((1-p)^n)
-	vector<int> mods = {(int)1e9+7, (int)1e9+9, (int)1e9+21, (int)1e9+33, (int)1e9+87};
+	const vector<int> mods = {(int)1e9+7, (int)1e9+9, (int)1e9+21, (int)1e9+33, (int)1e9+87};
 	vector<vector<int>> prefix, powB;
 
 	Hash(const T& s) :
-		prefix(mods.size(), vector<int>(s.size())),
-		powB(mods.size(), vector<int>(s.size(), 1)) {
+		prefix(mods.size(), vector<int>(s.size()+1, 0)),
+		powB(mods.size(), vector<int>(s.size()+1, 1)) {
 		//negatives may cause trivial collisions, when s[i]%mod = s[j]%mod, but s[i] != s[j]
 		//0's cause trivial collisions: "0" and "00" both hash to 0
 		for(auto val : s) assert(val > 0);
 		for(int i = 0; i < (int)mods.size(); i++) {
-			for(int j = 1; j < (int)s.size(); j++) {
-				powB[i][j] = 1LL * powB[i][j-1] * base % mods[i];
-			}
-			int sum = 0;
 			for(int j = 0; j < (int)s.size(); j++) {
-				sum = (1LL * base * sum + s[j]) % mods[i];
-				prefix[i][j] = sum;
+				powB[i][j+1] = 1LL * powB[i][j] * base % mods[i];
+				prefix[i][j+1] = (1LL * base * prefix[i][j] + s[j]) % mods[i];
 			}
 		}
 	}
 
-	//returns hashes of substring [L,R] inclusive, one hash per mod
+	//returns hashes of substring/subarray [L,R] inclusive, one hash per mod
 	vector<int> getHashes(int L, int R) const {
 		assert(L <= R);
-		vector<int> allHashes;
+		vector<int> allHashes(mods.size());
 		for(int i = 0; i < (int)mods.size(); i++) {
-			int currHash = prefix[i][R];
-			if(L) {
-				currHash -= 1LL * prefix[i][L-1] * powB[i][R-L+1] % mods[i];
-				if(currHash < 0) currHash += mods[i];
-			}
-			allHashes.push_back(currHash);
+			allHashes[i] = prefix[i][R+1] - 1LL * prefix[i][L] * powB[i][R-L+1] % mods[i];
+			if(allHashes[i] < 0) allHashes[i] += mods[i];
 		}
 		return allHashes;
 	}
---
+++ stress-tests/stringAlgs/rollingHashAndSuffixArray.cpp
@@ -0,0 +1,53 @@
+#include "../test_utilities/template.h"
+#include "../test_utilities/random.h"
+
+#include "../../content/stringAlgs/rollingHash.h"
+#include "../../content/stringAlgs/suffixArray.h"
+
+int main() {
+	for(int tests = 200; tests--;) {
+		int n = getRand(1, 100);
+		string s(n, 'a');
+		vector<int> arr(n);
+		for(int i = 0; i < n; i++) {
+			arr[i] = getRand(1, 4);
+			s[i] = arr[i] + 'a';
+		}
+
+		suffix_array sa(s);
+
+		Hash<string> hStr(s);
+		Hash<vector<int>> hArr(arr);
+
+		for(int i = 0; i < n; i++) {
+			for(int j = i; j < n; j++) {
+				bool sameNaive = true;
+				for(int len = 1; j+len-1 < n; len++) {
+					if(s[i+len-1] != s[j+len-1]) {
+						sameNaive = false;
+					}
+					bool sameHashStr = hStr.getHashes(i,i+len-1) == hStr.getHashes(j,j+len-1);
+					bool sameHashArr = hArr.getHashes(i,i+len-1) == hArr.getHashes(j,j+len-1);
+					bool sameSA = len <= sa.longest_common_prefix(i,j);
+					assert(sameNaive == sameHashStr);
+					assert(sameNaive == sameHashArr);
+					assert(sameNaive == sameSA);
+
+
+					//sanity check on different lengths
+					if(j+len < n) {
+						assert(hStr.getHashes(i,i+len-1) != hStr.getHashes(i, n-1));
+						assert(hArr.getHashes(i,i+len-1) != hArr.getHashes(i, n-1));
+					}
+					if(len > 1) {
+						assert(hStr.getHashes(i,i+len-1) != hStr.getHashes(i,i));
+						assert(hArr.getHashes(i,i+len-1) != hArr.getHashes(i,i));
+					}
+				}
+			}
+		}
+	}
+
+	cout << "Tests passed!" << endl;
+	return 0;
+}
