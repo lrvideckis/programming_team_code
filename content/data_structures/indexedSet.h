6230113ba37d4bf2a26f35238d48aed356ff7160
lrvideckis <lukevideckis@gmail.com>
2022-02-04 00:32:07 -0800

    nits to indexes set

--- content/data_structures/indexedSet.h
+++ content/data_structures/indexedSet.h
@@ -1,6 +1,6 @@
 #include <bits/extc++.h>
 using namespace __gnu_pbds;
-template<class TI>
-using indexed_set = tree<TI, null_type, std::less<TI>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T>
+using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 //order_of_key
 //find_by_order
