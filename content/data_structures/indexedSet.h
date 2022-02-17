c833ad2c941301dc50db96de96be2daa637ccebe
Luke Videckis <lukevideckis@gmail.com>
2022-02-17 02:01:46 -0800

    Update indexedSet.h

--- content/data_structures/indexedSet.h
+++ content/data_structures/indexedSet.h
@@ -1,8 +1,9 @@
 //status: not tested
 
+//having #define int long long makes this not compile :(
 #include <bits/extc++.h>
 using namespace __gnu_pbds;
 template<class T>
 using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-//order_of_key
-//find_by_order
+//order_of_key - # of elements *strictly* less than given element
+//find_by_order - find kth largest element, k is 0 based
