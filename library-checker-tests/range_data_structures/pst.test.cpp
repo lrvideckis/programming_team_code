7dd035be98b7704077e2453d8b381d8f12cc9ca5
Luke Videckis <lrvideckis@fb.com>
2022-03-17 14:39:50 -0400

    added library checker test for pst

---
+++ library-checker-tests/range_data_structures/pst.test.cpp
@@ -0,0 +1,45 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_kth_smallest"
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <climits>
+#include <memory>
+using namespace std;
+#define endl '\n'
+typedef long long ll;
+
+#include "../../content/range_data_structures/pst.h"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(false);
+	int n,q;
+	cin >> n >> q;
+	vector<int> arr(n);
+	for(int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	pst st(arr);
+	while(q--) {
+		int l,r,k;
+		cin >> l >> r >> k;
+		cout << st.find_kth(l, r-1, k+1) << endl;
+	}
+	return 0;
+}
