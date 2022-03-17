adc4325a66ba81f0ae06db0d25ea28a33b63dbac
Luke Videckis <lrvideckis@fb.com>
2022-03-17 15:01:32 -0400

    add l,r to seg tree node for merge sort tree

--- content/range_data_structures/mergeSortTree.h
+++ content/range_data_structures/mergeSortTree.h
@@ -5,47 +5,54 @@
 struct MergeSortTree {
 	struct Node {
 		vector<int> vals;
-	};
-	vector<Node> tree;
-	int n, size;
 
-	/*implement these*/
-	Node combine(const Node& L, const Node& R) {
+		int l, r;
+	};
+
+	vector<Node> tree;
+	int n;
+
+	Node combineChildren(const Node& L, const Node& R) {
 		vector<int> par(L.vals.size() + R.vals.size());
 		merge(L.vals.begin(), L.vals.end(), R.vals.begin(), R.vals.end(), par.begin());
-		return Node{par};
+		return Node{par, L.l, R.r};
 	}
 
-	//O(nlogn) time and space
+	//There's no constructor `SegmentTree(int size)` because how to initialize l,r in nodes without calling build?
+	//the whole point of this constructor was to be simpler by not calling build
 	MergeSortTree(const vector<int>& arr) : n((int) arr.size()) {
-		size = 1;
+		int size = 1;
 		while (size < n) size <<= 1;
 		size <<= 1;
 		tree.resize(size);
 		build(arr, 1, 0, n - 1);
 	}
 	void build(const vector<int>& arr, int node, int start, int end) {
-		if (start == end)
-			tree[node].vals.push_back(arr[start]);
-		else {
+		if (start == end) {
+			tree[node] = Node {
+				vector<int>{arr[start]},
+				start,
+				end
+			};
+		} else {
 			int mid = (start + end) / 2;
 			build(arr, 2 * node, start, mid);
 			build(arr, 2 * node + 1, mid + 1, end);
-			tree[node] = combine(tree[2 * node], tree[2 * node + 1]);
+			tree[node] = combineChildren(tree[2 * node], tree[2 * node + 1]);
 		}
 	}
-	//returns how many values of arr[l], arr[l+1], ..., arr[r] which are < x
-	//O(log^2)
+
+	//inclusive range: [l,r]
 	int query(int l, int r, int x) {
-		return query(1, 0, n - 1, l, r, x);
+		return query(1, l, r, x);
 	}
-	int query(int node, int start, int end, int l, int r, int x) {
+	int query(int node, int l, int r, int x) {
+		int start = tree[node].l, end = tree[node].r;
 		if (r < start || end < l) return 0;
 		if (l <= start && end <= r) {
-			auto& v = tree[node].vals;
+			vector<int>& v = tree[node].vals;
 			return lower_bound(v.begin(), v.end(), x) - v.begin();
 		}
-		int mid = (start + end) / 2;
-		return query(2 * node, start, mid, l, r, x) + query(2 * node + 1, mid + 1, end, l, r, x);
+		return query(2 * node, l, r, x) + query(2 * node + 1, l, r, x);
 	}
 };
