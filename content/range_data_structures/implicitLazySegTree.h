5114ccc819cb5a1bb55375d98cae3d79b8fd71a7
lrvideckis <lukevideckis@gmail.com>
2022-04-14 21:09:48 -0700

    remove l,r from node

--- content/range_data_structures/implicitLazySegTree.h
+++ content/range_data_structures/implicitLazySegTree.h
@@ -5,94 +5,86 @@
 struct Node {
 	ll val;//could represent max, sum, etc
 	ll lazy;
-
 	int lCh, rCh; // children, indexes into `tree`, -1 for null
-
-	int l, r; // range of node: [l,r]
-
-	int len() const {
-		return r - l + 1;
-	}
-
 } tree[(int)4e4/*TODO: update*/];
 
 struct implicitLazySegTree {
 
-	int NEW_NODE;
+	int NEW_NODE, sz;
 
-	implicitLazySegTree(int sz) : NEW_NODE(0) {
-		tree[NEW_NODE++] = {0, 0, -1, -1, 0, sz - 1}; //root node
+	implicitLazySegTree(int _sz) : NEW_NODE(0), sz(_sz) {
+		tree[NEW_NODE++] = {0, 0, -1, -1}; //root node
 	}
 
-	implicitLazySegTree(const vector<ll>& arr) : NEW_NODE(0) {
-		tree[NEW_NODE++] = {0, 0, -1, -1, 0, (int)arr.size() - 1}; //root node
-		build(arr, 0);
+	implicitLazySegTree(const vector<ll>& arr) : NEW_NODE(0), sz(arr.size()) {
+		tree[NEW_NODE++] = {0, 0, -1, -1}; //root node
+		build(arr, 0, 0, sz - 1);
 	}
-	void build(const vector<ll>& arr, int v) {
-		if (tree[v].len() == 1)
-			tree[v].val = arr[tree[v].l];
+	void build(const vector<ll>& arr, int v, int tl, int tr) {
+		if (tl == tr)
+			tree[v].val = arr[tl];
 		else {
-			push(v);
-			build(arr, tree[v].lCh);
-			build(arr, tree[v].rCh);
+			push(v, tl, tr);
+			int tm = (tl + tr) / 2;
+			build(arr, tree[v].lCh, tl, tm);
+			build(arr, tree[v].rCh, tm + 1, tr);
 			tree[v].val = combine(tree[tree[v].lCh].val, tree[tree[v].rCh].val);
 		}
 	}
 
-	ll combine(ll l, ll r) {
-		return max(l, r); //TODO: update
+	ll combine(ll val_l, ll val_r) {
+		return max(val_l, val_r); //TODO: update
 	}
 
-	void applyDeltaOnRange(int v, ll add) {
+	void applyDeltaOnRange(int v, int tl, int tr, ll add) {
 		tree[v].val += add; //TODO: update
-		if (tree[v].len() > 1) {
+		if (tl != tr) {
 			tree[tree[v].lCh].lazy += add;
 			tree[tree[v].rCh].lazy += add;
 		}
 	}
 
-	void push(int v) {
-		if (tree[v].len() > 1 && tree[v].lCh == -1) {
-			int tl = tree[v].l;
-			int tr = tree[v].r;
-			int tm = (tl + tr) / 2;
+	void push(int v, int tl, int tr) {
+		if (tl != tr && tree[v].lCh == -1) {
 			tree[v].lCh = NEW_NODE;
-			tree[NEW_NODE++] = {0, 0, -1, -1, tl, tm};
+			tree[NEW_NODE++] = {0, 0, -1, -1};
 			tree[v].rCh = NEW_NODE;
-			tree[NEW_NODE++] = {0, 0, -1, -1, tm + 1, tr};
+			tree[NEW_NODE++] = {0, 0, -1, -1};
 		}
 		if (tree[v].lazy) {
-			applyDeltaOnRange(v, tree[v].lazy);
+			applyDeltaOnRange(v, tl, tr, tree[v].lazy);
 			tree[v].lazy = 0;
 		}
 	}
 
 	//add `add` to each value in range [l,r]
 	void update(int l, int r, ll add) {
-		update(0, l, r, add);
+		update(0, 0, sz - 1, l, r, add);
 	}
-	void update(int v, int l, int r, ll add) {
-		push(v);
-		if (tree[v].r < l || r < tree[v].l)
+	void update(int v, int tl, int tr, int l, int r, ll add) {
+		push(v, tl, tr);
+		if (tr < l || r < tl)
 			return;
-		if (l <= tree[v].l && tree[v].r <= r)
-			return applyDeltaOnRange(v, add);
-		update(tree[v].lCh, l, r, add);
-		update(tree[v].rCh, l, r, add);
+		if (l <= tl && tr <= r)
+			return applyDeltaOnRange(v, tl, tr, add);
+		int tm = (tl + tr) / 2;
+		update(tree[v].lCh, tl, tm, l, r, add);
+		update(tree[v].rCh, tm + 1, tr, l, r, add);
 		tree[v].val = combine(tree[tree[v].lCh].val, tree[tree[v].rCh].val);
 	}
 
 	//max on range [l,r]
 	ll query(int l, int r) {
-		return query(0, l, r);
+		return query(0, 0, sz - 1, l, r);
 	}
-	ll query(int v, int l, int r) {
-		if (tree[v].r < l || r < tree[v].l)
+	ll query(int v, int tl, int tr, int l, int r) {
+		if (tr < l || r < tl)
 			return -1e18; //TODO: update
-		push(v);
-		if (l <= tree[v].l && tree[v].r <= r)
+		push(v, tl, tr);
+		if (l <= tl && tr <= r)
 			return tree[v].val;
-		return combine(query(tree[v].lCh, l, r),
-		               query(tree[v].rCh, l, r));
+		int tm = (tl + tr) / 2;
+		return combine(query(tree[v].lCh, tl, tm, l, r),
+		               query(tree[v].rCh, tm + 1, tr, l, r));
 	}
 };
