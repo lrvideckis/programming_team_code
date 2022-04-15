c9d66388c14361f8b8a032d6baba01b68b93f038
Luke Videckis <lukevideckis@gmail.com>
2022-04-14 23:15:38 -0700

    Update implicitLazySegTree.h

--- content/range_data_structures/implicitLazySegTree.h
+++ content/range_data_structures/implicitLazySegTree.h
@@ -6,7 +6,7 @@ struct Node {
 	ll val;//could represent max, sum, etc
 	ll lazy;
 	int lCh, rCh; // children, indexes into `tree`, -1 for null
-} tree[(int)2e7/*TODO: update*/];
+} tree[(int)1.5e7/*TODO: update*/];
 
 struct implicitLazySegTree {
 
