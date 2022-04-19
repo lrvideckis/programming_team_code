95cca178d7053ff3b4f09c2aaf8b2933f48aceb1
lrvideckis <lukevideckis@gmail.com>
2022-04-18 21:52:55 -0700

    make trie a struct #35

--- content/stringAlgs/trie.h
+++ content/stringAlgs/trie.h
@@ -4,50 +4,55 @@
 
 const int K = 26;//character size
 
-struct node {
-	bool leaf = 0;
-	int next[K], id, p = -1;
-	char pch;
-	node(int _p = -1, char ch = '#') : p(_p), pch(ch) {
-		fill(next, next + K, -1);
+struct trie {
+
+	struct node {
+		bool leaf = 0;
+		int next[K], id, p = -1;
+		char pch;
+		node(int _p = -1, char ch = '#') : p(_p), pch(ch) {
+			fill(next, next + K, -1);
+		}
+	};
+
+	vector<node> t;    //adj list
+
+	trie() : t(1) {}
+
+	void add_string(const string& s, int id) {
+		int c = 0;
+		for (char ch : s) {
+			int v = ch - 'a';
+			if (t[c].next[v] == -1) {
+				t[c].next[v] = t.size();
+				t.emplace_back(c, ch);
+			}
+			c = t[c].next[v];
+		}
+		t[c].leaf = 1;
+		t[c].id = id;
+	}
+
+	void remove_string(const string& s) {
+		int c = 0;
+		for (char ch : s) {
+			int v = ch - 'a';
+			if (t[c].next[v] == -1)
+				return;
+			c = t[c].next[v];
+		}
+		t[c].leaf = 0;
+	}
+
+	int find_string(const string& s) {
+		int c = 0;
+		for (char ch : s) {
+			int v = ch - 'a';
+			if (t[c].next[v] == -1)
+				return -1;
+			c = t[c].next[v];
+		}
+		if (!t[c].leaf) return -1;
+		return t[c].id;
 	}
 };
-
-vector<node> t(1);    //adj list
-
-void add_string(const string& s, int id) {
-	int c = 0;
-	for (char ch : s) {
-		int v = ch - 'a';
-		if (t[c].next[v] == -1) {
-			t[c].next[v] = t.size();
-			t.emplace_back(c, ch);
-		}
-		c = t[c].next[v];
-	}
-	t[c].leaf = 1;
-	t[c].id = id;
-}
-
-void remove_string(const string& s) {
-	int c = 0;
-	for (char ch : s) {
-		int v = ch - 'a';
-		if (t[c].next[v] == -1)
-			return;
-		c = t[c].next[v];
-	}
-	t[c].leaf = 0;
-}
-
-int find_string(const string& s) {
-	int c = 0;
-	for (char ch : s) {
-		int v = ch - 'a';
-		if (t[c].next[v] == -1)
-			return -1;
-		c = t[c].next[v];
-	}
-	if (!t[c].leaf) return -1;
-	return t[c].id;
-}
