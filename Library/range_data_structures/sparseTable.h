7372018788af30fe9b51595384922fd92da3b7cc
lrvideckis <lukevideckis@gmail.com>
2022-04-26 21:36:48 -0700

    code golfed sparse table, inspired by kactl's

--- Library/range_data_structures/sparseTable.h
+++ Library/range_data_structures/sparseTable.h
@@ -13,22 +13,19 @@
 //status: tested on random inputs, also on https://judge.yosupo.jp/problem/staticrmq
 template <class T>
 struct sparseTable {
-	vector<int> log2;
 	vector<vector<T>> dp;
 	function<T(const T&, const T&)> func;
-	sparseTable(const vector<T>& arr, const function<T(const T&, const T&)>& _func) : func(_func) {
-		const int n = arr.size();
-		log2.resize(n + 1, -1);
-		for (int i = 1; i <= n; ++i) log2[i] = 1 + log2[i / 2];
-		dp.resize(log2[n] + 1, arr);
-		for (int i = 1; i <= log2[n]; ++i) {
-			for (int j = 0; j + (1 << i) - 1 < n; ++j)
-				dp[i][j] = func(dp[i - 1][j], dp[i - 1][j + (1 << (i - 1))]);
+	sparseTable(const vector<T>& arr, const function<T(const T&, const T&)>& _func) : dp(1, arr), func(_func) {
+		int n = arr.size();
+		for (int pw = 1, k = 1; pw * 2 <= n; pw *= 2, k++) {
+			dp.emplace_back(n - pw * 2 + 1);
+			for(int j = 0; j < (int)dp[k].size(); j++)
+				dp[k][j] = func(dp[k - 1][j], dp[k - 1][j + pw]);
 		}
 	}
-	//returns func of arr[l], arr[l+1], ..., arr[r]
+	//inclusive range [l, r]
 	T query(int l, int r) const {
-		const int x = log2[r - l + 1];
-		return func(dp[x][l], dp[x][r - (1 << x) + 1]);
+		int lg = 31 - __builtin_clz(r - l + 1);
+		return func(dp[lg][l], dp[lg][r - (1 << lg) + 1]);
 	}
 };
