9a174474ba9f5cd934c96ec6f1f011068a001139
lrvideckis <lukevideckis@gmail.com>
2022-04-27 01:12:16 -0700

    #34

--- Library/range_data_structures/persistentLazySegTree.h
+++ Library/range_data_structures/persistentLazySegTree.h
@@ -1,11 +1,12 @@
 #pragma once
 
+//tested on https://codeforces.com/contest/707/problem/D
 struct persistentLazySegTree {
 
 	struct Node {
 		int lCh, rCh;//children, indexes into `tree`
-		long long mn;
-		long long lazyAdd;
+		int sum;
+		bool lazyTog;
 	};
 
 	int sz;
@@ -19,63 +20,85 @@ struct persistentLazySegTree {
 	}
 
 	void push(int v, int tl, int tr) {
-		if (tree[v].lazyAdd) {
-			tree[v].mn += tree[v].lazyAdd;
-			if (tl != tr) {
-				if (tree[v].lCh != 0) tree[tree[v].lCh].lazyAdd += tree[v].lazyAdd;
-				if (tree[v].rCh != 0) tree[tree[v].rCh].lazyAdd += tree[v].lazyAdd;
-			}
-			tree[v].lazyAdd = 0;
-		}
-	}
-
-	//add diff to each value in range [l,r]
-	void update(int l, int r, long long diff) {
-		tree.push_back(tree[roots.back()]);//allocate top down
-		roots.push_back(tree.size() - 1);
-		update(roots.back(), 0, sz - 1, l, r, diff);
-	}
-	void update(int v, int tl, int tr, int l, int r, long long diff) {
-		//allocate copies of previous version's children
+		assert(v != 0);
 		if (tl != tr) {
 			tree.push_back(tree[tree[v].lCh]);
 			tree[v].lCh = tree.size() - 1;
 			tree.push_back(tree[tree[v].rCh]);
 			tree[v].rCh = tree.size() - 1;
 		}
+		if (tree[v].lazyTog) {
+			tree[v].sum = (tr - tl + 1) - tree[v].sum;
+			tree[v].lazyTog = false;
+			if (tl != tr) {
+				tree[tree[v].lCh].lazyTog ^= 1;
+				tree[tree[v].rCh].lazyTog ^= 1;
+			}
+		}
+	}
+
+	void set(int idx, int new_val) {
+		tree.push_back(tree[roots.back()]);//allocate top down
+		roots.push_back(tree.size() - 1);
+		set(roots.back(), 0, sz - 1, idx, new_val);
+	}
+	void set(int v, int tl, int tr, int idx, int new_val) {
 		push(v, tl, tr);
-		if (tr < l || r < tl)
+		if (tr < idx || idx < tl)
 			return;
+		if (idx <= tl && tr <= idx) {
+			tree[v].sum = new_val;
+			return;
+		}
 		int tm = (tl + tr) / 2;
 		int lCh = tree[v].lCh;
 		int rCh = tree[v].rCh;
+		set(lCh, tl, tm, idx, new_val);
+		set(rCh, tm + 1, tr, idx, new_val);
+		tree[v].sum = tree[lCh].sum + tree[rCh].sum;
+	}
+
+	void toggleRange(int l, int r) {
+		tree.push_back(tree[roots.back()]);//allocate top down
+		roots.push_back(tree.size() - 1);
+		toggleRange(roots.back(), 0, sz - 1, l, r);
+	}
+	void toggleRange(int v, int tl, int tr, int l, int r) {
+		push(v, tl, tr);
+		if (tr < l || r < tl)
+			return;
+		int lCh = tree[v].lCh;
+		int rCh = tree[v].rCh;
 		if (l <= tl && tr <= r) {
-			tree[v].mn += diff;
+			tree[v].sum = (tr - tl + 1) - tree[v].sum;
 			if (tl != tr) {
-				assert(lCh != 0);
-				assert(rCh != 0);
-				tree[lCh].lazyAdd += diff;
-				tree[rCh].lazyAdd += diff;
+				tree[lCh].lazyTog ^= 1;
+				tree[rCh].lazyTog ^= 1;
 			}
 			return;
 		}
-		update(lCh, tl, tm, l, r, diff);
-		update(rCh, tm + 1, tr, l, r, diff);
-		tree[v].mn = min(tree[lCh].mn, tree[rCh].mn);
+		int tm = (tl + tr) / 2;
+		toggleRange(lCh, tl, tm, l, r);
+		toggleRange(rCh, tm + 1, tr, l, r);
+		tree[v].sum = tree[lCh].sum + tree[rCh].sum;
 	}
 
-	//min on range [l,r] for most recent version
-	long long query(int l, int r) {
-		return query(roots.back(), 0, sz - 1, l, r);
+	int query(int l, int r) {
+		int version = roots.size() - 1;
+		int root = roots[version];
+		return query(root, 0, sz - 1, l, r, tree[root].lazyTog);
 	}
-	long long query(int v, int tl, int tr, int l, int r) {
-		push(v, tl, tr);
-		if (tr < l || r < tl)
-			return 1e18;
-		if (l <= tl && tr <= r)
-			return tree[v].mn;
+	int query(int v, int tl, int tr, int l, int r, bool tog) {
+		if (v == 0 || tr < l || r < tl)
+			return 0;
+		if (l <= tl && tr <= r) {
+			int sum = tree[v].sum;
+			if (tree[v].lazyTog) sum = (tr - tl + 1) - sum;
+			return sum;
+		}
 		int tm = (tl + tr) / 2;
-		return min(query(tree[v].lCh, tl, tm, l, r),
-		           query(tree[v].rCh, tm + 1, tr, l, r));
+		tog ^= tree[v].lazyTog;
+		return query(tree[v].lCh, tl, tm, l, r, tog) +
+		       query(tree[v].rCh, tm + 1, tr, l, r, tog);
 	}
 };
