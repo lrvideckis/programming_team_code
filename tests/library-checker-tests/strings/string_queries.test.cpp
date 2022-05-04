965e6d4620d90e1985aa52f932e5a0ebfa8c0ac2
lrvideckis <lukevideckis@gmail.com>
2022-05-03 21:22:11 -0700

    #44

---
+++ tests/library-checker-tests/strings/string_queries.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+#include "../../template.h"
+
+#include "../../../Library/strings/string_queries.h"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	string s;
+	cin >> s;
+
+	str_queries sq(s);
+
+	for(int i = 0; i < (int) s.size(); i++) {
+		cout << sq.longest_common_prefix(i, 0) << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
