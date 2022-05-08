0c9c185e7c05794791f4c05f3fc17ca68fd0d36e
Luke Videckis <lukevideckis@gmail.com>
2022-05-08 12:48:09 -0700

    nit picking at comment

--- Library/graphs/floydWarshall.h
+++ Library/graphs/floydWarshall.h
@@ -3,11 +3,11 @@
 //status: not tested
 //
 //**for directed graphs only** if you initialize len[i][i] to infinity, then
-//afterwards, len[i][i] = length of shortest cycle including node `i`
+//afterward floyds, len[i][i] = length of shortest cycle including node `i`
 //
-//another trick: change len to 2d array of *bools* where len[i][j] = true if
-//there exists an edge from i -> j Then after floyds, len[i][j] = true iff
-//there's exists some path from node `i` to node `j`
+//another trick: change `len` to 2d array of *bools* where len[i][j] = true if
+//there exists an edge from i -> j in initial graph. Then after floyds,
+//len[i][j] = true iff there's exists some path from node `i` to node `j`
 
 for (int k = 0; k < n; k++)
 	for (int i = 0; i < n; i++)
