0c1f5741f1b85f9e962d23c73c31b9e8813d4e69
Luke Videckis <lukevideckis@gmail.com>
2022-05-09 10:42:17 -0700

    add unit test for totient

--- Library/math/totient.h
+++ Library/math/totient.h
@@ -1,9 +1,12 @@
 #pragma once
 
-//not tested, but used in misc. problems
+//status: tested on n in range [1, 800]
 
 //  Euler's totient function counts the positive integers
 //  up to a given integer n that are relatively prime to n.
+
+//To improve, use Pollard-rho to find prime factors
+
 int phi(int n) {
 	int result = n;
 	for (int i = 2, tempN = n; i * i <= tempN; i++) {
---
+++ tests/stress-tests/math/totient.cpp
@@ -0,0 +1,18 @@
+#include "../../template.h"
+#include "../../../Library/misc/random.h"
+
+#include "../../../Library/math/totient.h"
+
+int main() {
+
+	for(int i = 1; i <= 800; i++) {
+		int cnt = 0;
+		for(int j = 1; j <= i; j++) {
+			if(__gcd(i,j) == 1) cnt++;
+		}
+		assert(phi(i) == cnt);
+	}
+
+	cout << "Tests passed!" << endl;
+	return 0;
+}
