3dedbee8272608b10659fd76e5ccd76877ac57b0
Luke Videckis <lukevideckis@gmail.com>
2022-05-17 13:15:14 -0700

    Update bridges_and_cuts.h

--- Library/graphs/bridges_and_cuts.h
+++ Library/graphs/bridges_and_cuts.h
@@ -23,7 +23,7 @@ struct info {
 };
 
 info bridge_and_cut(const vector<vector<pair<int/*neighbor*/, int/*edge id*/>>>& adj/*undirected graph*/, int m/*number of edges*/) {
-	//stuff for both
+	//stuff for both (always keep)
 	int n = adj.size(), timer = 1;
 	vector<int> tin(n, 0), low(n, 0);
 	//2 edge CC stuff (delete if not needed)
