405f4d5a0821813c9530905f737fae0d86983490
Luke Videckis <lukevideckis@gmail.com>
2022-05-18 13:46:52 -0700

    #66

--- Library/range_data_structures/mergeSortTree.h
+++ Library/range_data_structures/mergeSortTree.h
@@ -7,51 +7,54 @@ struct MergeSortTree {
 		vector<int> vals;
 
 		int l, r;
+
+		//returns 1 + (# of nodes in left child's subtree)
+		//https://cp-algorithms.com/data_structures/segment_tree.html#memory-efficient-implementation
+		int rCh() const {
+			return ((r - l) & ~1) + 2;
+		}
 	};
 
 	vector<Node> tree;
 
-	Node combineChildren(const Node& L, const Node& R) {
-		vector<int> par(L.vals.size() + R.vals.size());
-		merge(L.vals.begin(), L.vals.end(), R.vals.begin(), R.vals.end(), par.begin());
-		return Node{par, L.l, R.r};
+	//There's no constructor `segTree(int size)` because how to initialize l,r in nodes without calling build?
+	//the whole point of `segTree(int size)` was to be simpler by not calling build
+	MergeSortTree(const vector<int>& arr) : tree(2 * (int) arr.size() - 1) {
+		build(arr, 0, 0, (int) arr.size() - 1);
 	}
-
-	//There's no constructor `SegmentTree(int size)` because how to initialize l,r in nodes without calling build?
-	//the whole point of this constructor was to be simpler by not calling build
-	MergeSortTree(const vector<int>& arr)  {
-		int n = arr.size(), size = 1;
-		while (size < n) size <<= 1;
-		size <<= 1;
-		tree.resize(size);
-		build(arr, 1, 0, n - 1);
-	}
-	void build(const vector<int>& arr, int node, int start, int end) {
-		if (start == end) {
-			tree[node] = Node {
-				vector<int>{arr[start]},
-				start,
-				end
+	void build(const vector<int>& arr, int v, int tl, int tr) {
+		if (tl == tr) {
+			tree[v] = {
+				{arr[tl]},
+				tl,
+				tr
 			};
 		} else {
-			int mid = (start + end) / 2;
-			build(arr, 2 * node, start, mid);
-			build(arr, 2 * node + 1, mid + 1, end);
-			tree[node] = combineChildren(tree[2 * node], tree[2 * node + 1]);
+			int tm = tl + (tr - tl) / 2;
+			build(arr, v + 1, tl, tm);
+			build(arr, v + 2 * (tm - tl + 1), tm + 1, tr);
+			tree[v] = combine(tree[v + 1], tree[v + 2 * (tm - tl + 1)]);
 		}
 	}
 
-	//inclusive range: [l,r]
-	int query(int l, int r, int x) {
-		return query(1, l, r, x);
+	Node combine(const Node& L, const Node& R) {
+		vector<int> par(L.vals.size() + R.vals.size());
+		merge(L.vals.begin(), L.vals.end(), R.vals.begin(), R.vals.end(), par.begin());
+		return {par, L.l, R.r};
 	}
-	int query(int node, int l, int r, int x) {
-		int start = tree[node].l, end = tree[node].r;
-		if (r < start || end < l) return 0;
-		if (l <= start && end <= r) {
-			vector<int>& v = tree[node].vals;
-			return lower_bound(v.begin(), v.end(), x) - v.begin();
+
+	//range [l,r]
+	int query(int l, int r, int x) {
+		return query(0, l, r, x);
+	}
+	int query(int v, int l, int r, int x) {
+		if (tree[v].r < l || r < tree[v].l)
+			return 0;
+		if (l <= tree[v].l && tree[v].r <= r) {
+			vector<int>& vals = tree[v].vals;
+			return lower_bound(vals.begin(), vals.end(), x) - vals.begin();
 		}
-		return query(2 * node, l, r, x) + query(2 * node + 1, l, r, x);
+		return query(v + 1, l, r, x) +
+			   query(v + tree[v].rCh(), l, r, x);
 	}
 };
