a30ed4af912b141ae9fd77fd10c6d9b671a51087
Luke Videckis <lukevideckis@gmail.com>
2022-05-18 15:02:18 -0700

    more nit picks

--- Library/math/exp_mod.h
+++ Library/math/exp_mod.h
@@ -2,13 +2,15 @@
 
 //status: tested on random inputs
 
-//returns a^pw % mod in O(log(pw))
-int fastPow(int a, long long pw, int mod) {
-	assert(0 <= pw && 0 <= a && a < mod);
+//returns base^pw % mod in O(log(pw))
+//returns 1 for 0^0, but 1 for 0^totient(mod)
+//for powers > INT_MAX, do: pw %= totient(mod)
+int fastPow(int base, int pw, int mod) {
+	assert(0 <= pw && 0 <= base && base < mod);
 	int res = 1;
 	while (pw > 0) {
-		if (pw & 1) res = 1LL * res * a % mod;
-		a = 1LL * a * a % mod;
+		if (pw & 1) res = 1LL * res * base % mod;
+		base = 1LL * base * base % mod;
 		pw >>= 1;
 	}
 	return res;
