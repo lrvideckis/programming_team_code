e80563259a047e46cfbc37e8e287bebb0c135d6f
Luke Videckis <lukevideckis@gmail.com>
2022-05-18 15:18:17 -0700

    Update exp_mod.h

--- Library/math/exp_mod.h
+++ Library/math/exp_mod.h
@@ -3,10 +3,10 @@
 //status: tested on random inputs
 
 //returns base^pw % mod in O(log(pw))
-//returns 1 for 0^0, but 0 for 0^totient(mod)
-//for powers > INT_MAX, do: pw %= totient(mod)
+//returns 1 for 0^0
+//for powers > INT_MAX, do: pw %= totient(mod) if gcd(base, mod) == 1 else switch `pw` to long long
 int fastPow(int base, int pw, int mod) {
-	assert(0 <= pw && 0 <= base && base < mod);
+	assert(0 <= pw && 0 <= base && 1 <= mod);
 	int res = 1;
 	while (pw > 0) {
 		if (pw & 1) res = 1LL * res * base % mod;
