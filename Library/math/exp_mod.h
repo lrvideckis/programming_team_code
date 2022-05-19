32bb242d0e97f208f026de420795bc3cb9594538
Luke Videckis <lukevideckis@gmail.com>
2022-05-19 13:57:11 -0700

    I changed my mind about longs

--- Library/math/exp_mod.h
+++ Library/math/exp_mod.h
@@ -4,10 +4,10 @@
 
 //returns base^pw % mod in O(log(pw))
 //returns 1 for 0^0
-//for powers > INT_MAX, do: pw %= totient(mod) if gcd(base, mod) == 1 or just switch `pw` to long long
-int fastPow(int base, int pw, int mod) {
+int fastPow(long long base, long long pw, int mod) {
 	assert(0 <= pw && 0 <= base && 1 <= mod);
 	int res = 1;
+	base %= mod;
 	while (pw > 0) {
 		if (pw & 1) res = 1LL * res * base % mod;
 		base = 1LL * base * base % mod;
