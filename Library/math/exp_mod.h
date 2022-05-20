b7bf200e142a243b6a441bbd8354d007dc59fee6
Luke Videckis <lukevideckis@gmail.com>
2022-05-20 00:44:55 -0700

    nits to explanation

--- Library/math/exp_mod.h
+++ Library/math/exp_mod.h
@@ -12,14 +12,14 @@
 //What if pw doesn't fit in long long?
 
 //case 1: mod is prime
-//(base^pw)%mod == (base^(pw%(mod-1)))%mod from Fermat's little theorem so calculate the exponent under mod of `mod-1`
+//(base^pw)%mod == (base^(pw%(mod-1)))%mod (from Fermat's little theorem)
+//so calculate pw under mod of `mod-1`
 
 //case 2: non-prime mod
 //let t = totient(mod)
-//Generalized Euler's theorem: if pw >= log2(mod) then (base^pw)%mod == (base^(t+(pw%t)))%mod
-//proof: https://cp-algorithms.com/algebra/phi-function.html#generalization
-//if pw >= log2(mod), calculate pw under mod of `t` and use formula
-//else pw < log2(mod) so normal binary exponentiation works
+//if pw >= log2(mod) then (base^pw)%mod == (base^(t+(pw%t)))%mod (proof https://cp-algorithms.com/algebra/phi-function.html#generalization)
+//so calculate pw under mod of `t`
+
 
 int fastPow(long long base, long long pw, int mod) {
 	assert(0 <= pw && 0 <= base && 1 <= mod);
