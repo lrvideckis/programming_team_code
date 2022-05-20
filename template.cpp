0524509f8ee66bbe9ba70bda39c9f2fcd97e9df4
Luke Videckis <lukevideckis@gmail.com>
2022-05-20 13:44:00 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,7 +5,7 @@ using namespace std;
 //https://codeforces.com/blog/entry/15547
 //g++ -g -Wall -Wextra -pedantic -Wno-unused-result -DLOCAL -std=c++20 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector a.cpp
 
-//more on templates: https://codeforces.com/blog/entry/77199
+//other people's templates (to get ideas for your own): https://codeforces.com/blog/entry/77199
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
