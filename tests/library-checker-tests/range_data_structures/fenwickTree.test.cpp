04f04deb296ef8f479860964b68a40906b4454f5
Luke Videckis <lukevideckis@gmail.com>
2022-05-20 00:39:00 -0700

    test both structs in lib checker test for BIT

--- tests/library-checker-tests/range_data_structures/fenwickTree.test.cpp
+++ tests/library-checker-tests/range_data_structures/fenwickTree.test.cpp
@@ -5,24 +5,32 @@
 
 int main() {
 	cin.tie(0)->sync_with_stdio(false);
-	int n,q;
+	int n, q;
 	cin >> n >> q;
 	vector<long long> arr(n);
 	for(int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
 	fenwickTree<long long> ft(arr);
+
+	vector<long long> init(arr);
+	for(int i = n-2; i >= 0; i--) init[i] += init[i+1];
+	fenwickInv<long long> ftInv(init);
 	while(q--) {
 		int type;
 		cin >> type;
 		if(type == 0) {
 			int p, x;
 			cin >> p >> x;
-			ft.update(p,x);
+			ft.update(p, x);
+			ftInv.update(0, p, x);
 		} else {
 			int l,r;
 			cin >> l >> r;
-			cout << ft.sum(l,r-1) << endl;
+			r--;
+			long long res = ft.sum(l, r);
+			assert(res == ftInv.query(l) - (r+1<n?ftInv.query(r+1):0));
+			cout << res << endl;
 		}
 	}
 	return 0;
