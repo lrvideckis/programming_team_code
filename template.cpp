e13b1db391e280957d74aab19b604976d07012c9
Luke Videckis <lukevideckis@gmail.com>
2022-05-20 13:41:55 -0700

    add comment with compile flags

--- template.cpp
+++ template.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 #define endl '\n'
 
+//https://codeforces.com/blog/entry/15547
+//g++ -g -Wall -Wextra -pedantic -Wno-unused-result -DLOCAL -std=c++20 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector a.cpp
+
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	return 0;
