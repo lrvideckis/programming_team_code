22040c3033f8fc54d51142c10868d561b085ef3d
lrvideckis <lukevideckis@gmail.com>
2022-05-21 23:21:12 -0700

    more code golf

--- Library/graphs/scc.h
+++ Library/graphs/scc.h
@@ -11,28 +11,26 @@ struct sccInfo {
 };
 
 sccInfo getSCCs(const vector<vector<int>>& adj /*directed, unweighted graph*/) {
-	int n = adj.size(), numSCCs = 0;
+	const int n = adj.size();
 	stack<int> seen;
 	{
 		vector<bool> vis(n, false);
 		auto dfs = [&](auto&& dfsPtr, int curr) -> void {
+			if (vis[curr]) return;
 			vis[curr] = true;
-			for (int x : adj[curr]) {
-				if (!vis[x])
-					dfsPtr(dfsPtr, x);
-			}
+			for (int x : adj[curr])
+				dfsPtr(dfsPtr, x);
 			seen.push(curr);
 		};
-		for (int i = 0; i < n; ++i) {
-			if (!vis[i])
-				dfs(dfs, i);
-		}
+		for (int i = 0; i < n; i++)
+			dfs(dfs, i);
 	}
 	vector<vector<int>> adjInv(n);
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; i++) {
 		for (int to : adj[i])
 			adjInv[to].push_back(i);
 	}
+	int numSCCs = 0;
 	vector<int> sccId(n, -1);
 	auto dfs = [&](auto&& dfsPtr, int curr) -> void {
 		sccId[curr] = numSCCs;
