549887d72ff490cb0907a9c3fc7fd52fc33ca8b7
Luke Videckis <lukevideckis@gmail.com>
2022-05-23 14:25:02 -0700

    Update partition.h

--- Library/math/partition.h
+++ Library/math/partition.h
@@ -3,7 +3,7 @@
 //status: tested on https://judge.yosupo.jp/problem/partition_function
 
 //https://oeis.org/A000041
-//O(n sqrt n) time, but small-ish constant factor
+//O(n sqrt n) time, but small-ish constant factor (there does exist a O(n log n) solution too)
 vector<int> partition(int n/*size of dp array*/, int mod) {
 	vector<int> dp(n, 1);
 	for (int i = 1; i < n; i++) {
