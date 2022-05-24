d8773f434362c3cea06398f661248d7927feba0a
Luke Videckis <lukevideckis@gmail.com>
2022-05-23 22:45:09 -0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'
 
 //https://codeforces.com/blog/entry/15547
 //g++ -g -Wall -Wextra -pedantic -Wno-unused-result -DLOCAL -std=c++20 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector a.cpp
