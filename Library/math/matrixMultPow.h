10110f03d9dfa45b7f53ff5e2f461ec01a24c46b
Luke Videckis <lukevideckis@gmail.com>
2022-05-25 11:54:40 -0700

    nits & adding library checker test

--- Library/math/matrixMultPow.h
+++ Library/math/matrixMultPow.h
@@ -1,32 +1,30 @@
 #pragma once
+//library checker tests: https://judge.yosupo.jp/problem/matrix_product
 
-//status: not tested
+//empty matrix -> RTE
 
-const int mod = 1e9 + 7;
-
-vector<vector<int>> mult(const vector<vector<int>>& a, const vector<vector<int>>& b) {
+vector<vector<int>> mult(const vector<vector<int>>& a, const vector<vector<int>>& b, int mod) {
 	assert(a[0].size() == b.size());
-	int resultRow = a.size(), resultCol = b[0].size(), n = a[0].size();
-	vector<vector<int>> product(resultRow, vector<int> (resultCol, 0));
-	for (int i = 0; i < resultRow; i++) {
-		for (int k = 0; k < n; k++) {
-			for (int j = 0; j < resultCol; j++)
-				product[i][j] = (product[i][j] + 1LL * a[i][k] * b[k][j]) % mod;
+	int n = a.size(), m = b[0].size(), inner = b.size();
+	vector<vector<int>> prod(n, vector<int>(m, 0));
+	for (int i = 0; i < n; i++) {
+		for (int k = 0; k < inner; k++) {
+			for (int j = 0; j < m; j++)
+				prod[i][j] = (prod[i][j] + 1LL * a[i][k] * b[k][j]) % mod;
 		}
 	}
-	return product;
+	return prod;
 }
 
-vector<vector<int>> power(vector<vector<int>> matrix, int b) {
+vector<vector<int>> power(vector<vector<int>> matrix/*intentional pass by value*/, long long pw, int mod) {
 	int n = matrix.size();
-	vector<vector<int>> res(n, vector<int> (n, 0));
+	vector<vector<int>> prod(n, vector<int>(n, 0));
 	for (int i = 0; i < n; i++)
-		res[i][i] = 1;
-	while (b > 0) {
-		if (b % 2 == 1)
-			res = mult(res, matrix);
-		matrix = mult(matrix, matrix);
-		b /= 2;
+		prod[i][i] = 1;
+	while (pw > 0) {
+		if (pw % 2 == 1) prod = mult(prod, matrix, mod);
+		matrix = mult(matrix, matrix, mod);
+		pw /= 2;
 	}
-	return res;
+	return prod;
 }
