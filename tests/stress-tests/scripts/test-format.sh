1e2fa72bc1c78452569d2dc7589a0dd6a7cc1692
Luke Videckis <lukevideckis@gmail.com>
2022-05-25 14:21:27 -0700

    fix CI checking for astyle format

--- tests/stress-tests/scripts/test-format.sh
+++ tests/stress-tests/scripts/test-format.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ASTYLE_COMMAND=$(cat README.md | grep '^astyle*')
+ASTYLE_COMMAND="astyle --indent=tab --style=attach --remove-braces --align-reference=type --align-pointer=type --delete-empty-lines --attach-classes --pad-oper --pad-header --unpad-paren --close-templates --recursive \"\*.h\" --suffix=none"
+
+if ! grep --quiet "$ASTYLE_COMMAND" README.md
+then
+	echo "command doesn't match what's in README.md. Maybe you changed the README's asyle command."
+	exit 1
+fi
 
 cd Library
 
