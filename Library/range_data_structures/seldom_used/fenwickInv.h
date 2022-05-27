1f43f82db0327b5a4239a1e61ce8aa115b0a402d
Luke Videckis <lukevideckis@gmail.com>
2022-05-27 10:40:58 -0700

    made function const

--- Library/range_data_structures/seldom_used/fenwickInv.h
+++ Library/range_data_structures/seldom_used/fenwickInv.h
@@ -8,7 +8,7 @@ struct fenwickInv {
 	fenwickTree<T> ft;
 	fenwickInv(int n) : ft(n) {}
 	fenwickInv(const vector<T>& arr) : ft(init(arr)) {}
-	fenwickTree<T> init(vector<T> arr/*intentional pass by value*/) {
+	fenwickTree<T> init(vector<T> arr/*intentional pass by value*/) const {
 		for (int i = (int) arr.size() - 1; i >= 1; i--)
 			arr[i] -= arr[i - 1];
 		return fenwickTree<T>(arr);
