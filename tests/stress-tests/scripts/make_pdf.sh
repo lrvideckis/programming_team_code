db5e0f730e44efe4c52f601454ad63f15413a5bc
Luke Videckis <lukevideckis@gmail.com>
2022-05-31 15:06:39 -0700

    finished scripts, still need to clean up

---
+++ tests/stress-tests/scripts/make_pdf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+./scripts/add_hash_code_comments.sh
+
+
+#needed to make boxes under headings empty. Else you get this weird text
+touch null
+
+#need to compile twice for it to generate the table of contents
+#I think it's related to not knowing page numbers after first pdf generation
+pdflatex scripts/25_pg-reference.tex
+pdflatex scripts/25_pg-reference.tex
+
+mv 25_pg-reference.pdf ../../
