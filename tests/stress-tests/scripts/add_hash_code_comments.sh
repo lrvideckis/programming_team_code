20b9d2baf68edf7c2593ca22ff94a2d2b3f46266
Luke Videckis <lukevideckis@gmail.com>
2022-05-31 14:27:14 -0700

    start of script to add hash codes

---
+++ tests/stress-tests/scripts/add_hash_code_comments.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DIR=${1:-.}
+
+tests="$(find $DIR/../../../Library -name "*.h")"
+
+for test in $tests; do
+	hash=$(cat $test | ./hash.sh)
+	comment="cat $(eval basename $test) | ./hash.sh"
+	sed -i "1s;^;//$comment\n//$hash\n;" $test
+done
