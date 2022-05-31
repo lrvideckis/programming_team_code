3dfbf59d650952c8fd48031c266ff9e1d1ad2816
Luke Videckis <lukevideckis@gmail.com>
2022-05-31 15:39:11 -0700

    fixed path

--- tests/scripts/run-all.sh
+++ tests/scripts/run-all.sh
@@ -2,9 +2,9 @@
 DIR=${1:-.}
 # use a precompiled header for the template to improve perf
 echo "start of compiling header"
-g++ $(cat $(dirname "$0")/compile_flags.txt) $DIR/../template.h
+g++ $(cat $(dirname "$0")/compile_flags.txt) $DIR/template.h
 echo "done compiling header"
-trap "rm -f $DIR/../template.h.gch" EXIT
+trap "rm -f $DIR/template.h.gch" EXIT
 if [[ $# -eq 1 ]] ; then
     tests="$(find $DIR -name '*.cpp')"
 else
