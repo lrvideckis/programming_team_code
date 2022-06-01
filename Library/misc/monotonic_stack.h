33eafc4bdc3a2435b4fdef2f319e69cc5413523c
lrvideckis <lukevideckis@gmail.com>
2022-06-01 01:14:51 -0700

    add example usage to monotonic stack

--- Library/misc/monotonic_stack.h
+++ Library/misc/monotonic_stack.h
@@ -5,6 +5,18 @@
 //Formally: for every index j with leftLower[i] < j < i: arr[j] >= arr[i]
 //and
 //arr[leftLower[i]] < arr[i] if leftLower[i] != -1
+//
+//example usage:
+//	vector<int> leftLower = monotonic_stack(arr);
+//	reverse(arr.begin(), arr.end());
+//	vector<int> rightLower = monotonic_stack(arr);
+//	reverse(arr.begin(), arr.end());
+//	vector<int> par(n, -1);
+//	for (int i = 0; i < n; i++) {
+//		int L = leftLower[i] + 1, R = n - 2 - rightLower[n - 1 - i];
+//		//arr[i] is the min of inclusive range [L, R], with 0 <= L <= R < n
+//	}
+
 vector<int> monotonic_stack(const vector<int>& arr) {
 	int n = arr.size();
 	stack<int> st;
