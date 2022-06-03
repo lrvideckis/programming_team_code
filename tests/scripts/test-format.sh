05754d44458148b3c8bbabc29d4190680388881e
Luke Videckis <lukevideckis@gmail.com>
2022-06-02 18:26:28 -0700

    format checking script now is updated

--- tests/scripts/test-format.sh
+++ tests/scripts/test-format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ASTYLE_COMMAND="astyle --indent=tab --style=attach --remove-braces --align-reference=type --align-pointer=type --delete-empty-lines --attach-classes --pad-oper --pad-header --unpad-paren --close-templates --recursive \"\*.h\" --suffix=none"
+ASTYLE_COMMAND="astyle --indent=tab --style=attach --remove-braces --align-reference=type --align-pointer=type --delete-empty-lines --attach-classes --pad-oper --pad-header --unpad-paren --close-templates --indent-col1-comments --recursive \"\*.h\" --suffix=none"
 
 if ! grep --quiet "$ASTYLE_COMMAND" README.md
 then
