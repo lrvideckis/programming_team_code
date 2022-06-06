bc2cb6a522ab9d2c1a7882a4563dd8cc3ac3df29
Luke Videckis <lukevideckis@gmail.com>
2022-06-06 08:22:35 -0700

    better variable names

--- Library/graphs/block_vertex_tree.h
+++ Library/graphs/block_vertex_tree.h
@@ -10,14 +10,14 @@ vector<vector<int>> block_vertex_tree(const vector<vector<pair<int, int>>>& adj,
 	vector<vector<int>> tree(n + cc.numBCCs);
 	vector<int> cnt(cc.numBCCs, 0);
 	for (int i = 0; i < n; i++) {
-		for (auto [to, eId] : adj[i]) {
+		for (auto [_, eId] : adj[i]) {
 			int bcc = cc.bccID[eId];
 			if (cnt[bcc]++ == 0) {
 				tree[i].push_back(bcc + n); // Add edge between each original node, and each BCC node
 				tree[bcc + n].push_back(i);
 			}
 		}
-		for (auto [to, eId] : adj[i])
+		for (auto [_, eId] : adj[i])
 			cnt[cc.bccID[eId]]--;
 	}
 	return tree;
