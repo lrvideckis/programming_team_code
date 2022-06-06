c6e7f4134a42f617675629833becb63c4a39243b
Luke Videckis <lukevideckis@gmail.com>
2022-06-06 08:48:53 -0700

    update comments

--- Library/graphs/block_vertex_tree.h
+++ Library/graphs/block_vertex_tree.h
@@ -5,6 +5,15 @@
 // usage:
 //	info cc = bridge_and_cut(adj, m);
 //	vector<vector<int>> bvt = block_vertex_tree(adj, cc);
+// to loop over *unique* BCCs for a node i:
+//	for(int bccid : bvt[i]) {
+//		bccid -= n;
+//		...
+//	}
+// to loop over *unique* nodes for a BCC:
+//	for(int i : bvt[bccid + n]) {
+//		...
+//	}
 vector<vector<int>> block_vertex_tree(const vector<vector<pair<int, int>>>& adj, const info& cc) {
 	int n = adj.size();
 	vector<vector<int>> tree(n + cc.numBCCs);
@@ -13,7 +22,7 @@ vector<vector<int>> block_vertex_tree(const vector<vector<pair<int, int>>>& adj,
 		for (auto [_, eId] : adj[i]) {
 			int bcc = cc.bccID[eId];
 			if (cnt[bcc]++ == 0) {
-				tree[i].push_back(bcc + n); // Add edge between each original node, and each BCC node
+				tree[i].push_back(bcc + n); // Add edge between original node, and BCC node
 				tree[bcc + n].push_back(i);
 			}
 		}
