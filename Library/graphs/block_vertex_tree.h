60e2ed62c753e854a28a2b095d75d6fbb554d473
Luke Videckis <lukevideckis@gmail.com>
2022-06-06 08:20:02 -0700

    add usage comment

--- Library/graphs/block_vertex_tree.h
+++ Library/graphs/block_vertex_tree.h
@@ -2,6 +2,9 @@
 #include "bridges_and_cuts.h"
 // status: not tested
 // returns adjacency list of block vertex tree
+// usage:
+//	info cc = bridge_and_cut(adj, m);
+//	vector<vector<int>> bvt = block_vertex_tree(adj, cc);
 vector<vector<int>> block_vertex_tree(const vector<vector<pair<int, int>>>& adj, const info& cc) {
 	int n = adj.size();
 	vector<vector<int>> tree(n + cc.numBCCs);
