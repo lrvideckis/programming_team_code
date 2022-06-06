d2a203fb80fef24a57eeff6fb1337d4c738df0f2
Luke Videckis <lukevideckis@gmail.com>
2022-06-06 09:03:50 -0700

    add test comment

--- Library/graphs/block_vertex_tree.h
+++ Library/graphs/block_vertex_tree.h
@@ -1,16 +1,16 @@
 #pragma once
 #include "bridges_and_cuts.h"
-// status: not tested
-// returns adjacency list of block vertex tree
-// usage:
+//library checker tests: https://judge.yosupo.jp/problem/biconnected_components
+//returns adjacency list of block vertex tree
+//usage:
 //	info cc = bridge_and_cut(adj, m);
 //	vector<vector<int>> bvt = block_vertex_tree(adj, cc);
-// to loop over *unique* BCCs for a node i:
+//to loop over *unique* BCCs for a node i:
 //	for(int bccid : bvt[i]) {
 //		bccid -= n;
 //		...
 //	}
-// to loop over *unique* nodes for a BCC:
+//to loop over *unique* nodes for a BCC:
 //	for(int i : bvt[bccid + n]) {
 //		...
 //	}
@@ -22,7 +22,7 @@ vector<vector<int>> block_vertex_tree(const vector<vector<pair<int, int>>>& adj,
 		for (auto [_, eId] : adj[i]) {
 			int bcc = cc.bccID[eId];
 			if (cnt[bcc]++ == 0) {
-				tree[i].push_back(bcc + n); // Add edge between original node, and BCC node
+				tree[i].push_back(bcc + n);// Add edge between original node, and BCC node
 				tree[bcc + n].push_back(i);
 			}
 		}
