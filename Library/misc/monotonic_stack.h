9f9860b82b533cbf7408bd8637b5138a9696b9fb
Luke Videckis <lukevideckis@gmail.com>
2022-06-08 12:27:40 -0700

    code golf

--- Library/misc/monotonic_stack.h
+++ Library/misc/monotonic_stack.h
@@ -1,27 +1,26 @@
 #pragma once
 //library checker tests: https://judge.yosupo.jp/problem/cartesian_tree
-//leftLower[i] is the closest smaller index where arr is less.
-//Formally: for every index j with leftLower[i] < j < i: arr[j] >= arr[i]
+//left[i] is the closest smaller index where arr is less.
+//Formally: for every index j with left[i] < j < i: arr[j] >= arr[i]
 //and
-//arr[leftLower[i]] < arr[i] if leftLower[i] != -1
+//arr[left[i]] < arr[i] if left[i] != -1
 //O(n)
 //
 //example usage:
-//	vector<int> leftLower = monotonic_stack(arr);
+//	vector<int> left = monotonic_stack(arr);
 //	reverse(arr.begin(), arr.end());
-//	vector<int> rightLower = monotonic_stack(arr);
+//	vector<int> right = monotonic_stack(arr);
 //	reverse(arr.begin(), arr.end());
 //	for (int i = 0; i < n; i++) {
-//		int L = leftLower[i] + 1, R = n - 2 - rightLower[n - 1 - i];
+//		int L = left[i] + 1, R = n - 2 - right[n - 1 - i];
 //		//arr[i] is the min of inclusive range [L, R], with 0 <= L <= i <= R < n
 //	}
 vector<int> monotonic_stack(const vector<int>& arr) {
 	int n = arr.size();
-	vector<int> leftLower(n);
+	vector<int> left(n);
 	for (int i = 0; i < n; i++) {
-		int j = i - 1;
-		while (j >= 0 && arr[j] >= arr[i]) j = leftLower[j];
-		leftLower[i] = j;
+		int& j = left[i] = i - 1;
+		while (j >= 0 && arr[j] >= arr[i]) j = left[j];
 	}
-	return leftLower;
+	return left;
 }
