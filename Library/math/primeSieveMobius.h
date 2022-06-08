2d8cd5cd13a4654b230c480e8382712c69ec7f0d
Luke Videckis <lukevideckis@gmail.com>
2022-06-08 11:21:10 -0700

    add test status comment

--- Library/math/primeSieveMobius.h
+++ Library/math/primeSieveMobius.h
@@ -1,4 +1,5 @@
 #pragma once
+//stress tests: tests/stress-tests/math/prime_sieve_mobius.cpp
 //status: not tested
 //mobius[i] = 0 iff there exists a prime p s.t. i%(p^2)=0
 //mobius[i] = -1 iff i has an odd number of distinct prime factors
