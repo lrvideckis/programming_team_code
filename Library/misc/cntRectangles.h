8847e55ee6db740c7bcf4241058faa12764ff70d
Luke Videckis <lukevideckis@gmail.com>
2022-06-08 18:20:56 -0700

    different style of coding it

--- Library/misc/cntRectangles.h
+++ Library/misc/cntRectangles.h
@@ -8,23 +8,21 @@
 //O(R*C)
 vector<vector<int>> getNumRectangles(const vector<vector<bool>>& grid) {
 	const int n = grid.size(), m = grid[0].size();
-	vector<vector<int>> cnt(n + 1, vector<int>(m + 1, 0)), arr(n, vector<int>(m + 1, 0));
-	for (int j = 0; j < m; j++) {
-		for (int i = 0; i < n; i++) {
-			if (grid[i][j])
-				arr[i][j] = 1 + (i ? arr[i - 1][j] : 0);
-		}
-	}
+	vector<vector<int>> cnt(n + 1, vector<int>(m + 1, 0));
+	vector<int> arr(m, 0);
 	for (int i = 0; i < n; i++) {
-		vector<int> left = monotonic_stack(arr[i]);
-		for (int j = 0; j <= m; j++) {
-			int k = j - 1;
-			while (k >= 0 && arr[i][k] > arr[i][j]) {
-				int idx = k, len = j - 1 - left[idx];
-				k = left[k];
-				cnt[arr[i][idx]][len]++;
-				cnt[max(arr[i][j], k < 0 ? 0 : arr[i][k])][len]--;
-			}
+		for (int j = 0; j < m; j++)
+			arr[j] = grid[i][j] * (arr[j] + 1);
+		vector<int> left = monotonic_stack(arr);
+		vector<pair<int, int>> arrRev(m);
+		for (int j = 0; j < m; j++)
+			arrRev[m - 1 - j] = {arr[j], j};
+		vector<int> right = monotonic_stack(arrRev);
+		for (int j = 0; j < m; j++) {
+			int rig = (m - 1 - right[m - 1 - j]) - j, lef = j - left[j] - 1;
+			cnt[arr[j]][rig + lef]++;
+			cnt[arr[j]][lef]--;
+			cnt[arr[j]][rig - 1]--;
 		}
 	}
 	for (int i = 1; i <= n; i++)
