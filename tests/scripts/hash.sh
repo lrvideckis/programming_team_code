611ea40ff84e1e690b9b569cf32798f99aedef6b
lrvideckis <lukevideckis@gmail.com>
2022-06-10 04:39:00 -0700

    hash codes now ignores asserts

--- tests/scripts/hash.sh
+++ tests/scripts/hash.sh
@@ -1,9 +1,14 @@
-# Hashes a file, ignoring all whitespace, comments, and includes. Use for
-# verifying that code was correctly typed.
+#Hashes a file, ignoring all:
+#	- whitespace
+#	- comments
+#	- asserts
+#	- includes
+#	- pragmas
+#Use to verify that code was correctly typed.
 
-# usage:
+#usage:
 #	chmod +x hash.sh
 #	cat <file> | ./hash.sh
-# or just copy this command:
-#	cat <file> | sed -r '/^#(include|pragma)/d' | cpp -fpreprocessed -P | tr -d '[:space:]' | md5sum | cut -c-6
-sed -r '/^#(include|pragma)/d' | cpp -fpreprocessed -P | tr -d '[:space:]' | md5sum | cut -c-6
+#or just copy this command:
+#	cat <file> | sed -r '/(assert|include|pragma)/d' | cpp -fpreprocessed -P | tr -d '[:space:]' | md5sum | cut -c-6
+sed -r '/(assert|include|pragma)/d' | cpp -fpreprocessed -P | tr -d '[:space:]' | md5sum | cut -c-6
