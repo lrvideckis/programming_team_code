1c50370ce9774954b012847e55bbb724a52f5379
lrvideckis <lukevideckis@gmail.com>
2022-06-09 19:45:08 -0700

    fix script finally

--- tests/scripts/check_snake_case.sh
+++ tests/scripts/check_snake_case.sh
@@ -41,11 +41,11 @@ do
 			{ key: readability-identifier-naming.TypedefCase, value: lower_case },
 			{ key: readability-identifier-naming.TemplateParameterCase, value: UPPER_CASE }
 		]}" \
+		--use-color --warnings-as-errors="*" $tmp_file -- -std=c++17
 #		--line-filter='[
 #				{ "name":"BIT" },
 #				{ "name":"KMP" }
 #		]' \
-		--use-color --warnings-as-errors="*" $tmp_file -- -std=c++17
 	if (($? != 0))
 	then
 		fail+=1
