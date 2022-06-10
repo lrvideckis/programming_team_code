a08cb094afb166074e3f02a318c089ee37098216
lrvideckis <lukevideckis@gmail.com>
2022-06-10 04:29:47 -0700

    fix make command

--- tests/scripts/add_hash_code_comments.sh
+++ tests/scripts/add_hash_code_comments.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-DIR=${1:-.}
+tests="$(find ../library -name "*.h")"
 
-tests="$(find $DIR/../library -name "*.h")"
+# temporarily grant executable permissions
+chmod +x scripts/hash.sh
 
 for test in $tests; do
-	hash=$(cat $test | ./$DIR/scripts/hash.sh)
+	hash=$(cat $test | scripts/hash.sh)
 	comment="cat $(eval basename $test) | ./hash.sh"
 	sed --in-place "1s;^;//$comment\n//$hash\n;" $test
 done
+
+chmod -x scripts/hash.sh
