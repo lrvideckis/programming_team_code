307f1ee77f4018feeb10943af465821f14cc0fca
lrvideckis <lukevideckis@gmail.com>
2022-06-10 04:22:44 -0700

    made pdf

--- tests/scripts/hash.sh
+++ tests/scripts/hash.sh
@@ -2,7 +2,7 @@
 # verifying that code was correctly typed.
 
 # usage:
-#	chmod u+x hash.sh
+#	chmod +x hash.sh
 #	cat <file> | ./hash.sh
 # or just copy this command:
 #	cat <file> | sed -r '/^#(include|pragma)/d' | cpp -fpreprocessed -P | tr -d '[:space:]' | md5sum | cut -c-6
--- tests/scripts/test.sh
+++ tests/scripts/test.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+#runs 2 programs against each other on random inputs until they output different results
+#usage:
+#	chmod +x test.sh
+#	./test.sh
 for((i = 1; ; ++i)); do
-    echo $i
-    ./test.out > in
-    diff --ignore-all-space <(./a.out < in) <(./brute.out < in) || break
+	echo $i
+	./test.out > in
+	diff --ignore-all-space <(./a.out < in) <(./brute.out < in) || break
 done
