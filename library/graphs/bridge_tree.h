f006e81b66fea6438d607cfb89aa2ceef0ba50dd
lrvideckis <lukevideckis@gmail.com>
2022-06-17 01:20:27 -0700

    code golf

--- library/graphs/bridge_tree.h
+++ library/graphs/bridge_tree.h
@@ -7,11 +7,9 @@
 //	vector<vector<int>> bt = bridge_tree(adj, cc);
 vector<vector<int>> bridge_tree(const vector<vector<pair<int, int>>>& adj, const info& cc) {
 	vector<vector<int>> tree(cc.num_2_edge_ccs);
-	for (int i = 0; i < (int)adj.size(); i++) {
-		for (auto [to, e_id] : adj[i]) {
+	for (int i = 0; i < (int)adj.size(); i++)
+		for (auto [to, e_id] : adj[i])
 			if (cc.is_bridge[e_id])
 				tree[cc.two_edge_ccid[i]].push_back(cc.two_edge_ccid[to]);
-		}
-	}
 	return tree;
 }
