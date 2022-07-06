6b3c3b5427ee42c3005a981f43fa36efb96856f0
lrvideckis <lukevideckis@gmail.com>
2022-07-06 00:06:11 -0700

    re-merge master's seg tree into MST

--- library/range_data_structures/uncommon/merge_sort_tree.h
+++ library/range_data_structures/uncommon/merge_sort_tree.h
@@ -3,11 +3,11 @@
 //For point updates: either switch to policy based BST, or use sqrt decomposition
 struct merge_sort_tree {
 	struct node {
-		vector<int> vals;
+		vector<int> val; //sorted list of values
 		int l, r;
 		//returns 1 + (# of nodes in left child's subtree)
 		//https://cp-algorithms.com/data_structures/segment_tree.html#memory-efficient-implementation
-		int rch() const {
+		int rch() const { //right child
 			return (r - l + 2) & ~1;
 		}
 	};
@@ -17,21 +17,18 @@ struct merge_sort_tree {
 		int timer = 0;
 		build(arr, timer, 0, (int)arr.size() - 1);
 	}
-	node build(const vector<int>& arr, int& timer, int tl, int tr) {
+	vector<int> build(const vector<int>& arr, int& timer, int tl, int tr) {
 		node& curr = tree[timer++];
 		if (tl == tr) {
-			return curr = {
-				{arr[tl]},
-				tl,
-				tr
-			};
+			curr.val = {arr[tl]};
+		} else {
+			int tm = tl + (tr - tl) / 2;
+			const vector<int>& l = build(arr, timer, tl, tm);
+			const vector<int>& r = build(arr, timer, tm + 1, tr);
+			merge(l.begin(), l.end(), r.begin(), r.end(), back_inserter(curr.val));
 		}
-		int tm = tl + (tr - tl) / 2;
-		const vector<int>& l = build(arr, timer, tl, tm).vals;
-		const vector<int>& r = build(arr, timer, tm + 1, tr).vals;
-		merge(l.begin(), l.end(), r.begin(), r.end(), back_inserter(curr.vals));
 		curr.l = tl, curr.r = tr;
-		return curr;
+		return curr.val;
 	}
 	//How many of arr[l], arr[l+1], ..., arr[r] are < x?
 	//O(log^2(n))
@@ -42,8 +39,8 @@ struct merge_sort_tree {
 		if (tree[v].r < l || r < tree[v].l)
 			return 0;
 		if (l <= tree[v].l && tree[v].r <= r) {
-			const vector<int>& vals = tree[v].vals;
-			return lower_bound(vals.begin(), vals.end(), x) - vals.begin();
+			const vector<int>& val = tree[v].val;
+			return lower_bound(val.begin(), val.end(), x) - val.begin();
 		}
 		return query(v + 1, l, r, x) +
 		       query(v + tree[v].rch(), l, r, x);
