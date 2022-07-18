87c7cf2ab6fb304de8b8d66a887dec0147d3313b
Luke Videckis <lukevideckis@gmail.com>
2022-07-18 03:27:10 -0700

    fix IST test

--- tests/stress_tests/range_data_structures/implicit_seg_tree.cpp
+++ tests/stress_tests/range_data_structures/implicit_seg_tree.cpp
@@ -12,21 +12,22 @@ int main() {
 		int l = getRand(-mx, mx);
 		int r = getRand(-mx, mx);
 		if(l > r) swap(l,r);
+		if(l == r) r++;
 
 		implicit_seg_tree ist(l, r);
 
 		map<int, long long> naive;
 
 		for(int iterations = 1000; iterations--;) {
-			int L = getRand(l, r), R = getRand(l, r);
-			if(L > R) swap(L,R);
+			int L = getRand(l, r - 1), R = getRand(l, r - 1);
+			if(L > R) swap(L, R);
 			if(getRand(1,2) == 1) {//update
 				int diff = getRand<int>(-1e9, 1e9);
 				ist.update(L, R, diff);
-				for(int i = L; i <= R; i++) naive[i] += diff;
+				for(int i = L; i < R; i++) naive[i] += diff;
 			} else {//query
-				array<long long, 3> curr_naive = {naive[L], naive[L], naive[L]};
-				for(int i = L+1; i <= R; i++) {
+				array<long long, 3> curr_naive = {0, -(long long)1e18, (long long)1e18};
+				for(int i = L; i < R; i++) {
 					curr_naive[0] += naive[i];
 					curr_naive[1] = max(curr_naive[1], naive[i]);
 					curr_naive[2] = min(curr_naive[2], naive[i]);
