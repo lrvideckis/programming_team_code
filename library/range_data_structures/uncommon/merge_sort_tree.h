65679aa712154e5cd0e6fc494ee5e53e47104811
lrvideckis <lukevideckis@gmail.com>
2022-07-18 13:52:55 -0700

    rebase MST on top of new hooks

--- library/range_data_structures/uncommon/merge_sort_tree.h
+++ library/range_data_structures/uncommon/merge_sort_tree.h
@@ -1,46 +1,38 @@
 #pragma once
 //library checker tests: https://judge.yosupo.jp/problem/static_range_frequency, https://judge.yosupo.jp/problem/range_kth_smallest
 //For point updates: either switch to policy based BST, or use sqrt decomposition
+#include "../range_hook.h"
 struct merge_sort_tree {
 	struct node {
 		vector<int> val; //sorted list of values
 		int l, r;
 	};
+	range_hook rh;
 	vector<node> tree;
 	//RTE's when `arr` is empty
-	merge_sort_tree(const vector<int>& arr) : tree(4 * arr.size()) {
-		build(arr, 1, 0, arr.size());
-	}
-	void build(const vector<int>& arr, int v, int tl, int tr) {
-		if (tr - tl == 1) {
-			tree[v] = {
-				{arr[tl]},
-				tl,
-				tr
+	merge_sort_tree(const vector<int>& arr) : rh(range_hook(arr.size())), tree(2 * rh.n) {
+		for (int i = 0; i < rh.n; i++) {
+			tree[rh.leaf_idx(i)] = {
+				{arr[i]},
+				i,
+				i + 1
 			};
-		} else {
-			int tm = tl + (tr - tl) / 2;
-			build(arr, 2 * v, tl, tm);
-			build(arr, 2 * v + 1, tm, tr);
-			const auto& l = tree[2 * v].val;
-			const auto& r = tree[2 * v + 1].val;
-			merge(l.begin(), l.end(), r.begin(), r.end(), back_inserter(tree[v].val));
-			tree[v].l = tl, tree[v].r = tr;
+		}
+		for (int i = rh.n - 1; i >= 1; i--) {
+			const auto& l = tree[2 * i].val;
+			const auto& r = tree[2 * i + 1].val;
+			merge(l.begin(), l.end(), r.begin(), r.end(), back_inserter(tree[i].val));
+			tree[i].l = tree[2 * i].l, tree[i].r = tree[2 * i + 1].r;
 		}
 	}
 	//How many values in range [l, r) are < x?
 	//O(log^2(n))
 	int query(int l, int r, int x) {
-		return query(1, l, r, x);
-	}
-	int query(int v, int l, int r, int x) {
-		if (r <= tree[v].l || tree[v].r <= l)
-			return 0;
-		if (l <= tree[v].l && tree[v].r <= r) {
+		int res = 0;
+		rh.for_each(l, r, [&](int v) -> void {
 			const vector<int>& val = tree[v].val;
-			return lower_bound(val.begin(), val.end(), x) - val.begin();
-		}
-		return query(2 * v, l, r, x) +
-		       query(2 * v + 1, l, r, x);
+			res += lower_bound(val.begin(), val.end(), x) - val.begin();
+		});
+		return res;
 	}
 };
