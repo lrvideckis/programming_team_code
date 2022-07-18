d9bfde110fc93f8006834fa90d442b9b173a9ad0
lrvideckis <lukevideckis@gmail.com>
2022-07-18 15:09:43 -0700

    log 2 now handles negatives

--- library/misc/helpers.h
+++ library/misc/helpers.h
@@ -1,5 +1,5 @@
 #pragma once
 //floor of log_2(a); index of highest 1-bit
 int log_2(int a) {
-	return a ? (8 * sizeof(a)) - 1 - __builtin_clz(a) : -1;
+	return a > 0 ? (8 * sizeof(a)) - 1 - __builtin_clz(a) : -1;
 }
