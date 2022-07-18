a90fc0d18e33ce33c1c7f8cc8804edfd50a56db3
lrvideckis <lukevideckis@gmail.com>
2022-07-17 22:16:58 -0700

    made lcp queries use inclusive-exclusive ranges

--- library/strings/lcp_queries.h
+++ library/strings/lcp_queries.h
@@ -12,18 +12,18 @@ struct lcp_queries {
 		for (int i = 0; i < (int)s.size(); i++)
 			inv_sa[sa[i]] = i;
 	}
-	//length of longest common prefix of suffixes s[idx1 ... n-1], s[idx2 ... n-1], 0-based indexing
+	//length of longest common prefix of suffixes s[idx1 ... n), s[idx2 ... n), 0-based indexing
 	//
-	//You can check if two substrings s[l1..r1], s[l2..r2] are equal in O(1) by:
-	//r2-l2 == r1-l1 && longest_common_prefix(l1, l2) >= r2-l2+1
+	//You can check if two substrings s[l1..r1), s[l2..r2) are equal in O(1) by:
+	//r1-l1 == r2-l2 && longest_common_prefix(l1, l2) >= r1-l1
 	int longest_common_prefix(int idx1, int idx2) const {
 		if (idx1 == idx2) return (int)sa.size() - idx1;
 		idx1 = inv_sa[idx1];
 		idx2 = inv_sa[idx2];
 		if (idx1 > idx2) swap(idx1, idx2);
-		return st.query(idx1, idx2 - 1);
+		return st.query(idx1, idx2);
 	}
-	//returns true if suffix s[idx1 ... n-1] < s[idx2 ... n-1]
+	//returns true if suffix s[idx1 ... n) < s[idx2 ... n)
 	//(so false if idx1 == idx2)
 	bool less(int idx1, int idx2) const {
 		return inv_sa[idx1] < inv_sa[idx2];
