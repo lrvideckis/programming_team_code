ee1c1b739e26b10080a954c3d7a50386796edfe4
lrvideckis <lukevideckis@gmail.com>
2022-07-17 22:19:06 -0700

    fix monotonic stack test

--- tests/library_checker_tests/misc/monotonic_stack.test.cpp
+++ tests/library_checker_tests/misc/monotonic_stack.test.cpp
@@ -26,8 +26,8 @@ int main() {
 	for(int i = 0; i < n; i++) {
 		int L = leftLower[i], R = n-1-rightLower[n-1-i];
 		assert(0 <= L+1 && L+1 <= i && i <= R-1 && R-1 < n);
-		if(L+1 < i) assert(st.query(L+1, i-1) > arr[i]);
-		if(i < R-1) assert(st.query(i+1, R-1) > arr[i]);
+		if(L+1 < i) assert(st.query(L+1, i) > arr[i]);
+		if(i < R-1) assert(st.query(i+1, R) > arr[i]);
 		if(L >= 0) assert(arr[L] <= arr[i]);
 		if(R < n) assert(arr[R] <= arr[i]);
 
