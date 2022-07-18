7e0d7898df4d9c29395e23799977aff3c0317be5
lrvideckis <lukevideckis@gmail.com>
2022-07-18 14:52:36 -0700

    code golf MST

--- library/range_data_structures/uncommon/merge_sort_tree.h
+++ library/range_data_structures/uncommon/merge_sort_tree.h
@@ -2,26 +2,17 @@
 //For point updates: either switch to policy based BST, or use sqrt decomposition
 #include "../range_hook.h"
 struct merge_sort_tree {
-	struct node {
-		vector<int> val; //sorted list of values
-		int l, r;
-	};
 	range_hook rh;
-	vector<node> tree;
+	vector<vector<int>> tree;
 	//RTE's when `arr` is empty
 	merge_sort_tree(const vector<int>& arr) : rh(range_hook(arr.size())), tree(2 * rh.n) {
 		for (int i = 0; i < rh.n; i++) {
-			tree[rh.leaf_idx(i)] = {
-				{arr[i]},
-				i,
-				i + 1
-			};
+			tree[rh.leaf_idx(i)] = {arr[i]};
 		}
 		for (int i = rh.n - 1; i >= 1; i--) {
-			const auto& l = tree[2 * i].val;
-			const auto& r = tree[2 * i + 1].val;
-			merge(l.begin(), l.end(), r.begin(), r.end(), back_inserter(tree[i].val));
-			tree[i].l = tree[2 * i].l, tree[i].r = tree[2 * i + 1].r;
+			const auto& l = tree[2 * i];
+			const auto& r = tree[2 * i + 1];
+			merge(l.begin(), l.end(), r.begin(), r.end(), back_inserter(tree[i]));
 		}
 	}
 	//How many values in range [l, r) are < x?
@@ -29,7 +20,7 @@ struct merge_sort_tree {
 	int query(int l, int r, int x) {
 		int res = 0;
 		rh.for_each(l, r, [&](int v) -> void {
-			const vector<int>& val = tree[v].val;
+			const vector<int>& val = tree[v];
 			res += lower_bound(val.begin(), val.end(), x) - val.begin();
 		});
 		return res;
