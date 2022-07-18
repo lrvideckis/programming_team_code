2f8b90a5d7d7f9b0c90d6edf92f0e194fb30b8f4
lrvideckis <lukevideckis@gmail.com>
2022-07-17 18:34:20 -0700

    remove old/unnecessary tests

--- tests/stress_tests/range_data_structures/seg_tree.cpp
+++ tests/stress_tests/range_data_structures/seg_tree.cpp
@@ -1,10 +1,3 @@
-#include "../../template.h"
-#include "../../random.h"
-
-#include "../../../library/range_data_structures/seg_tree.h"
-
-const int mx_val = 1e9;
-
 tuple<seg_tree, vector<long long>> treeFactory(int n) {
 	if(getRand(1, 2) == 1) {
 		vector<long long> arr(n);
@@ -29,7 +22,7 @@ int main() {
 				st.update(L, R, diff);
 				for(int i = L; i < R; i++) arr[i] += diff;
 			} else {//query
-				long long mn = 1e18, mx = -1e18, sum = 0;
+				long long sum = 0, mx = -1e18, mn = 1e18;
 				for(int i = L; i < R; i++) {
 					mn = min(mn, arr[i]);
 					mx = max(mx, arr[i]);
