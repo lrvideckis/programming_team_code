bfb29f0983e27b83a386e7dacca05bb52ac60c40
lrvideckis <lukevideckis@gmail.com>
2022-07-18 12:49:33 -0700

    split apart seg tree from hooks

--- library/range_data_structures/seg_tree.h
+++ library/range_data_structures/seg_tree.h
@@ -1,48 +1,7 @@
 #pragma once
 //stress tests: tests/stress_tests/range_data_structures/seg_tree.cpp
 //source: https://codeforces.com/blog/entry/18051, https://github.com/ecnerwala/cp-book/blob/master/src/seg_tree.hpp, https://github.com/yosupo06/Algorithm/blob/master/src/datastructure/segtree.hpp
-//floor of log_2(a); index of highest 1-bit
-int log_2(int a) {
-	return a ? (8 * sizeof(a)) - 1 - __builtin_clz(a) : -1;
-}
-struct range_hook {
-	const int n, lg;//lg is the smallest integer satisfying 2^lg >= n
-	range_hook(int a_n) : n(a_n), lg(log_2(2 * n - 1)) {}
-	int leaf_idx(int i) const {
-		assert(0 <= i && i < n);
-		i += 1 << lg;
-		return i < 2 * n ? i : i - n;
-	}
-	int range_idx(int i) const {
-		assert(0 <= i && i <= n);
-		i += 1 << lg;
-		return i < 2 * n ? i : 2 * (i - n);
-	}
-	//calls function `f` on all nodes making up range [l, r)
-	template <typename F> void for_each(int l, int r, F f) const {
-		assert(0 <= l && l <= r && r <= n);
-		for (l = range_idx(l), r = range_idx(r); l < r; l >>= 1, r >>= 1) {
-			if (l & 1) f(l++);
-			if (r & 1) f(--r);
-		}
-	}
-	//calls function `f` on ancestors of nodes making up range [l, r), bottom up
-	template <typename F> void for_pars_up(int l, int r, F f) const {
-		assert(0 <= l && l <= r && r <= n);
-		for (int i = 1, a = range_idx(l), b = range_idx(r); i <= lg; i++) {
-			if (((a >> i) << i) != a) f(a >> i);
-			if (((b >> i) << i) != b) f((b - 1) >> i);
-		}
-	}
-	//calls function `f` on ancestors of nodes making up range [l, r), top down
-	template <typename F> void for_pars_down(int l, int r, F f) const {
-		assert(0 <= l && l <= r && r <= n);
-		for (int i = lg, a = range_idx(l), b = range_idx(r); i >= 1; i--) {
-			if (((a >> i) << i) != a) f(a >> i);
-			if (((b >> i) << i) != b) f((b - 1) >> i);
-		}
-	}
-};
+#include "range_hook.h"
 const long long inf = 1e18;
 struct seg_tree {
 	using dt /*data type*/ = array<long long, 3>; //sum, max, min
