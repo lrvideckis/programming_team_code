6469169e1183348235ea74a97f99799b135b545f
lrvideckis <lukevideckis@gmail.com>
2022-07-18 18:54:04 -0700

    the define int long long breaks the previous implementation

--- library/misc/log_2.h
+++ library/misc/log_2.h
@@ -1,5 +1,6 @@
 #pragma once
 //floor of log_2(a); index of highest 1-bit
-int log_2(int a) {
-	return a > 0 ? (8 * sizeof(a)) - 1 - __builtin_clz(a) : -1;
+//returns lg, the smallest int such that 2^lg >= n
+int log_2(int n) {
+	return n > 0 ? 31 - __builtin_clz(n) : -1;
 }
