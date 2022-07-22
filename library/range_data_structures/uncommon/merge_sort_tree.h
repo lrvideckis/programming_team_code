d7df9b013e131b28e397c376bebe7846f0630257
Luke Videckis <lukevideckis@gmail.com>
2022-07-22 14:05:08 -0700

    MST now compiles

--- library/range_data_structures/uncommon/merge_sort_tree.h
+++ library/range_data_structures/uncommon/merge_sort_tree.h
@@ -1,27 +1,35 @@
 #pragma once
 //For point updates: either switch to policy based BST, or use sqrt decomposition
-#include "../range_hook.h"
 struct merge_sort_tree {
-	range_hook rh;
+	const int n;
 	vector<vector<int>> tree;
 	//RTE's when `arr` is empty
-	merge_sort_tree(const vector<int>& arr) : rh(range_hook(arr.size())), tree(2 * rh.n) {
-		for (int i = 0; i < rh.n; i++)
-			tree[rh.leaf_idx(i)] = {arr[i]};
-		for (int i = rh.n - 1; i >= 1; i--) {
+	merge_sort_tree(const vector<int>& arr) : n(arr.size()), tree(2 * n) {
+		for (int i = 0, j = 1 << __lg(2 * n - 1); i < n; i++, j = (j + 1) % n + n)
+			tree[j] = {arr[i]};
+		for (int i = n - 1; i >= 1; i--) {
 			const auto& l = tree[2 * i];
 			const auto& r = tree[2 * i + 1];
 			merge(l.begin(), l.end(), r.begin(), r.end(), back_inserter(tree[i]));
 		}
 	}
+	int num_less(int v, int x) const {
+		const vector<int>& val = tree[v];
+		return lower_bound(val.begin(), val.end(), x) - val.begin();
+	}
+	int range_idx(int i) const {
+		i += 1 << __lg(2 * n - 1);
+		return i < 2 * n ? i : 2 * (i - n);
+	}
 	//How many values in range [l, r) are < x?
 	//O(log^2(n))
-	int query(int l, int r, int x) {
+	int query(int l, int r, int x) const {
+		l = range_idx(l), r = range_idx(r);
 		int res = 0;
-		rh.for_each(l, r, [&](int v) -> void {
-			const vector<int>& val = tree[v];
-			res += lower_bound(val.begin(), val.end(), x) - val.begin();
-		});
+		for (; l < r; l >>= 1, r >>= 1) {
+			if (l & 1) res += num_less(l++, x);
+			if (r & 1) res += num_less(--r, x);
+		}
 		return res;
 	}
 };
