683416b2a527d680f676d828537b57b261a49c00
lrvideckis <lukevideckis@gmail.com>
2022-07-24 00:11:58 -0700

    capitolize last const; fix test

--- library/strings/trie.h
+++ library/strings/trie.h
@@ -2,15 +2,15 @@
 //status: not tested
 //source: https://cp-algorithms.com/string/aho_corasick.html#construction-of-the-trie
 //intended to be a base template and to be modified
-const int k = 26;//alphabet size
+const int K = 26;//alphabet size
 struct trie {
 	const char MIN_CH = 'a';//'A' for uppercase, '0' for digits
 	struct node {
-		int next[k], id, p = -1;
+		int next[K], id, p = -1;
 		char ch;
 		bool leaf = 0;
 		node(int a_p = -1, char a_ch = '#') : p(a_p), ch(a_ch) {
-			fill(next, next + k, -1);
+			fill(next, next + K, -1);
 		}
 	};
 	vector<node> t;
