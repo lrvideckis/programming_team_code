94e571470c2dcd47de2e88145baa1d66cd141bca
Luke Videckis <lukevideckis@gmail.com>
2022-07-30 07:43:45 -0700

    Remove old comment

--- library/graphs/hopcroft_karp.h
+++ library/graphs/hopcroft_karp.h
@@ -1,5 +1,4 @@
 #pragma once
-//with asserts checking correctness of min vertex cover
 //source: https://github.com/foreverbell/acm-icpc-cheat-sheet/blob/master/src/graph-algorithm/hopcroft-karp.cpp
 //Worst case O(E*sqrt(V)) but faster in practice
 struct match {
