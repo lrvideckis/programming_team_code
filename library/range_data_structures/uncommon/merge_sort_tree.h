4475160a92b75cb26a087f5f4e8b183178a877ef
Luke Videckis <lukevideckis@gmail.com>
2022-08-02 12:07:33 -0700

    Golf

--- library/range_data_structures/uncommon/merge_sort_tree.h
+++ library/range_data_structures/uncommon/merge_sort_tree.h
@@ -1,10 +1,10 @@
 #pragma once
 //For point updates: either switch to policy based BST, or use sqrt decomposition
 struct merge_sort_tree {
-	const int N;
+	const int N, S;
 	vector<vector<int>> tree;
-	merge_sort_tree(const vector<int>& arr) : N(arr.size()), tree(2 * N) {
-		for (int i = 0, j = 1 << __lg(2 * N - 1); i < N; i++, j = (j + 1) % N + N)
+	merge_sort_tree(const vector<int>& arr) : N(arr.size()), S(1 << __lg(2 * N - 1)), tree(2 * N) {
+		for (int i = 0, j = S; i < N; i++, j = (j + 1) % N + N)
 			tree[j] = {arr[i]};
 		for (int i = N - 1; i >= 1; i--)
 			merge(tree[2 * i].begin(), tree[2 * i].end(), tree[2 * i + 1].begin(), tree[2 * i + 1].end(), back_inserter(tree[i]));
@@ -13,7 +13,7 @@ struct merge_sort_tree {
 		return lower_bound(tree[v].begin(), tree[v].end(), x) - tree[v].begin();
 	}
 	int to_leaf(int i) const {
-		i += 1 << __lg(2 * N - 1);
+		i += S;
 		return i < 2 * N ? i : 2 * (i - N);
 	}
 	//How many values in range [l, r) are < x?
