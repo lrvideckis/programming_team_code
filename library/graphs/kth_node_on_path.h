4734018079ce728aa6b1fe9f31c2449df52f40d6
Luke Videckis <lukevideckis@gmail.com>
2022-08-03 03:20:18 -0700

    comment now consistent with assert

--- library/graphs/kth_node_on_path.h
+++ library/graphs/kth_node_on_path.h
@@ -4,7 +4,7 @@ struct kth_node_on_path {
 	LCA lca;
 	kth_node_on_path(const vector<vector<pair<int, long long>>>& adj, int root) : lca(adj, root) {}
 	//consider path {u, u's par, ..., LCA(u,v), ..., v's par, v}. This returns the node at index k
-	//assumes 0 <= k < number of nodes on path from u to v
+	//assumes 0 <= k <= number of edges on path from u to v
 	int query(int u, int v, int k) const {
 		int lca_uv = lca.get_lca(u, v);
 		int u_lca = lca.tree[u].depth - lca.tree[lca_uv].depth;
