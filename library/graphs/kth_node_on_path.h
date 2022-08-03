d4f62db9c820524a8bd82ae876321e2c467e1c6a
lrvideckis <lukevideckis@gmail.com>
2022-08-03 03:14:30 -0700

    add kth node on path query

---
+++ library/graphs/kth_node_on_path.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "lca.h"
+struct kth_node_on_path {
+	LCA lca;
+	kth_node_on_path(const vector<vector<pair<int, long long>>>& adj, int root) : lca(adj, root) {}
+	//consider path {u, u's par, ..., LCA(u,v), ..., v's par, v}. This returns the node at index k
+	//assumes 0 <= k < number of nodes on path from u to v
+	int query(int u, int v, int k) const {
+		int lca_uv = lca.get_lca(u, v);
+		int u_lca = lca.tree[u].depth - lca.tree[lca_uv].depth;
+		int v_lca = lca.tree[v].depth - lca.tree[lca_uv].depth;
+		assert(0 <= k && k <= u_lca + v_lca);
+		return k <= u_lca ? lca.kth_par(u, k) : lca.kth_par(v, u_lca + v_lca - k);
+	}
+};
---
+++ tests/library_checker_tests/graphs/kth_node_on_path.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/jump_on_tree"
+#include "../../template.h"
+
+#include "../../../library/graphs/kth_node_on_path.h"
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n, q;
+	cin >> n >> q;
+	vector<vector<pair<int, long long>>> adj(n);
+	for (int i = 0; i < n - 1; ++i) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].emplace_back(v, 1ll);
+		adj[v].emplace_back(u, 1ll);
+	}
+	kth_node_on_path kpath(adj, 0);
+	while (q--) {
+		int u, v, k;
+		cin >> u >> v >> k;
+		if (k > kpath.lca.dist_edges(u, v)) cout << -1 << '\n';
+		else cout << kpath.query(u, v, k) << '\n';
+	}
+}
