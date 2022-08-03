bd676d8c9d4e092105651a13d6a5cdb4a1a2367d
lrvideckis <lukevideckis@gmail.com>
2022-08-03 03:39:21 -0700

    add tree diameter test

---
+++ tests/library_checker_tests/graphs/tree_diameter.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/tree_diameter"
+#include "../../template.h"
+
+#include "../../../library/graphs/kth_node_on_path.h"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int n;
+	cin >> n;
+	vector<vector<pair<int, long long>>> adj(n);
+	for (int i = 0; i < n - 1; i++) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		adj[u].emplace_back(v, w);
+		adj[v].emplace_back(u, w);
+	}
+	kth_node_on_path kpath(adj, 0);
+	int u = 0;
+	for (int i = 1; i < n; i++)
+		if (kpath.lca.dist_weight(0, i) > kpath.lca.dist_weight(0, u)) u = i;
+	int v = 0;
+	for (int i = 1; i < n; i++)
+		if (kpath.lca.dist_weight(u, i) > kpath.lca.dist_weight(u, v)) v = i;
+	int len_diam = kpath.lca.dist_edges(u, v) + 1;
+	cout << kpath.lca.dist_weight(u, v) << " " << len_diam << endl;
+	for (int i = 0; i < len_diam; i++)
+		cout << kpath.query(u, v, i) << " ";
+	cout << endl;
+}
