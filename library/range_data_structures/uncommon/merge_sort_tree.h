f919ef7a2472d29f3033825487e28234403c84f1
lrvideckis <lukevideckis@gmail.com>
2022-08-09 17:04:23 -0700

    #183

--- library/range_data_structures/uncommon/merge_sort_tree.h
+++ library/range_data_structures/uncommon/merge_sort_tree.h
@@ -3,11 +3,17 @@
 struct merge_sort_tree {
 	const int N, S/*smallest power of 2 >= N*/;
 	vector<vector<int>> tree;
+	//doesn't work with empty array
 	merge_sort_tree(const vector<int>& arr) : N(arr.size()), S(1 << __lg(2 * N - 1)), tree(2 * N) {
-		for (int i = 0, j = S; i < N; i++, j = (j + 1) % N + N)
-			tree[j] = {arr[i]};
-		for (int i = N - 1; i >= 1; i--)
-			merge(tree[2 * i].begin(), tree[2 * i].end(), tree[2 * i + 1].begin(), tree[2 * i + 1].end(), back_inserter(tree[i]));
+		for (int i = 0; i < N; i++)
+			tree[i + N] = {arr[i]};
+		rotate(tree.rbegin(), tree.rbegin() + S - N, tree.rbegin() + N);
+		for (int i = N - 1; i >= 1; i--) {
+			const auto& l = tree[2 * i];
+			const auto& r = tree[2 * i + 1];
+			tree[i].reserve(l.size() + r.size());
+			merge(l.begin(), l.end(), r.begin(), r.end(), back_inserter(tree[i]));
+		}
 	}
 	int value(int v, int x) const {
 		return lower_bound(tree[v].begin(), tree[v].end(), x) - tree[v].begin();
