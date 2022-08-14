df01e739716ac1ebe0fc93a96f7076a6798750f7
Luke Videckis <lukevideckis@gmail.com>
2022-08-14 02:22:20 -0700

    fix format error

--- library/strings/pal_query.h
+++ library/strings/pal_query.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "lcp_query.h"
 struct pal_query {
-	const int n;
+	const int N;
 	lcp_query lq;
-	pal_query(const string& s) : n(s.size()), lq(s + string(s.rbegin(), s.rend())) {}
+	pal_query(const string& s) : N(s.size()), lq(s + string(s.rbegin(), s.rend())) {}
 	//returns 1 if substring s[l...r) is a palindrome
 	bool is_pal(int l, int r) const {
-		assert(0 <= l && l <= r && r <= n);
-		return l == r ? 1 : lq.longest_common_prefix(l, 2 * n - r) >= r - l;
+		assert(0 <= l && l <= r && r <= N);
+		return l == r ? 1 : lq.longest_common_prefix(l, 2 * N - r) >= r - l;
 	}
 };
