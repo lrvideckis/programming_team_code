d34bcfac3f00292c02973e946e901b396845c657
lrvideckis <lukevideckis@gmail.com>
2022-08-14 02:16:51 -0700

    fix typos

--- library/strings/pal_query.h
+++ library/strings/pal_query.h
@@ -4,7 +4,7 @@ struct pal_query {
 	const int n;
 	lcp_query lq;
 	pal_query(const string& s) : n(s.size()), lq(s + string(s.rbegin(), s.rend())) {}
-	//returns 1 if substring s[l...r) is a palendrome
+	//returns 1 if substring s[l...r) is a palindrome
 	bool is_pal(int l, int r) const {
 		assert(0 <= l && l <= r && r <= n);
 		return l == r ? 1 : lq.longest_common_prefix(l, 2 * n - r) >= r - l;
