d880db0aab6a7e1eac19aa0d99897d95ee5c4626
lrvideckis <lukevideckis@gmail.com>
2022-08-16 04:17:18 -0700

    migrate seg tree test to aizu judge problem

---
+++ tests/library_checker_tests/range_data_structures/seg_tree.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_2_H"
+#include "../../template.h"
+
+#include "../../../library/range_data_structures/seg_tree.h"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int n, q;
+	cin >> n >> q;
+
+	seg_tree st(vector<long long>(n, 0));
+
+	while(q--) {
+		int type, l, r;
+		cin >> type >> l >> r;
+		r++;
+		if(type == 0) {
+			int x;
+			cin >> x;
+			st.update(l, r, x);
+		} else {
+			cout << st.query(l, r) << '\n';
+		}
+	}
+	return 0;
+}
