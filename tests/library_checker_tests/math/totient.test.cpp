7cc0ef08130bb1591e8652810b79dd7ffabfd198
lrvideckis <lukevideckis@gmail.com>
2022-08-16 14:27:35 -0700

    migrate totient test to aizu judge problem

---
+++ tests/library_checker_tests/math/totient.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/all/NTL_1_D"
+#include "../../template.h"
+
+#include "../../../library/math/totient.h"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+
+	cout << totient(n) << '\n';
+
+	return 0;
+}
