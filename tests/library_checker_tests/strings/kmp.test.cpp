e0c0f1d1a1901d8a7925036a0693e1fd7c6718b7
lrvideckis <lukevideckis@gmail.com>
2022-08-16 15:28:31 -0700

    move KMP to aizu judge problem

---
+++ tests/library_checker_tests/strings/kmp.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_14_B"
+#include "../../template.h"
+
+#include "../../../library/strings/kmp.h"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	string haystack, needle;
+	cin >> haystack >> needle;
+
+	KMP kmp(needle);
+
+	for(int idx : kmp.find(haystack)) {
+		cout << idx << "\n";
+	}
+
+	return 0;
+}
