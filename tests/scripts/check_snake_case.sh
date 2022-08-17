9de722f3ee9a9b54d830d65d7b08275b178fde77
lrvideckis <lukevideckis@gmail.com>
2022-08-16 20:49:54 -0700

    start of combining scripts into one

--- tests/scripts/check_snake_case.sh
+++ tests/scripts/check_snake_case.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+#check file names are snake case
 files_snake_case="find .. -name '*[A-Z]*' -or -name '*-*' | \
 	grep --invert-match '\.git' | \
 	grep --invert-match '\.verify-helper' | \
 	grep --invert-match --extended-regexp '(LICENSE|Makefile|README)'"
-
 if eval $files_snake_case --quiet
 then
 	echo "these files/directories are not snake case:"
@@ -12,26 +12,33 @@ then
 	exit 1
 fi
 
+#check files are formatted with astyle
+ASTYLE_COMMAND="astyle --indent=tab --style=attach --remove-braces --align-reference=type --align-pointer=type --delete-empty-lines --attach-classes --pad-oper --pad-header --unpad-paren --close-templates --indent-col1-comments --suffix=none"
+if ! grep --quiet "$ASTYLE_COMMAND" ../README.md
+then
+	echo "command doesn't match what's in README.md. Maybe you changed the README's asyle command."
+	exit 1
+fi
+cd ..
+astyle_output=$(
+	$(echo $ASTYLE_COMMAND) --dry-run --formatted --recursive "*.test.cpp" &&
+	$(echo $ASTYLE_COMMAND) --dry-run --formatted --recursive "*.h"
+)
+cd tests/
+if grep --quiet "Formatted" <<< "$astyle_output"
+then
+	echo "some files are not formatted, output of astyle: "
+	echo "$astyle_output"
+	exit 1
+fi
+
+#check snake case
 declare -i pass=0
 declare -i fail=0
 failTests=""
-
-
-
-#TODO
-#if [[ $# -eq 1 ]] ; then
-#    tests="$(find $DIR/../library -name '*.h')"
-#else
-#    tests="$(find $DIR/../library -name "*$2*")"
-#fi
-
-
-
-
 for test in $(find library_checker_tests/ -type f -name '*.test.cpp')
 do
 	echo "file is "$test
-
 	# clang's "lower_case" == the traditional snake_case
 	clang-tidy -checks="readability-identifier-naming" \
 		-config="{CheckOptions: [
@@ -54,7 +61,6 @@ do
 		pass+=1
 	fi
 done
-
 echo "$pass/$(($pass+$fail)) tests passed"
 if (($fail == 0)); then
 	echo "No tests failed"
