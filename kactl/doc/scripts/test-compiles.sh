a54b247c646d2473b8cb414dabb3a7b400490151
lrvideckis <lukevideckis@gmail.com>
2022-08-17 23:24:52 -0700

    removing to attempt git submodules

--- kactl/doc/scripts/test-compiles.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p build
-FILE="$1"
-echo "
-#include \"../content/contest/template.cpp\"
-#include \"../$FILE\"
-" >build/temp.cpp
-g++ -Wall -Wextra -Wfatal-errors -Wconversion -std=c++14 build/temp.cpp && rm a.out build/temp.cpp
