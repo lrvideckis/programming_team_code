ef4fbeb224523699a827a22f388cc650c88c59cc
lrvideckis <lukevideckis@gmail.com>
2022-08-18 01:49:23 -0700

    #189

--- library/strings/pal_query.h
+++ library/strings/pal_query.h
@@ -1,12 +1,13 @@
 #pragma once
-#include "lcp_query.h"
+#include "../../kactl/content/strings/Manacher.h"
 struct pal_query {
 	const int N;
-	lcp_query lq;
-	pal_query(const string& s) : N(s.size()), lq(s + string(s.rbegin(), s.rend())) {}
+	array<vi, 2> pal_len;
+	pal_query(const string& s) : N(s.size()), pal_len(manacher(s)) {}
 	//returns 1 if substring s[l...r) is a palindrome
 	bool is_pal(int l, int r) const {
 		assert(0 <= l && l <= r && r <= N);
-		return l == r ? 1 : lq.longest_common_prefix(l, 2 * N - r) >= r - l;
+		int len = r - l;
+		return pal_len[len & 1][l + len / 2] >= len / 2;
 	}
 };
---
+++ tests/library_checker_tests/strings/palindrome_query.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_palindromes"
+#include "../../../kactl/stress-tests/utilities/template.h"
+
+#include "../../random.h"
+#include "../../../library/strings/pal_query.h"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	string s;
+	cin >> s;
+	int n = s.size();
+	pal_query pal_q(s);
+	{
+		vector<pair<int, int>> tests;
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j <= min(n, i + int(1e7) / n); j++) {
+				tests.emplace_back(i, j);
+			}
+		}
+		for(int i = 0; i < int(1e7) / n; i++) {
+			int l = get_rand(0, n), r = get_rand(0, n);
+			if (l > r) swap(l, r);
+			tests.emplace_back(min(l, r), max(l, r));
+		}
+		for(auto [l, r] : tests) {
+			string substr = s.substr(l, r - l);
+			assert(pal_q.is_pal(l, r) == (substr == string(substr.rbegin(), substr.rend())));
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout << 2 * pal_q.pal_len[1][i] + 1 << " ";
+		if (i + 1 < n) cout << 2 * pal_q.pal_len[0][i + 1] << " ";
+	}
+	return 0;
+}
