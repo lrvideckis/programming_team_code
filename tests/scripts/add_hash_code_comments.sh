693fd7306054240485e59bd52c54aa9f80ffc4f8
lrvideckis <lukevideckis@gmail.com>
2022-08-19 11:13:43 -0700

    made hash; test executables

--- tests/scripts/add_hash_code_comments.sh
+++ tests/scripts/add_hash_code_comments.sh
@@ -2,13 +2,8 @@
 
 tests="$(find ../library -name "*.h")"
 
-# temporarily grant executable permissions
-chmod +x scripts/hash.sh
-
 for test in $tests; do
 	hash=$(cat $test | scripts/hash.sh)
 	comment="cat $(eval basename $test) | ./hash.sh"
 	sed --in-place "1s;^;//$comment\n//$hash\n;" $test
 done
-
-chmod -x scripts/hash.sh
