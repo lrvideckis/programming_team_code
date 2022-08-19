a2ef3e311e77b1308c856c7968134ebdf30a6df4
lrvideckis <lukevideckis@gmail.com>
2022-08-19 01:21:23 -0700

    nit

--- library/range_data_structures/rmq.h
+++ library/range_data_structures/rmq.h
@@ -14,18 +14,18 @@
 //NOLINTNEXTLINE(readability-identifier-naming)
 template <class T> struct RMQ {
 	vector<vector<T>> dp;
-	function<T(const T&, const T&)> func;
-	RMQ(const vector<T>& arr, const function<T(const T&, const T&)>& a_func) : dp(1, arr), func(a_func) {
+	function<T(const T&, const T&)> op;
+	RMQ(const vector<T>& arr, const function<T(const T&, const T&)>& a_op) : dp(1, arr), op(a_op) {
 		for (int pw = 1, k = 1, n = arr.size(); 2 * pw <= n; pw *= 2, k++) {
 			dp.emplace_back(n - 2 * pw + 1);
 			for (int j = 0; j < n - 2 * pw + 1; j++)
-				dp[k][j] = func(dp[k - 1][j], dp[k - 1][j + pw]);
+				dp[k][j] = op(dp[k - 1][j], dp[k - 1][j + pw]);
 		}
 	}
 	//inclusive-exclusive range [l, r)
 	T query(int l, int r) const {
 		assert(0 <= l && l < r && r <= (int)dp[0].size());
 		int lg = __lg(r - l);
-		return func(dp[lg][l], dp[lg][r - (1 << lg)]);
+		return op(dp[lg][l], dp[lg][r - (1 << lg)]);
 	}
 };
