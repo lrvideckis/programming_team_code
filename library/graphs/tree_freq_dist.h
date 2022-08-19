7d9a487e1dc389b7bf8dae230fd5fdfbe6576c3e
lrvideckis <lukevideckis@gmail.com>
2022-08-18 17:40:30 -0700

    fix lint

--- library/graphs/tree_freq_dist.h
+++ library/graphs/tree_freq_dist.h
@@ -6,13 +6,13 @@ vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, conne
 	int n = adj.size();
 	vector<int> vis(n, 0), sizes(n);
 	vector<long long> res(n, 0);
-	auto dfs_depth_cnt = [&](auto self, int node, int par, int currDist, vector<int>& cntDist) -> void {
-		if ((int)cntDist.size() <= currDist) cntDist.resize(currDist + 1, 0);
-		cntDist[currDist]++;
+	auto dfs_depth_cnt = [&](auto self, int node, int par, int curr_dist, vector<int>& cnt_dist) -> void {
+		if ((int)cnt_dist.size() <= curr_dist) cnt_dist.resize(curr_dist + 1, 0);
+		cnt_dist[curr_dist]++;
 
 		for (int child : adj[node]) {
 			if (child == par || vis[child]) continue;
-			self(self, child, node, currDist + 1, cntDist);
+			self(self, child, node, curr_dist + 1, cnt_dist);
 		}
 	};
 	auto dfs_sz = [&](auto self, int node, int par) -> void {
@@ -44,21 +44,21 @@ vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, conne
 		node = find_centroid(node);
 		assert(!vis[node]);
 		vis[node] = true;
-		vector<double> totalCnt(1, 1.0);
+		vector<double> total_cnt(1, 1.0);
 		for (int to : adj[node]) {
 			if (!vis[to]) {
-				vector<int> cntDist;
-				dfs_depth_cnt(dfs_depth_cnt, to, node, 1, cntDist);
+				vector<int> cnt_dist;
+				dfs_depth_cnt(dfs_depth_cnt, to, node, 1, cnt_dist);
 				{
-					vector<double> tmpCntDist(cntDist.size());
-					for (int i = 0; i < (int)cntDist.size(); i++) tmpCntDist[i] = cntDist[i];
-					vector<double> prod = conv(totalCnt, tmpCntDist);
+					vector<double> tmp_cnt_dist(cnt_dist.size());
+					for (int i = 0; i < (int)cnt_dist.size(); i++) tmp_cnt_dist[i] = cnt_dist[i];
+					vector<double> prod = conv(total_cnt, tmp_cnt_dist);
 					for (int i = 1; i < (int)prod.size(); i++) {
 						res[i] += prod[i] + 0.5;//for rounding
 					}
 				}
-				if (totalCnt.size() < cntDist.size()) totalCnt.resize(cntDist.size(), 0.0);
-				for (int i = 1; i < (int)cntDist.size(); i++) totalCnt[i] += cntDist[i];
+				if (total_cnt.size() < cnt_dist.size()) total_cnt.resize(cnt_dist.size(), 0.0);
+				for (int i = 1; i < (int)cnt_dist.size(); i++) total_cnt[i] += cnt_dist[i];
 				self(self, to);
 			}
 		}
