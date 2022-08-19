7ebf0ca764b08732fae0c5b3137cbe90879bcb5b
lrvideckis <lukevideckis@gmail.com>
2022-08-18 19:05:04 -0700

    fix failing test

--- tests/oj_tests/strings/z_func.test.cpp
+++ tests/oj_tests/strings/z_func.test.cpp
@@ -1,7 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
 #include "../../template.h"
 
-#include "../../../library/strings/prefix_function.h"
 #include "../../../library/strings/lcp_query.h"
 
 int main() {
@@ -9,25 +8,8 @@ int main() {
 	string s;
 	cin >> s;
 	lcp_query sq(s);
-	int n = s.size();
-	vector<int> pi = prefix_function(s);
-	//prefix -> z func conversion
-	//source: https://codeforces.com/blog/entry/9612#comment-217621
-	vector<int> z(n, 0);
-	for (int i = 1; i < n; ++i) {
-		if (pi[i])
-			z[i - pi[i] + 1] = pi[i];
-	}
-	for (int i = 1; i < n;) {
-		int j, v;
-		for (j = 1; j < z[i] && (v = min(z[j], z[i] - j)) >= z[i + j] ; ++j)
-			z[i + j] = v;
-		i += j;
-	}
-	z[0] = n;
-	for (int i = 0; i < n; i++) {
-		assert(z[i] == sq.longest_common_prefix(i, 0));
-		cout << z[i] << " ";
+	for (int i = 0; i < (int)s.size(); i++) {
+		cout << sq.longest_common_prefix(i, 0) << " ";
 	}
 	cout << endl;
 	return 0;
