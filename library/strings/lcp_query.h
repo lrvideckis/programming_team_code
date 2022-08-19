f5c9473a178602271cf526e4d6800e58b11b3c40
lrvideckis <lukevideckis@gmail.com>
2022-08-19 10:48:03 -0700

    remove SA in favor of atcoders

--- library/strings/lcp_query.h
+++ library/strings/lcp_query.h
@@ -1,26 +1,23 @@
 #pragma once
-#include "suffix_array.h"
-#include "lcp.h"
+#include "../../ac-library/atcoder/string.hpp"
 #include "../range_data_structures/rmq.h"
 //computes suffix array, lcp array, and then sparse table over lcp array
 //O(n log n)
 struct lcp_query {
+	const int N;
 	vector<int> sa, inv_sa, lcp;
 	RMQ<int> st;
-	lcp_query(const string& s) : sa(sa_is(s, 255)), inv_sa(init_inv()), lcp(LCP(s, sa, inv_sa)), st(lcp, [](int x, int y) {
+	lcp_query(const string& s) : N(s.size()), sa(atcoder::suffix_array(s)), inv_sa(N), lcp(atcoder::lcp_array(s, sa)), st(lcp, [](int x, int y) {
 		return min(x, y);
-	}) {}
-	vector<int> init_inv() const {
-		vector<int> inv(sa.size());
-		for (int i = 0; i < (int)sa.size(); i++) inv[sa[i]] = i;
-		return inv;
+	}) {
+		for (int i = 0; i < N; i++) inv_sa[sa[i]] = i;
 	}
 	//length of longest common prefix of suffixes s[idx1 ... n), s[idx2 ... n), 0-based indexing
 	//
 	//You can check if two substrings s[l1..r1), s[l2..r2) are equal in O(1) by:
 	//r1-l1 == r2-l2 && longest_common_prefix(l1, l2) >= r1-l1
 	int longest_common_prefix(int idx1, int idx2) const {
-		if (idx1 == idx2) return (int)sa.size() - idx1;
+		if (idx1 == idx2) return N - idx1;
 		idx1 = inv_sa[idx1];
 		idx2 = inv_sa[idx2];
 		if (idx1 > idx2) swap(idx1, idx2);
