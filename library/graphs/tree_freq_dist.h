6fa78e3582d72cc558be8b70e61742259a68c96a
lrvideckis <lukevideckis@gmail.com>
2022-08-18 17:07:20 -0700

    format

--- library/graphs/tree_freq_dist.h
+++ library/graphs/tree_freq_dist.h
@@ -6,9 +6,8 @@ vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, conne
 	int n = adj.size();
 	vector<int> vis(n, 0), sizes(n);
 	vector<long long> res(n, 0);
-
 	auto dfs_depth_cnt = [&](auto self, int node, int par, int currDist, vector<int>& cntDist) -> void {
-		if((int)cntDist.size() <= currDist) cntDist.resize(currDist+1, 0);
+		if ((int)cntDist.size() <= currDist) cntDist.resize(currDist + 1, 0);
 		cntDist[currDist]++;
 
 		for (int child : adj[node]) {
@@ -16,7 +15,6 @@ vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, conne
 			self(self, child, node, currDist + 1, cntDist);
 		}
 	};
-
 	auto dfs_sz = [&](auto self, int node, int par) -> void {
 		sizes[node] = 1;
 		for (int child : adj[node]) {
@@ -25,7 +23,6 @@ vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, conne
 			sizes[node] += sizes[child];
 		}
 	};
-
 	auto find_centroid = [&](int node) -> int {
 		dfs_sz(dfs_sz, node, node);
 		int size_cap = sizes[node] / 2, par = -1;
@@ -42,7 +39,6 @@ vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, conne
 			if (!found) return node;
 		}
 	};
-
 	auto dfs = [&](auto self, int node) -> void {
 		assert(!vis[node]);
 		node = find_centroid(node);
@@ -51,12 +47,11 @@ vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, conne
 		vector<double> totalCnt(1, 1.0);
 		for (int to : adj[node]) {
 			if (!vis[to]) {
-
 				vector<int> cntDist;
 				dfs_depth_cnt(dfs_depth_cnt, to, node, 1, cntDist);
 				{
 					vector<double> tmpCntDist(cntDist.size());
-					for(int i = 0; i < (int)cntDist.size(); i++) tmpCntDist[i] = cntDist[i];
+					for (int i = 0; i < (int)cntDist.size(); i++) tmpCntDist[i] = cntDist[i];
 					vector<double> prod = conv(totalCnt, tmpCntDist);
 					for (int i = 1; i < (int)prod.size(); i++) {
 						res[i] += prod[i] + 0.5;//for rounding
@@ -64,12 +59,10 @@ vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, conne
 				}
 				if (totalCnt.size() < cntDist.size()) totalCnt.resize(cntDist.size(), 0.0);
 				for (int i = 1; i < (int)cntDist.size(); i++) totalCnt[i] += cntDist[i];
-
 				self(self, to);
 			}
 		}
 	};
 	dfs(dfs, 0);
-
 	return res;
 }
--- tests/oj_tests/graphs/freq_table_tree_distance.test.cpp
+++ tests/oj_tests/graphs/freq_table_tree_distance.test.cpp
@@ -5,22 +5,18 @@
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-
 	int n;
 	cin >> n;
 	vector<vector<int>> adj(n);
-	for(int i = 0; i < n-1; i++) {
+	for (int i = 0; i < n - 1; i++) {
 		int u, v;
 		cin >> u >> v;
 		adj[u].push_back(v);
 		adj[v].push_back(u);
 	}
-
 	vector<long long> cnt_len = tree_freq_dist(adj);
-	for(int i = 1; i < n; i++) {
+	for (int i = 1; i < n; i++)
 		cout << cnt_len[i] << " ";
-	}
 	cout << '\n';
-
 	return 0;
 }
