116b22cb21b05465bff2cc1177bdfdc1651cc1f4
lrvideckis <lukevideckis@gmail.com>
2022-08-19 08:28:20 -0700

    add test for trie

--- library/strings/trie.h
+++ library/strings/trie.h
@@ -3,49 +3,35 @@
 //intended to be a base template and to be modified
 const int K = 26;//alphabet size
 struct trie {
-	const char MIN_CH = 'a';//'A' for uppercase, '0' for digits
+	const char MIN_CH = 'A';//'a' for lowercase, '0' for digits
 	struct node {
-		int next[K], id, p = -1;
+		int next[K], cnt_words = 0, par = -1;
 		char ch;
-		bool leaf = 0;
-		node(int a_p = -1, char a_ch = '#') : p(a_p), ch(a_ch) {
+		node(int a_par = -1, char a_ch = '#') : par(a_par), ch(a_ch) {
 			fill(next, next + K, -1);
 		}
 	};
 	vector<node> t;
 	trie() : t(1) {}
-	void add_string(const string& s, int id) {
-		int c = 0;
+	void add_string(const string& s) {
+		int v = 0;
 		for (char ch : s) {
-			int v = ch - MIN_CH;
-			if (t[c].next[v] == -1) {
-				t[c].next[v] = t.size();
-				t.emplace_back(c, ch);
+			int let = ch - MIN_CH;
+			if (t[v].next[let] == -1) {
+				t[v].next[let] = t.size();
+				t.emplace_back(v, ch);
 			}
-			c = t[c].next[v];
+			v = t[v].next[let];
 		}
-		t[c].leaf = 1;
-		t[c].id = id;
+		t[v].cnt_words++;
 	}
-	void remove_string(const string& s) {
-		int c = 0;
+	bool find_string(const string& s) const {
+		int v = 0;
 		for (char ch : s) {
-			int v = ch - MIN_CH;
-			if (t[c].next[v] == -1)
-				return;
-			c = t[c].next[v];
+			int let = ch - MIN_CH;
+			if (t[v].next[let] == -1) return 0;
+			v = t[v].next[let];
 		}
-		t[c].leaf = 0;
-	}
-	int find_string(const string& s) const {
-		int c = 0;
-		for (char ch : s) {
-			int v = ch - MIN_CH;
-			if (t[c].next[v] == -1)
-				return -1;
-			c = t[c].next[v];
-		}
-		if (!t[c].leaf) return -1;
-		return t[c].id;
+		return t[v].cnt_words;
 	}
 };
---
+++ tests/oj_tests/strings/trie.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_4_C"
+#include "../../template.h"
+
+#include "../../../library/strings/trie.h"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int n;
+	cin >> n;
+	trie tr;
+	for (int i = 0; i < n; i++) {
+		string type, s;
+		cin >> type >> s;
+		if (type == "insert")
+			tr.add_string(s);
+		else
+			cout << (tr.find_string(s) ? "yes" : "no") << '\n';
+	}
+	return 0;
+}
