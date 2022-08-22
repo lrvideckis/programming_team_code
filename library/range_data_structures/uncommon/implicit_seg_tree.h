d629e8af9a4bceffbe35915c76a7c8dc13305aa8
Luke Videckis <lukevideckis@gmail.com>
2022-08-22 14:54:24 -0700

    fix clang warning

--- library/range_data_structures/uncommon/implicit_seg_tree.h
+++ library/range_data_structures/uncommon/implicit_seg_tree.h
@@ -16,8 +16,8 @@ template <int N> struct implicit_seg_tree {
 		node() {}
 		node(const dt& a_val) : val(a_val), lazy(0), lch(-1), rch(-1) {}
 	} tree[N];
-	int ptr, root_l, root_r;//[root_l, root_r) defines range of root node; handles negatives
-	implicit_seg_tree(int l, int r) : ptr(0), root_l(l), root_r(r) {
+	int ptr = 0, root_l, root_r;//[root_l, root_r) defines range of root node; handles negatives
+	implicit_seg_tree(int l, int r) : root_l(l), root_r(r) {
 		tree[ptr++] = node(dt{0, r - l});
 	}
 	void apply(int v, ch add) {
