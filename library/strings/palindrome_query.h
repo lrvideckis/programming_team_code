97c4459e54d95d134543a99b83f5f968ddd7780e
Luke Videckis <lukevideckis@gmail.com>
2022-08-22 12:21:40 -0700

    add clarifying comment

--- library/strings/palindrome_query.h
+++ library/strings/palindrome_query.h
@@ -5,6 +5,7 @@ struct pal_query {
 	array<vi, 2> pal_len;
 	pal_query(const string& s) : N(s.size()), pal_len(manacher(s)) {}
 	//returns 1 if substring s[l...r) is a palindrome
+	//(returns 1 when l == r)
 	bool is_pal(int l, int r) const {
 		assert(0 <= l && l <= r && r <= N);
 		int len = r - l;
