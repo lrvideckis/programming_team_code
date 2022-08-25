fffdd0cc456946896b9bdb5325d85592f7dde358
lrvideckis <lukevideckis@gmail.com>
2022-08-25 00:59:56 -0700

    add max test for count paths per node

--- tests/oj_tests/graphs/freq_table_tree_distance.test.cpp
+++ tests/oj_tests/graphs/freq_table_tree_distance.test.cpp
@@ -1,7 +1,9 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/frequency_table_of_tree_distance"
 #include "../../../kactl/stress-tests/utilities/template.h"
+#include "../../random.h"
 
 #include "../../../library/graphs/tree_freq_dist.h"
+#include "../../../library/graphs/k_len_path_node_count.h"
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
@@ -15,6 +17,14 @@ int main() {
 		adj[v].push_back(u);
 	}
 	vector<long long> cnt_len = tree_freq_dist(adj);
+	{
+		int k = get_rand(2, n + 1);
+		long long sum = 0;
+		for (long long num_paths : get_num_paths(adj, k))
+			sum += num_paths;
+		assert(sum % k == 0);
+		assert(sum / k == cnt_len[k - 1]);
+	}
 	for (int i = 1; i < n; i++)
 		cout << cnt_len[i] << " ";
 	cout << '\n';
