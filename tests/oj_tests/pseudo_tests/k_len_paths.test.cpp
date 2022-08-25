830dfa970a3d494db1f87238e3759cf4312c13ba
lrvideckis <lukevideckis@gmail.com>
2022-08-24 23:46:06 -0700

    format test

--- tests/oj_tests/pseudo_tests/k_len_paths.test.cpp
+++ tests/oj_tests/pseudo_tests/k_len_paths.test.cpp
@@ -10,13 +10,12 @@ vector<long long> naive(const vector<vector<pair<int, long long>>>& adj_weighted
 	int n = adj_weighted.size();
 	kth_node_on_path path(adj_weighted, 0);
 	vector<long long> cnts_naive(n, 0);
-	for(int u = 0; u < n; u++) {
-		for(int v = u + 1; v < n; v++) {
+	for (int u = 0; u < n; u++) {
+		for (int v = u + 1; v < n; v++) {
 			int path_len_nodes = path.lca.dist_edges(u, v) + 1;
-			if(path_len_nodes == k) {
-				for(int i = 0; i < path_len_nodes; i++) {
+			if (path_len_nodes == k) {
+				for (int i = 0; i < path_len_nodes; i++)
 					cnts_naive[path.query(u, v, i)]++;
-				}
 			}
 		}
 	}
@@ -24,11 +23,10 @@ vector<long long> naive(const vector<vector<pair<int, long long>>>& adj_weighted
 }
 
 int main() {
-
-	for(int n = 2; n <= 100; n++) {
+	for (int n = 2; n <= 100; n++) {
 		vector<vector<int>> adj(n);
 		vector<vector<pair<int, long long>>> adj_weighted(n);
-		for(auto [u, v] : genRandomTree(n)) {
+		for (auto [u, v] : genRandomTree(n)) {
 			adj[u].push_back(v);
 			adj[v].push_back(u);
 			adj_weighted[u].emplace_back(v, 1LL);
@@ -37,7 +35,6 @@ int main() {
 		int k = get_rand(1, n + 1);
 		assert(get_num_paths(adj, k) == naive(adj_weighted, k));
 	}
-
 	cout << "Hello World\n";
 	return 0;
 }
