3cbb8450a89939a96350f4057c50205c32395b6c
Luke Videckis <lukevideckis@gmail.com>
2022-08-27 16:29:07 -0700

    golf

--- library/graphs/dijkstra.h
+++ library/graphs/dijkstra.h
@@ -4,8 +4,7 @@
 const long long INF = 1e18;
 vector<long long> dijkstra(const vector<vector<pair<int, long long>>>& adj /*directed or undirected, weighted graph*/, int start) {
 	using node = pair<long long, int>;
-	int n = adj.size();
-	vector<long long> len(n, INF);
+	vector<long long> len(adj.size(), INF);
 	len[start] = 0;
 	priority_queue<node, vector<node>, greater<node>> q;
 	q.emplace(0, start);
