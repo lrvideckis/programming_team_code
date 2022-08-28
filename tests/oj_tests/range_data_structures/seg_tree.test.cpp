c6435766fd1810c1a062330879c13973f8ea8913
lrvideckis <lukevideckis@gmail.com>
2022-08-28 14:08:46 -0700

    test empty ranges

--- tests/oj_tests/range_data_structures/seg_tree.test.cpp
+++ tests/oj_tests/range_data_structures/seg_tree.test.cpp
@@ -22,8 +22,13 @@ int main() {
 			int x;
 			cin >> x;
 			st.update(l, r, x);
-		} else
+			st.update(l, l, 1);
+			st.update(r, r, 1);
+		} else {
 			cout << st.query(l, r) << '\n';
+			assert(st.query(l, l) == seg_tree::INF);
+			assert(st.query(r, r) == seg_tree::INF);
+		}
 	}
 	return 0;
 }
