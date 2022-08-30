d3cddf2a163baa54d42ec03e114f2715d0898150
lrvideckis <lukevideckis@gmail.com>
2022-08-29 23:47:37 -0700

    split apart centroid decomp

--- library/graphs/k_len_path_node_count.h
+++ library/graphs/k_len_path_node_count.h
@@ -1,55 +1,5 @@
 #pragma once
-
-struct centroid_decomp {
-	vector<vector<int>> adj;
-	function<void(const vector<vector<int>>&, int)> func;
-	vector<int> subtree_sizes;
-
-	centroid_decomp(const vector<vector<int>>& a_adj,
-	                const function<void(const vector<vector<int>>&, int)>& a_func)
-		: adj(a_adj), func(a_func), subtree_sizes(adj.size()) {
-		decomp(find_centroid(0));
-	}
-
-	void calc_subtree_sizes(int u, int p = -1) {
-		subtree_sizes[u] = 1;
-		for (auto v : adj[u]) {
-			if (v == p)
-				continue;
-			calc_subtree_sizes(v, u);
-			subtree_sizes[u] += subtree_sizes[v];
-		}
-	}
-
-	int find_centroid(int root) {
-		calc_subtree_sizes(root);
-		auto dfs = [&](auto self, int u, int p) -> int {
-			int biggest_ch = -1;
-			for (auto v : adj[u]) {
-				if (v == p)
-					continue;
-				if (biggest_ch == -1 ||
-				        subtree_sizes[biggest_ch] < subtree_sizes[v])
-					biggest_ch = v;
-			}
-
-			if (biggest_ch != -1 &&
-			        2 * subtree_sizes[biggest_ch] > subtree_sizes[root])
-				return self(self, biggest_ch, u);
-			return u;
-		};
-		return dfs(dfs, root, root);
-	}
-
-	void decomp(int root) {
-		func(adj, root);
-		for (auto v : adj[root]) {
-			adj[v].erase(find(adj[v].begin(), adj[v].end(), root));
-			decomp(find_centroid(v));
-		}
-	}
-};
-
+#include "centroid_decomposition.h"
 //0-based nodes
 //returns array `num_paths` where `num_paths[i]` = number of paths with k edges where node `i` is on the path
 //O(n log n)
