bfec74ffdfa31e03695ac3deb8cefee202f07523
lrvideckis <lukevideckis@gmail.com>
2022-08-30 00:02:38 -0700

    refactor count paths per length to use centroid decomp

--- library/graphs/count_paths_per_length.h
+++ library/graphs/count_paths_per_length.h
@@ -1,51 +1,22 @@
 #pragma once
 #include "../../kactl/content/numerical/FastFourierTransform.h"
+#include "centroid_decomposition.h"
 //returns array `cnt_paths` where `cnt_paths[i]` = # of paths in tree with `i` edges
-//centroid decomposition + FFT
 //O(n log^2 n)
-vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, connected tree*/) {
-	int n = adj.size();
-	vector<int> vis(n, 0), sizes(n);
-	auto dfs_sz = [&](auto self, int node, int par) -> void {
-		sizes[node] = 1;
-		for (int ch : adj[node]) {
-			if (ch == par || vis[ch]) continue;
-			self(self, ch, node);
-			sizes[node] += sizes[ch];
-		}
-	};
-	auto find_centroid = [&](int node) -> int {
-		dfs_sz(dfs_sz, node, -1);
-		int size_cap = sizes[node] / 2, par = -1;
-		while (1) {
-			bool found = 0;
-			for (int ch : adj[node]) {
-				if (ch != par && !vis[ch] && sizes[ch] > size_cap) {
-					found = 1;
-					par = node;
-					node = ch;
-					break;
-				}
-			}
-			if (!found) return node;
-		}
-	};
-	vector<long long> cnt_paths(n, 0);
-	auto dfs = [&](auto self, int node) -> void {
-		node = find_centroid(node);
-		vis[node] = 1;
+vector<long long> tree_freq_dist(const vector<vector<int>>& a_adj/*unrooted, connected tree*/) {
+	vector<long long> cnt_paths(a_adj.size(), 0);
+	auto func = [&](const vector<vector<int>>& adj, int root) {
 		vector<double> total_depth(1, 1.0);
-		for (int to : adj[node]) {
-			if (vis[to]) continue;
+		for (int to : adj[root]) {
 			vector<double> cnt_depth(1, 0.0);
-			for (queue<pair<int, int>> q({{to, node}}); !q.empty();) {
+			for (queue<pair<int, int>> q({{to, root}}); !q.empty();) {
 				cnt_depth.push_back(q.size());
 				queue<pair<int, int>> new_q;
 				while (!q.empty()) {
 					auto [curr, par] = q.front();
 					q.pop();
 					for (int ch : adj[curr]) {
-						if (ch == par || vis[ch]) continue;
+						if (ch == par) continue;
 						new_q.emplace(ch, curr);
 					}
 				}
@@ -57,9 +28,8 @@ vector<long long> tree_freq_dist(const vector<vector<int>>& adj/*unrooted, conne
 			}
 			if (total_depth.size() < cnt_depth.size()) total_depth.resize(cnt_depth.size(), 0.0);
 			for (int i = 1; i < (int)cnt_depth.size(); i++) total_depth[i] += cnt_depth[i];
-			self(self, to);
 		}
 	};
-	dfs(dfs, 0);
+	centroid_decomp decomp(a_adj, func);
 	return cnt_paths;
 }
