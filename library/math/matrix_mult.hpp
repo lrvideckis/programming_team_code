9911941c7b44cde1901275aeea262f99103561d0
Cameron Custer <custer.cameron@gmail.com>
2022-09-07 17:42:43 -0400

    matrix mult is back and better than ever

---
+++ library/math/matrix_mult.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// generic matrix multiplication (not overflow safe)
+// will RTE if the given matricies are not compatible
+
+template<typename T> vector<vector<T>> operator * (const vector<vector<T>>& a, const vector<vector<T>>& b) {
+	assert(a[0].size() == b.size());
+	int n = a.size(), m = b[0].size(), inner = b.size();
+	vector<vector<T>> c(n, vector<T>(m));
+	for (int i = 0; i < n; i++) {
+		for (int k = 0; k < inner; k++) {
+			for (int j = 0; j < m; j++)
+				c[i][j] = c[i][j] + a[i][k] * b[k][j];
+		}
+	}
+	return c;
+}
