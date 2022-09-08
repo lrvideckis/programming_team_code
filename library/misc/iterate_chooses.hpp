9ee7b9e4f309b7ddc73929d3510af0906351b66f
Cameron Custer <custer.cameron@gmail.com>
2022-09-07 23:22:00 -0400

    add iterate chooses bithack

---
+++ library/misc/iterate_chooses.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+// iterates all submasks of size n with k bits set
+// Time Complexity: O(n choose k)
+// Space Complexity: O(1)
+
+void iterate_chooses(int n, int k, const function<void(int)>& func) {
+	for (int mask = (1 << k) - 1, r, c; mask <= (1 << n) - (1 << (n - k));
+	        c = mask & -mask, r = mask + c, mask = r | (((r ^ mask) >> 2) / c))
+		func(mask);
+}
