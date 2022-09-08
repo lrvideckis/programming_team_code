4ebb8b79bcbb058c540a4346c0b7cca39bafa10f
lrvideckis <lukevideckis@gmail.com>
2022-09-07 22:38:02 -0700

    remove temporary variables

--- library/misc/iterate_chooses.hpp
+++ library/misc/iterate_chooses.hpp
@@ -4,8 +4,12 @@
 // Time Complexity: O(n choose k)
 // Space Complexity: O(1)
 
+int next_subset(int mask) {
+	int c = mask & -mask, r = mask + c;
+	return r | (((r ^ mask) >> 2) / c);
+}
+
 void iterate_chooses(int n, int k, const function<void(int)>& func) {
-	for (int mask = (1 << k) - 1, r, c; mask <= (1 << n) - (1 << (n - k));
-	        c = mask & -mask, r = mask + c, mask = r | (((r ^ mask) >> 2) / c))
+	for (int mask = (1 << k) - 1; mask <= (1 << n) - (1 << (n - k)); mask = next_subset(mask))
 		func(mask);
 }
