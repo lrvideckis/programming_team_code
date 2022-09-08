beb0fc031a8dd36c62c81cae468898243e2d37b3
Cameron Custer <ccuster@stockholm.ptl.ucf.edu>
2022-09-08 15:49:18 -0400

    udpate make script to remove null and build once

--- tests/scripts/make_hackpack.sh
+++ tests/scripts/make_hackpack.sh
@@ -10,12 +10,9 @@ do
 done
 
 
-#needed to make boxes under headings empty. Else you get this weird text
+#needed to make boxes under headings empty, otherwise you get filler text touch NULL
 touch NULL
 
-#need to compile twice for it to generate the table of contents
-#I think it's related to not knowing page numbers until after first pdf generation
-pdflatex scripts/hackpack.tex
 pdflatex scripts/hackpack.tex
 
 mv hackpack.pdf ../
@@ -25,3 +22,5 @@ for test in $tests
 do
 	sed --in-place '1,2d' $test
 done
+
+rm NULL
