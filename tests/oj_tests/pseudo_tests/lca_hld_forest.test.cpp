49258da2192c30aac333cc861100434ebce03a9d
lrvideckis <lukevideckis@gmail.com>
2022-09-08 16:44:25 -0700

    fix lint warnings

--- tests/oj_tests/pseudo_tests/lca_hld_forest.test.cpp
+++ tests/oj_tests/pseudo_tests/lca_hld_forest.test.cpp
@@ -9,18 +9,18 @@
 
 int main() {
 	for (int tests = 30; tests--;) {
-		const int n = get_rand<int>(1, 100);
-		vector<vector<int>> adj(n);
-		vector<vector<long long>> matrix(n, vector<long long>(n, inf)), matrix_weighted(n, vector<long long>(n, inf));
-		vector<vector<pair<int, long long>>> adj_weighted(n);
-		UF uf(n);
+		const int N = get_rand<int>(1, 100);
+		vector<vector<int>> adj(N);
+		vector<vector<long long>> matrix(N, vector<long long>(N, inf)), matrix_weighted(N, vector<long long>(N, inf));
+		vector<vector<pair<int, long long>>> adj_weighted(N);
+		UF uf(N);
 		int edges = 0;
-		for (int q = n; q--;) {
-			int u = get_rand<int>(0, n);
-			int v = get_rand<int>(0, n);
+		for (int q = N; q--;) {
+			int u = get_rand<int>(0, N);
+			int v = get_rand<int>(0, N);
 			if (u == v) continue;
 			if (uf.join(u, v)) {
-				long long w = get_rand<long long>(0, (long long)1e15);
+				auto w = get_rand<long long>(0, (long long)1e15);
 				edges++;
 				adj[u].push_back(v);
 				adj[v].push_back(u);
@@ -32,13 +32,13 @@ int main() {
 				matrix_weighted[v][u] = w;
 			}
 		}
-		cerr << "edges added: " << edges << " number of nodes: " << n << endl;
+		cerr << "edges added: " << edges << " number of nodes: " << N << endl;
 		floydWarshall(matrix);
 		floydWarshall(matrix_weighted);
 		LCA lca(adj_weighted);
 		HLD hld(adj);
-		for (int u = 0; u < n; u++) {
-			for (int v = 0; v < n; v++) {
+		for (int u = 0; u < N; u++) {
+			for (int v = 0; v < N; v++) {
 				if (uf.sameSet(u, v)) {
 					assert(lca.dist_weight(u, v) == matrix_weighted[u][v]);
 					assert(lca.dist_edges(u, v) == matrix[u][v]);
