eb45bdf2e43f2043d07e92b6db9304eb43082023
Luke Videckis <lukevideckis@gmail.com>
2022-09-10 12:11:43 -0700

    add assert for mod inverse

--- tests/oj_tests/math/n_choose_k.test.cpp
+++ tests/oj_tests/math/n_choose_k.test.cpp
@@ -5,7 +5,18 @@
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-	n_choose_k nk(1001, 1e9 + 7);
+	const int MOD = 1e9 + 7;
+	n_choose_k nk(1001, MOD);
+	//test mod inverse
+	{
+		//source: https://codeforces.com/blog/entry/83075
+		vector<int> inverse(1001);
+		inverse[1] = 1;
+		for (int i = 2; i < (int)inverse.size(); i ++)
+			inverse[i] = 1LL * (MOD - MOD / i) * inverse[MOD % i] % MOD;
+		for (int i = 1; i < (int)inverse.size(); i ++)
+			assert(nk.inv(i) == inverse[i]);
+	}
 	int n, k;
 	cin >> n >> k;
 	cout << nk.choose(k, n) << '\n';
