c0ee8276d3f541e08bb0b838512695e5df2a37b1
lrvideckis <lukevideckis@gmail.com>
2022-09-17 18:35:09 -0700

    switch to simpler method of generating random numbers

--- library/misc/random.hpp
+++ library/misc/random.hpp
@@ -1,16 +1,8 @@
 #pragma once
 
-//source: https://codeforces.com/blog/entry/60442
+//source: https://codeforces.com/blog/entry/61675
 
-//MUCH RANDOM!!!
-seed_seq seed{
-	(uint32_t)chrono::duration_cast<chrono::nanoseconds>
-	(chrono::high_resolution_clock::now().time_since_epoch()).count(),
-	(uint32_t)random_device()(),
-	(uint32_t)(uintptr_t)make_unique<char>().get(),
-	(uint32_t)__builtin_ia32_rdtsc()
-};
-mt19937 rng(seed);
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 //intended types: int, unsigned, long long
 //returns a random number in range [l, r)
@@ -18,3 +10,6 @@ template<class T> inline T get_rand(T l, T r) {
 	assert(l < r);
 	return uniform_int_distribution<T>(l, r - 1)(rng);
 }
+
+//vector<int> a;
+//shuffle(a.begin(), a.end(), rng);
