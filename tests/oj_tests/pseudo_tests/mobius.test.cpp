b2389c422ed816948da68b68cb49bd1dc1325e67
Luke Videckis <lukevideckis@gmail.com>
2022-09-19 11:17:42 -0700

    #250

---
+++ tests/oj_tests/pseudo_tests/mobius.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+#include "../../template.hpp"
+#include "../../../library/math/prime_sieve.hpp"
+
+#include "../../../library/math/mobius_inversion.hpp"
+
+int main() {
+	calc_sieve();
+	calc_mobius();
+	for (int i = 1; i < min(N, MX_N); i++) {
+		int val = i;
+		map<int, int> factors;
+		while (val > 1) {
+			int p = a_prime[val];
+			//p is some prime factor of val
+			factors[p]++;
+			val /= p;
+		}
+		{
+			bool found_square = 0;
+			for (auto [prime, exponent] : factors) {
+				if (exponent > 1) {
+					assert(mobius[i] == 0);
+					found_square = 1;
+					break;
+				}
+			}
+			if (found_square) continue;
+		}
+		assert(1 - 2 * (factors.size() % 2) == mobius[i]);
+	}
+	cout << "Hello World\n";
+	return 0;
+}
