b24b7bfca0150d7a29c78f3b776bb4bd65292d33
Luke Videckis <lukevideckis@gmail.com>
2022-09-19 20:19:58 -0700

    bring back lcp palindrome test

---
+++ tests/online_judge_tests/handmade_tests/lcp_query_palindrome.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_palindromes"
+#include "../../template.hpp"
+
+#include "../../../library/strings/longest_common_prefix_query.hpp"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	string s;
+	cin >> s;
+	int n = s.size();
+	s = s + '$' + string(s.rbegin(), s.rend());
+	lcp_query lcp_q(s);
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < min(i + 2, n); j++)
+			cout << lcp_q.get_lcp(j, (n - i - 1) + n + 1) * 2 - (i == j) << " ";
+	}
+	cout << endl;
+	return 0;
+}
