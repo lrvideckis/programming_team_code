88905ee5bd5f8cd7cfffa33eacec023353f4642c
lrvideckis <lukevideckis@gmail.com>
2022-09-19 22:37:42 -0700

    initial commit

---
+++ tests/scripts/tests_by_git_modification.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#get all test files
+all_test_files=$(find online_judge_tests/ -type f -name "*.test.cpp")
+
+for file in $all_test_files
+do
+	echo "start of file"
+	for dependency in $(g++ -MM $file)
+	do
+		case $dependency in *.hpp|*.test.cpp)
+			git log -n 1 --date=iso-local --format="%ad $dependency" $dependency
+		esac
+	done | sort
+
+	echo "middle of file"
+
+	for dependency in $(g++ -MM $file)
+	do
+		case $dependency in *.hpp|*.test.cpp)
+			git log -n 1 --date=iso-local --format="%ad $dependency" $dependency
+		esac
+	done | sort | tail -1
+
+	echo "end of file"
+done
