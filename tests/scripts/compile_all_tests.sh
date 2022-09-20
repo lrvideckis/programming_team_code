a1fd80e7bbd350211d50e0a6ced2fe739fecff80
Luke Videckis <lukevideckis@gmail.com>
2022-09-19 17:03:25 -0700

    split apart compile and clang-tidy to improve perf

---
+++ tests/scripts/compile_all_tests.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+git submodule init
+git submodule update
+
+source scripts/add_symlink.sh
+
+#compile code
+declare -i pass=0
+declare -i fail=0
+failTests=""
+for test in $(find oj_tests/ -type f -name '*.test.cpp')
+do
+	echo "file is "$test
+	g++ $test $(cat scripts/compile_flags.txt)
+	if (($? != 0))
+	then
+		fail+=1
+		failTests="$failTests$test\n"
+	else
+		pass+=1
+	fi
+done
+echo "$pass/$(($pass+$fail)) tests passed"
+if (($fail == 0)); then
+	echo "No tests failed"
+	exit 0
+else
+	echo -e "These tests failed: \n $failTests"
+	exit 1
+fi
