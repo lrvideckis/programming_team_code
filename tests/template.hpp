55ffc992ee8d643453918bc91498f065834e098b
Luke Videckis <lukevideckis@gmail.com>
2022-09-19 18:04:09 -0700

    test

--- tests/template.hpp
+++ tests/template.hpp
@@ -1,2 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
 using namespace std;
