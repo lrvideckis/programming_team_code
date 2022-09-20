de52953494e953cec0090970a1572084b8b6857e
Luke Videckis <lukevideckis@gmail.com>
2022-09-20 01:29:26 -0700

    Update check_format.sh

--- tests/scripts/check_format.sh
+++ tests/scripts/check_format.sh
@@ -47,10 +47,27 @@ git submodule update
 
 source scripts/add_symlink.sh
 
-#check snake case & lint
-clang-tidy $(source scripts/tests_by_git_modification.sh | awk '{print $NF}') -- -std=c++17
-if (($? != 0))
-then
-	echo "clang-tidy failed"
+#lint code
+declare -i pass=0
+declare -i fail=0
+failTests=""
+for test in $(source scripts/tests_by_git_modification.sh | awk '{print $NF}')
+do
+	echo "file is "$test
+	clang-tidy $test -- -std=c++17
+	if (($? != 0))
+	then
+		fail+=1
+		failTests="$failTests$test\n"
+	else
+		pass+=1
+	fi
+done
+echo "$pass/$(($pass+$fail)) tests passed"
+if (($fail == 0)); then
+	echo "No tests failed"
+	exit 0
+else
+	echo -e "These tests failed: \n $failTests"
 	exit 1
 fi
