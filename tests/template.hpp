9782351eeb4b462f88db6c4d02b5777986680e31
Luke Videckis <lukevideckis@gmail.com>
2022-09-19 18:12:21 -0700

    Update template.hpp

--- tests/template.hpp
+++ tests/template.hpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <cstdio>
-#include <cstdlib>
 #include <algorithm>
 #include <cmath>
 #include <vector>
@@ -9,13 +7,10 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <queue>
-#include <ctime>
 #include <cassert>
 #include <complex>
 #include <string>
-#include <cstring>
 #include <chrono>
-#include <random>
 #include <bitset>
 #include <array>
 using namespace std;
