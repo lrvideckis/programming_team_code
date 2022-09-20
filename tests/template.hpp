616327971db1cefd281786435515968261845cea
Luke Videckis <lukevideckis@gmail.com>
2022-09-19 18:13:21 -0700

    See if this is faster (#255)
    * test
    
    * Update template.hpp

--- tests/template.hpp
+++ tests/template.hpp
@@ -1,2 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <chrono>
+#include <bitset>
+#include <array>
 using namespace std;
