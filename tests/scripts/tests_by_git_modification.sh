08da34540bbd1ff8196ba4de35900afd694ab106
Luke Videckis <lukevideckis@gmail.com>
2022-09-20 00:25:26 -0700

    Update tests_by_git_modification.sh

--- tests/scripts/tests_by_git_modification.sh
+++ tests/scripts/tests_by_git_modification.sh
@@ -10,10 +10,10 @@ all_test_files=$(find online_judge_tests/ -type f -name "*.test.cpp")
 
 for file in $all_test_files
 do
-	$(for dependency in $(g++ -MM $file)
+	for dependency in $(g++ -MM $file)
 	do
 		case $dependency in *.hpp|*.test.cpp)
 			git log -n 1 --date=iso-local --format="%ad" $dependency
 		esac
-	done | sort | tail -1)" $file"
+	done | sort | tail -1 && echo $file
 done
