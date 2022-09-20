dca9dadc3baac18ea3fe1ce307dedde7b1f5caba
Luke Videckis <lukevideckis@gmail.com>
2022-09-20 15:29:53 -0700

    more verbous output of compile script

--- tests/scripts/compile_all_tests.sh
+++ tests/scripts/compile_all_tests.sh
@@ -8,25 +8,27 @@ scripts/add_symlink.sh
 #compile code
 declare -i pass=0
 declare -i fail=0
-failTests=""
-for test in $(scripts/tests_by_git_modification.sh | awk '{print $NF}')
+fail_tests=""
+
+scripts/tests_by_git_modification.sh | while read test
 do
-	echo "file is "$test
 	#can't compile 2 files with main at the same time, so we need to compile each test separately
-	g++ $test $(cat scripts/compile_flags.txt)
+	echo "compiling "$test
+	g++ $(echo $test | awk '{print $NF}') $(cat scripts/compile_flags.txt)
 	if (($? != 0))
 	then
 		fail+=1
-		failTests="$failTests$test\n"
+		fail_tests="$fail_tests$test_name\n"
 	else
 		pass+=1
 	fi
 done
+
 echo "$pass/$(($pass+$fail)) tests passed"
 if (($fail == 0)); then
 	echo "No tests failed"
 	exit 0
 else
-	echo -e "These tests failed: \n $failTests"
+	echo -e "These tests failed: \n $fail_tests"
 	exit 1
 fi
