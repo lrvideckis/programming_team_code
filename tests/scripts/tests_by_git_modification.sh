b2c92bf4b95bbdacc9d124a6f09315c56605cced
Luke Videckis <lukevideckis@gmail.com>
2022-09-19 23:54:34 -0700

    Update tests_by_git_modification.sh

--- tests/scripts/tests_by_git_modification.sh
+++ tests/scripts/tests_by_git_modification.sh
@@ -5,8 +5,6 @@ git submodule update
 
 source scripts/add_symlink.sh
 
-git log -n 10
-
 #get all test files
 all_test_files=$(find online_judge_tests/ -type f -name "*.test.cpp")
 
@@ -19,16 +17,4 @@ do
 			git log -n 1 --date=iso-local --format="%ad $dependency" $dependency
 		esac
 	done | sort
-
-	echo "middle of file"
-
-	for dependency in $(g++ -MM $file)
-	do
-		case $dependency in *.hpp|*.test.cpp)
-			#git log -n 1 --date=iso-local --format="%ad $dependency" $dependency
-			git log $dependency
-		esac
-	done | sort | tail -1
-
-	echo "end of file"
 done
