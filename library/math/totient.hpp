dbf0028e91d8f43ae21cf13bef688d809f8aefc3
Luke Videckis <lukevideckis@gmail.com>
2022-09-21 02:41:39 -0700

    revert

--- library/math/totient.hpp
+++ library/math/totient.hpp
@@ -4,13 +4,13 @@
 //
 //To improve, pre-calc prime factors or use Pollard-rho to find prime factors.
 int totient(int n) {
-	int RES = n;
+	int res = n;
 	for (int i = 2; i * i <= n; i++) {
 		if (n % i == 0) {
 			while (n % i == 0) n /= i;
-			RES -= RES / i;
+			res -= res / i;
 		}
 	}
-	if (n > 1) RES -= RES / n;
-	return RES;
+	if (n > 1) res -= res / n;
+	return res;
 }
