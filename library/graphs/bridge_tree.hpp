8f1dcc41c635940894c58cd146e1c72efd39f114
Luke Videckis <lukevideckis@gmail.com>
2022-09-21 02:20:53 -0700

    test astyle fails

--- library/graphs/bridge_tree.hpp
+++ library/graphs/bridge_tree.hpp
@@ -6,7 +6,7 @@
 //	vector<vector<int>> bt = bridge_tree(adj, cc);
 vector<vector<int>> bridge_tree(const vector<vector<pair<int, int>>>& adj, const graph_info& cc) {
 	vector<vector<int>> tree(cc.num_2_edge_ccs);
-	for (int i = 0; i < (int)adj.size(); i++)
+	for(int i = 0; i < (int)adj.size(); i++)
 		for (auto [to, e_id] : adj[i])
 			if (cc.is_bridge[e_id])
 				tree[cc.two_edge_ccid[i]].push_back(cc.two_edge_ccid[to]);
