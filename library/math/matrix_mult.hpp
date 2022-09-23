0e648aa4fa87ff447c6f543928ad51f98d4825a3
Luke Videckis <lukevideckis@gmail.com>
2022-09-22 23:12:13 -0700

    matrix mult

--- library/math/matrix_mult.hpp
+++ library/math/matrix_mult.hpp
@@ -8,8 +8,8 @@
 // Space: O(n * m)
 
 template<typename T> vector<vector<T>> operator * (const vector<vector<T>>& a, const vector<vector<T>>& b) {
-	assert(a[0].size() == b.size());
-	int n = a.size(), m = b[0].size(), inner = b.size();
+	assert(ssize(a[0]) == ssize(b));
+	int n = ssize(a), m = ssize(b[0]), inner = ssize(b);
 	vector<vector<T>> c(n, vector<T>(m));
 	for (int i = 0; i < n; i++) {
 		for (int k = 0; k < inner; k++) {
