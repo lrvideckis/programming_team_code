9923daa766011201d3ef6301abef7cd8c9ed2864
Luke Videckis <lukevideckis@gmail.com>
2022-09-23 01:17:46 -0700

    golf

--- library/strings/longest_common_prefix_query.hpp
+++ library/strings/longest_common_prefix_query.hpp
@@ -4,20 +4,20 @@
 //computes suffix array, lcp array, and then sparse table over lcp array
 //O(n log n)
 template<typename T> struct lcp_query {
-	const int N;
 	vector<int> sa, lcp, inv_sa;
 	RMQ<int> rmq;
-	lcp_query(const T& s) : N(ssize(s)), sa(atcoder::suffix_array(s)), lcp(atcoder::lcp_array(s, sa)), inv_sa(N), rmq(lcp, [](int x, int y) {
+	lcp_query(const T& s) : sa(atcoder::suffix_array(s)), lcp(atcoder::lcp_array(s, sa)), inv_sa(ssize(s)), rmq(lcp, [](int x, int y) {
 		return min(x, y);
 	}) {
-		for (int i = 0; i < N; i++) inv_sa[sa[i]] = i;
+		for (int i = 0; i < ssize(sa); i++)
+			inv_sa[sa[i]] = i;
 	}
 	//length of longest common prefix of suffixes s[idx1 ... n), s[idx2 ... n), 0-based indexing
 	//
 	//You can check if two substrings s[l1..r1), s[l2..r2) are equal in O(1) by:
 	//r1-l1 == r2-l2 && longest_common_prefix(l1, l2) >= r1-l1
 	int get_lcp(int idx1, int idx2) const {
-		if (idx1 == idx2) return N - idx1;
+		if (idx1 == idx2) return ssize(sa) - idx1;
 		auto [l, r] = minmax(inv_sa[idx1], inv_sa[idx2]);
 		return rmq.query(l, r);
 	}
