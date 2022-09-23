0bac7da29caf5bb4ca0dbe6e920024fd324f14c9
Luke Videckis <lukevideckis@gmail.com>
2022-09-23 11:59:54 -0700

    Golf

--- library/misc/monotonic_stack_related/max_rect_histogram.hpp
+++ library/misc/monotonic_stack_related/max_rect_histogram.hpp
@@ -1,14 +1,13 @@
 #pragma once
 #include "monotonic_stack.hpp"
 long long max_rect_histogram(const vector<int>& arr) {
-	int n = ssize(arr);
 	auto rv /*reverse*/ = [&](int i) -> int {
-		return n - 1 - i;
+		return ssize(arr) - 1 - i;
 	};
 	vector<int> left = monotonic_stack<int>(arr, greater_equal());
 	vector<int> right = monotonic_stack<int>(vector<int>(arr.rbegin(), arr.rend()), greater_equal());
 	long long max_area = 0;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < ssize(arr); i++) {
 		int l = left[i], r = rv(right[rv(i)]);//arr[i] is the max of range (l, r)
 		max_area = max(max_area, 1LL * arr[i] * (r - l - 1));
 	}
