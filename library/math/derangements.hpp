4998de97652941754b2d5d657663ab33ec2eb364
Luke Videckis <lukevideckis@gmail.com>
2022-09-23 00:22:11 -0700

    more nits

--- library/math/derangements.hpp
+++ library/math/derangements.hpp
@@ -5,10 +5,10 @@
 //there are (i-1) places to move 0 to not be at index 0. Let's say we moved 0 to index j (j>0).
 //If we move value j to index 0 (forming a cycle of length 2), then there are dp[i-2] derangements of the remaining i-2 elements
 //else there are dp[i-1] derangements of the remaining i-1 elements (including j)
-vector<int> derangements(int n, int mod) {
-	vector<int> dp(n, 0);
+vector<long long> derangements(int n, long long mod) {
+	vector<long long> dp(n, 0);
 	dp[0] = 1;
 	for (int i = 2; i < n; i++)
-		dp[i] = 1LL * (i - 1) * (dp[i - 1] + dp[i - 2]) % mod;
+		dp[i] = (i - 1) * (dp[i - 1] + dp[i - 2]) % mod;
 	return dp;
 }
