b8dd634a97c9f8c770c0393e82533916ddbf1e1e
Luke Videckis <lukevideckis@gmail.com>
2022-09-22 23:19:09 -0700

    did pal

--- library/strings/palindrome_query.hpp
+++ library/strings/palindrome_query.hpp
@@ -3,7 +3,7 @@
 struct pal_query {
 	const int N;
 	array<vi, 2> pal_len;
-	pal_query(const string& s) : N(s.size()), pal_len(manacher(s)) {}
+	pal_query(const string& s) : N(ssize(s)), pal_len(manacher(s)) {}
 	//returns 1 if substring s[l...r) is a palindrome
 	//(returns 1 when l == r)
 	bool is_pal(int l, int r) const {
