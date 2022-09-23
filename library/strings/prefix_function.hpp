b6147c007a7d90cdc0e40c6c51fccb1a7cbd4592
Luke Videckis <lukevideckis@gmail.com>
2022-09-23 14:29:34 -0700

    golf

--- library/strings/prefix_function.hpp
+++ library/strings/prefix_function.hpp
@@ -1,9 +1,8 @@
 #pragma once
 //source: https://cp-algorithms.com/string/prefix-function.html#implementation
 template <class T> vector<int> prefix_function(const T& s) {
-	int n = ssize(s);
-	vector<int> pi(n, 0);
-	for (int i = 1; i < n; i++) {
+	vector<int> pi(ssize(s), 0);
+	for (int i = 1; i < ssize(s); i++) {
 		int j = pi[i - 1];
 		while (j > 0 && s[i] != s[j]) j = pi[j - 1];
 		pi[i] = j + (s[i] == s[j]);
