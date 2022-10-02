48cbbd5d83c1703f71bc1803b18dfad53f5a6dee
Luke Videckis <lukevideckis@gmail.com>
2022-10-01 23:32:39 -0700

    run clang in parallel

--- tests/scripts/check_format.sh
+++ tests/scripts/check_format.sh
@@ -21,32 +21,6 @@ git submodule update
 
 ./scripts/add_symlink.sh
 
-#lint code
-declare -i pass=0
-declare -i fail=0
-fail_tests=""
-
-echo "run clang-tidy on all files:"
-while read test
-do
-	# run clang tidy one-by-one to get quicker output
-	# also running clang-tidy on all tests in a single command gives no speed-up
-	echo "last modified date; file to run clang-tidy "$test
-	if clang-tidy $(echo $test | awk '{print $NF}') -- -std=c++17
-	then
-		pass+=1
-	else
-		fail+=1
-		fail_tests="$fail_tests$test\n"
-	fi
-done < <(./scripts/tests_by_git_modification.sh)
-
-echo "$pass/$(($pass+$fail)) tests passed"
-if (($fail == 0))
-then
-	echo "No tests failed"
-	exit 0
-else
-	echo -e "These tests failed: \n $fail_tests"
-	exit 1
-fi
+./scripts/tests_by_git_modification.sh | \
+	awk '{print $NF}' | \
+	parallel --bar clang-tidy {} -- -std=c++17
