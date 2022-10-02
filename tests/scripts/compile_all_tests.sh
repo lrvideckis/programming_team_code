f94700fef5274461088f552aa5ee5114b0765967
Luke Videckis <lukevideckis@gmail.com>
2022-10-01 23:55:02 -0700

    Compile in parallel (#268)
    * test
    
    * install parallel
    
    * trying this
    
    * fix bug
    
    * revert
    
    * run clang in parallel
    
    * Update test_most_code.yml
    
    * revert

--- tests/scripts/compile_all_tests.sh
+++ tests/scripts/compile_all_tests.sh
@@ -5,30 +5,6 @@ git submodule update
 
 ./scripts/add_symlink.sh
 
-#compile code
-declare -i pass=0
-declare -i fail=0
-fail_tests=""
-
-while read test
-do
-	#can't compile 2 files with main at the same time, so we need to compile each test separately
-	echo "last modified date; file to compile "$test
-	if g++ $(echo $test | awk '{print $NF}') $(cat scripts/compile_flags.txt)
-	then
-		pass+=1
-	else
-		fail+=1
-		fail_tests="$fail_tests$test\n"
-	fi
-done < <(./scripts/tests_by_git_modification.sh)
-
-echo "$pass/$(($pass+$fail)) tests passed"
-if (($fail == 0))
-then
-	echo "No tests failed"
-	exit 0
-else
-	echo -e "These tests failed: \n $fail_tests"
-	exit 1
-fi
+./scripts/tests_by_git_modification.sh | \
+	awk '{print $NF}' | \
+	parallel --bar g++ {} $(cat scripts/compile_flags.txt)
