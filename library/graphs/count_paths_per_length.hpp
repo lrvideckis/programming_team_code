37e0a577567104b07ae6934e45a1ea7481a9c74f
Luke Videckis <lukevideckis@gmail.com>
2022-10-13 22:19:51 -0700

    this should fix TLE

--- library/graphs/count_paths_per_length.hpp
+++ library/graphs/count_paths_per_length.hpp
@@ -6,11 +6,11 @@
 vector<long long> count_paths_per_length(const vector<vector<int>>& a_adj/*unrooted, connected tree*/) {
 	vector<long long> num_paths(ssize(a_adj), 0);
 	auto func = [&](const vector<vector<int>>& adj, int root) -> void {
-		vector<double> total_depth(1, 1.0);
+		vector<vector<double>> child_depths;
 		for (int to : adj[root]) {
-			vector<double> cnt_depth(1, 0.0);
+			child_depths.emplace_back(1, 0.0);
 			for (queue<pair<int, int>> q({{to, root}}); !q.empty();) {
-				cnt_depth.push_back(ssize(q));
+				child_depths.back().push_back(ssize(q));
 				queue<pair<int, int>> new_q;
 				while (!q.empty()) {
 					auto [curr, par] = q.front();
@@ -22,12 +22,19 @@ vector<long long> count_paths_per_length(const vector<vector<int>>& a_adj/*unroo
 				}
 				swap(q, new_q);
 			}
-			{
-				vector<double> prod = conv(total_depth, cnt_depth);
-				for (int i = 1; i < ssize(prod); i++) num_paths[i] += llround(prod[i]);
-			}
-			if (ssize(total_depth) < ssize(cnt_depth)) total_depth.resize(ssize(cnt_depth), 0.0);
-			for (int i = 1; i < ssize(cnt_depth); i++) total_depth[i] += cnt_depth[i];
+		}
+		sort(child_depths.begin(), child_depths.end(), [&](const auto & x, const auto & y) {
+			return x.size() < y.size();
+		});
+		vector<double> total_depth(1, 1.0);
+		for (const auto& cnt_depth : child_depths) {
+			vector<double> prod = conv(total_depth, cnt_depth);
+			for (int i = 1; i < ssize(prod); i++)
+				num_paths[i] += llround(prod[i]);
+			if (ssize(total_depth) < ssize(cnt_depth))
+				total_depth.resize(ssize(cnt_depth), 0.0);
+			for (int i = 1; i < ssize(cnt_depth); i++)
+				total_depth[i] += cnt_depth[i];
 		}
 	};
 	centroid_decomp decomp(a_adj, func);
