4541c614fd5bdefcfa1b97ebbbc31686763d1d0b
Luke Videckis <lukevideckis@gmail.com>
2022-10-17 20:25:10 -0700

    Move comments outside struct for those who like to copy paste directly from GitHub

--- library/strings/kmp.hpp
+++ library/strings/kmp.hpp
@@ -12,25 +12,28 @@
 //kmp doubling trick: to check if 2 arrays are rotationally equivalent: run kmp
 //with one array as the needle and the other array doubled (excluding the first
 //& last characters) as the haystack or just use kactl's min rotation code
+//
+// if haystack = "bananas"
+// needle = "ana"
+//
+// then we find 2 matches:
+// bananas
+// _ana___
+// ___ana_
+// 0123456 (indexes)
+// and KMP::find returns {1,3} - the indexes in haystack where
+// each match starts.
+//
+// You can also pass in 0 for "all" and KMP::find will only
+// return the first match: {1}. Useful for checking if there exists
+// some match:
+//
+// ssize(KMP::find(<haystack>,0)) > 0
 //NOLINTNEXTLINE(readability-identifier-naming)
 template <class T> struct KMP {
+	T needle;
+	vector<int> pi;
 	KMP(const T& a_needle) : needle(a_needle), pi(prefix_function(needle)) {}
-	// if haystack = "bananas"
-	// needle = "ana"
-	//
-	// then we find 2 matches:
-	// bananas
-	// _ana___
-	// ___ana_
-	// 0123456 (indexes)
-	// and KMP::find returns {1,3} - the indexes in haystack where
-	// each match starts.
-	//
-	// You can also pass in 0 for "all" and KMP::find will only
-	// return the first match: {1}. Useful for checking if there exists
-	// some match:
-	//
-	// ssize(KMP::find(<haystack>,0)) > 0
 	vector<int> find(const T& haystack, bool all = 1) const {
 		vector<int> matches;
 		for (int i = 0, j = 0; i < ssize(haystack); i++) {
@@ -44,6 +47,4 @@ template <class T> struct KMP {
 		}
 		return matches;
 	}
-	T needle;
-	vector<int> pi;
 };
