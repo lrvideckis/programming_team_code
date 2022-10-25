5792118553faacef9ce0309dbfcd2d82e176fd22
Luke Videckis <lukevideckis@gmail.com>
2022-10-25 15:55:40 -0700

    revert

--- library/graphs/centroid_decomposition.hpp
+++ library/graphs/centroid_decomposition.hpp
@@ -8,9 +8,9 @@
 // centroid_decomp runs the function on every decomposition
 
 // Example usage:
-//centroid_decomp decomp(adj, [&](const vector<vector<int>>& adj_with_removed_edges, int centroid) -> void {
-//	...
-//});
+//	centroid_decomp decomp(adj, [&](const vector<vector<int>>& adj_with_removed_edges, int centroid) -> void {
+//		...
+//	});
 template<typename F> struct centroid_decomp {
 	vector<vector<int>> adj;
 	F func;
