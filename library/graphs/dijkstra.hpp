1ef40386835e518accafe81306f85edbc85448c0
Luke Videckis <lukevideckis@gmail.com>
2022-10-25 13:19:28 -0700

    more specific comment

--- library/graphs/dijkstra.hpp
+++ library/graphs/dijkstra.hpp
@@ -11,7 +11,7 @@ vector<long long> dijkstra(const vector<vector<pair<int, long long>>>& adj /*dir
 	while (!q.empty()) {
 		auto [curr_len, v] = q.top();
 		q.pop();
-		if (len[v] < curr_len) continue;//important check: TLE without it
+		if (len[v] < curr_len) continue;//important check: O(n*m) without it
 		for (auto [to, weight] : adj[v])
 			if (len[to] > weight + len[v]) {
 				len[to] = weight + len[v];
