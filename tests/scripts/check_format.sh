75d5b56c22e2865fcadf184d229af9279a19fc14
Luke Videckis <lukevideckis@gmail.com>
2022-10-26 14:59:30 -0700

    moving suppression list to file as it's getting pretty long

--- tests/scripts/check_format.sh
+++ tests/scripts/check_format.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+#check snake case && astyle *before* initializing git submodules to avoid warnings not in our code
 echo "check files and directories are snake_case:"
 find ../library/ online_judge_tests/ -name "*[A-Z]*" -or -name "*-*" | \
 	grep --invert-match ".verify-helper" | \
@@ -11,14 +12,13 @@ echo "run astyle on all files:"
 	astyle --options=.astylerc --recursive "../library/*.hpp"
 ) | grep "Formatted" && exit 1
 
-#run cppcheck formatter before initializing git submodules to avoid warnings not in our code
-echo "run cppcheck on all files:"
-cppcheck online_judge_tests/ --enable=all --inconclusive --force --file-filter="*.test.cpp" --language=c++ --suppress=noExplicitConstructor --error-exitcode=1 --std=c++17 --max-ctu-depth=50 || exit 1
-
 git submodule init
 git submodule update
 
 ./scripts/add_symlink.sh
+
+echo "run cppcheck on all files:"
+cppcheck --enable=all --inconclusive --suppressions-list=.cppcheck_suppression_list --force --language=c++ --error-exitcode=1 --std=c++17 --max-ctu-depth=50 online_judge_tests/ ../library/ || exit 1
 
 ./scripts/tests_by_git_modification.sh | \
 	awk '{print $NF}' | \
