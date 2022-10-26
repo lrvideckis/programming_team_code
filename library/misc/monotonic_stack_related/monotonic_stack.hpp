6099673b343fa11c4a84994cab591f36b2c226b9
Luke Videckis <lukevideckis@gmail.com>
2022-10-26 11:28:23 -0700

    I like this version more

--- library/misc/monotonic_stack_related/monotonic_stack.hpp
+++ library/misc/monotonic_stack_related/monotonic_stack.hpp
@@ -1,17 +1,17 @@
 #pragma once
 //usages:
-//	vector<int> left = monotonic_stack<int>(arr, less()); //(or replace `less` with: less_equal, greater, greater_equal
-//	vector<int> left = monotonic_stack<int>(arr, [&](int x, int y) {return x < y;});
+//	vector<int> le = monotonic_stack<int>(arr, less()); //(or replace `less` with: less_equal, greater, greater_equal
+//	vector<int> le = monotonic_stack<int>(arr, [&](int x, int y) {return x < y;});
 //
-//returns array `left` where `left[i]` = max index such that:
-//	`left[i]` < i && !op(arr[left[i]], arr[i])
+//returns array `le` where `le[i]` = max index such that:
+//	`le[i]` < i && !op(arr[le[i]], arr[i])
 //or -1 if no index exists
 //O(n)
 template<class T> vector<int> monotonic_stack(const vector<T>& arr, const function<bool(const T&, const T&)>& op) {
-	vector<int> left(ssize(arr));
+	vector<int> le(ssize(arr));
 	for (int i = 0; i < ssize(arr); i++) {
-		int& j = left[i] = i - 1;
-		while (j >= 0 && op(arr[j], arr[i])) j = left[j];
+		le[i] = i - 1;
+		while (le[i] >= 0 && op(arr[le[i]], arr[i])) le[i] = le[le[i]];
 	}
-	return left;
+	return le;
 }
