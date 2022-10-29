ed452acb61d1da671e547b0d24e3e2254042eb59
Luke Videckis <lukevideckis@gmail.com>
2022-10-29 02:00:36 -0700

    more nits

--- library/range_data_structures/rmq.hpp
+++ library/range_data_structures/rmq.hpp
@@ -4,11 +4,11 @@
 //usage:
 //	vector<long long> arr;
 //	...
-//	RMQ<long long> rmq(arr, [&](auto x, auto y) { return min(x,y); });
+//	RMQ<long long> rmq(arr, [&](auto x, auto y) { return min(x, y); });
 //
 //to also get index of min element, do:
-//	RMQ<pair<long long, int>> rmq(arr, [&](auto x, auto y) { return min(x,y); });
-//and initialize arr[i].second = i (0<=i<n)
+//	RMQ<pair<long long, int>> rmq(arr, [&](auto x, auto y) { return min(x, y); });
+//and initialize arr[i].second = i
 //If there are multiple indexes of min element, it'll return the smallest
 //(left-most) one
 //mnemonic: Range Min/Max Query
@@ -17,10 +17,10 @@ template <typename T> struct RMQ {
 	vector<vector<T>> dp;
 	function<T(const T&, const T&)> op;
 	RMQ(const vector<T>& arr, const function<T(const T&, const T&)>& a_op) : dp(1, arr), op(a_op) {
-		for (int k = 0; (2 << k) <= ssize(arr); k++) {
-			dp.emplace_back(ssize(arr) - (2 << k) + 1);
+		for (int i = 0; (2 << i) <= ssize(arr); i++) {
+			dp.emplace_back(ssize(arr) - (2 << i) + 1);
 			for (int j = 0; j < ssize(dp.back()); j++)
-				dp[k + 1][j] = op(dp[k][j], dp[k][j + (1 << k)]);
+				dp[i + 1][j] = op(dp[i][j], dp[i][j + (1 << i)]);
 		}
 	}
 	//inclusive-exclusive range [le, ri)
