17fd16bce37845018390512337fbeda5357c3f54
Luke Videckis <lukevideckis@gmail.com>
2022-10-29 16:38:54 -0700

    #283

--- tests/online_judge_tests/handmade_tests/fib_matrix_expo.test.cpp
+++ tests/online_judge_tests/handmade_tests/fib_matrix_expo.test.cpp
@@ -6,22 +6,34 @@
 #include "../../../kactl/content/data-structures/Matrix.h"
 #include "../../../ac-library/atcoder/modint.hpp"
 #include "../../../library/math/fibonacci.hpp"
+#include "../../../library/range_data_structures/uncommon/disjoint_rmq.hpp"
 
-void check(long long n) {
+void check(long long n, bool is_small) {
 	const int MOD = 1e9 + 7;
-	Matrix<atcoder::static_modint<MOD>, 2> mat;
+	using mod_int = atcoder::static_modint<MOD>;
+	using mat_2_by_2 = Matrix<mod_int, 2>;
+	mat_2_by_2 mat;
 	mat.d = {{{{1, 1}}, {{1, 0}}}};
-	vector<atcoder::static_modint<MOD>> vec = {1, 0};
-	assert(fib(n, MOD) == ((mat ^ n) * vec)[0]);
+	vector<mod_int> vec = {1, 0};
+	long long res = fib(n, MOD);
+	assert(res == ((mat ^ n) * vec)[0]);
+	if (is_small && n > 0) {
+		mat_2_by_2 identity;
+		identity.d = {{{{1, 0}}, {{0, 1}}}};
+		disjoint_rmq<mat_2_by_2> rmq(vector<mat_2_by_2>(n + 5, mat), identity, [](const auto& x, const auto& y) {
+			return x * y;
+		});
+		for (int le = 0, ri = n; ri <= n + 5; le++, ri++) {
+			assert(res == (rmq.query(le, ri) * vec)[0]);
+		}
+	}
 }
 
 int main() {
 	for (int i = 0; i < 500; i++)
-		check(i);
-	for (int tests = 1000; tests--;) {
-		auto pw = get_rand<long long>(0, 1e18);
-		check(pw);
-	}
+		check(i, 1);
+	for (int tests = 1000; tests--;)
+		check(get_rand<long long>(0, 1e18), 0);
 	cout << "Hello World\n";
 	return 0;
 }
