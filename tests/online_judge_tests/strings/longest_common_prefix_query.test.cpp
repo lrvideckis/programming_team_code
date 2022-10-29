195c639720a85848cd7fbf71f3b680969abaa283
Luke Videckis <lukevideckis@gmail.com>
2022-10-28 19:09:14 -0700

    fix test

--- tests/online_judge_tests/strings/longest_common_prefix_query.test.cpp
+++ tests/online_judge_tests/strings/longest_common_prefix_query.test.cpp
@@ -8,7 +8,7 @@ int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	string s;
 	cin >> s;
-	lcp_query<string> sq(s);
+	lcp_query<string> sq(s, 256);
 	//test `less` function
 	{
 		for (int num_tests = 50; num_tests--;) {
