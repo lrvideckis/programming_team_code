f4fdfaa4de310e4ef6c7953ef4a477183ba1026a
Luke Videckis <lukevideckis@gmail.com>
2022-10-29 20:24:01 -0700

    add source

--- tests/scripts/find_files_not_in_pdf.sh
+++ tests/scripts/find_files_not_in_pdf.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+#source: https://unix.stackexchange.com/a/194702
+
 ! grep --only-matching --fixed-strings --file=<(find ../library/ -type f -name "*.hpp") scripts/hackpack.tex \
 	| grep --invert-match --fixed-strings --file=- <(find ../library/ -type f -name "*.hpp")
