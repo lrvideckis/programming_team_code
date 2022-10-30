093eea5c445eb9e7b74dac0740ac30b41e6a2210
Luke Videckis <lukevideckis@gmail.com>
2022-10-29 19:56:23 -0700

    use UCF fork of KACTL's SA

--- library/strings/longest_common_prefix_query.hpp
+++ library/strings/longest_common_prefix_query.hpp
@@ -1,30 +1,25 @@
 #pragma once
-#include "suffix_array.hpp"
-#include "longest_common_prefix.hpp"
+#include "../../hackpack-cpp/content/strings/SuffixArray.h"
 #include "../range_data_structures/rmq.hpp"
 //computes suffix array, lcp array, and then sparse table over lcp array
 //O(n log n)
-template <typename T> struct lcp_query {
-	const int N;
-	vector<int> sa, inv_sa, lcp;
+struct lcp_query {
+	SuffixArray info;
 	RMQ<int> rmq;
-	lcp_query(const T& s, int max_val) : N(ssize(s)), sa(suffix_array(s, max_val)), inv_sa(N), lcp(calc_lcp(s, sa)), rmq(lcp, [](int i, int j) -> int {return min(i, j);}) {
-		for (int i = 0; i < N; i++)
-			inv_sa[sa[i]] = i;
-	}
+	lcp_query(string& s) : info(SuffixArray(s)), rmq(vi(info.lcp.begin() + 1, info.lcp.end()), [](int i, int j) -> int {return min(i, j);}) {}
 	//length of longest common prefix of suffixes s[idx1 ... N), s[idx2 ... N), 0-based indexing
 	//
 	//You can check if two substrings s[l1..r1), s[l2..r2) are equal in O(1) by:
 	//r1-l1 == r2-l2 && longest_common_prefix(l1, l2) >= r1-l1
 	int get_lcp(int idx1, int idx2) const {
 		if (idx1 == idx2)
-			return N - idx1;
-		auto [le, ri] = minmax(inv_sa[idx1], inv_sa[idx2]);
+			return ssize(info.sa) - idx1;
+		auto [le, ri] = minmax(info.rank[idx1], info.rank[idx2]);
 		return rmq.query(le, ri);
 	}
 	//returns 1 if suffix s[idx1 ... N) < s[idx2 ... N)
 	//(so 0 if idx1 == idx2)
 	bool less(int idx1, int idx2) const {
-		return inv_sa[idx1] < inv_sa[idx2];
+		return info.rank[idx1] < info.rank[idx2];
 	}
 };
--- tests/online_judge_tests/strings/lcp_query_palindrome.test.cpp
+++ tests/online_judge_tests/strings/lcp_query_palindrome.test.cpp
@@ -1,5 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/enumerate_palindromes"
 #include "../template.hpp"
+#include "../kactl_macros.hpp"
 
 #include "../../../library/strings/longest_common_prefix_query.hpp"
 
@@ -9,7 +10,7 @@ int main() {
 	cin >> s;
 	int n = ssize(s);
 	s = s + '$' + string(s.rbegin(), s.rend());
-	lcp_query lcp_q(s, 256);
+	lcp_query lcp_q(s);
 	for (int i = 0; i < n; i++) {
 		for (int j = i; j < min(i + 2, n); j++)
 			cout << lcp_q.get_lcp(j, (n - i - 1) + n + 1) * 2 - (i == j) << " ";
