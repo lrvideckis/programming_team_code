53efe96364c22e6891971c80cc0c9cf8fea50e21
Luke Videckis <lukevideckis@gmail.com>
2022-11-02 03:47:27 -0700

    better script output

--- tests/scripts/parse_coverxygen.py
+++ tests/scripts/parse_coverxygen.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 import sys, json;
 data = json.load(sys.stdin)
-data = data["total"]
-assert(data["documented_symbol_count"] == data["symbol_count"])
+cnt_documented_files = data["total"]["documented_symbol_count"]
+total_files = data["total"]["symbol_count"]
+print("number of documented files: " + str(cnt_documented_files))
+print("total files: " + str(total_files))
+print("asserting these are equal")
+print(".")
+print(".")
+print(".")
+assert(cnt_documented_files == total_files)
