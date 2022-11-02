6e5d90241dba7c09f385d27265fdf727e1aab887
Luke Videckis <lukevideckis@gmail.com>
2022-11-02 06:58:44 -0700

    document ncr

--- library/math/n_choose_k_mod.hpp
+++ library/math/n_choose_k_mod.hpp
@@ -1,13 +1,18 @@
 #pragma once
-//for mod inverse
 #include "binary_exponentiation_mod.hpp"
-// usage:
-//     n_choose_k nk(n, 1e9+7) to use `choose`, `inv` with inputs strictly < n
-// or:
-//     n_choose_k nk(mod, mod) to use `choose_lucas` with arbitrarily large inputs
+/**
+ * @file
+ * @brief N Choose K
+ * @code{.cpp}
+ *     n_choose_k nk(n, 1e9+7); // to use `choose`, `inv` with inputs strictly < n
+ *     n_choose_k nk(mod, mod); // to use `choose_lucas` with arbitrarily large inputs
+ * @endcode
+ * @memory O(n) precomp
+ */
 struct n_choose_k {
 	n_choose_k(int n, long long a_mod) : mod(a_mod), fact(n, 1), inv_fact(n, 1) {
-		//this implementation doesn't work if n > mod because n! % mod = 0 when n >= mod. So `inv_fact` array will be all 0's
+		//this implementation doesn't work if n > mod because n! % mod = 0 when
+		//n >= mod. So `inv_fact` array will be all 0's
 		assert(max(n, 2) <= mod);
 		//assert mod is prime. mod is intended to fit inside an int so that
 		//multiplications fit in a longlong before being modded down. So this
@@ -20,17 +25,22 @@ struct n_choose_k {
 		for (int i = n - 2; i >= 2; i--)
 			inv_fact[i] = inv_fact[i + 1] * (i + 1) % mod;
 	}
-	//classic n choose k
-	//fails when n >= mod
+	/**
+	 * @brief Classic n choose k; fails when n >= mod.
+	 * @time O(1)
+	 */
 	long long choose(int n, int k) const {
 		if (k < 0 || k > n)
 			return 0;
 		//now we know 0 <= k <= n so 0 <= n
 		return fact[n] * inv_fact[k] % mod * inv_fact[n - k] % mod;
 	}
-	//lucas theorem to calculate n choose k in O(log(k))
-	//need to calculate all factorials in range [0,mod), so O(mod) time&space, so need smallish prime mod (< 1e6 maybe)
-	//handles n >= mod correctly
+	/**
+	 * @brief Lucas theorem - n choose k for n, k up to LLONG_MAX. Handles n >=
+	 * mod correctly.
+	 * @time O(log(k))
+	 * @memory O(mod) precomp needed, so can't use 1e9 + 7.
+	 */
 	long long choose_lucas(long long n, long long k) const {
 		if (k < 0 || k > n)
 			return 0;
@@ -38,7 +48,10 @@ struct n_choose_k {
 			return 1;
 		return choose_lucas(n / mod, k / mod) * choose(int(n % mod), int(k % mod)) % mod;
 	}
-	//returns x such that x * n % mod == 1
+	/**
+	 * @brief Returns x such that x * n % mod == 1.
+	 * @time O(1)
+	 */
 	long long inv(int n) const {
 		assert(1 <= n); //don't divide by 0 :)
 		return fact[n - 1] * inv_fact[n] % mod;
