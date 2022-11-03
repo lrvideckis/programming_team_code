495252f8f3617424723a59a6b393fb225c75fe35
Luke Videckis <lukevideckis@gmail.com>
2022-11-03 00:30:38 -0700

    fix formatting of rmq

--- library/range_data_structures/rmq.hpp
+++ library/range_data_structures/rmq.hpp
@@ -1,20 +1,18 @@
 #pragma once
 /**
- * @file
  * @brief Range Minimum Query
  * @code{.cpp}
  *     vector<long long> arr;
  *     RMQ<long long> rmq(arr, [&](auto x, auto y) { return min(x, y); });
  * @endcode
- *
  * @trick To get index of min element, see below. If there are multiple indexes
- * of min element, it'll return the smallest (left-most) one.
+ *     of min element, it'll return the smallest (left-most) one.
  * @code{.cpp}
  *     vector<pair<long long, int>> arr; //initialize arr[i].second = i
  *     RMQ<pair<long long, int>> rmq(arr, [&](auto x, auto y) { return min(x, y); });
  * @endcode
- *
- * @see https://github.com/kth-competitive-programming/ kactl/blob/main/content/data-structures/RMQ.h
+ * @see https://github.com/kth-competitive-programming/
+ *     kactl/blob/main/content/data-structures/RMQ.h
  * @time O(n log n) precomp, O(1) per query
  * @memory O(n log n)
  */
@@ -29,7 +27,10 @@ template <typename T> struct RMQ {
 				dp[i + 1][j] = op(dp[i][j], dp[i][j + (1 << i)]);
 		}
 	}
-	//inclusive-exclusive range [le, ri)
+	/**
+	 * @note Inclusive-exclusive range [le, ri).
+	 * @time O(1)
+	 */
 	T query(int le, int ri) const {
 		assert(0 <= le && le < ri && ri <= ssize(dp[0]));
 		int lg = __lg(ri - le);
