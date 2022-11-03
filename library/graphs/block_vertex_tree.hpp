ca2b80759d1733b2735f992bc5acc84d3feaf085
Luke Videckis <lukevideckis@gmail.com>
2022-11-03 02:45:02 -0700

    document BVT

--- library/graphs/block_vertex_tree.hpp
+++ library/graphs/block_vertex_tree.hpp
@@ -1,18 +1,20 @@
 #pragma once
 #include "bridges_and_cuts.hpp"
-//returns adjacency list of block vertex tree
-//usage:
-//	graph_info cc = bridge_and_cut(adj, m);
-//	vector<vector<int>> bvt = block_vertex_tree(adj, cc);
-//to loop over each *unique* bcc containing a node v:
-//	for (int bccid : bvt[v]) {
-//		bccid -= n;
-//		...
-//	}
-//to loop over each *unique* node inside a bcc:
-//	for (int v : bvt[bccid + n]) {
-//		...
-//	}
+/**
+ * @brief Returns adjacency list of block vertex tree.
+ * @code{.cpp}
+ *     graph_info cc = bridge_and_cut(adj, m);
+ *     vector<vector<int>> bvt = block_vertex_tree(adj, cc);
+ *     //to loop over each *unique* bcc containing a node v:
+ *     for (int bccid : bvt[v]) {
+ *         bccid -= n;
+ *     }
+ *     //to loop over each *unique* node inside a bcc:
+ *     for (int v : bvt[bccid + n]) {}
+ * @endcode
+ * @time O(n + m)
+ * @memory O(n + m)
+ */
 vector<vector<int>> block_vertex_tree(const vector<vector<pair<int, int>>>& adj, const graph_info& cc) {
 	int n = ssize(adj);
 	vector<vector<int>> bvt(n + cc.num_bccs);
