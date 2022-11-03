549e54b145e1bf1b03b46be0a621a5382907779b
Luke Videckis <lukevideckis@gmail.com>
2022-11-03 03:12:07 -0700

    document this

--- library/graphs/kth_node_on_path.hpp
+++ library/graphs/kth_node_on_path.hpp
@@ -1,17 +1,14 @@
 #pragma once
 #include "lca.hpp"
-//consider path {u, u's par, ..., LCA(u,v), ..., v's par, v}. This returns the node at index k
-//assumes 0 <= k <= number of edges on path from u to v
-// u, v must be in the same component
-//
-//To loop over all nodes on a path from u to v:
-//kth_node_on_path kpath(adj);
-//for (int i = 0; i <= kpath.lca.dist_edges(u, v); i++) {
-//	int node = kpath.query(u, v, i);
-//	...
 struct kth_node_on_path {
 	LCA lca;
 	kth_node_on_path(const vector<vector<pair<int, long long>>>& adj/*forest of weighted trees*/) : lca(adj) {}
+	/**
+	 * @brief Consider path {u, u's par, ..., LCA(u,v), ..., v's par, v}. This
+	 *     returns the node at index k. k=0 returns u; k=#path_edges returns v.
+	 * @note u, v must be in the same component.
+	 * @time O(log n)
+	 */
 	int query(int u, int v, int k) const {
 		int lca_uv = lca.get_lca(u, v);
 		int u_lca = lca.tree[u].depth - lca.tree[lca_uv].depth;
