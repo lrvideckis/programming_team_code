c64664384222ef23493343dc57b93c8bc97ab03b
Luke Videckis <lukevideckis@gmail.com>
2022-11-03 00:49:12 -0700

    document pal query

--- library/strings/palindrome_query.hpp
+++ library/strings/palindrome_query.hpp
@@ -3,9 +3,16 @@
 struct pal_query {
 	const int N;
 	array<vi, 2> pal_len;
+	/**
+	 * @time O(n)
+	 * @memory O(n)
+	 */
 	pal_query(const string& s) : N(ssize(s)), pal_len(manacher(s)) {}
-	//returns 1 if substring s[le...ri) is a palindrome
-	//(returns 1 when le == ri)
+	/**
+	 * @brief Returns 1 if substring s[le...ri) is a palindrome. Returns 1 when
+	 * le == ri.
+	 * @time O(1)
+	 */
 	bool is_pal(int le, int ri) const {
 		assert(0 <= le && le <= ri && ri <= N);
 		int len = ri - le;
