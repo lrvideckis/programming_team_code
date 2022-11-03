b44805e887b03695acdb7a7505c8bc17e92757a3
Luke Videckis <lukevideckis@gmail.com>
2022-11-03 01:02:38 -0700

    document sa query

--- library/strings/suffix_array_query.hpp
+++ library/strings/suffix_array_query.hpp
@@ -37,30 +37,42 @@ struct sa_query {
 	string s;
 	SuffixArray info;
 	RMQ<int> rmq_lcp, rmq_sa;
+	/**
+	 * @time O(n log n)
+	 * @memory O(n log n) - because of RMQ
+	 */
 	sa_query(string& a_s) :
 		s(a_s),
 		info(SuffixArray(s)),
 		rmq_lcp(vi(info.lcp.begin() + 1, info.lcp.end()), [](int i, int j) -> int {return min(i, j);}),
 		rmq_sa(info.sa, [](int i, int j) -> int {return min(i, j);}) {}
-	//length of longest common prefix of suffixes s[idx1 ... N), s[idx2 ... N), 0-based indexing
-	//
-	//You can check if two substrings s[l1..r1), s[l2..r2) are equal in O(1) by:
-	//r1-l1 == r2-l2 && longest_common_prefix(l1, l2) >= r1-l1
+	/**
+	 * @brief Returns length of longest common prefix of suffixes s[idx1...N),
+	 *     s[idx2...N), 0-based indexing.
+	 * @trick To check if two substrings s[l1..r1), s[l2..r2) are equal:
+	 *     r1-l1 == r2-l2 && longest_common_prefix(l1, l2) >= r1-l1
+	 * @time O(1)
+	 */
 	int get_lcp(int idx1, int idx2) const {
 		if (idx1 == idx2)
 			return ssize(s) - idx1;
 		auto [le, ri] = minmax(info.rank[idx1], info.rank[idx2]);
 		return rmq_lcp.query(le, ri);
 	}
-	//returns 1 if suffix s[idx1 ... N) < s[idx2 ... N)
-	//(so 0 if idx1 == idx2)
+	/**
+	 * @brief Returns 1 if suffix s[idx1 ... N) < s[idx2 ... N) (so 0 if
+	 *     idx1 == idx2).
+	 * @time O(1)
+	 */
 	bool less(int idx1, int idx2) const {
 		return info.rank[idx1] < info.rank[idx2];
 	}
-	//returns range [le, ri) such that:
-	//	- for all i in [le, ri): t == s.substr(info.sa[i], ssize(t))
-	//	- `ri - le` is the # of matches of t in s
-	//O(|t| * log(|s|))
+	/**
+	 * @brief Returns range [le, ri) such that:
+	 *   - for all i in [le, ri): t == s.substr(info.sa[i], ssize(t))
+	 *   - `ri - le` is the # of matches of t in s is okay.
+	 * @time O(|t| * log(|s|))
+	 */
 	pair<int, int> find(const string& t) const {
 		auto cmp = [&](int i, int cmp_val) -> bool {
 			return s.compare(i, ssize(t), t) < cmp_val;
@@ -69,9 +81,12 @@ struct sa_query {
 		auto ri = lower_bound(le, info.sa.end(), 1, cmp);
 		return {le - info.sa.begin(), ri - info.sa.begin()};
 	}
-	//returns min i such that t == s.substr(i, ssize(t)) or -1
-	//for example, replace RMQ with kth-smallest PST/Wavelet to solve https://open.kattis.com/problems/anothersubstringqueryproblem
-	//O(|t| * log(|s|))
+	/**
+	 * @brief Returns min i such that t == s.substr(i, ssize(t)) or -1. For
+	 *     example, replace RMQ with kth-smallest PST/Wavelet to solve
+	 *     https://open.kattis.com/problems/anothersubstringqueryproblem
+	 * @time O(|t| * log(|s|))
+	 */
 	int find_first(const string& t) const {
 		auto [le, ri] = find(t);
 		if (le == ri)
