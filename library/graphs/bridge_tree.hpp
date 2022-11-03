a71cace0a0bd0fa81f28fb36d68710b3e68c1cf3
Luke Videckis <lukevideckis@gmail.com>
2022-11-03 02:47:28 -0700

    document bridge tree

--- library/graphs/bridge_tree.hpp
+++ library/graphs/bridge_tree.hpp
@@ -1,9 +1,16 @@
 #pragma once
 #include "bridges_and_cuts.hpp"
-//never adds multiple edges as bridges_and_cuts.hpp correctly marks them as non-bridges
-//usage:
-//	graph_info cc = bridge_and_cut(adj, m);
-//	vector<vector<int>> bt = bridge_tree(adj, cc);
+
+/**
+ * @note Never adds multiple edges as bridges_and_cuts.hpp correctly marks them
+ *     as non-bridges.
+ * @code{.cpp}
+ *     graph_info cc = bridge_and_cut(adj, m);
+ *     vector<vector<int>> bt = bridge_tree(adj, cc);
+ * @endcode
+ * @time O(n + m)
+ * @memory O(n + m)
+ */
 vector<vector<int>> bridge_tree(const vector<vector<pair<int, int>>>& adj, const graph_info& cc) {
 	vector<vector<int>> tree(cc.num_2_edge_ccs);
 	for (int i = 0; i < ssize(adj); i++)
