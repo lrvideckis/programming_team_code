aa5de1a4dfb154ff4e20f11b71c662f809ab1b6b
Luke Videckis <lukevideckis@gmail.com>
2022-11-04 03:47:47 -0700

    shortened

--- library/math/binary_exponentiation_mod.hpp
+++ library/math/binary_exponentiation_mod.hpp
@@ -1,14 +1,8 @@
 #pragma once
-//returns (base^pw)%mod in O(log(pw)), but returns 1 for 0^0
-//
-//What if base doesn't fit in long long?
-//Since (base^pw)%mod == ((base%mod)^pw)%mod we can calculate base under mod of `mod`
-//
-//What if pw doesn't fit in long long?
-//assuming mod is prime:
-//(base^pw)%mod == (base^(pw%(mod-1)))%mod (from Fermat's little theorem)
-//so calculate pw under mod of `mod-1`
-//note `mod-1` is not prime, so you need to be able to calculate `pw%(mod-1)` without division
+/**
+ * Returns (base^pw)%mod; returns 1 for 0^0.
+ * @time O(log pw)
+ */
 long long bin_exp(long long base, long long pw, long long mod) {
 	assert(0 <= pw && 0 <= base && 1 <= mod);
 	long long res = 1;
