746c48b45018f0ed9a5587c916a5e6503b1b0c5c
Luke Videckis <lukevideckis@gmail.com>
2022-11-04 20:02:09 -0700

    saving this in branch to help debug

--- tests/scripts/hackpack.sh
+++ tests/scripts/hackpack.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-#adds hash code comments
-tests=$(find ../library/ -name "*.hpp")
-for test in $tests
-do
-	hash=$(cat $test | ../library/contest/hash.sh)
-	comment="cat $(basename $test) | ./hash.sh"
-	sed --in-place "1s;^;//$comment\n//$hash\n;" $test
-done
+#takes in file name
+remove_pragma_add_hash() {
+	sed --in-place '/#pragma once/d' $1
+
+	hash=$(cat $1 | ../library/contest/hash.sh)
+	comment="cat $(basename $1) | ./hash.sh"
+	sed --in-place "1s;^;//$comment\n//$hash\n;" $1
+}
+find ../library/ -type f -name "*.hpp" -exec remove_pragma_add_hash {} \;
 
 #needed to make boxes under headings empty, otherwise you get filler text
 touch NULL
@@ -19,10 +20,11 @@ pdflatex scripts/hackpack.tex
 
 mv hackpack.pdf ../
 
-#removes hash code comments
-for test in $tests
-do
-	sed --in-place '1,2d' $test
-done
+#takes in file name
+remove_hash_add_pragma() {
+	sed --in-place '1,2d' $1
+	sed --in-place '1 i\#pragma once' $1
+}
+find ../library/ -type f -name "*.hpp" -exec remove_hash_add_pragma {} \;
 
 rm NULL hackpack.aux hackpack.lol hackpack.log
