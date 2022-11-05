f85eb056344a952973847acf947f34e75ae4beed
Luke Videckis <lukevideckis@gmail.com>
2022-11-04 22:09:14 -0700

    Hackpack script nit #288 (#289)
    * saving this in branch to help debug
    
    * nit
    
    * seeing if this works

--- tests/scripts/hackpack.sh
+++ tests/scripts/hackpack.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+#remove pragma once
+find ../library/ -type f -name "*.hpp" | xargs \
+	sed --in-place '/#pragma once/d'
+
 #adds hash code comments
-tests=$(find ../library/ -name "*.hpp")
-for test in $tests
+for test in $(find ../library/ -type f -name "*.hpp")
 do
 	hash=$(cat $test | ../library/contest/hash.sh)
 	comment="cat $(basename $test) | ./hash.sh"
@@ -19,10 +22,8 @@ pdflatex scripts/hackpack.tex
 
 mv hackpack.pdf ../
 
-#removes hash code comments
-for test in $tests
-do
-	sed --in-place '1,2d' $test
-done
+#removes hash code comments and adds back pragma once
+find ../library/ -type f -name "*.hpp" | xargs \
+	sed --in-place --expression='1,2d' --expression='3i\#pragma once'
 
 rm NULL hackpack.aux hackpack.lol hackpack.log
