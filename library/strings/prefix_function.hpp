8619899f9c4756cbf4be4d7967061ac9e81866be
Luke Videckis <lukevideckis@gmail.com>
2022-11-14 22:16:12 -0800

    I actually like this style better

--- library/strings/prefix_function.hpp
+++ library/strings/prefix_function.hpp
@@ -5,9 +5,10 @@
  */
 template <typename T> vector<int> prefix_function(const T& s) {
 	vector<int> pi(ssize(s), 0);
-	transform(pi.begin(), pi.end() - 1, s.begin() + 1, pi.begin() + 1, [&](int j, auto val) {
-		while (j > 0 && val != s[j]) j = pi[j - 1];
-		return j + (val == s[j]);
-	});
+	for (int i = 1; i < ssize(s); i++) {
+		int j = pi[i - 1];
+		while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+		pi[i] = j + (s[i] == s[j]);
+	}
 	return pi;
 }
