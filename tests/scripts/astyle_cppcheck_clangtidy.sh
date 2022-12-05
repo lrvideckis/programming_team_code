2089a583cf18bdc2f83fe6b45bd3a3a3c032748e
Luke Videckis <lukevideckis@gmail.com>
2022-12-04 18:22:51 -0800

    70 was actually a test

--- tests/scripts/astyle_cppcheck_clangtidy.sh
+++ tests/scripts/astyle_cppcheck_clangtidy.sh
@@ -17,7 +17,7 @@ find ../library/ online_judge_tests/ -name "*[A-Z]*" -or -name "*-*" \
 	| grep --invert-match "README" \
 	&& exit 1
 
-WORD_LENGTH_THRESHOLD=70
+WORD_LENGTH_THRESHOLD=80
 echo "The following words are > $WORD_LENGTH_THRESHOLD characters, and won't wrap in PDF:"
 find ../library/ -type f -name "*.hpp" \
 	| xargs cat \
