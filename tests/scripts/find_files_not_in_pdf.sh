534ad88f86a3d74c4826cd45d677ebf522ee8235
Luke Videckis <lukevideckis@gmail.com>
2022-12-05 12:17:35 -0800

    don't need this script now that we enforce that the latex building of PDF succeeds

--- tests/scripts/find_files_not_in_pdf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#source: https://unix.stackexchange.com/a/194702
-
-! grep --only-matching --fixed-strings --file=<(find ../library/ -type f -name "*.hpp") scripts/hackpack.tex \
-	| grep --invert-match --fixed-strings --file=- <(find ../library/ -type f -name "*.hpp")
