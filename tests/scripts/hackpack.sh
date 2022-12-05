0eabacf707e8d269215ae14c8cf478252803e5f5
Luke Videckis <lukevideckis@gmail.com>
2022-12-05 12:38:43 -0800

    now pdflatex gives non-zero error code which makes CI fail

--- tests/scripts/hackpack.sh
+++ tests/scripts/hackpack.sh
@@ -13,8 +13,8 @@ touch NULL
 
 #need to compile twice for it to generate the table of contents
 #I think it's related to not knowing page numbers until after first pdf generation
-pdflatex scripts/hackpack.tex
-pdflatex scripts/hackpack.tex
+pdflatex -halt-on-error scripts/hackpack.tex
+pdflatex -halt-on-error scripts/hackpack.tex
 
 mv hackpack.pdf ../
 
