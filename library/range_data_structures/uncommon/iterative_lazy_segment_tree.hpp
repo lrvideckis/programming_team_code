6d81c9a0b0edd470ee68e9c9106842649a7f495d
Luke Videckis <lukevideckis@gmail.com>
2023-03-04 11:43:00 -0600

    golf

--- library/range_data_structures/uncommon/iterative_lazy_segment_tree.hpp
+++ library/range_data_structures/uncommon/iterative_lazy_segment_tree.hpp
@@ -24,14 +24,6 @@ struct iter_seg_tree {
 		return {(x >= 2 * st.N ? (x >> 1) + st.N : x) - S, (y >= 2 * st.N ? (y >> 1) + st.N : y) - S};
 	}
 
-	int get_node_split(int a) const {//TODO: prefer my split over this
-		assert(1 <= a && a < st.N);
-		int l = __builtin_clz(2 * a + 1) - __builtin_clz(2 * st.N - 1);
-		int x = (2 * a + 1) << l;
-		assert(S <= x && x < 2 * S);
-		return (x >= 2 * st.N ? (x >> 1) + st.N : x) - S;
-	}
-
 
 	int to_leaf(int i) const {
 		i += S;
@@ -44,11 +36,23 @@ struct iter_seg_tree {
 		assert(0 <= le && le <= ri && ri <= st.N);
 		le = to_leaf(le), ri = to_leaf(ri);
 		int lca_l_r = __lg((le - 1) ^ ri);
-		for (int lg = __lg(le); lg > __builtin_ctz(le); lg--) st.push(le >> lg, get_node_bounds(le >> lg)[0], get_node_split(le >> lg), get_node_bounds(le >> lg)[1]);
-		for (int lg = lca_l_r; lg > __builtin_ctz(ri); lg--) st.push(ri >> lg, get_node_bounds(ri >> lg)[0], get_node_split(ri >> lg), get_node_bounds(ri >> lg)[1]);
+		for (int lg = __lg(le); lg > __builtin_ctz(le); lg--) {
+			auto [tl, tr] = get_node_bounds(le >> lg);
+			st.push(le >> lg, tl, split(tl, tr), tr);
+		}
+		for (int lg = lca_l_r; lg > __builtin_ctz(ri); lg--) {
+			auto [tl, tr] = get_node_bounds(ri >> lg);
+			st.push(ri >> lg, tl, split(tl, tr), tr);
+		}
 		for (int x = le, y = ri; x < y; x >>= 1, y >>= 1) {
-			if (x & 1) st.apply(x, get_node_bounds(x)[0], get_node_bounds(x)[1], change), x++;
-			if (y & 1) y--, st.apply(y, get_node_bounds(y)[0], get_node_bounds(y)[1], change);
+			if (x & 1) {
+				auto [tl, tr] = get_node_bounds(x);
+				st.apply(x++, tl, tr, change);
+			}
+			if (y & 1) {
+				auto [tl, tr] = get_node_bounds(--y);
+				st.apply(y, tl, tr, change);
+			}
 		}
 		for (int lg = __builtin_ctz(ri) + 1; lg <= lca_l_r; lg++) st.pull(ri >> lg);
 		for (int lg = __builtin_ctz(le) + 1; lg <= __lg(le); lg++) st.pull(le >> lg);
@@ -60,8 +64,14 @@ struct iter_seg_tree {
 		assert(0 <= le && le <= ri && ri <= st.N);
 		le = to_leaf(le), ri = to_leaf(ri);
 		int lca_l_r = __lg((le - 1) ^ ri);
-		for (int lg = __lg(le); lg > __builtin_ctz(le); lg--) st.push(le >> lg, get_node_bounds(le >> lg)[0], get_node_split(le >> lg), get_node_bounds(le >> lg)[1]);
-		for (int lg = lca_l_r; lg > __builtin_ctz(ri); lg--) st.push(ri >> lg, get_node_bounds(ri >> lg)[0], get_node_split(ri >> lg), get_node_bounds(ri >> lg)[1]);
+		for (int lg = __lg(le); lg > __builtin_ctz(le); lg--) {
+			auto [tl, tr] = get_node_bounds(le >> lg);
+			st.push(le >> lg, tl, split(tl, tr), tr);
+		}
+		for (int lg = lca_l_r; lg > __builtin_ctz(ri); lg--) {
+			auto [tl, tr] = get_node_bounds(ri >> lg);
+			st.push(ri >> lg, tl, split(tl, tr), tr);
+		}
 		long long resl = 0, resr = 0;
 		for (; le < ri; le >>= 1, ri >>= 1) {
 			if (le & 1) resl = op(resl, st.tree[le++]);
