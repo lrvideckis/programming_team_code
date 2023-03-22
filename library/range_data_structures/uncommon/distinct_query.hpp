ac3c4fb23e678e71b32b4d36e65d10fbf9e3aab7
Luke Videckis <lukevideckis@gmail.com>
2023-03-22 11:31:55 -0500

    Atcoder param order (#307)
    * atcoder param order
    
    * [auto-verifier] verify commit 016b5ea0848236d6048d33356a2f5c2e50d046cb
    
    * switch this
    
    * [auto-verifier] verify commit 7bc5df9c00ae1a0865678dc1a6b0b80b654b62be
    
    * [auto-verifier] verify commit ba4d8c81293cc0ffe7187bfea70c1cd9f1cf8238
    
    * ditto
    
    * [auto-verifier] verify commit 0d5e65b015709ea0655c49ab0590ab381b306f32
    
    * nits
    
    * [auto-verifier] verify commit 19e283f80af849bce3a4e6c623693db0c98831fa
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/range_data_structures/uncommon/distinct_query.hpp
+++ library/range_data_structures/uncommon/distinct_query.hpp
@@ -22,11 +22,11 @@ struct distinct_query {
 		tree.emplace_back(0, 0, 0); //acts as null
 		map<int, int> last_idx;
 		for (int i = 0; i < N; i++) {
-			roots[i + 1] = update_impl(roots[i], 0, N, last_idx[arr[i]]);
+			roots[i + 1] = update_impl(last_idx[arr[i]], 0, N, roots[i]);
 			last_idx[arr[i]] = i + 1;
 		}
 	}
-	int update_impl(int v, int tl, int tr, int idx) {
+	int update_impl(int idx, int tl, int tr, int v) {
 		if (tr - tl == 1) {
 			tree.emplace_back(tree[v].sum + 1, 0, 0);
 			return ssize(tree) - 1;
@@ -34,8 +34,10 @@ struct distinct_query {
 		int tm = tl + (tr - tl) / 2;
 		int lch = tree[v].lch;
 		int rch = tree[v].rch;
-		if (idx < tm) lch = update_impl(lch, tl, tm, idx);
-		else rch = update_impl(rch, tm, tr, idx);
+		if (idx < tm)
+			lch = update_impl(idx, tl, tm, lch);
+		else
+			rch = update_impl(idx, tm, tr, rch);
 		tree.emplace_back(tree[lch].sum + tree[rch].sum, lch, rch);
 		return ssize(tree) - 1;
 	}
@@ -46,13 +48,14 @@ struct distinct_query {
 	 */
 	int query(int le, int ri) const {
 		assert(0 <= le && le <= ri && ri <= N);
-		return query_impl(roots[le], roots[ri], 0, N, le + 1);
+		return query_impl(le + 1, 0, N, roots[le], roots[ri]);
 	}
-	int query_impl(int vl, int vr, int tl, int tr, int idx) const {
+	int query_impl(int idx, int tl, int tr, int vl, int vr) const {
 		if (tree[vr].sum == 0 || idx <= tl) return 0;
 		if (tr <= idx) return tree[vr].sum - tree[vl].sum;
 		int tm = tl + (tr - tl) / 2;
-		return query_impl(tree[vl].lch, tree[vr].lch, tl, tm, idx) +
-			query_impl(tree[vl].rch, tree[vr].rch, tm, tr, idx);
+		return
+			query_impl(idx, tl, tm, tree[vl].lch, tree[vr].lch) +
+			query_impl(idx, tm, tr, tree[vl].rch, tree[vr].rch);
 	}
 };
