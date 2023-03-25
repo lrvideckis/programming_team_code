8d3f5942e9bd6143118bc62994aa22bfc4173df1
Luke Videckis <lukevideckis@gmail.com>
2023-03-25 12:52:14 -0500

    revert

--- library/range_data_structures/lazy_segment_tree.hpp
+++ library/range_data_structures/lazy_segment_tree.hpp
@@ -1,7 +1,7 @@
 /** @file */
 #pragma once
 /**
- * @see https://codeforces.com/blog/entry/112755ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+ * @see https://codeforces.com/blog/entry/112755
  * @param tl,tr defines range [tl, tr)
  * @returns split point of range which makes the segment tree a complete
  * binary tree
