af707f4c8e513874a9ce259edf07697e181b7287
Luke Videckis <lukevideckis@gmail.com>
2023-03-25 14:45:44 -0500

    nits

--- tests/scripts/format_and_check_bash_scripts.sh
+++ tests/scripts/format_and_check_bash_scripts.sh
@@ -16,6 +16,19 @@ comm -23 --check-order <(
 	grep . &&
 	exit 1
 
-shellcheck --shell=bash --check-sourced --enable=check-set-e-suppressed,quote-safe-variables ./library/**/*.sh || exit 1
+echo "bash scripts in tests/scripts/ missing set -euo pipefail:"
+comm -23 --check-order <(
+	find tests/scripts/ -type f -name "*.sh" |
+		sort |
+		uniq
+) <(
+	grep --recursive --fixed-strings --files-with-matches "set -euo pipefail" tests/scripts/ |
+		sort |
+		uniq
+) |
+	grep . &&
+	exit 1
 
-shfmt -ln=bash -s -d -w ./tests/**/*.sh || exit 1
+shellcheck --shell=bash --check-sourced --enable=check-set-e-suppressed,quote-safe-variables ./library/**/*.sh ./tests/**/*.sh || exit 1
+
+shfmt -ln=bash -s -d -w ./library/**/*.sh ./tests/**/*.sh || exit 1
