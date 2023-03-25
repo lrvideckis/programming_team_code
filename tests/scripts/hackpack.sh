ec5a9d8d3a3c1e02d43131b689b618a2f0bbd5f6
Luke Videckis <lukevideckis@gmail.com>
2023-03-24 21:20:10 -0500

    fix more warnings

--- tests/scripts/hackpack.sh
+++ tests/scripts/hackpack.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 #adds hash code comments
 for header in $(find ../library/ -type f -name "*.hpp"); do
-	hash=$(cat $header | ../library/contest/hash.sh)
-	comment="cat $(basename $header) | ./hash.sh"
-	sed --in-place "1s;^;//$comment\n//$hash\n;" $header
+	hash=$(../library/contest/hash.sh < "$header")
+	comment="cat $(basename "$header") | ./hash.sh"
+	sed --in-place "1s;^;//$comment\n//$hash\n;" "$header"
 done
 
 #needed to make boxes under headings empty, otherwise you get filler text
