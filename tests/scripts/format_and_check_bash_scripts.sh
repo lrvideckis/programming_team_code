81939ebc558db5f584cb746d73b7b1b90b0fc31c
Luke Videckis <lukevideckis@gmail.com>
2023-03-24 20:42:04 -0500

    correct flag

--- tests/scripts/format_and_check_bash_scripts.sh
+++ tests/scripts/format_and_check_bash_scripts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-shellcheck --shell bash scripts/*.sh || exit 1
+shellcheck --shell=bash scripts/*.sh || exit 1
 
 find ../library/ scripts/ -type f -name "*.sh" |
 	xargs shfmt -ln=bash -s -d -w
