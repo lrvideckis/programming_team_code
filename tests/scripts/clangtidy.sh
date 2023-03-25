a5abc96268649356ecba1658452482d44d72ca3f
Luke Videckis <lukevideckis@gmail.com>
2023-03-25 12:11:29 -0500

    not init git submodules before clangtidy

--- tests/scripts/clangtidy.sh
+++ tests/scripts/clangtidy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+git submodule init
+git submodule update
+
 find online_judge_tests/ -type f -name "*.test.cpp" |
 	parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=c++17 ||
 	exit 1
