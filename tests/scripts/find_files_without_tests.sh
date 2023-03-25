db7b46cdecaaaf72f50b989cf68c07970dbcb294
Luke Videckis <lukevideckis@gmail.com>
2023-03-25 15:02:07 -0500

    use globs to avoid find+xargs

--- tests/scripts/find_files_without_tests.sh
+++ tests/scripts/find_files_without_tests.sh
@@ -6,13 +6,11 @@ git submodule update
 
 echo ".hpp files included by no .test.cpp file:"
 comm -23 --check-order <(
-	find ../library/ -type f -name "*.hpp" -print0 |
-		xargs basename --multiple |
+	basename --multiple ../library/**/*.hpp |
 		sort |
 		uniq
 ) <(
-	find online_judge_tests/ -type f -name "*.test.cpp" -print0 |
-		xargs g++ -MM |
+	g++ -MM online_judge_tests/**/*.test.cpp |
 		xargs basename --multiple |
 		grep "\.hpp$" |
 		sort |
