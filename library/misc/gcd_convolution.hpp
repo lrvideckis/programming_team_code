a447b285caa161fe176568432fb6812d3a826de2
Cameron Custer <73217097+cameroncuster@users.noreply.github.com>
2023-03-29 13:24:55 -0400

    fix gcd convolution variable name conflict (#318)
    * fix gcd convolution variable name conflict
    
    * [auto-verifier] verify commit 116d44af8d362303867cadf4cc5b4485ba64161d
    
    ---------
    
    Co-authored-by: Cameron Custer <ccuster@philadelphia.ptl.ucf.edu>
    Co-authored-by: GitHub <noreply@github.com>

--- library/misc/gcd_convolution.hpp
+++ library/misc/gcd_convolution.hpp
@@ -11,13 +11,13 @@ template<int MOD> vector<int> gcd_convolution(const vector<int>& a, const vector
 	assert(ssize(a) == ssize(b));
 	int n = ssize(a);
 	vector<int> c(n);
-	for (int gcd = n - 1; gcd >= 1; gcd--) {
+	for (int g = n - 1; g >= 1; g--) {
 		int sum_a = 0, sum_b = 0;
-		for (int i = gcd; i < n; i += gcd) {
+		for (int i = g; i < n; i += g) {
 			sum_a = (sum_a + a[i]) % MOD, sum_b = (sum_b + b[i]) % MOD;
-			c[gcd] = (c[gcd] - c[i] + MOD) % MOD;
+			c[g] = (c[g] - c[i] + MOD) % MOD;
 		}
-		c[gcd] = int((c[gcd] + 1LL * sum_a * sum_b) % MOD);
+		c[g] = int((c[g] + 1LL * sum_a * sum_b) % MOD);
 	}
 	return c;
 }
