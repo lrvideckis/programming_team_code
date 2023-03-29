3a6fadca8d36b7870f0d079e0a934748f7aa5a7f
Cameron Custer <73217097+cameroncuster@users.noreply.github.com>
2023-03-29 13:43:24 -0400

    LCM Convolution (#317)
    * create lcm convolution
    
    * [auto-verifier] verify commit da31dced8528227d2c61384dcf84d6a01556aa0a
    
    * fix CI
    
    * [auto-verifier] verify commit 364bdb1c3a72fdcef24b47548faba09dbeaf5169
    
    ---------
    
    Co-authored-by: Cameron Custer <ccuster@philadelphia.ptl.ucf.edu>
    Co-authored-by: GitHub <noreply@github.com>
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>

---
+++ tests/online_judge_tests/misc/lcm_convolution.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/lcm_convolution"
+#include "../template.hpp"
+
+#include "../../../library/misc/lcm_convolution.hpp"
+
+istream& operator >> (istream& is, vector<int>& v) {
+	for (int i = 1; i < ssize(v); i++)
+		is >> v[i];
+	return is;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	const int MOD = 998'244'353;
+	int n;
+	cin >> n;
+	vector<int> a(n + 1), b(n + 1);
+	cin >> a >> b;
+	auto c = lcm_convolution<MOD>(a, b);
+	for (int i = 1; i <= n; i++)
+		cout << c[i] << ' ';
+	cout << '\n';
+	return 0;
+}
