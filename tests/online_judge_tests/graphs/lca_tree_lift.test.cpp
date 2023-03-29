122b885b512c7b2eab0b38854b4cae9c897f6be3
Luke Videckis <lukevideckis@gmail.com>
2023-03-29 14:37:38 -0500

    ran astyle

--- tests/online_judge_tests/graphs/lca_tree_lift.test.cpp
+++ tests/online_judge_tests/graphs/lca_tree_lift.test.cpp
@@ -8,14 +8,14 @@ int main() {
 	int n, q;
 	cin >> n >> q;
 	vector<vector<int>> adj(n);
-	for(int i = 1; i < n; i++) {
+	for (int i = 1; i < n; i++) {
 		int par;
 		cin >> par;
 		adj[i].push_back(par);
 		adj[par].push_back(i);
 	}
 	tree_lift lift(adj);
-	while(q--) {
+	while (q--) {
 		int u, v;
 		cin >> u >> v;
 		cout << lift.lca(u, v) << '\n';
