bdb3f12cd2ec8566aaca4be5c796d89154a7eec1
Luke Videckis <lukevideckis@gmail.com>
2023-03-31 18:42:06 -0500

    Pq ds undo (#327)
    * first draft
    
    * fix some CI errors
    
    * [auto-verifier] verify commit 3f2015f405ed30b73ae2a1c9241572d018e6723b
    
    * made some progress
    
    * didn't mean to commit this
    
    * it now compiles
    
    * it now ACs
    
    * [auto-verifier] verify commit 253883e2e75676e8ccc88075bbcdf4c364e29d59
    
    * break up >80 char word
    
    * add pragma once
    
    * formatted with astyle
    
    * [auto-verifier] verify commit da64caf0ee5cbe40a0a2d31496cad30ee59b9e9d
    
    * better representation of an update in DSU
    
    * nit
    
    * vector is faster
    
    * nit
    
    * [auto-verifier] verify commit 102b90e348377c0186e87d34aed7fe9673479d04
    
    * cleaning up todos
    
    * correct comment now that I switched to map
    
    * [auto-verifier] verify commit 2c8fc04efa36d0419efb207ff34c9d6785059497
    
    * shorten template name
    
    * [auto-verifier] verify commit 3d38d586d4977e89e973998cd0681f63a934e11b
    
    * remove extra space
    
    * [auto-verifier] verify commit 0ecbe6dbfb5b1c911c28b51d558e637bc6b8b79c
    
    * added file doxygen tag
    
    * [auto-verifier] verify commit a8e2d644818480989cebaf13ca285052f13af56d
    
    * naming nits
    
    * [auto-verifier] verify commit f3d1b592be00a4ea7399246d0e4b185e3e63aa1a
    
    * [auto-verifier] verify commit 61a0f7f0e7d4f6ed3e64cc3241c03a3953ebef2e
    
    * nits
    
    * [auto-verifier] verify commit c6998aed2ebfdc35042e7c1b92c53b6c64c1d488
    
    * [auto-verifier] verify commit bf0dcb0c84cc8d23153dd980a3cec715cb910099
    
    * fix merge in timestamps file
    
    * ran astyle
    
    * more naming nits
    
    * nits
    
    * [auto-verifier] verify commit a7e18394c5ff333c5320d79da95fc7d638aba928
    
    * add doxygen to pq updates
    
    * [auto-verifier] verify commit 0d845a7774654b9fbbeeed655dbbb6486556d46d
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/data_structures/priority_queue_of_updates.hpp
@@ -0,0 +1,60 @@
+/** @file */
+#pragma once
+/**
+ * @see https://codeforces.com/blog/entry/111117
+ * @code{.cpp}
+ *     pq_updates<dsu_restorable, int, int> pq{dsu_restorable(n)};
+ *     for (int i = 0; i < n; i++) pq.ds.add(i, initial_values[i]);
+ *     pq.push_update(u, v, curr_pri);
+ *     cout << pq.ds.sum(v) << '\n';
+ * @endcode
+ * @time n interweaved calls to pop_update, push_update take O(T(n)*nlogn)
+ * where O(T(n)) = time complexity of DS::update and DS::undo
+ */
+template <typename DS, typename... ARGS> struct pq_updates {
+	DS ds; /**< arbitrary data structure */
+	using upd = pair<tuple<ARGS...>, map<int, int>::iterator>; /**< update */
+	vector<upd> upd_st; /**< stack of updates */
+	map<int, int> mp; /**< priority -> index into update stack */
+	/**
+	 * @param a_ds any data structure with member functions `update` and `undo`
+	 */
+	pq_updates(const DS& a_ds) : ds(a_ds) {}
+	/**
+	 * Remove update with max priority
+	 * @time O(log(n) + k*T(n)) where k = # of pops off the update stack
+	 */
+	void pop_update() {
+		assert(!upd_st.empty());
+		vector<upd> extra;
+		int idx = ssize(upd_st) - 1, lowest_pri = INT_MAX;
+		for (auto it = mp.rbegin(); 2 * ssize(extra) < ssize(upd_st) - idx; it++) {
+			int idx_sk = it->second;
+			extra.push_back(upd_st[idx_sk]);
+			idx = min(idx, idx_sk);
+			lowest_pri = it->first;
+		}
+		auto it = remove_if(upd_st.begin() + idx, upd_st.end(), [&](const auto & curr) {
+			return curr.second->first >= lowest_pri;
+		});
+		reverse_copy(extra.begin(), extra.end(), it);
+		for (int i = idx; i < ssize(upd_st); i++) ds.undo();
+		upd_st.pop_back();
+		mp.erase(prev(mp.end()));
+		for (int i = idx; i < ssize(upd_st); i++) {
+			apply(&DS::update, tuple_cat(make_tuple(&ds), upd_st[i].first));
+			upd_st[i].second->second = i;
+		}
+	}
+	/**
+	 * @param args arguments to DS::update
+	 * @param priority associated with the update
+	 * @time O(log(n) + T(n))
+	 */
+	void push_update(ARGS... args, int priority) {
+		ds.update(args...);
+		auto [it, ins] = mp.emplace(priority, ssize(upd_st));
+		assert(ins);//enforce distinct priorities
+		upd_st.emplace_back(make_tuple(args...), it);
+	}
+};
---
+++ tests/online_judge_tests/data_structures/pq_ds_undo_with_dsu.test.cpp
@@ -0,0 +1,75 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/dynamic_graph_vertex_add_component_sum"
+//since _GLIBCXX_DEBUG causes std::map insert/erase operations to be O(n)
+#undef _GLIBCXX_DEBUG
+#include "../template.hpp"
+
+#include "../../../library/data_structures/dsu_restorable.hpp"
+#include "../../../library/data_structures/priority_queue_of_updates.hpp"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int n, q;
+	cin >> n >> q;
+	vector<int> initial_values(n);
+	for (int i = 0; i < n; i++) cin >> initial_values[i];
+	vector<int> time_remove(q, -1);
+	struct query {
+		int type, u, v, x;
+	};
+	vector<query> queries(q);
+	{
+		map<pair<int, int>, int> insert_time;
+		for (int i = 0; i < q; i++) {
+			int type;
+			cin >> type;
+			if (type == 0) {
+				int u, v;
+				cin >> u >> v;
+				if (u > v) swap(u, v);
+				assert(!insert_time.count({u, v}));
+				insert_time[ {u, v} ] = i;
+				queries[i] = {type, u, v, -1};
+			} else if (type == 1) {
+				int u, v;
+				cin >> u >> v;
+				if (u > v) swap(u, v);
+				assert(insert_time.count({u, v}));
+				time_remove[insert_time[ {u, v} ]] = i;
+				insert_time.erase({u, v});
+				queries[i] = {type, -1, -1, -1};
+			} else if (type == 2) {
+				int v, x;
+				cin >> v >> x;
+				queries[i] = {type, -1, v, x};
+			} else {
+				assert(type == 3);
+				int v;
+				cin >> v;
+				queries[i] = {type, -1, v, -1};
+			}
+		}
+	}
+	pq_updates<dsu_restorable, int, int> pq{dsu_restorable(n)};
+	for (int i = 0; i < n; i++) pq.ds.add(i, initial_values[i]);
+	int curr_priority_counter = -q;
+	for (int i = 0; i < q; i++) {
+		int type = queries[i].type;
+		if (type == 0) {
+			int u = queries[i].u, v = queries[i].v;
+			int curr_pri;
+			if (time_remove[i] == -1) curr_pri = curr_priority_counter--;
+			else curr_pri = -time_remove[i];
+			pq.push_update(u, v, curr_pri);
+		} else if (type == 1)
+			pq.pop_update();
+		else if (type == 2) {
+			int v = queries[i].v, x = queries[i].x;
+			pq.ds.add(v, x);
+		} else {
+			assert(type == 3);
+			int v = queries[i].v;
+			cout << pq.ds.sum(v) << '\n';
+		}
+	}
+	return 0;
+}
