9710fd73991879e24d9aed0c5460373ce3f1cc9c
Luke Videckis <lukevideckis@gmail.com>
2023-04-01 13:16:28 -0500

    Add test for pq ds (#329)
    * add another test for priority queue of updates
    
    * ran astyle
    
    * fix some compile errors
    
    * fix another compile error
    
    * correct formatting of output
    
    * fix output for test
    
    * [auto-verifier] verify commit 9968b4106f89686b33ce0f6551d7b622da06a2fb
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ tests/online_judge_tests/data_structures/pq_ds_undo_sliding_window.test.cpp
@@ -0,0 +1,39 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/all/DSL_3_D"
+//since _GLIBCXX_DEBUG causes std::map insert/erase operations to be O(n)
+#undef _GLIBCXX_DEBUG
+#include "../template.hpp"
+
+#include "../../../library/data_structures/priority_queue_of_updates.hpp"
+
+struct stack_with_get_max {
+	vector<pair<int, int>> st;
+	void update(int val) {
+		st.emplace_back(val, st.empty() ? val : min(val, st.back().second));
+	}
+	void undo() {
+		st.pop_back();
+	}
+	int get_max() const {
+		return st.back().second;
+	}
+};
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int n, l;
+	cin >> n >> l;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	pq_updates<stack_with_get_max, int> pq{stack_with_get_max()};
+	int priority = (n - l) / 2;
+	for (int i = 0; i < l; i++)
+		pq.push_update(arr[i], priority--);
+	cout << pq.ds.get_max();
+	for (int i = l; i < n; i++) {
+		pq.push_update(arr[i], priority--);
+		pq.pop_update();
+		cout << " " << pq.ds.get_max();
+	}
+	cout << '\n';
+	return 0;
+}
