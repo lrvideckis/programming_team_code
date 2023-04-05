3893bfecbdd814e7c485c28f4f2e2096a5378549
Luke Videckis <lukevideckis@gmail.com>
2023-04-04 19:52:31 -0500

    nits/golf

--- library/graphs/least_common_ancestor.hpp
+++ library/graphs/least_common_ancestor.hpp
@@ -8,37 +8,35 @@
  */
 #include "../data_structures/sparse_table.hpp"
 struct LCA {
-	int T = 0;//TODO: remove, and use ssize(tour) instead
-	vector<int> in, out, d, p, tour, tour2;
-	RMQ<int> rmq_lca, rmq_path;
+	const int N;
+	vector<int> in, sz, d, p, tour;
+	RMQ<int> rmq;
 	LCA(const vector<vector<int>>& adj) :
-		in(ssize(adj)), out(in), d(in), p(in),
-		rmq_lca((dfs(adj, 0, -1), tour), [&](int u, int v) {return in[u] < in[v] ? u : v;}),
-		rmq_path(tour2, [&](int u, int v) {return pair(d[u], -in[u]) < pair(d[v], -in[v]) ? u : v;}) {}
+		N(ssize(adj)), in(N, -1), sz(N, 1), d(N), p(N), rmq(init(adj)) {}
+	RMQ<int> init(const vector<vector<int>>& adj) {
+		tour.reserve(N);
+		for(int i = 0; i < N; i++)
+			if(in[i] == -1) dfs(adj, i, -1);
+		return RMQ<int>(tour, [&](int u, int v) {
+			return pair(d[u], -in[u]) < pair(d[v], -in[v]) ? u : v;
+		});
+	}
 	void dfs(const vector<vector<int>>& adj, int u, int par) {
-		tour2.push_back(u);
-		in[u] = T++, p[u] = par;
+		in[u] = ssize(tour), p[u] = par;
+		tour.push_back(u);
 		for (int v : adj[u])
 			if (v != par)
-				d[v] = 1 + d[u], tour.push_back(u), dfs(adj, v, u);
-		out[u] = T;
+				d[v] = 1 + d[u], dfs(adj, v, u), sz[u] += sz[v];
 	}
 	int lca(int u, int v) const {
 		if (u == v) return u;
 		tie(u, v) = minmax(in[u], in[v]);
-		return rmq_lca.query(u, v);
+		return p[rmq.query(u+1, v+1)];
 	}
-	/*
-	int size_subtree(int u) const {return out[u] - in[u];}
-	*/
 	int dist_edges(int u, int v) const {return d[u] + d[v] - 2 * d[lca(u, v)];}
-	bool in_subtree(int u, int v) const {return in[u] <= in[v] && in[v] < out[u];}
+	bool in_subtree(int u, int v) const {return in[u] <= in[v] && in[v] < in[u] + sz[u];}
 	int first_on_path(int u, int v) const {
 		assert(u != v);
-		if(in_subtree(u,v)) {
-			assert(in[u] < in[v]);
-			//assert(rmq_path.query(in[u]+1, in[v]+1) == u);
-		}
-		return in_subtree(u, v) ? rmq_path.query(in[u]+1, in[v]+1) : p[u];
+		return in_subtree(u, v) ? rmq.query(in[u]+1, in[v]+1) : p[u];
 	}
 };
