e1b9640639afcd6ca58e1a9f93224afa0a4f3b37
Luke Videckis <lukevideckis@gmail.com>
2023-04-04 21:20:26 -0500

    combine both LCA tests into one

--- tests/library_checker_aizu_tests/graphs/lca_both_methods.test.cpp
+++ tests/library_checker_aizu_tests/graphs/lca_both_methods.test.cpp
@@ -2,6 +2,7 @@
 #include "../template.hpp"
 
 #include "../../../library/graphs/tree_lifting.hpp"
+#include "../../../library/graphs/least_common_ancestor.hpp"
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
@@ -11,13 +12,21 @@ int main() {
 	for (int i = 1; i < n; i++) {
 		int par;
 		cin >> par;
-		adj[i].push_back(par);
 		adj[par].push_back(i);
 	}
 	tree_lift lift(adj);
+	LCA lca(adj);
+	for (int i = 0; i < n; i++) {
+		assert(lift.lca(i, i) == i);
+		assert(lca.lca(i, i) == i);
+	}
 	while (q--) {
 		int u, v;
 		cin >> u >> v;
-		cout << lift.lca(u, v) << '\n';
+		int curr_lca = lift.lca(u, v);
+		assert(curr_lca == lca.lca(u, v));
+		assert((curr_lca == u) == lca.in_subtree(u, v));
+		assert((curr_lca == v) == lca.in_subtree(v, u));
+		cout << curr_lca << '\n';
 	}
 }
