502584890a37be8742476e4ff320d4a40f7ced53
cameron custer <custer.cameron@gmail.com>
2023-04-06 09:36:47 -0400

    transition nCk struct -> functions

--- library/math/n_choose_k_mod.hpp
+++ library/math/n_choose_k_mod.hpp
@@ -1,46 +1,44 @@
 /** @file */
 #pragma once
+const int N = 1001;
 /**
- * @code{.cpp}
- *     n_choose_k<1'000'000'007> nk(n); // to use `choose` with inputs strictly < n
- *     n_choose_k<1'000'003> nk(1'000'003); // to use `choose_lucas` with arbitrarily large inputs
- * @endcode
+ * nCk with arbitrarily large integers requires O(MOD) precalc
  */
-template<int MOD> struct n_choose_k {
-	vector<long long> inv, fact, inv_fact;
-	/**
-	 * @param n size
-	 * @time O(n + sqrt(MOD))
-	 * @memory O(n)
-	 */
-	n_choose_k(int n) : inv(n, 1), fact(n, 1), inv_fact(n, 1) {
-		assert(max(n, 2) <= MOD);
-		for (int i = 2; i * i <= MOD; i++) assert(MOD % i);
-		for (int i = 2; i < n; i++) {
-			inv[i] = MOD - (MOD / i) * inv[MOD % i] % MOD;
-			fact[i] = fact[i - 1] * i % MOD;
-			inv_fact[i] = inv_fact[i - 1] * inv[i] % MOD;
-		}
+long long inv[N], fact[N], inv_fact[N];
+/**
+ * @time O(n + sqrt(MOD))
+ * @memory O(n)
+ */
+template<int MOD> void calc_chooses() {
+	for (int i = 2; i * i <= MOD; i++) assert(MOD % i);
+	for (int i = 0; i < 2; i++)
+		inv[i] = fact[i] = inv_fact[i] = 1;
+	for (int i = 2; i < N; i++) {
+		inv[i] = MOD - (MOD / i) * inv[MOD % i] % MOD;
+		fact[i] = fact[i - 1] * i % MOD;
+		inv_fact[i] = inv_fact[i - 1] * inv[i] % MOD;
 	}
-	/**
-	 * @param n,k requires n < ssize(fact)
-	 * @returns number of ways to choose k objects out of n
-	 * @time O(1)
-	 */
-	long long choose(int n, int k) const {
-		if (k < 0 || n < k) return 0;
-		return fact[n] * inv_fact[k] % MOD * inv_fact[n - k] % MOD;
-	}
-	/**
-	 * @param n,k arbitrarily large integers
-	 * @returns number of ways to choose k objects out of n
-	 * @time O(log(k))
-	 */
-	long long choose_lucas(long long n, long long k) const {
-		if (k < 0 || n < k) return 0;
-		long long res = 1;
-		for (; k && k < n && res; n /= MOD, k /= MOD)
-			res = res * choose(int(n % MOD), int(k % MOD)) % MOD;
-		return res;
-	}
-};
+}
+/**
+ * @param n,k requires n < N
+ * @returns number of ways to choose k objects out of n
+ * @time O(1)
+ */
+//NOLINTNEXTLINE(readability-identifier-naming)
+template<int MOD> long long C(int n, int k) {
+	if (k < 0 || n < k) return 0;
+	return fact[n] * inv_fact[k] % MOD * inv_fact[n - k] % MOD;
+}
+/**
+ * @param n,k arbitrarily large integers
+ * @returns number of ways to choose k objects out of n
+ * @time O(log(k))
+ */
+//NOLINTNEXTLINE(readability-identifier-naming)
+template<int MOD> long long C(long long n, long long k) {
+	if (k < 0 || n < k) return 0;
+	long long res = 1;
+	for (; k && k < n && res; n /= MOD, k /= MOD)
+		res = res * C<MOD>(int(n % MOD), int(k % MOD)) % MOD;
+	return res;
+}
--- tests/library_checker_aizu_tests/math/n_choose_k.test.cpp
+++ tests/library_checker_aizu_tests/math/n_choose_k.test.cpp
@@ -4,42 +4,41 @@
 
 #include "../../../library/math/n_choose_k_mod.hpp"
 
-template<int N> void test_lucas(const n_choose_k<N>& choose_l) {
-	const int MAX_N = 1000;
-	assert(N < MAX_N);
-	vector<vector<long long>> naive_choose(MAX_N, vector<long long>(MAX_N, 0));
-	for (int i = 0; i < MAX_N; i++) {
+template<int MOD> void test_lucas() {
+	calc_chooses<MOD>();
+	vector<vector<long long>> naive_choose(N, vector<long long>(N, 0));
+	for (int i = 0; i < N; i++) {
 		naive_choose[i][0] = 1;
 		for (int j = 1; j <= i; j++)
-			naive_choose[i][j] = (naive_choose[i - 1][j] + naive_choose[i - 1][j - 1]) % N;
+			naive_choose[i][j] = (naive_choose[i - 1][j] + naive_choose[i - 1][j - 1]) % MOD;
 	}
-	for (int i = 0; i < MAX_N; i++) {
-		assert(choose_l.choose_lucas(i, -1) == 0);
-		assert(choose_l.choose_lucas(i, i + 1) == 0);
-		assert(choose_l.choose_lucas(-1, i) == 0);
-		assert(choose_l.choose_lucas(i, i - 1) == i % N);
-		for (int j = 0; j < MAX_N; j++)
-			assert(choose_l.choose_lucas(i, j) == naive_choose[i][j]);
+	for (int i = 0; i < N; i++) {
+		assert(C<MOD>(1LL * i, -1LL) == 0);
+		assert(C<MOD>(1LL * i, 1LL * i + 1) == 0);
+		assert(C<MOD>(-1LL, 1LL * i) == 0);
+		assert(C<MOD>(1LL * i, 1LL * i - 1) == i % MOD);
+		for (int j = 0; j < N; j++)
+			assert(C<MOD>(1LL * i, 1LL * j) == naive_choose[i][j]);
 	}
 }
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	//test choose with lucas theorem
-	test_lucas<2>(n_choose_k<2>(2));
-	test_lucas<3>(n_choose_k<3>(3));
-	test_lucas<5>(n_choose_k<5>(5));
-	test_lucas<7>(n_choose_k<7>(7));
-	test_lucas<11>(n_choose_k<11>(11));
-	test_lucas<13>(n_choose_k<13>(13));
-	const int N = 1001, MOD = 1'000'000'007;
-	n_choose_k<MOD> nk(N);
+	test_lucas<2>();
+	test_lucas<3>();
+	test_lucas<5>();
+	test_lucas<7>();
+	test_lucas<11>();
+	test_lucas<13>();
+	const int MOD = 1'000'000'007;
+	calc_chooses<MOD>();
 	//test mod inverse
 	for (int i = 1; i < N; i++)
-		assert(nk.inv[i] == bin_exp(i, MOD - 2, MOD));
+		assert(inv[i] == bin_exp(i, MOD - 2, MOD));
 	int balls, boxes;
 	cin >> balls >> boxes;
 	//you have to choose the subset of boxes to place balls into
-	cout << nk.choose(boxes, balls) << '\n';
+	cout << C<MOD>(boxes, balls) << '\n';
 	return 0;
 }
