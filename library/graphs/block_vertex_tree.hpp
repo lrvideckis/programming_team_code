4570b946e311665e4b4450d762a440941640833f
Luke Videckis <lukevideckis@gmail.com>
2023-04-06 15:42:24 -0500

    switch BVT

--- library/graphs/block_vertex_tree.hpp
+++ library/graphs/block_vertex_tree.hpp
@@ -6,13 +6,13 @@
  *     graph_info cc = bridge_and_cut(adj, m);
  *     vector<vector<int>> bvt = block_vertex_tree(adj, cc);
  *
- *     //to loop over each *unique* bcc containing a node v:
- *     for (auto bccid : bvt[v]) {
+ *     //to loop over each *unique* bcc containing a node u:
+ *     for (auto bccid : bvt[u]) {
  *         bccid -= n;
  *     }
  *
  *     //to loop over each *unique* node inside a bcc:
- *     for (auto v : bvt[bccid + n]) {}
+ *     for (auto u : bvt[bccid + n]) {}
  * @endcode
  *
  * [0, n) are original nodes
@@ -28,16 +28,16 @@ vector<vector<int>> block_vertex_tree(const vector<vector<pair<int, int>>>& adj,
 	int n = ssize(adj);
 	vector<vector<int>> bvt(n + cc.num_bccs);
 	vector<bool> vis(cc.num_bccs, 0);
-	for (int v = 0; v < n; v++) {
-		for (auto [_, e_id] : adj[v]) {
+	for (int u = 0; u < n; u++) {
+		for (auto [_, e_id] : adj[u]) {
 			int bccid = cc.bcc_id[e_id];
 			if (!vis[bccid]) {
 				vis[bccid] = 1;
-				bvt[v].push_back(bccid + n);
-				bvt[bccid + n].push_back(v);
+				bvt[u].push_back(bccid + n);
+				bvt[bccid + n].push_back(u);
 			}
 		}
-		for (auto bccid : bvt[v]) vis[bccid - n] = 0;
+		for (auto bccid : bvt[u]) vis[bccid - n] = 0;
 	}
 	return bvt;
 }
