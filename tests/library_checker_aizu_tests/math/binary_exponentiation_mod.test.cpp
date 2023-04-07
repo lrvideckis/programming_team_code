3351233df303cab3b0e46994695a451e87412b54
Luke Videckis <lukevideckis@gmail.com>
2023-04-06 23:05:09 -0500

    readd back test

---
+++ tests/library_checker_aizu_tests/math/binary_exponentiation_mod.test.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/all/NTL_1_B"
+#include "../template.hpp"
+
+#include "../../../library/math/tetration_mod.hpp"
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int m, n;
+	cin >> m >> n;
+	cout << bin_exp(m, n, 1'000'000'007) << '\n';
+	return 0;
+}
