1b06a0cbfafc82358062ac8e2aeb32ee63b12741
Luke Videckis <lukevideckis@gmail.com>
2023-04-06 21:17:51 -0500

    fix doxygen

--- library/math/partitions.hpp
+++ library/math/partitions.hpp
@@ -4,7 +4,6 @@ const int MOD = 998'244'353;
 /**
  * @see https://oeis.org/A000041
  * @param n an integer
- * @param mod an integer
  * @returns array p where p[i] = number of partitions of i numbers
  * @time O(n sqrt n) note there does exist a O(n log n) solution as well
  */
