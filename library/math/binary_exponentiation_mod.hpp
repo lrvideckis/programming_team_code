b21933e8a233965441b75018e3bd7727210d7125
Luke Videckis <lukevideckis@gmail.com>
2023-04-06 21:05:11 -0500

    switch to global const mod

--- library/math/binary_exponentiation_mod.hpp
+++ library/math/binary_exponentiation_mod.hpp
@@ -1,17 +1,19 @@
 /** @file */
 #pragma once
+const int MOD = 998'244'353;
 /**
- * @param base,pw,mod see return
- * @returns (base^pw)%mod, 1 for 0^0.
+ * @param base,pw see return
+ * @returns (base^pw)%MOD, 1 for 0^0.
  * @time O(log pw)
  */
-long long bin_exp(long long base, long long pw, long long mod) {
-	assert(0 <= pw && 0 <= base && 1 <= mod);
+long long bin_exp(long long base, long long pw) {
+	assert(0 <= pw && 1 <= MOD);
 	long long res = 1;
-	base %= mod;
+	base %= MOD;
+	if (base < 0) base += MOD;
 	while (pw > 0) {
-		if (pw & 1) res = res * base % mod;
-		base = base * base % mod;
+		if (pw & 1) res = res * base % MOD;
+		base = base * base % MOD;
 		pw >>= 1;
 	}
 	return res;
