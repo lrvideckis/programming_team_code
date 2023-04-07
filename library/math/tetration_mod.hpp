92fee28f1327e640e2ec358876bcdfff0ab2f999
Luke Videckis <lukevideckis@gmail.com>
2023-04-06 21:57:20 -0500

    make param names consistent

--- library/math/tetration_mod.hpp
+++ library/math/tetration_mod.hpp
@@ -20,23 +20,23 @@ long long bin_exp(long long b, long long e, int mod) {
  * @see https://cp-algorithms.com/algebra/phi-function.html#generalization
  *
  * Let t = totient(mod).
- * If log2(mod) <= pw then (base^pw)%mod == (base^(t+(pw%t)))%mod
- * So you need enough base cases to cover when log2(mod) > pw
+ * If log2(mod) <= e then (b^e)%mod == (b^(t+(e%t)))%mod
+ * So you need enough b cases to cover when log2(mod) > e
  *
- * @param base,pw,mod see return
- * @returns base ^ (base ^ (base ^ ... )) % mod, where the height of the tower
- * is pw.
+ * @param b,e,mod see return
+ * @returns b ^ (b ^ (b ^ ... )) % mod, where the height of the tower
+ * is e.
  * @time O(sqrt(mod) * log(mod))
  */
-long long tetration(long long base, long long pw, long long mod) {
+long long tetration(long long b, long long e, int mod) {
 	if (mod == 1) return 0;
-	if (base == 0) return (pw + 1) % 2 % mod;
-	if (base == 1 || pw == 0) return 1;
-	if (pw == 1) return base % mod;
-	if (base == 2 && pw == 2) return 4 % mod;
-	if (base == 2 && pw == 3) return 16 % mod;
-	if (base == 3 && pw == 2) return 27 % mod;
-	int t = totient(int(mod));
-	long long exp = tetration(base, pw - 1, t);
-	return bin_exp(base, exp + t, mod);
+	if (b == 0) return (e + 1) % 2 % mod;
+	if (b == 1 || e == 0) return 1;
+	if (e == 1) return b % mod;
+	if (b == 2 && e == 2) return 4 % mod;
+	if (b == 2 && e == 3) return 16 % mod;
+	if (b == 3 && e == 2) return 27 % mod;
+	int t = totient(mod);
+	long long exp = tetration(b, e - 1, t);
+	return bin_exp(b, exp + t, mod);
 }
