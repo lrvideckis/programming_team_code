86edd4ebdd7f0c4985e4413b3dcb22483b18e469
Luke Videckis <lukevideckis@gmail.com>
2023-04-06 21:48:18 -0500

    update docs

--- library/math/binary_exponentiation_mod.hpp
+++ library/math/binary_exponentiation_mod.hpp
@@ -5,7 +5,7 @@ const int MOD = 998'244'353;
  * @param b base
  * @param e exponent
  * @returns (b^e)%MOD, 1 for 0^0.
- * @time O(log pw)
+ * @time O(log e)
  */
 long long bin_exp(long long b, long long e) {
 	assert(0 <= e);
