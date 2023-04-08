2bd9a66dd6b89381bc588f2179fa90c278812a83
Luke Videckis <lukevideckis@gmail.com>
2023-04-08 02:23:33 -0500

    saving progress

--- library/misc/monotonic_stack_related/monotonic_stack.hpp
+++ library/misc/monotonic_stack_related/monotonic_stack.hpp
@@ -6,17 +6,17 @@
  *     vector<int> le = monotonic_stack<int>(arr, [&](int x, int y) {return x < y;});
  * @endcode
  * @param arr array
- * @param cmp one of less, less_equal, greater, greater_equal
+ * @param less one of less, less_equal, greater, greater_equal
  * @returns array `le` where `le[i]` = max integer such that: `le[i]` < i and
- * !cmp(arr[le[i]], arr[i]). Returns -1 if no number exists.
+ * !less(arr[le[i]], arr[i]). Returns -1 if no number exists.
  * @time O(n)
  * @memory O(n)
  */
-template <typename T> vector<int> monotonic_stack(const vector<T>& arr, const function<bool(const T&, const T&)>& cmp) {
+template <typename T> vector<int> monotonic_stack(const vector<T>& arr, const function<bool(const T&, const T&)>& less) {
 	vector<int> le(ssize(arr));
 	for (int i = 0; i < ssize(arr); i++) {
 		le[i] = i - 1;
-		while (le[i] >= 0 && cmp(arr[le[i]], arr[i])) le[i] = le[le[i]];
+		while (le[i] >= 0 && less(arr[le[i]], arr[i])) le[i] = le[le[i]];
 	}
 	return le;
 }
