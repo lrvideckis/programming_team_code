ebbc3f8cffd913ee5b13c6c8abdee95bc20aa05f
Luke Videckis <lukevideckis@gmail.com>
2023-04-08 15:35:55 -0500

    Update mono st comp (#344)
    * now, think about the function as a compare operator
    
    * [auto-verifier] verify commit b17d5f69953292a9011eaf73bfba1fcc1de7edc3
    
    * fix test
    
    * [auto-verifier] verify commit 3aa9431fad4cacc8d4b67fe707b550e956f505ba
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/misc/monotonic_stack_related/monotonic_stack.hpp
+++ library/misc/monotonic_stack_related/monotonic_stack.hpp
@@ -2,21 +2,21 @@
 #pragma once
 /**
  * @code{.cpp}
- *     vector<int> le = monotonic_stack<int>(arr, less());
+ *     vector<int> le = monotonic_stack<int>(arr, less()); //less, less_equal, greater, greater_equal
  *     vector<int> le = monotonic_stack<int>(arr, [&](int x, int y) {return x < y;});
  * @endcode
  * @param arr array
- * @param op one of less, less_equal, greater, greater_equal
- * @returns array `le` where `le[i]` = max integer such that: `le[i]` < i and
- * !op(arr[le[i]], arr[i]). Returns -1 if no number exists.
+ * @param less any transitive compare operator
+ * @returns array le where le[i] = max integer such that: le[i] < i and
+ * less(arr[le[i]], arr[i]). If no integer exists, le[i] = -1
  * @time O(n)
  * @memory O(n)
  */
-template <typename T> vector<int> monotonic_stack(const vector<T>& arr, const function<bool(const T&, const T&)>& op) {
+template <typename T> vector<int> monotonic_stack(const vector<T>& arr, const function<bool(const T&, const T&)>& less) {
 	vector<int> le(ssize(arr));
 	for (int i = 0; i < ssize(arr); i++) {
 		le[i] = i - 1;
-		while (le[i] >= 0 && op(arr[le[i]], arr[i])) le[i] = le[le[i]];
+		while (le[i] >= 0 && !less(arr[le[i]], arr[i])) le[i] = le[le[i]];
 	}
 	return le;
 }
