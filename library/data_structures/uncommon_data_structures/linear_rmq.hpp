68f07650830e56d3232639549812fe4e3274bb08
Luke Videckis <lukevideckis@gmail.com>
2023-04-08 16:53:36 -0500

    more accurate statement

--- library/data_structures/uncommon_data_structures/linear_rmq.hpp
+++ library/data_structures/uncommon_data_structures/linear_rmq.hpp
@@ -47,7 +47,7 @@ template <typename T> struct linear_rmq {
 	int min_ind(int le, int ri) const {
 		return less(arr[le], arr[ri]) ? le : ri;
 	}
-	int query_idx(int le, int ri) const {//time: theoretically O(log(n) / log(log(n))), practically the for-loop runs only <=4 times
+	int query_idx(int le, int ri) const {//time: theoretically O(log(n) / log(log(n))), practically ssize(mask) <= 4 if n <= 2^24
 		assert(0 <= le && le < ri && ri <= N);
 		int res = le;
 		for (int level = 0; le < ri && level < ssize(mask); level++, le = (le >> 6) + 1, ri = ((ri - 1) >> 6)) {
