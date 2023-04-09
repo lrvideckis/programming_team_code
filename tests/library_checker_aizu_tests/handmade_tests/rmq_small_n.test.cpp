541b2f025d546018f61752e75dffa797e3df9ad6
Luke Videckis <lukevideckis@gmail.com>
2023-04-08 23:14:55 -0500

    format

--- tests/library_checker_aizu_tests/handmade_tests/rmq_small_n.test.cpp
+++ tests/library_checker_aizu_tests/handmade_tests/rmq_small_n.test.cpp
@@ -8,15 +8,15 @@
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
-	for(int n = 1; n <= 100; n++) {
-		for(int times = 0; times < 40; times++) {
+	for (int n = 1; n <= 100; n++) {
+		for (int times = 0; times < 40; times++) {
 			vector<int> arr(n);
 			for (int i = 0; i < n; i++) arr[i] = get_rand<int>(INT_MIN, INT_MAX);
 			RMQ<int> rmq(arr, [](auto x, auto y) {return min(x, y);});
 			disjoint_rmq<int> dis_rmq(arr, [](auto x, auto y) {return min(x, y);});
 			linear_rmq<int> lin_rmq(arr, less());
-			for(int le = 0; le < n; le++) {
-				for(int ri = le+1; ri <= n; ri++) {
+			for (int le = 0; le < n; le++) {
+				for (int ri = le + 1; ri <= n; ri++) {
 					int idx_min = lin_rmq.query_idx(le, ri);
 					assert(le <= idx_min && idx_min < ri);
 					assert(arr[idx_min] == rmq.query(le, ri));
