83d128a5a9f834d3aa57238040e0327b7eb2eefe
Luke Videckis <lukevideckis@gmail.com>
2023-04-09 03:20:34 -0500

    added more doxygen

--- library/data_structures/uncommon_data_structures/linear_rmq.hpp
+++ library/data_structures/uncommon_data_structures/linear_rmq.hpp
@@ -1,18 +1,26 @@
 /** @file */
 #pragma once
-//https://codeforces.com/blog/entry/78931
-//https://codeforces.com/blog/entry/92310
-//https://github.com/okwedook/olymp/blob/master/code/data-structures/LinearMinTable.hpp
-//TODO: have least_common_ancestor.hpp depend on this
-//TODO: prefer `less()` over `less{}` for documentation
+/**
+ * @see https://codeforces.com/blog/entry/78931 https://codeforces.com/blog/entry/92310
+ * @code{.cpp}
+ *     vector<long long> arr;
+ *     linear_rmq<long long> rmq(arr, less());//minimum query
+ *     linear_rmq<long long> rmq(arr, greater());//maximum query
+ * @endcode
+ */
 template <typename T> struct linear_rmq {
 	using ull = unsigned long long;
 	const int N;
 	vector<T> arr;
-	function<bool(const T&, const T&)> less; //any transitive compare operator
-	vector<vector<int>> idx; //idx[level][i] = index of min in range [i, i + 64^(level+1))
-	vector<vector<ull>> mask; //mask[level][le] = stack representing subarray arr[le, le + 64)
-	//time & memory: O(n)
+	function<bool(const T&, const T&)> less;
+	vector<vector<ull>> mask;
+	vector<vector<int>> idx;
+	/**
+	 * @param a_arr an array
+	 * @param a_less transitive compare operator
+	 * @time O(n)
+	 * @memory O(n)
+	 */
 	linear_rmq(const vector<T>& a_arr, function<bool(const T&, const T&)> a_less) : N(ssize(a_arr)), arr(a_arr), less(a_less) {
 		for (int n = N; n >= 2; n = ((n + 63) >> 6)) {
 			int level = ssize(idx);
@@ -36,7 +44,12 @@ template <typename T> struct linear_rmq {
 	int mn(int le, int ri) const {
 		return less(arr[le], arr[ri]) ? le : ri;
 	}
-	int query_idx(int le, int ri) const {//time: theoretically O(log(n) / log(log(n))), practically if n <= 2^24 then ssize(mask) <= 4
+	/**
+	 * @param le,ri defines range [le, ri)
+	 * @returns index of minimum in range
+	 * @time O(log(n) / log(log(n))), practically if n <= 2^24 then ssize(mask) <= 4
+	 */
+	int query_idx(int le, int ri) const {
 		assert(0 <= le && le < ri && ri <= N);
 		int res = le;
 		for (int level = 0; le < ri && level < ssize(mask); level++, le = (le >> 6) + 1, ri = ((ri - 1) >> 6)) {
@@ -48,7 +61,12 @@ template <typename T> struct linear_rmq {
 		}
 		return res;
 	}
-	void update(int pos, const T& val) {//time: theoretically O((log(n)^2) / log(log(n)))
+	/**
+	 * @param pos index to update
+	 * @param val new value
+	 * @time O((log(n)^2) / log(log(n)))
+	 */
+	void update(int pos, const T& val) {
 		assert(0 <= pos && pos < N);
 		arr[pos] = val;
 		for (int level = 0; level < ssize(mask); level++, pos >>= 6)
