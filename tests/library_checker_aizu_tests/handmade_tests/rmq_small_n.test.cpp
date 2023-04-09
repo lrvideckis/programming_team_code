3a7841eb421586e9caab47ad1921652e3a363a4e
Luke Videckis <lukevideckis@gmail.com>
2023-04-09 00:40:07 -0500

    beef up test

--- tests/library_checker_aizu_tests/handmade_tests/rmq_small_n.test.cpp
+++ tests/library_checker_aizu_tests/handmade_tests/rmq_small_n.test.cpp
@@ -6,23 +6,35 @@
 #include "../../../library/data_structures/uncommon_data_structures/disjoint_sparse_table.hpp"
 #include "../../../library/data_structures/uncommon_data_structures/linear_rmq.hpp"
 
+void test_all_subarrays(const vector<int>& arr) {
+	int n = ssize(arr);
+	RMQ<int> rmq(arr, [](auto x, auto y) {return min(x, y);});
+	disjoint_rmq<int> dis_rmq(arr, [](auto x, auto y) {return min(x, y);});
+	linear_rmq<int> lin_rmq(arr, less());
+	for (int le = 0; le < n; le++) {
+		for (int ri = le + 1; ri <= n; ri++) {
+			int idx_min = lin_rmq.query_idx(le, ri);
+			assert(le <= idx_min && idx_min < ri);
+			assert(arr[idx_min] == rmq.query(le, ri));
+			assert(arr[idx_min] == dis_rmq.query(le, ri));
+		}
+	}
+}
+
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
+	for (int n = 1; n <= 8; n++) {
+		vector<int> perm(n);
+		iota(perm.begin(), perm.end(), 0);
+		do {
+			test_all_subarrays(perm);
+		} while (next_permutation(perm.begin(), perm.end()));
+	}
 	for (int n = 1; n <= 100; n++) {
 		for (int times = 0; times < 40; times++) {
 			vector<int> arr(n);
 			for (int i = 0; i < n; i++) arr[i] = get_rand<int>(INT_MIN, INT_MAX);
-			RMQ<int> rmq(arr, [](auto x, auto y) {return min(x, y);});
-			disjoint_rmq<int> dis_rmq(arr, [](auto x, auto y) {return min(x, y);});
-			linear_rmq<int> lin_rmq(arr, less());
-			for (int le = 0; le < n; le++) {
-				for (int ri = le + 1; ri <= n; ri++) {
-					int idx_min = lin_rmq.query_idx(le, ri);
-					assert(le <= idx_min && idx_min < ri);
-					assert(arr[idx_min] == rmq.query(le, ri));
-					assert(arr[idx_min] == dis_rmq.query(le, ri));
-				}
-			}
+			test_all_subarrays(arr);
 		}
 	}
 	cout << "Hello World\n";
