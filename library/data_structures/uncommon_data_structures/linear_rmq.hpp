dcd6622463c75def8fd1b22a34fd45371e165e70
Luke Videckis <lukevideckis@gmail.com>
2023-04-09 00:58:00 -0500

    don't repeat work

--- library/data_structures/uncommon_data_structures/linear_rmq.hpp
+++ library/data_structures/uncommon_data_structures/linear_rmq.hpp
@@ -25,7 +25,8 @@ template <typename T> struct linear_rmq {
 	void update(int level, int le, int ri) {
 		for (int i = ri - 1; i >= le; i--) {
 			ull st = mask[level][i + 1];
-			while (st && less(arr[f(level, i)], arr[f(level, i + 1 + __builtin_ctzll(st))])) st &= st - 1;
+			const T& curr = arr[f(level, i)];
+			while (st && less(curr, arr[f(level, i + 1 + __builtin_ctzll(st))])) st &= st - 1;
 			mask[level][i] = st = ((st << 1) | 1);
 			idx[level][i] = f(level, i + int(__lg(st)));
 		}
