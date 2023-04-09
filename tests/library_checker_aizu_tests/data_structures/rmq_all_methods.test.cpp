730200f25f564ce37bf344e39f2cd61efd67c8d4
Luke Videckis <lukevideckis@gmail.com>
2023-04-09 15:37:19 -0500

    Linear rmq near constant query (#347)
    * finish interface
    
    * add memory, and reminder to myself
    
    * add which test
    
    * test now compiles
    
    * [auto-verifier] verify commit c49318c8a96bfde1d702099571e8b7d1b0496270
    
    * its done
    
    * nits
    
    * saving progress
    
    * [auto-verifier] verify commit 2bd9a66dd6b89381bc588f2179fa90c278812a83
    
    * it now AC's, still need to clean/golf it
    
    * [auto-verifier] verify commit d3178738323dc06c1009f5439ceb38690e55ecd6
    
    * ran astyle
    
    * [auto-verifier] verify commit 3e4d68d4574f7c83f1207df8fdebcf8be97acaf9
    
    * [auto-verifier] verify commit 5cb4f35a07b68832a6c6e6807e8e64aff68dde16
    
    * merge tests
    
    * [auto-verifier] verify commit e3b749094aa755fbeb49d373056658697ab05821
    
    * more accurate statement
    
    * [auto-verifier] verify commit 68f07650830e56d3232639549812fe4e3274bb08
    
    * reorder
    
    * [auto-verifier] verify commit d49391edd11339e8218c2245c4c6ea0806ac72b2
    
    * remove block_mn array
    
    * [auto-verifier] verify commit 3031d742c14e7c31f45760919639b920bc9ca35d
    
    * combine both functions into one
    
    * [auto-verifier] verify commit 2d9e3a853d15788d270bded4ff9960db740d4602
    
    * more golf
    
    * [auto-verifier] verify commit 2592bc737aae9f0f971b9ed2df684f5a525625ef
    
    * finished update
    
    * [auto-verifier] verify commit 14c3bfab774537ff8d7cb9a252748062a2f18644
    
    * refactor common logic into function
    
    * [auto-verifier] verify commit 98e3a16eaefdf5b756f4e2cccf540af7fcbb5638
    
    * nits
    
    * [auto-verifier] verify commit 94748c49dc7a73dfcec34280006711d87e0ba5ec
    
    * nits/golf
    
    * fix bug
    
    * fix gcc warning
    
    * update member var order
    
    * [auto-verifier] verify commit 7f795f2f39508d559b6c66bce740b74bec1c46e5
    
    * remove function which was only called from one place
    
    * [auto-verifier] verify commit 86f0349410559cf24c066eb8ad70ae597e9b65a9
    
    * fix bug + add small test
    
    * format
    
    * [auto-verifier] verify commit 541b2f025d546018f61752e75dffa797e3df9ad6
    
    * beef up test
    
    * [auto-verifier] verify commit 3a7841eb421586e9caab47ad1921652e3a363a4e
    
    * don't repeat work
    
    * [auto-verifier] verify commit dcd6622463c75def8fd1b22a34fd45371e165e70
    
    * optimize
    
    * [auto-verifier] verify commit a10d8c8d380dfba34eb6e39f3a4b9a3fc37f1f94
    
    * rename func
    
    * [auto-verifier] verify commit f8ac9a0bfb3441c4afc6c6ffad9c5b8886fa9521
    
    * rename other func
    
    * change other func names
    
    * [auto-verifier] verify commit c0b69d9e3d61fb9851b4dedc6e133418d4aa7596
    
    * nits
    
    * astyle
    
    * [auto-verifier] verify commit c25aa08bceafaa824b851523a2324867b762a6a2
    
    * added more doxygen
    
    * [auto-verifier] verify commit 83d128a5a9f834d3aa57238040e0327b7eb2eefe
    
    * finish doxygen
    
    * [auto-verifier] verify commit 49a19b5f9e238e3b3ec76a16bb556de1afb73f6d
    
    * nits to documentation
    
    * [auto-verifier] verify commit d23c78aa2af9a5e453eeb7ecab6d42ff20ce731e
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- tests/library_checker_aizu_tests/data_structures/rmq_all_methods.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/rmq_all_methods.test.cpp
@@ -3,6 +3,7 @@
 
 #include "../../../library/data_structures/sparse_table.hpp"
 #include "../../../library/data_structures/uncommon_data_structures/disjoint_sparse_table.hpp"
+#include "../../../library/data_structures/uncommon_data_structures/linear_rmq.hpp"
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
@@ -12,11 +13,14 @@ int main() {
 	for (int i = 0; i < n; i++) cin >> arr[i];
 	RMQ<int> rmq(arr, [](auto x, auto y) {return min(x, y);});
 	disjoint_rmq<int> dis_rmq(arr, [](auto x, auto y) {return min(x, y);});
+	linear_rmq<int> lin_rmq(arr, less());
 	while (q--) {
 		int le, ri;
 		cin >> le >> ri;
-		int res = rmq.query(le, ri);
-		assert(res == dis_rmq.query(le, ri));
-		cout << res << '\n';
+		int idx_min = lin_rmq.query_idx(le, ri);
+		assert(le <= idx_min && idx_min < ri);
+		assert(arr[idx_min] == rmq.query(le, ri));
+		assert(arr[idx_min] == dis_rmq.query(le, ri));
+		cout << arr[idx_min] << '\n';
 	}
 }
