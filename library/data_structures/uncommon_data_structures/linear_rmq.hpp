3031d742c14e7c31f45760919639b920bc9ca35d
Luke Videckis <lukevideckis@gmail.com>
2023-04-08 20:38:15 -0500

    remove block_mn array

--- library/data_structures/uncommon_data_structures/linear_rmq.hpp
+++ library/data_structures/uncommon_data_structures/linear_rmq.hpp
@@ -4,12 +4,12 @@
 //https://codeforces.com/blog/entry/92310
 //https://github.com/okwedook/olymp/blob/master/code/data-structures/LinearMinTable.hpp
 //TODO: have least_common_ancestor.hpp depend on this
+//TODO: prefer `less()` over `less{}` for documentation
 template <typename T> struct linear_rmq {
 	using ull = unsigned long long;
 	const int N;
 	vector<T> arr;
-	vector<vector<int>> idx, block_mn; //block_mn[level][i] = index of min in range [i, i + 64^(level+1))
-
+	vector<vector<int>> idx; //idx[level][i] = index of min in range [i, i + 64^(level+1))
 	vector<vector<ull>> mask; //mask[level][le] = stack representing subarray arr[le, le + 64)
 
 
@@ -18,31 +18,31 @@ template <typename T> struct linear_rmq {
 	linear_rmq(const vector<T>& a_arr, function<bool(const T&, const T&)> a_less) :
 		N(ssize(a_arr)),
 		arr(a_arr),
-		idx(1, vector<int>(N)),//TODO: try to remove this array
-		block_mn(idx),
 		mask(1, vector<ull>(N)), less(a_less) {
-		iota(idx[0].begin(), idx[0].end(), 0);
+		auto g = [&](int i) -> int {
+			return idx.size() ? idx.back()[i << 6] : i;
+		};
 		while (1) {
 			int n = ssize(mask.back());
 			ull st = 0;
+			vector<int> new_idx_row(n);
 			for (int i = n - 1; i >= 0; i--) {
-				while (st && less(arr[idx.back()[i]], arr[idx.back()[i + __builtin_ctzll(st) + 1]])) st &= st - 1;
+				while (st && less(arr[g(i)], arr[g(i + 1 + __builtin_ctzll(st))])) st &= st - 1;
 				mask.back()[i] = st = ((st << 1) | 1);
-				block_mn.back()[i] = idx.back()[i + __lg(st)];
+				new_idx_row[i] = g(i + __lg(st));
 			}
+			idx.emplace_back(new_idx_row);
 			if (n <= 2 * 64) break;
-			int new_sz = (n + 63) >> 6;
-			idx.emplace_back(new_sz);
-			for (int i = 0; (i << 6) < n; i++)
-				idx.back()[i] = block_mn.back()[i << 6];
-			block_mn.emplace_back(new_sz);
-			mask.emplace_back(new_sz);
+			mask.emplace_back((n + 63) >> 6);
 		}
+	}
+	int f(int level, int i) const {
+		return level ? idx[level - 1][i << 6] : i;
 	}
 	int min_idx_block(int level, int le, int ri) const {//returns index of min in range [le, ri)
 		int x = 64 - (ri - le);
 		assert(x > 0);
-		return idx[level][le + __lg((mask[level][le] << x) >> x)];
+		return f(level, le + __lg((mask[level][le] << x) >> x));
 	}
 	int min_ind(int le, int ri) const {
 		return less(arr[le], arr[ri]) ? le : ri;
@@ -53,11 +53,13 @@ template <typename T> struct linear_rmq {
 		for (int level = 0; le < ri && level < ssize(mask); level++, le = (le >> 6) + 1, ri = ((ri - 1) >> 6)) {
 			if (ri - le < 64) //TODO: avoid min_idx_block if it's the last block
 				return min_ind(res, min_idx_block(level, le, ri));
-			res = min_ind(res, min_ind(block_mn[level][le], block_mn[level][ri - 64]));
+			res = min_ind(res, min_ind(idx[level][le], idx[level][ri - 64]));
 		}
 		return res;
 	}
+	/*
 	void update(int idx, const T& val) {//time: theoretically O((log(n)^2) / log(log(n))), practically 4 * 64 array indexes
 		//TODO
 	}
+	*/
 };
