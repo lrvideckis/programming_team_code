c10cfea09729d1f9f847e3eb2cfd3b9285bf7b41
Luke Videckis <lukevideckis@gmail.com>
2023-04-16 23:36:20 -0500

    Revert "see version"
    This reverts commit 2f1ebd5108fe2609dbb3ea1084c6f6e53213c8a8.

--- tests/scripts/hackpack.sh
+++ tests/scripts/hackpack.sh
@@ -2,8 +2,6 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 
-rename -V
-
 WORD_LENGTH_THRESHOLD=65
 echo "The following words are > $WORD_LENGTH_THRESHOLD characters, and won't wrap in PDF:"
 cat ../library/**/*.hpp |
@@ -28,11 +26,11 @@ git submodule update
 	npm run test
 )
 
-find ../library/ -depth -execdir rename --all '_' ' ' {} \;
+find ../library/ -depth -execdir rename 'y/_/ /' {} \;
 
 ./../notebook-generator/bin/notebookgen ../library/ --author "SDSMT" --initials SDSMT --output ./hackpack.pdf --size 8 --columns 3
 
-find ../library/ -depth -execdir rename --all ' ' '_' {} \;
+find ../library/ -depth -execdir rename 'y/ /_/' {} \;
 
 #remove hash code comments
 sed -i '1,2d' ../library/**/*.hpp
