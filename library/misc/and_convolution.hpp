890279af9bd50c4532eb4eb4569bb51d250668fc
Cameron Custer <73217097+cameroncuster@users.noreply.github.com>
2023-05-01 01:35:53 -0400

    add and convolution to the hackpack (#362)
    * add and convolution to the hackpack
    
    * [auto-verifier] verify commit e0ec671e63f323549b38f7e89e54d439ccd25b17
    
    * fix int lint issue
    
    * [auto-verifier] verify commit b2af37b0b9e878464e54259939c7d23b93fb2b9a
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/misc/and_convolution.hpp
@@ -0,0 +1,30 @@
+/** @file */
+#pragma once
+const int MOD = 998'244'353;
+/**
+ * @param a,b arrays of the same length, where the length is a power of 2
+ * @returns array `c` where `c[k]` = the sum of (a[i] * b[j]) for all pairs
+ * (i,j) where i & j == k
+ * @time O(2^n * n)
+ * @memory O(2^n)
+ */
+vector<int> and_convolution(vector<int> a, vector<int> b) {
+    int n = __lg(ssize(a));
+    assert(ssize(a) == ssize(b) && (1 << n) == ssize(a));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (1 << n); j++) {
+            if (!((j >> i) & 1)) {
+                a[j] = (a[j] + a[j ^ (1 << i)]) % MOD;
+                b[j] = (b[j] + b[j ^ (1 << i)]) % MOD;
+            }
+        }
+    }
+    vector<int> c(1 << n);
+    for (int i = 0; i < (1 << n); i++)
+        c[i] = int(1LL * a[i] * b[i] % MOD);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < (1 << n); j++)
+            if (!((j >> i) & 1))
+                c[j] = (c[j] - c[j ^ (1 << i)] + MOD) % MOD;
+    return c;
+}
