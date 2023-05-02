de8d81ab59c8b991680bcb69f4bd42fdefd7e7f8
cameron custer <custer.cameron@gmail.com>
2023-05-01 22:55:14 -0400

    fix lint error

--- library/contest/run.sh
+++ library/contest/run.sh
@@ -3,7 +3,7 @@
 #source: https://github.com/Errichto/youtube/blob/master/testing/s.sh
 #usage: bash test.sh
 for ((i = 1; ; ++i)); do
-	echo $i
+	echo "$i"
 	./test.out > input
 	diff -w <(./a.out < input) <(./brute.out < input) || break
 done
