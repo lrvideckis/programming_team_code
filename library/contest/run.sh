6c634557f4aa7d77de6c6516acf4a6f0c5091be6
cameron custer <custer.cameron@gmail.com>
2023-05-01 22:51:25 -0400

    small nits to test run script for linux OS

--- library/contest/run.sh
+++ library/contest/run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #runs 2 programs against each other on random inputs until they output different results
 #source: https://github.com/Errichto/youtube/blob/master/testing/s.sh
-#usage:
-#	chmod +x test.sh
-#	./test.sh
+#usage: bash test.sh
 for ((i = 1; ; ++i)); do
-	echo "$i"
-	./test.out >in
-	diff --ignore-all-space <(./a.out <in) <(./brute.out <in) || break
+	echo $i
+	./test.out > input
+	diff -w <(./a.out < input) <(./brute.out < input) || break
 done
