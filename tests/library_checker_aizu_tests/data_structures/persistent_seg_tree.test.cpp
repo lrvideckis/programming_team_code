e92f50e681c37bb220ca059cfe7346c35d5c32c1
Luke Videckis <lukevideckis@gmail.com>
2023-05-09 23:40:20 -0600

    don't compress Y cords to test large range

--- tests/library_checker_aizu_tests/data_structures/persistent_seg_tree.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/persistent_seg_tree.test.cpp
@@ -11,28 +11,20 @@ int main() {
     int n, q;
     cin >> n >> q;
     vector<array<int, 3>> points(n);
-    vector<int> all_y(n);
     for (int i = 0; i < n; i++) {
         int x, y, w;
         cin >> x >> y >> w;
         points[i] = {x, y, w};
-        all_y[i] = y;
     }
     sort(begin(points), end(points));
-    sort(begin(all_y), end(all_y));
-    all_y.erase(unique(begin(all_y), end(all_y)), end(all_y));
-    PST pst(-5, ssize(all_y));
-    for (const auto& point : points) {
-        int val = int(lower_bound(begin(all_y), end(all_y), point[1]) - begin(all_y));
-        pst.update(val, point[2], ssize(pst.roots) - 1);
-    }
+    PST pst(-5, 1'000'000'000);
+    for (const auto& point : points)
+        pst.update(point[1], point[2], ssize(pst.roots) - 1);
     while (q--) {
         int le, down, ri, up;
         cin >> le >> down >> ri >> up;
         le = int(lower_bound(begin(points), end(points), array<int, 3>({le, -1, -1})) - begin(points));
         ri = int(lower_bound(begin(points), end(points), array<int, 3>({ri, -1, -1})) - begin(points));
-        down = int(lower_bound(begin(all_y), end(all_y), down) - begin(all_y));
-        up = int(lower_bound(begin(all_y), end(all_y), up) - begin(all_y));
         cout << pst.query(down, up, ri) - pst.query(down, up, le) << '\n';
     }
     return 0;
