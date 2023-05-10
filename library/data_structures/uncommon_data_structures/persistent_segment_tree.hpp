5dbd3cf3809a0961496a6c46f4a1c968d596dd22
Luke Videckis <lukevideckis@gmail.com>
2023-05-09 19:38:01 -0600

    it compiles

--- library/data_structures/uncommon_data_structures/persistent_segment_tree.hpp
+++ library/data_structures/uncommon_data_structures/persistent_segment_tree.hpp
@@ -12,32 +12,32 @@ struct PST {
     void update(int idx, long long change, int version) {
         assert(root_l <= idx && idx < root_r);
         assert(0 <= version && version < ssize(roots));
-        roots.push_back(update_impl(idx, change, root_l, root_r, roots[version]);
+        roots.push_back(update_impl(idx, change, root_l, root_r, roots[version]));
     }
     int update_impl(int idx, long long change, int tl, int tr, int v) {
         if (tr - tl == 1) {
             assert(tl == idx);
-            tree.emplace_back(tree[v].sum + str_len, 0, 0);
+            tree.emplace_back(tree[v].sum + change, 0, 0);
             return ssize(tree) - 1;
         }
         int tm = tl + (tr - tl) / 2;
         int lch = tree[v].lch;
         int rch = tree[v].rch;
         if (idx < tm)
-            lch = update(idx, str_len, tl, tm, lch);
+            lch = update_impl(idx, change, tl, tm, lch);
         else
-            rch = update(idx, str_len, tm, tr, rch);
+            rch = update_impl(idx, change, tm, tr, rch);
         tree.emplace_back(tree[lch].sum + tree[rch].sum, lch, rch);
         return ssize(tree) - 1;
     }
     long long query(int le, int ri, int version) const {
-        assert(0 <= idx && idx < N);
+        assert(root_l <= le && ri <= root_r);
         assert(0 <= version && version < ssize(roots));
         return query_impl(le, ri, root_l, root_r, roots[version]);
     }
     long long query_impl(int le, int ri, int tl, int tr, int v) const {
-        if (v == 0 || ) return 0;
-        if () return tree[v].sum;
+        if (v == 0 || ri <= tl || tr <= le) return 0;
+        if (le <= tl && tr <= ri) return tree[v].sum;
         int tm = tl + (tr - tl) / 2;
         return query_impl(le, ri, tl, tm, tree[v].lch) +
                query_impl(le, ri, tm, tr, tree[v].rch);
