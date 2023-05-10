e32c32e629996eb9391cad31c98a4a89b841c1c4
Luke Videckis <lukevideckis@gmail.com>
2023-05-09 23:53:58 -0600

    added doxygen

--- library/data_structures/uncommon_data_structures/persistent_segment_tree.hpp
+++ library/data_structures/uncommon_data_structures/persistent_segment_tree.hpp
@@ -1,5 +1,9 @@
 /** @file */
 #pragma once
+/**
+ * @see https://cp-algorithms.com/data_structures/segment_tree.html
+ *     preserving-the-history-of-its-values-persistent-segment-tree
+ */
 //NOLINTNEXTLINE(readability-identifier-naming)
 struct PST {
     struct node {
@@ -7,19 +11,29 @@ struct PST {
         int lch, rch; /**< children, indexes into `tree` */
         node(long long a_sum, int a_lch, int a_rch) : sum(a_sum), lch(a_lch), rch(a_rch) {}
     };
-    const int L, R;//TODO: check that clang tidy enforces all caps here
-    vector<int> roots;
+    const int ROOT_L, ROOT_R;
+    vector<int> roots; /**< tree[roots[i]] = root node at version i */
     deque<node> tree;
+    /**
+     * @param root_l,root_r defines range [ROOT_L, ROOT_L) of root node, can be
+     * negative
+     * @time O(1)
+     */
     PST(int root_l, int root_r) :
-        L(root_l), R(root_r), roots(1), tree(1, {0LL, 0, 0}) {}
+        ROOT_L(root_l), ROOT_R(root_r), roots(1), tree(1, {0LL, 0, 0}) {}
+    /**
+     * @param idx,change does arr[idx] += change
+     * @param version which version to update. Each call to update creates a
+     * new version. Initially there is a dummy version.
+     * @time O(log(ROOT_R - ROOT_L))
+     */
     void update(int idx, long long change, int version) {
-        assert(L <= idx && idx < R);
+        assert(ROOT_L <= idx && idx < ROOT_R);
         assert(0 <= version && version < ssize(roots));
-        roots.push_back(update_impl(idx, change, L, R, roots[version]));
+        roots.push_back(update_impl(idx, change, ROOT_L, ROOT_R, roots[version]));
     }
     int update_impl(int idx, long long change, int tl, int tr, int v) {
         if (tr - tl == 1) {
-            assert(tl == idx);//TODO: remove
             tree.emplace_back(tree[v].sum + change, 0, 0);
             return ssize(tree) - 1;
         }
@@ -33,10 +47,16 @@ struct PST {
         tree.emplace_back(tree[lch].sum + tree[rch].sum, lch, rch);
         return ssize(tree) - 1;
     }
+    /**
+     * @param le, ri defines range [le, ri)
+     * @param version which version to query
+     * @returns sum of range for that version
+     * @time O(log(ROOT_R - ROOT_L))
+     */
     long long query(int le, int ri, int version) const {
-        assert(L <= le && ri <= R);
+        assert(ROOT_L <= le && ri <= ROOT_R);
         assert(0 <= version && version < ssize(roots));
-        return query_impl(le, ri, L, R, roots[version]);
+        return query_impl(le, ri, ROOT_L, ROOT_R, roots[version]);
     }
     long long query_impl(int le, int ri, int tl, int tr, int v) const {
         if (v == 0 || ri <= tl || tr <= le) return 0;
