fdce6baf3b8eeea9d2bb6e1e6392783d3d1e92c1
Luke Videckis <lukevideckis@gmail.com>
2023-05-10 17:24:26 -0600

    Inline (#380)
    * make lca inline
    
    * [auto-verifier] verify commit a1761ff2b5a272c832b82cbd96c69fe124c09b1f
    
    * add inline
    
    * [auto-verifier] verify commit 6371fed09636c48373215c1354e8bc24172f4906
    
    * remove inline
    
    * make disjoint rmq inline
    
    * [auto-verifier] verify commit 6cf31061bf213c3b24ce819d3fd27f379172ab3a
    
    * inline
    
    * [auto-verifier] verify commit b8c89ff5e1504cfb14a872c81b1a2eca352d4906
    
    * [auto-verifier] verify commit 9370ba18c126bd9380f4f411f76a03417f604aa0
    
    * add more inline
    
    * [auto-verifier] verify commit c0577ef54b4ad8ba6e21f0c15c77197ac307358f
    
    * more inline
    
    * dsu inline
    
    * iter seg tree inline
    
    * [auto-verifier] verify commit 06bdcb5582ebdbec9c21f39633601d20a75860a9
    
    * make safe hash inline and add test
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/data_structures/uncommon_data_structures/iterative_lazy_segment_tree.hpp
+++ library/data_structures/uncommon_data_structures/iterative_lazy_segment_tree.hpp
@@ -12,18 +12,18 @@ struct iter_seg_tree {
     seg_tree st;
     iter_seg_tree(int n) : S(1 << __lg(2 * n - 1)), st(n) {}
     iter_seg_tree(const vector<long long>& arr) : S(1 << __lg(2 * ssize(arr) - 1)), st(arr) {}
-    void pull(int v) {st.tree[v] = op(st.tree[2 * v], st.tree[2 * v + 1]);}
+    inline void pull(int v) {st.tree[v] = op(st.tree[2 * v], st.tree[2 * v + 1]);}
     /**
      * @see https://github.com/ecnerwala/cp-book /blob/master/src/seg_tree.hpp
      */
-    int to_leaf(int i) const {
+    inline int to_leaf(int i) const {
         i += S;
         return i < 2 * st.N ? i : 2 * (i - st.N);
     }
     /**
      * @see https://github.com/ecnerwala/cp-book /blob/master/src/seg_tree.hpp
      */
-    pair<int, int> get_node_bounds(int a) const {
+    inline pair<int, int> get_node_bounds(int a) const {
         assert(1 <= a && a < 2 * st.N);
         int l = __builtin_clz(a) - __builtin_clz(2 * st.N - 1);
         int x = a << l, y = (a + 1) << l;
