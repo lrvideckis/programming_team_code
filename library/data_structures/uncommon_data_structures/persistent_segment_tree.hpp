4e1e3ba5e1221f6b3dbb9271a1d616ff04457a4a
Luke Videckis <lukevideckis@gmail.com>
2023-05-09 23:22:26 -0600

    first draft of test

--- library/data_structures/uncommon_data_structures/persistent_segment_tree.hpp
+++ library/data_structures/uncommon_data_structures/persistent_segment_tree.hpp
@@ -4,19 +4,19 @@ struct PST {
         int lch, rch; /**< children, indexes into `tree` */
         node(long long a_sum, int a_lch, int a_rch) : sum(a_sum), lch(a_lch), rch(a_rch) {}
     };
-    const int root_l, root_r;//TODO: check that clang tidy enforces all caps here
+    const int L, R;//TODO: check that clang tidy enforces all caps here
     vector<int> roots;
     deque<node> tree;
     PST(int a_root_l, int a_root_r) :
-        root_l(a_root_l), root_r(a_root_r), roots(1), tree(1, {0LL, 0, 0}) {}
+        L(a_root_l), R(a_root_r), roots(1), tree(1, {0LL, 0, 0}) {}
     void update(int idx, long long change, int version) {
-        assert(root_l <= idx && idx < root_r);
+        assert(L <= idx && idx < R);
         assert(0 <= version && version < ssize(roots));
-        roots.push_back(update_impl(idx, change, root_l, root_r, roots[version]));
+        roots.push_back(update_impl(idx, change, L, R, roots[version]));
     }
     int update_impl(int idx, long long change, int tl, int tr, int v) {
         if (tr - tl == 1) {
-            assert(tl == idx);
+            assert(tl == idx);//TODO: remove
             tree.emplace_back(tree[v].sum + change, 0, 0);
             return ssize(tree) - 1;
         }
@@ -31,9 +31,9 @@ struct PST {
         return ssize(tree) - 1;
     }
     long long query(int le, int ri, int version) const {
-        assert(root_l <= le && ri <= root_r);
+        assert(L <= le && ri <= R);
         assert(0 <= version && version < ssize(roots));
-        return query_impl(le, ri, root_l, root_r, roots[version]);
+        return query_impl(le, ri, L, R, roots[version]);
     }
     long long query_impl(int le, int ri, int tl, int tr, int v) const {
         if (v == 0 || ri <= tl || tr <= le) return 0;
--- tests/library_checker_aizu_tests/data_structures/persistent_seg_tree.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/persistent_seg_tree.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/static_rectangle_add_rectangle_sum"
+#define PROBLEM "https://judge.yosupo.jp/problem/rectangle_sum"
 #include "../template.hpp"
 
 #include "../../../library/data_structures/uncommon_data_structures/persistent_segment_tree.hpp"
@@ -7,6 +7,32 @@ int main() {
     cin.tie(0)->sync_with_stdio(0);
     int n, q;
     cin >> n >> q;
-    PST pst(-5, 1e9);
+
+    vector<array<int, 3>> points(n);
+    for(int i = 0; i < n; i++) {
+        int x, y, w;
+        cin >> x >> y >> w;
+        points[i] = {x, y, w};
+    }
+
+    //TODO: check that CI catches these non-cam style iterators
+    sort(points.begin(), points.end(), [](const array<int, 3>& a, const array<int, 3>& b) -> bool {
+        return a[0] < b[0];
+    });
+
+    PST pst(-5, 1'000'000'000);
+
+    for(const auto& point : points) {
+        pst.update(point[1], point[2], ssize(pst.roots) - 1);
+    }
+
+    while(q--) {
+        int le, down, ri, up;
+        cin >> le >> down >> ri >> up;
+        le = lower_bound(points.begin(), points.end(), array<int, 3>({le, -1, -1})) - points.begin();
+        ri = lower_bound(points.begin(), points.end(), array<int, 3>({ri, -1, -1})) - points.begin();
+        cout << pst.query(down, up, ri) - pst.query(down, up, le) << '\n';
+    }
+
     return 0;
 }
