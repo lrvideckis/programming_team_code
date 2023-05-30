dcda9d2c10f5f03dfcbd47bab0f55b7f94bbb9e1
Luke Videckis <lukevideckis@gmail.com>
2023-05-30 11:41:55 -0500

    fibonacci without unordered map (#393)
    * a different implementation without unordered map
    
    * [auto-verifier] verify commit 9dcdc47b041f7da33ac77e02a7d3f8e0550b7d1d
    
    * break up long word so it doesn't overlap in hackpack
    
    * [auto-verifier] verify commit bfaaae002797b44e65cd8cf316767e69d68e64ce
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/math/fibonacci.hpp
+++ library/math/fibonacci.hpp
@@ -1,16 +1,25 @@
 /** @file */
 #pragma once
 const int MOD = 998'244'353;
-unordered_map<long long, long long> table; /**< for memoization */
 /**
- * @see https://codeforces.com/blog/entry/14516
+ * @see https://codeforces.com/blog/entry/14516 https://cp-algorithms.com/algebra /fibonacci-numbers.html#fast-doubling-method
+ * @code{.cpp}
+ *     auto val = fib(n).first;
+ *     //n=0 -> val=0
+ *     //n=1 -> val=1
+ *     //n=2 -> val=1
+ *     //n=3 -> val=2
+ *     //n=4 -> val=3
+ *     //n=5 -> val=5
+ * @endcode
  * @param n an integer
- * @returns nth fibonacci number
+ * @returns a pair {nth fibonacci number, (n+1)th fibonacci number}
  * @time O(log n)
  */
-long long fib(long long n) {
-    if (n < 2) return 1;
-    if (table.find(n) != end(table)) return table[n];
-    table[n] = (fib((n + 1) / 2) * fib(n / 2) + fib((n - 1) / 2) * fib((n - 2) / 2)) % MOD;
-    return table[n];
+pair<long long, long long> fib(long long n) {
+    if (n == 0) return {0LL, 1LL};
+    auto [x, y] = fib(n >> 1);
+    long long c = x * (2 * y - x + MOD) % MOD, d = (x * x + y * y) % MOD;
+    if (n & 1) return {d, (c + d) % MOD};
+    return {c, d};
 }
