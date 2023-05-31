86ab5d376bbc5af05496756c34a589fb282780e0
Luke Videckis <lukevideckis@gmail.com>
2023-05-30 23:15:12 -0500

    I don't like reusing the same variable to mean 2 different things (#398)
    * I don't like reusing the same variable to mean 2 different things
    
    * [auto-verifier] verify commit 4ea2f4a498cbc2eee63bca65241f282e149b77ed
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

--- library/graphs/least_common_ancestor.hpp
+++ library/graphs/least_common_ancestor.hpp
@@ -41,8 +41,8 @@ struct LCA {
      */
     inline int lca(int u, int v) const {
         if (u == v) return u;
-        tie(u, v) = minmax(in[u], in[v]);
-        return p[rmq.query(u + 1, v + 1)];
+        auto [x, y] = minmax(in[u], in[v]);
+        return p[rmq.query(x + 1, y + 1)];
     }
     /**
      * @param u,v endpoint nodes of path
