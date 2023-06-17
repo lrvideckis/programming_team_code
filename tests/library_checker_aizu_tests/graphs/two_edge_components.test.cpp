724952dfc84e9ebf6bba3ddfb8b65d0bcba3add8
Luke Videckis <lukevideckis@gmail.com>
2023-06-17 15:16:30 -0500

    fix paths

--- tests/library_checker_aizu_tests/graphs/two_edge_components.test.cpp
+++ tests/library_checker_aizu_tests/graphs/two_edge_components.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/two_edge_connected_components"
 #include "../template.hpp"
 #include "../../../library/graphs/bridge_tree.hpp"
-#include "../../../library/data_structures/dsu_restorable.hpp"
+#include "../../../library/data_structures/uncommon_data_structures/dsu_restorable.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
