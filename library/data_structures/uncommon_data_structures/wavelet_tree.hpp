ca49e185b8c389c243e97010395cd8c192530feb
Luke Videckis <lukevideckis@gmail.com>
2023-06-20 10:39:57 -0500

    address some todos

--- library/data_structures/uncommon_data_structures/wavelet_tree.hpp
+++ library/data_structures/uncommon_data_structures/wavelet_tree.hpp
@@ -23,7 +23,7 @@ struct bit_presum {
             presum[i+1] = __builtin_popcountll(mask[i]) + presum[i];
     }
     // 0 <= idx < i
-    int popcount(int i) const {
+    inline int popcount(int i) const {
         assert(0 <= i && i <= n);
         return presum[i>>6] + __builtin_popcountll(mask[i>>6] & ((1ULL << (i & 63))-1));
     }
@@ -34,9 +34,6 @@ inline int split(int tl, int tr) {
     return min(tl + pw2, tr - pw2 / 2);
 }
 
-//TODO rearrange params to match other seg trees
-//TODO rename params to le, ri
-//TODO which functions to make inline
 //TODO figure out which debug flag makes this test TLE
 struct wavelet_tree {
     const int N, MINV, MAXV;
@@ -46,30 +43,30 @@ struct wavelet_tree {
     wavelet_tree(vector<int> arr, int minv, int maxv) : N(ssize(arr)), MINV(minv), MAXV(maxv), tree(2*(MAXV-MINV), vector<bool>()) {
         for(int val : arr) assert(MINV <= val && val < MAXV);//TODO: should I keep this?
         arr.reserve(2 * ssize(arr));//so that stable_partition is O(n)
-        build(arr, 0, N, 1, MINV, MAXV);
+        build(arr, 0, N, MINV, MAXV, 1);
     }
 
-    void build(vector<int>& arr, int b, int e, int p, int l, int r) {
-        int m = split(l,r);
-        auto low = [&](int val) -> bool {return val < m;};
-        vector<bool> bits(e-b);
-        transform(begin(arr) + b, begin(arr) + e, begin(bits), low);
-        tree[p] = bit_presum(bits);
-        if (r-l == 1) return;
-        int m2 = stable_partition(begin(arr)+b, begin(arr)+e, low) - begin(arr);
-        build(arr, b, m2, 2*p, l, m);
-        build(arr, m2, e, 2*p+1, m, r);
+    void build(vector<int>& arr, int le, int ri, int tl, int tr, int v) {
+        int tm = split(tl,tr);
+        auto low = [&](int val) -> bool {return val < tm;};
+        vector<bool> bits(ri-le);
+        transform(begin(arr) + le, begin(arr) + ri, begin(bits), low);
+        tree[v] = bit_presum(bits);
+        if (tr-tl == 1) return;
+        int mi = stable_partition(begin(arr)+le, begin(arr)+ri, low) - begin(arr);
+        build(arr, le, mi, tl, tm, 2*v);
+        build(arr, mi, ri, tm, tr, 2*v+1);
     }
 
-    //kth(i,j,0) returns min of range [i,j)
-    int kth(int i, int j, int k) const {
+    //kth(le,ri,0) returns min of range [le,ri)
+    int kth(int le, int ri, int k) const {
         //TODO: add asserts cleaning params
-        return kth(i, j, k, 1, MINV, MAXV);
+        return kth(le, ri, k, MINV, MAXV, 1);
     }
-    int kth(int i, int j, int k, int p, int l, int r) const {
-        if (r-l == 1) return l;
-        int m = split(l,r), ei = tree[p].popcount(i), ej = tree[p].popcount(j);
-        if (k < ej-ei) return kth(ei, ej, k, 2*p, l, m);
-        return kth(i-ei, j-ej, k-(ej-ei), 2*p+1, m, r);
+    int kth(int le, int ri, int k, int tl, int tr, int v) const {
+        if (tr-tl == 1) return tl;
+        int tm = split(tl,tr), pl = tree[v].popcount(le), pr = tree[v].popcount(ri);
+        if (k < pr-pl) return kth(pl, pr, k, tl, tm, 2*v);
+        return kth(le-pl, ri-pr, k-(pr-pl), tm, tr, 2*v+1);
     }
 };
