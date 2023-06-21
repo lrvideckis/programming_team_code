e74c7e754b351f705cb92f60625e309fa1376e9b
Luke Videckis <lukevideckis@gmail.com>
2023-06-21 14:45:49 -0500

    revert old test for speed

--- tests/library_checker_aizu_tests/data_structures/kth_smallest.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/kth_smallest.test.cpp
@@ -2,7 +2,6 @@
 #include "../template.hpp"
 
 #include "../../../library/data_structures/uncommon_data_structures/kth_smallest.hpp"
-#include "../../../library/data_structures/uncommon_data_structures/wavelet_tree.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -14,24 +13,18 @@ int main() {
     vector<int> sorted(arr);
     sort(begin(sorted), end(sorted));
     sorted.erase(unique(begin(sorted), end(sorted)), end(sorted));
-    for (int& val : arr)
-        val = int(lower_bound(begin(sorted), end(sorted), val) - begin(sorted)) - 50;
     kth_smallest st(arr);
-    wavelet_tree wt(arr, -50, ssize(sorted) - 50);
     for (int i = 0; i < n; i++) {
         int mx = arr[i];
         for (int j = i + 1; j <= min(i + 5, n); j++) {
             mx = max(mx, arr[j - 1]);
             assert(st.query(i, j, j - i) == mx);
-            assert(wt.kth_smallest(i, j, j - i) == mx);
         }
     }
     while (q--) {
         int l, r, k;
         cin >> l >> r >> k;
-        int res = st.query(l, r, k + 1);
-        assert(res == wt.kth_smallest(l, r, k + 1));
-        cout << sorted[res + 50] << '\n';
+        cout << st.query(l, r, k + 1) << '\n';
     }
     return 0;
 }
---
+++ tests/library_checker_aizu_tests/data_structures/kth_smallest_wavelet.test.cpp
@@ -0,0 +1,38 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_kth_smallest"
+#undef _GLIBCXX_DEBUG
+#include "../template.hpp"
+
+#include "../../../library/data_structures/uncommon_data_structures/kth_smallest.hpp"
+#include "../../../library/data_structures/uncommon_data_structures/wavelet_tree.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, q;
+    cin >> n >> q;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    vector<int> sorted(arr);
+    sort(begin(sorted), end(sorted));
+    sorted.erase(unique(begin(sorted), end(sorted)), end(sorted));
+    for (int& val : arr)
+        val = int(lower_bound(begin(sorted), end(sorted), val) - begin(sorted)) - 50;
+    kth_smallest st(arr);
+    wavelet_tree wt(arr, -50, ssize(sorted) - 50);
+    for (int i = 0; i < n; i++) {
+        int mx = arr[i];
+        for (int j = i + 1; j <= min(i + 5, n); j++) {
+            mx = max(mx, arr[j - 1]);
+            assert(st.query(i, j, j - i) == mx);
+            assert(wt.kth_smallest(i, j, j - i) == mx);
+        }
+    }
+    while (q--) {
+        int l, r, k;
+        cin >> l >> r >> k;
+        int res = st.query(l, r, k + 1);
+        assert(res == wt.kth_smallest(l, r, k + 1));
+        cout << sorted[res + 50] << '\n';
+    }
+    return 0;
+}
