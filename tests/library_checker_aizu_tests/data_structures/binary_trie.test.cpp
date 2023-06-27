2918d6a36bb6c19b5fff049e1546687f1d285bc1
Luke Videckis <lukevideckis@gmail.com>
2023-06-27 15:38:49 -0700

    reorganize

--- tests/library_checker_aizu_tests/data_structures/binary_trie.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/binary_trie.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/set_xor_min"
 #include "../template.hpp"
 
-#include "../../../library/strings/binary_trie.hpp"
+#include "../../../library/data_structures/binary_trie.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
