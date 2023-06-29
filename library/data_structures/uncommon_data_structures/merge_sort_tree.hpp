5567192eeb6462601a1f4e9cc5bf7ebdb13c6e2d
Luke Videckis <lukevideckis@gmail.com>
2023-06-29 01:45:32 -0700

    refactor to use less space

--- library/data_structures/uncommon_data_structures/merge_sort_tree.hpp
+++ library/data_structures/uncommon_data_structures/merge_sort_tree.hpp
@@ -1,24 +1,24 @@
 /** @file */
 #pragma once
-
+#include "bit_presum.hpp"
 inline int split_2(int tl, int tr) {
     int pw2 = 1 << __lg(tr - tl);
     return min(tl + pw2, tr - pw2 / 2);
 }
-
 /**
  * For point updates: either switch to policy based BST, or use sqrt
  * decomposition.
  */
 struct merge_sort_tree {
     const int N;
-    vector<vector<int>> tree, num_went_left/*TODO: rename*/;
+    vector<vector<int>> tree;
+    vector<bit_presum> bit_presums;
     /**
      * @param arr static array
      * @time O(n log n)
      * @space O(n log n) for `tree` vector
      */
-    merge_sort_tree(const vector<int>& arr) : N(ssize(arr)), tree(2 * N), num_went_left(N) {
+    merge_sort_tree(const vector<int>& arr) : N(ssize(arr)), tree(2 * N), bit_presums(N, vector<bool>()) {
         transform(begin(arr), end(arr), begin(tree) + N, [](int val) -> vector<int> {return {val};});
         rotate(begin(tree) + N, begin(tree) + (N ? 3 * N - (2 << __lg(N)) : 0), end(tree));
         vector<pair<int, bool>> both(N);
@@ -30,9 +30,10 @@ struct merge_sort_tree {
             transform(begin(ri), end(ri), begin(both) + ssize(le), [](int val) {return pair(val, 0);});
             inplace_merge(begin(both), begin(both) + ssize(le), begin(both) + tot);
             tree[i].resize(tot);
+            vector<bool> bits(tot);
             transform(begin(both), begin(both) + tot, begin(tree[i]), [](auto val) {return val.first;});
-            num_went_left[i].resize(tot + 1);
-            transform_inclusive_scan(begin(both), begin(both) + tot, begin(num_went_left[i]) + 1, plus{}, [](auto val) {return int(val.second);});
+            transform(begin(both), begin(both) + tot, begin(bits), [](auto val) {return val.second;});
+            bit_presums[i] = bit_presum(bits);
         }
     }
     int query(int le, int ri, int x, int y) const {
@@ -43,7 +44,7 @@ struct merge_sort_tree {
     int query_impl(int le, int ri, int xi, int yi, int tl, int tr, int v) const {
         if (ri <= tl || tr <= le) return 0;
         if (le <= tl && tr <= ri) return yi - xi;
-        int tm = split_2(tl, tr), pl = num_went_left[v][xi], pr = num_went_left[v][yi];
+        int tm = split_2(tl, tr), pl = bit_presums[v].popcount(xi), pr = bit_presums[v].popcount(yi);
         return query_impl(le, ri, pl, pr, tl, tm, 2 * v) +
                query_impl(le, ri, xi - pl, yi - pr, tm, tr, 2 * v + 1);
     }
