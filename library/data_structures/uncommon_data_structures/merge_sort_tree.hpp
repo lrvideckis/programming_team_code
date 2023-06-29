cb0e5c43ac13088214ec2bc9b7c4372ce15ca77f
Luke Videckis <lukevideckis@gmail.com>
2023-06-29 00:59:17 -0700

    could be an idea for golfing

--- library/data_structures/uncommon_data_structures/merge_sort_tree.hpp
+++ library/data_structures/uncommon_data_structures/merge_sort_tree.hpp
@@ -18,35 +18,25 @@ struct merge_sort_tree {
      * @time O(n log n)
      * @space O(n log n) for `tree` vector
      */
-    merge_sort_tree(const vector<int>& arr) : N(ssize(arr)), tree(2 * N), num_went_left(N, vector<int>(1)) {
+    merge_sort_tree(const vector<int>& arr) : N(ssize(arr)), tree(2 * N), num_went_left(N) {
         transform(begin(arr), end(arr), begin(tree) + N, [](int val) -> vector<int> {return {val};});
         rotate(begin(tree) + N, begin(tree) + (N ? 3 * N - (2 << __lg(N)) : 0), end(tree));
         for (int i = N - 1; i >= 1; i--) {
             const auto& le = tree[2 * i];
             const auto& ri = tree[2 * i + 1];
-            //TODO: golf this: change tree to vector<vector<pair<int, int>>> maybe?
-            //well no cuz we want num_went_left to be only length N
-            int ptrL = 0, ptrR = 0;
-            while(ptrL < ssize(le) && ptrR < ssize(ri)) {
-                if(le[ptrL] < ri[ptrR]) {
-                    tree[i].push_back(le[ptrL++]);
-                    num_went_left[i].push_back(num_went_left[i].back() + 1);
-                } else {
-                    tree[i].push_back(ri[ptrR++]);
-                    num_went_left[i].push_back(num_went_left[i].back());
+            tree[i].resize(ssize(le) + ssize(ri));
+            merge(begin(le), end(le), begin(ri), end(ri), begin(tree[i]));
+            num_went_left[i].resize(ssize(tree[i]) + 1);
+            auto it = begin(le);
+            for(int j = 0; j < ssize(tree[i]); j++) {
+                it = find_if(it, end(le), [&](int val) {return val >= tree[i][j];});
+                if(it == end(le)) break;
+                if(*it == tree[i][j]) {
+                    num_went_left[i][j + 1] = 1;
+                    it++;
                 }
             }
-            while(ptrL < ssize(le)) {
-                tree[i].push_back(le[ptrL++]);
-                num_went_left[i].push_back(num_went_left[i].back() + 1);
-            }
-            while(ptrR < ssize(ri)) {
-                tree[i].push_back(ri[ptrR++]);
-                num_went_left[i].push_back(num_went_left[i].back());
-            }
-
-            //tree[i].resize(ssize(le) + ssize(ri));
-            //merge(begin(le), end(le), begin(ri), end(ri), begin(tree[i]));
+            partial_sum(begin(num_went_left[i]), end(num_went_left[i]), begin(num_went_left[i]));
         }
     }
     int query(int le, int ri, int x, int y) const {
@@ -56,10 +46,8 @@ struct merge_sort_tree {
     }
     int query_impl(int le, int ri, int xi, int yi, int tl, int tr, int v) const {
         if (ri <= tl || tr <= le) return 0;
-        //invariant: all values in subarray [xi, yi) in tree[v] are in range [x, y)
         if (le <= tl && tr <= ri) return yi - xi;
-        int tm = split_2(tl, tr);
-        int pl = num_went_left[v][xi], pr = num_went_left[v][yi];
+        int tm = split_2(tl, tr), pl = num_went_left[v][xi], pr = num_went_left[v][yi];
         return query_impl(le, ri, pl, pr, tl, tm, 2 * v) +
                query_impl(le, ri, xi - pl, yi - pr, tm, tr, 2 * v + 1);
     }
