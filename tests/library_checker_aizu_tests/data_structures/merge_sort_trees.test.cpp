38e12ccf6b04e768cfc278082db76c7530a7713f
Luke Videckis <lukevideckis@gmail.com>
2023-07-02 07:48:39 -1000

    Update and rename merge_sort_tree.test.cpp to merge_sort_trees.test.cpp

--- tests/library_checker_aizu_tests/data_structures/merge_sort_trees.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/merge_sort_trees.test.cpp
@@ -5,6 +5,7 @@
 #include "../template.hpp"
 
 #include "../../../library/data_structures/uncommon_data_structures/merge_sort_tree.hpp"
+#include "../../../library/data_structures/uncommon_data_structures/merge_sort_tree_updates.hpp
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -14,11 +15,15 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> arr[i];
     merge_sort_tree mst(arr);
+    merge_sort_tree_updates mstu(arr);
     while (q--) {
         int le, ri, x;
         cin >> le >> ri >> x;
         assert(mst.query(le, ri, x, x) == 0);
-        cout << mst.query(le, ri, x, x + 1) << '\n';
+        assert(mstu.query(le, ri, x, x) == 0);
+        int res = mst.query(le, ri, x, x + 1);
+        assert(res == mstu.query(le, ri, x, x + 1));
+        cout << res << '\n';
     }
     return 0;
 }
