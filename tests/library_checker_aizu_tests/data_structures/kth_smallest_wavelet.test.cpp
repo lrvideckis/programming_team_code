642b63d94691ebabfe4f654b94b828884d3ed644
Luke Videckis <lukevideckis@gmail.com>
2023-07-04 09:40:52 -1000

    Update kth_smallest_wavelet.test.cpp

--- tests/library_checker_aizu_tests/data_structures/kth_smallest_wavelet.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/kth_smallest_wavelet.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/range_kth_smallest"
 #include "../template.hpp"
 
-#include "../../../library/data_structures/uncommon_data_structures/wavelet_tree.hpp"
+#include "../../../library/data_structures/wavelet_merge/wavelet_tree.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
