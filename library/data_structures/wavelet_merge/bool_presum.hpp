f52655c6021d606ffa427b436033aff815069850
Luke Videckis <lukevideckis@gmail.com>
2023-07-04 22:08:33 -1000

    Update bool_presum.hpp

--- library/data_structures/wavelet_merge/bool_presum.hpp
+++ library/data_structures/wavelet_merge/bool_presum.hpp
@@ -17,8 +17,9 @@ struct bool_presum {
     bool_presum(const vector<bool>& arr) : n(ssize(arr)), mask(n / 64 + 1), presum(ssize(mask)) {
         for (int i = 0; i < n; i++)
             mask[i >> 6] |= (uint64_t(arr[i]) << (i & 63));
-        for (int i = 0; i < ssize(mask) - 1; i++)
-            presum[i + 1] = __builtin_popcountll(mask[i]) + presum[i];
+        exclusive_scan(begin(mask), end(mask), begin(presum), 0, [&](int sum, int i) {return sum + __builtin_popcountll(mask[i]);});
+        //for (int i = 0; i < ssize(mask) - 1; i++)
+            //presum[i + 1] = __builtin_popcountll(mask[i]) + presum[i];
     }
     /**
      * @param i defines range [0, i)
