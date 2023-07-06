1888104d369ec9d807ffc28b673df3fa8b3ed404
Luke Videckis <lukevideckis@gmail.com>
2023-07-06 16:00:11 -0700

    run astyle

--- tests/library_checker_aizu_tests/data_structures/wavelet_merge_predecessor.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/wavelet_merge_predecessor.test.cpp
@@ -17,25 +17,25 @@ int main() {
     vector<int> wavelet_init(n);
     iota(begin(wavelet_init), end(wavelet_init), 0);
     wavelet_tree_updates wtu(wavelet_init, 0, n);
-    for(int i = 0; i < n; i++)
-        if(s[i] == '0') {
+    for (int i = 0; i < n; i++)
+        if (s[i] == '0') {
             mstu.set_active(i, 0);
             wtu.set_active(i, 0);
         }
-    while(q--) {
+    while (q--) {
         int type, k;
         cin >> type >> k;
-        if(type == 0) {
+        if (type == 0) {
             mstu.set_active(k, 1);
             wtu.set_active(k, 1);
-        } else if(type == 1) {
+        } else if (type == 1) {
             mstu.set_active(k, 0);
             wtu.set_active(k, 0);
-        } else if(type == 2) {
+        } else if (type == 2) {
             int res = mstu.rect_count(k, k + 1, 0, 1);
             assert(res == wtu.rect_count(k, k + 1, 0, 1));
             cout << res << '\n';
-        } else if(type == 3) {
+        } else if (type == 3) {
             int num_larger = mstu.rect_count(k, n, 0, 1);
             assert(num_larger == wtu.rect_count(k, n, 0, 1));
             if (num_larger == 0) cout << -1 << '\n';
