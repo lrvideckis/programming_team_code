52ad1357cb9b5adb1944ff57155361fe7bb23a93
Luke Videckis <lukevideckis@gmail.com>
2023-07-06 15:45:17 -0700

    fix compile error

--- tests/library_checker_aizu_tests/data_structures/wavelet_merge_predecessor.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/wavelet_merge_predecessor.test.cpp
@@ -2,6 +2,7 @@
 #include "../template.hpp"
 
 #include "../../../library/data_structures/wavelet_merge/merge_sort_tree_updates.hpp"
+#include "../../../library/data_structures/wavelet_merge/wavelet_tree_updates.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -9,21 +10,28 @@ int main() {
     string s;
     cin >> n >> q >> s;
     merge_sort_tree_updates mstu(vector<int>(n, 0));
+    wavelet_tree_updates wtu(vector<int>(n, 0));
     for(int i = 0; i < n; i++)
-        if(s[i] == '0')
+        if(s[i] == '0') {
             mstu.set_active(i, 0);
+            wtu.set_active(i, 0);
+        }
     while(q--) {
         int type, k;
         cin >> type >> k;
         if(type == 0) {
             mstu.set_active(k, 1);
+            wtu.set_active(k, 1);
         } else if(type == 1) {
             mstu.set_active(k, 0);
+            wtu.set_active(k, 0);
         } else if(type == 2) {
-            cout << mstu.rect_count(k, k + 1, 0, 1) << '\n';
+            int res = mstu.rect_count(k, k + 1, 0, 1);
+            assert(res == wtu.rect_count(k, k + 1, 0, 1));
+            cout << res << '\n';
         } else if(type == 3) {
             int num_larger = mstu.rect_count(k + 1, n, 0, 1);
-            if (num_smaller == 0) cout << -1 << '\n';
+            if (num_larger == 0) cout << -1 << '\n';
             else {
                 cout << mstu.kth_smallest(0, 1, mstu.rect_count(0, k + 1, 0, 1) + 1) << '\n';
             }
