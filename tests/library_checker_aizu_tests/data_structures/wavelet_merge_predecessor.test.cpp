0a6579e0096fc60fbc4b8cd74423b1aaedc3c90e
Luke Videckis <lukevideckis@gmail.com>
2023-07-06 16:32:44 -0700

    trying this

--- tests/library_checker_aizu_tests/data_structures/wavelet_merge_predecessor.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/wavelet_merge_predecessor.test.cpp
@@ -5,8 +5,8 @@
 #include "../template.hpp"
 
 #include "../../../library/data_structures/wavelet_merge/merge_sort_tree_updates.hpp"
-#define split split_2
-#include "../../../library/data_structures/wavelet_merge/wavelet_tree_updates.hpp"
+//#define split split_2
+//#include "../../../library/data_structures/wavelet_merge/wavelet_tree_updates.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -14,44 +14,44 @@ int main() {
     string s;
     cin >> n >> q >> s;
     merge_sort_tree_updates mstu(vector<int>(n, 0));
-    vector<int> wavelet_init(n);
-    iota(begin(wavelet_init), end(wavelet_init), 0);
-    wavelet_tree_updates wtu(wavelet_init, 0, n);
+    //vector<int> wavelet_init(n);
+    //iota(begin(wavelet_init), end(wavelet_init), 0);
+    //wavelet_tree_updates wtu(wavelet_init, 0, n);
     for (int i = 0; i < n; i++)
         if (s[i] == '0') {
             mstu.set_active(i, 0);
-            wtu.set_active(i, 0);
+            //wtu.set_active(i, 0);
         }
     while (q--) {
         int type, k;
         cin >> type >> k;
         if (type == 0) {
             mstu.set_active(k, 1);
-            wtu.set_active(k, 1);
+            //wtu.set_active(k, 1);
         } else if (type == 1) {
             mstu.set_active(k, 0);
-            wtu.set_active(k, 0);
+            //wtu.set_active(k, 0);
         } else if (type == 2) {
             int res = mstu.rect_count(k, k + 1, 0, 1);
-            assert(res == wtu.rect_count(k, k + 1, 0, 1));
+            //assert(res == wtu.rect_count(k, k + 1, 0, 1));
             cout << res << '\n';
         } else if (type == 3) {
             int num_larger = mstu.rect_count(k, n, 0, 1);
-            assert(num_larger == wtu.rect_count(k, n, 0, 1));
+            //assert(num_larger == wtu.rect_count(k, n, 0, 1));
             if (num_larger == 0) cout << -1 << '\n';
             else {
                 int res = mstu.kth_smallest(0, 1, mstu.rect_count(0, k, 0, 1) + 1);
-                assert(res == wtu.kth_smallest(k, n, 1));
+                //assert(res == wtu.kth_smallest(k, n, 1));
                 cout << res << '\n';
             }
         } else {
             assert(type == 4);
             int num_smaller = mstu.rect_count(0, k + 1, 0, 1);
-            assert(num_smaller == wtu.rect_count(0, k + 1, 0, 1));
+            //assert(num_smaller == wtu.rect_count(0, k + 1, 0, 1));
             if (num_smaller == 0) cout << -1 << '\n';
             else {
                 int res = mstu.kth_smallest(0, 1, num_smaller);
-                assert(res == wtu.kth_smallest(0, k + 1, num_smaller));
+                //assert(res == wtu.kth_smallest(0, k + 1, num_smaller));
                 cout << res << '\n';
             }
         }
