08cfff8345bd616ddc0a512d6ad20f10e26d5999
Luke Videckis <lukevideckis@gmail.com>
2023-07-09 23:55:20 -0700

    add fix for sparse table

--- library/data_structures/sparse_table.hpp
+++ library/data_structures/sparse_table.hpp
@@ -20,7 +20,7 @@ template <typename T, typename F = function<T(const T&, const T&)>> struct RMQ {
      * @time O(n log n)
      * @space O(n log n) for `dp` vector
      */
-    RMQ(const vector<T>& arr, const F& a_op) : dp(1, arr), op(a_op) {
+    RMQ(const vector<T>& arr, F a_op) : dp(1, arr), op(a_op) {
         for (int i = 0; (2 << i) <= ssize(arr); i++) {
             dp.emplace_back(ssize(arr) - (2 << i) + 1);
             transform(begin(dp[i]) + (1 << i), end(dp[i]), begin(dp[i]), begin(dp[i + 1]), op);
