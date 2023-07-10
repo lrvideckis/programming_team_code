1edf8cd1fe8572b5d73dcd5a7040f80d27eacd39
Luke Videckis <lukevideckis@gmail.com>
2023-07-10 15:52:29 -0700

    format bash script

--- tests/scripts/print_which_tests_havent_run.sh
+++ tests/scripts/print_which_tests_havent_run.sh
@@ -8,17 +8,17 @@ echo "total number of tests:"
 find tests/library_checker_aizu_tests/ -type f -name "*.test.cpp" | wc --lines
 
 echo "number of tests which have run:"
-grep "test.cpp" .verify-helper/timestamps.remote.json | wc --lines
+grep --count "test.cpp" .verify-helper/timestamps.remote.json
 
 echo "tests which have *not* run:"
 ! comm -23 --check-order <(
-    find tests/library_checker_aizu_tests/ -type f -name "*.test.cpp" |
+	find tests/library_checker_aizu_tests/ -type f -name "*.test.cpp" |
 		sort |
 		uniq
 ) <(
-    grep --only-matching '".*test.cpp"' .verify-helper/timestamps.remote.json |
-        tr -d '"' |
+	grep --only-matching '".*test.cpp"' .verify-helper/timestamps.remote.json |
+		tr -d '"' |
 		sort |
 		uniq
 ) |
-    grep .
+	grep .
