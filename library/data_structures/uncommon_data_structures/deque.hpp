29772fa4b915e798b6d22c1b2e8c559cc08d5e61
Luke Videckis <lukevideckis@gmail.com>
2023-07-09 17:59:24 -0700

    adding first buggy version

---
+++ library/data_structures/uncommon_data_structures/deque.hpp
@@ -0,0 +1,51 @@
+/** @file */
+#pragma once
+template <typename T, typename F = function<T(const T&, const T&)>> struct deq {
+    F op;
+    vector<pair<T, T>> le, ri;
+    deq(const F& a_op) : op(a_op) {}
+    inline T query() const {
+        assert(size());
+        if(le.empty()) return ri.back().second;
+        if(ri.empty()) return le.back().second;
+        return op(le.back().second, le.back().second);
+    }
+    inline int size() const {return ssize(le) + ssize(ri);}
+    inline T front() const {return le.empty() ? ri[0].first : le.back().first;}
+    inline T back() const {return ri.empty() ? le[0].first : ri.back().first;}
+    inline void push_front(const T& elem) {
+        le.emplace_back(elem, le.empty() ? elem : op(elem, le.back().second));
+    }
+    inline void push_back(const T& elem) {
+        ri.emplace_back(elem, ri.empty() ? elem : op(ri.back().second, elem));
+    }
+    inline void pop_front() {
+        assert(size());
+        if(le.empty()) {
+            vector<T> arr(ssize(ri));
+            transform(begin(ri), end(ri), begin(arr), [](pair<T,T> x) {return x.first;});
+            rebuild(arr, (ssize(arr)+1)/2);
+        }
+        assert(!le.empty());
+        le.pop_back();
+    }
+    inline void pop_back() {
+        assert(size());
+        if(ri.empty()) {
+            vector<T> arr(ssize(le));
+            transform(begin(le), end(le), begin(arr), [](pair<T,T> x) {return x.first;});
+            rebuild(arr, ssize(arr)/2);
+        }
+        assert(!ri.empty());
+        ri.pop_back();
+    }
+    inline void rebuild(const vector<T>& arr, int szf) {
+        vector<T> presum(arr);
+        partial_sum(begin(presum) + szf, end(presum), begin(presum) + szf, op);
+        partial_sum(rend(presum) - szf, rend(presum), rend(presum) - szf, [&](T x, T y) {return op(y, x);});
+        le.resize(szf);
+        transform(begin(arr), begin(arr) + szf, begin(presum), begin(le), [](T x, T y) {return pair(x, y);});
+        ri.resize(ssize(arr) - szf);
+        transform(begin(arr) + szf, end(arr), begin(presum) + szf, begin(ri), [](T x, T y) {return pair(x, y);});
+    }
+};
---
+++ tests/library_checker_aizu_tests/data_structures/deque_op.test.cpp
@@ -0,0 +1,57 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/deque_operate_all_composite"
+#include "../template.hpp"
+
+#include "../../../library/data_structures/uncommon_data_structures/deque.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    const int mod = 998'244'353;
+    int q;
+    cin >> q;
+    using line = pair<int, int>;
+    //f1 = begin, f2 = second after begin
+    //we want op(f1, f2) = the function f2(f1(x))
+    deq<line> dq([](const line& a, const line& b) -> line {
+        return pair(1LL * a.first * b.first % mod,
+                    (1LL * b.first * a.second + b.second) % mod
+                    );
+    });
+    deque<line> stl_dq;
+    while(q--) {
+        int type;
+        cin >> type;
+        if(type == 0) {
+            int a, b;
+            cin >> a >> b;
+            dq.push_front(pair(a, b));
+            stl_dq.emplace_front(a, b);
+        } else if(type == 1) {
+            int a, b;
+            cin >> a >> b;
+            dq.push_back(pair(a, b));
+            stl_dq.emplace_back(a, b);
+        } else if(type == 2) {
+            dq.pop_front();
+            stl_dq.pop_front();
+        } else if(type == 3) {
+            dq.pop_back();
+            stl_dq.pop_back();
+        } else {
+            assert(type == 4);
+            int x;
+            cin >> x;
+            if(dq.size() == 0) {
+                cout << x << '\n';
+            } else {
+                line curr = dq.query();
+                cout << (1LL * curr.first * x + curr.second) % mod << '\n';
+            }
+        }
+        assert(ssize(stl_dq) == dq.size());
+        if(dq.size()) {
+            assert(stl_dq.front() == dq.front());
+            assert(stl_dq.back() == dq.back());
+        }
+    }
+    return 0;
+}
