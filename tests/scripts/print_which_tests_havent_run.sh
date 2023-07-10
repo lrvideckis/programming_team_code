2ac2a904dab322e01a77d3cc2c499415f574f67c
Luke Videckis <lukevideckis@gmail.com>
2023-07-10 15:54:54 -0700

    new script! (#439)
    * new script!
    
    * [auto-verifier] verify commit 9919c29d66510ecc99c37673e33f3688390b87d8
    
    * update yml file
    
    * fix script
    
    * format bash script
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ tests/scripts/print_which_tests_havent_run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+
+cd ..
+
+echo "total number of tests:"
+find tests/library_checker_aizu_tests/ -type f -name "*.test.cpp" | wc --lines
+
+echo "number of tests which have run:"
+grep --count "test.cpp" .verify-helper/timestamps.remote.json
+
+echo "tests which have *not* run:"
+! comm -23 --check-order <(
+	find tests/library_checker_aizu_tests/ -type f -name "*.test.cpp" |
+		sort |
+		uniq
+) <(
+	grep --only-matching '".*test.cpp"' .verify-helper/timestamps.remote.json |
+		tr -d '"' |
+		sort |
+		uniq
+) |
+	grep .
