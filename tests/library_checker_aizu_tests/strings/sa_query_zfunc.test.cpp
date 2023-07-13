94b2b6232a249a52aaa3967087f2e8226120c202
Luke Videckis <lukevideckis@gmail.com>
2023-07-13 00:19:10 -0700

    missed these

--- tests/library_checker_aizu_tests/strings/sa_query_zfunc.test.cpp
+++ tests/library_checker_aizu_tests/strings/sa_query_zfunc.test.cpp
@@ -2,13 +2,13 @@
 #include "../template.hpp"
 #include "../../../library/misc/random.hpp"
 
-#include "../../../library/strings/suffix_array_query.hpp"
+#include "../../../library/strings/enhanced_suffix_array.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     string s;
     cin >> s;
-    sa_query sq(s, 128);
+    enhanced_sa esa(s, 128);
     //test `less` function
     {
         for (int num_tests = 50; num_tests--;) {
@@ -16,11 +16,11 @@ int main() {
             auto ri = get_rand<int>(0, ssize(s) - 1);
             if (le > ri)
                 swap(le, ri);
-            assert(sq.less(le, ri) == (s.substr(le) < s.substr(ri)));
+            assert(esa.less(le, ri) == (s.substr(le) < s.substr(ri)));
         }
     }
     for (int i = 0; i < ssize(s); i++)
-        cout << sq.get_lcp(i, 0) << " ";
+        cout << esa.get_lcp(i, 0) << " ";
     cout << '\n';
     return 0;
 }
