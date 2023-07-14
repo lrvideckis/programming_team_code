4970e4174474377b9f4c29df3d8c506c536c1365
Luke Videckis <lukevideckis@gmail.com>
2023-07-13 23:00:11 -0700

    max rect hist. now uses get_range

--- library/monotonic_stack_related/max_rect_histogram.hpp
+++ library/monotonic_stack_related/max_rect_histogram.hpp
@@ -9,15 +9,9 @@
  * @space besides O(n) param `arr`, O(n) extra space is allocated temporarily
  */
 long long max_rect_histogram(const vector<int>& arr) {
-    auto rv = [&](int i) -> int {
-        return ssize(arr) - 1 - i;
-    };
-    vector<int> left = monotonic_stack<int>(arr, less());
-    vector<int> right = monotonic_stack<int>(vector<int>(rbegin(arr), rend(arr)), less());
+    auto [le, ri] = get_range(arr);
     long long max_area = 0;
-    for (int i = 0; i < ssize(arr); i++) {
-        int le = left[i], ri = rv(right[rv(i)]);//arr[i] is the max of range (le, ri)
-        max_area = max(max_area, 1LL * arr[i] * (ri - le - 1));
-    }
+    for (int i = 0; i < ssize(arr); i++)
+        max_area = max(max_area, 1LL * arr[i] * (ri[i] - le[i] - 1));
     return max_area;
 }
