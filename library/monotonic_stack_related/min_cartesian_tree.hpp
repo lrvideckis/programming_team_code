dfaf24f80a5b8f91e6d085f95c40276356d96b74
Luke Videckis <lukevideckis@gmail.com>
2023-07-14 15:58:07 -0700

    nit

--- library/monotonic_stack_related/min_cartesian_tree.hpp
+++ library/monotonic_stack_related/min_cartesian_tree.hpp
@@ -7,7 +7,6 @@
  *       (---x------) | (------x)   (x)
  *       (x) | (x)    | (x---) |     |
  *        |  |  |     |  |     |     |
- *        |  |  |     |  |     |     |
  * index: 0  1  2  3  4  5  6  7  8  9
  * root = 4; note root could = -1, when arr is empty
  * node | children
