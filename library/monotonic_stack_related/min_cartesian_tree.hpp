d49f7471fba8571782bd80ffb06cc386b6cc5ef8
Luke Videckis <lukevideckis@gmail.com>
2023-07-14 16:04:32 -0700

    Cartesian tree dups (#445)
    * initial commit
    
    * [auto-verifier] verify commit 6a7203e1dd9cb5f49648480d8d2437b18b5a3718
    
    * seeing if this fixes it
    
    * [auto-verifier] verify commit 1a48a6b5eaf99ada3dbaebd8b224a6f3b8b50ba8
    
    * fix doxygen
    
    * fix compile error
    
    * [auto-verifier] verify commit d77d01d6ddf83d4d96952f1e61f54846d1090b4c
    
    * rename
    
    * fix path
    
    * rename again
    
    * [auto-verifier] verify commit e47591a3dedb6adf9a03c5e393e5f4518533a7b7
    
    * add ascii art
    
    * [auto-verifier] verify commit 142ee68158635c8f94543e0f418a82c8c43398da
    
    * improve example
    
    * nit
    
    * make example not RTE on empty array
    
    * [auto-verifier] verify commit 62397e2ee6cbae7a27ae28c9a4fda79742710116
    
    ---------
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/monotonic_stack_related/min_cartesian_tree.hpp
@@ -0,0 +1,60 @@
+/** @file */
+#pragma once
+#include "monotonic_stack.hpp"
+/**
+ * arr = {2, 1, 3, 1, 0, 2, 2, 1, 0, 2}
+ *       (------------x---------------)
+ *       (---x------) | (------x)   (x)
+ *       (x) | (x)    | (x---) |     |
+ *        |  |  |     |  |     |     |
+ * index: 0  1  2  3  4  5  6  7  8  9
+ * root = 4; note root could = -1, when arr is empty
+ * node | children
+ * 4    | 1, 7, 9
+ * 1    | 0, 2
+ * 7    | 5
+ * indexes 3, 6, 8 are not nodes, as they are not the left-most min of their subarrays
+ * note: to_min[3] = 1
+ *       to_min[6] = 5
+ *       to_min[8] = 4
+ * @code{.cpp}
+ *     auto [le, ri] = get_range(arr);
+ *     auto [root, adj] = min_cartesian_tree(arr, le, ri);
+ *     queue<int> q;
+ *     if (root != -1) q.push(root);
+ *     while (!q.empty()) {
+ *         int u = q.front();
+ *         //arr[u] is the min value of exclusive-exclusive range (le[u], ri[u])
+ *         q.pop();
+ *         for (int v : adj[u])
+ *             q.push(v);
+ *     }
+ * @endcode
+ * @param arr array of integers (there can be duplicates)
+ * @param le,ri arr[i] is the min of range (le[i], ri[i])
+ * @returns root of tree, and adjacency list (only childs)
+ * @time O(n)
+ * @space this function allocates/returns an adj list which is O(n)
+ */
+pair<int, vector<vector<int>>> min_cartesian_tree(const vector<int>& arr, const vector<int>& le, const vector<int>& ri) {
+    int n = ssize(arr);
+    assert(ssize(le) == n && ssize(ri) == n);
+    auto leftmost_min = [&](int i) -> bool {return le[i] == -1 || arr[le[i]] < arr[i];};
+    vector<int> to_min(n);
+    iota(begin(to_min), end(to_min), 0);
+    for (int i = 0; i < n; i++)
+        if (!leftmost_min(i))
+            to_min[i] = to_min[le[i]];
+    vector<vector<int>> adj(n);
+    int root = -1;
+    for (int i = 0; i < n; i++) {
+        if (le[i] == -1 && ri[i] == n) {
+            assert(root == -1);
+            root = i;
+        } else if (leftmost_min(i)) {
+            bool le_par = (le[i] >= 0 && (ri[i] == n || arr[le[i]] > arr[ri[i]]));
+            adj[to_min[le_par ? le[i] : ri[i]]].push_back(i);
+        }
+    }
+    return {root, adj};
+}
