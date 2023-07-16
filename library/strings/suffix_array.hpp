53bdbcc51d307303781cdfbe186aafa180d2f44d
Luke Videckis <lukevideckis@gmail.com>
2023-07-15 23:58:20 -0700

    convert suffix array to function

--- library/strings/suffix_array.hpp
+++ library/strings/suffix_array.hpp
@@ -1,17 +1,6 @@
 /** @file */
 #pragma once
-/**
- * @see https://github.com/kth-competitive-programming/kactl /blob/main/content/strings/SuffixArray.h
- *
- * @code{.cpp}
- *     string s;
- *     suffix_array info(s, 128);
- *     vector<int> arr;
- *     suffix_array info(arr, 100'005);
- * @endcode
- */
-template <typename T> struct suffix_array {
-    const int N;
+struct sa_info {
     /**
      * suffixes of "banana":
      * 0 banana
@@ -39,38 +28,43 @@ template <typename T> struct suffix_array {
      */
     vector<int> sa, rank, lcp;
     /** @} */
-    /**
-     * @param s,max_val string/array with 0 <= s[i] < max_val
-     * @time O((n log n) + max_val)
-     * @space this function allocates O(n) space for `sa`, `rank`, and `lcp`
-     * vectors, but also allocates a O(max_val) vector `freq` temporarily
-     */
-    suffix_array(const T& s, int max_val) : N(ssize(s)), sa(N), rank(begin(s), end(s)), lcp(max(0, N - 1)) {
-        iota(begin(sa), end(sa), 0);
-        vector<int> tmp(N);
-        for (int len = 0; len < N; len = max(1, 2 * len)) {
-            iota(begin(tmp), begin(tmp) + len, N - len);
-            copy_if(begin(sa), end(sa), begin(tmp) + len, [&](int& val) {return (val -= len) >= 0;});
-            vector<int> freq(max_val);
-            for (auto val : rank) freq[val]++;
-            partial_sum(begin(freq), end(freq), begin(freq));
-            for_each(rbegin(tmp), rend(tmp), [&](int t) {
-                sa[--freq[rank[t]]] = t;
-            });
-            swap(rank, tmp);
-            max_val = 1, rank[sa[0]] = 0;
-            auto prev_rank = [&](int i) {return pair(tmp[i], i + len < N ? tmp[i + len] : -1);};
-            for (int i = 1; i < N; i++) {
-                max_val += prev_rank(sa[i - 1]) != prev_rank(sa[i]);
-                rank[sa[i]] = max_val - 1;
-            }
-            if (max_val == N) break;
-        }
-        for (int i = 0, k = 0; i < N; i++) {
-            if (k > 0) k--;
-            if (rank[i] == 0) continue;
-            for (int j = sa[rank[i] - 1]; max(i, j) + k < N && s[i + k] == s[j + k];) k++;
-            lcp[rank[i] - 1] = k;
-        }
-    }
 };
+/**
+ * @see https://github.com/kth-competitive-programming/kactl /blob/main/content/strings/SuffixArray.h
+ *
+ * @code{.cpp}
+ *     string s;
+ *     auto [sa, rank, lcp] = get_suffix_array(s, 128);
+ *     vector<int> arr;
+ *     auto [sa, rank, lcp] = get_suffix_array(arr, 100'005);
+ * @endcode
+ */
+template <typename T> sa_info get_suffix_array(const T& s, int max_val) {
+    int n = ssize(s);
+    vector<int> sa(n), rank(n), tmp(n);
+    iota(begin(sa), end(sa), 0);
+    for (int len = 0; len < n; len = max(1, 2 * len)) {
+        iota(begin(tmp), begin(tmp) + len, n - len);
+        copy_if(begin(sa), end(sa), begin(tmp) + len, [&](int& val) {return (val -= len) >= 0;});
+        vector<int> freq(max_val);
+        for (auto val : rank) freq[val]++;
+        partial_sum(begin(freq), end(freq), begin(freq));
+        for_each(rbegin(tmp), rend(tmp), [&](int t) {sa[--freq[rank[t]]] = t;});
+        swap(rank, tmp);
+        max_val = 1, rank[sa[0]] = 0;
+        auto prev_rank = [&](int i) {return pair(tmp[i], i + len < n ? tmp[i + len] : -1);};
+        for (int i = 1; i < n; i++) {
+            max_val += prev_rank(sa[i - 1]) != prev_rank(sa[i]);
+            rank[sa[i]] = max_val - 1;
+        }
+        if (max_val == n) break;
+    }
+    vector<int> lcp(max(0, n - 1));
+    for (int i = 0, k = 0; i < n; i++) {
+        if (k > 0) k--;
+        if (rank[i] == 0) continue;
+        for (int j = sa[rank[i] - 1]; max(i, j) + k < n && s[i + k] == s[j + k];) k++;
+        lcp[rank[i] - 1] = k;
+    }
+    return {sa, rank, lcp};
+}
--- tests/library_checker_aizu_tests/strings/suffix_array.test.cpp
+++ tests/library_checker_aizu_tests/strings/suffix_array.test.cpp
@@ -5,23 +5,19 @@
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-    {
-        suffix_array<string> sa_empty("", 128);
-        assert(sa_empty.lcp.empty());
-        suffix_array<string> sa_single("a", 128);
-        assert(sa_single.lcp.empty());
-    }
+    assert(get_suffix_array<string>("", 128).lcp.empty());
+    assert(get_suffix_array<string>("a", 128).lcp.empty());
     string s;
     cin >> s;
     int n = ssize(s);
-    suffix_array str_info(s, 128);
-    assert(ssize(str_info.sa) == n);
-    assert(ssize(str_info.rank) == n);
+    auto [sa, rank, _] = get_suffix_array(s, 128);
+    assert(ssize(sa) == n);
+    assert(ssize(rank) == n);
     for (int i = 0; i < n; i++) {
-        assert(str_info.sa[str_info.rank[i]] == i);
-        assert(str_info.rank[str_info.sa[i]] == i);
+        assert(sa[rank[i]] == i);
+        assert(rank[sa[i]] == i);
     }
-    for (auto val : str_info.sa)
+    for (auto val : sa)
         cout << val << " ";
     cout << '\n';
 }
