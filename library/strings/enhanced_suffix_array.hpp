4efd313ad2c7c55ee01fde55475716178bf961be
Luke Videckis <lukevideckis@gmail.com>
2023-07-16 00:23:46 -0700

    now acs

--- library/strings/enhanced_suffix_array.hpp
+++ library/strings/enhanced_suffix_array.hpp
@@ -1,6 +1,5 @@
 /** @file */
 #pragma once
-#include "suffix_array.hpp"
 #include "../monotonic_stack_related/min_cartesian_tree.hpp"
 /**
  * @see https://citeseerx.ist.psu.edu /viewdoc/download?doi=10.1.1.88.1129
@@ -8,23 +7,19 @@
  */
 template <typename T> struct enhanced_sa {
     T s;
-    suffix_array<T> info;
-    vector<int> le, ri;
+    vector<int> sa, rank, lcp, le, ri;
     int root;
     vector<map<int, int>> child;
     /**
-     * @param a_s,max_val string/array with 0 <= a_s[i] < max_val
-     * @time O((n log n) + max_val)
-     * @space O(n) for all member arrays; O(max_val) for `freq` array used
-     * temporarily in suffix_array constructor
+     * @param a_s,a_sa,a_rank,a_lcp a string and its suffix,lcp arrays
+     * @time O(n)
+     * @space all member arrays are O(n)
      */
-    enhanced_sa(const T& a_s, int max_val) :
-        s(a_s),
-        info(suffix_array(s, max_val)),
-        child(ssize(info.lcp)) {
-        tie(le, ri) = get_range(info.lcp);
+    enhanced_sa(const T& a_s, const vector<int>& a_sa, const vector<int>& a_rank, const vector<int>& a_lcp) :
+        s(a_s), sa(a_sa), rank(a_rank), lcp(a_lcp), child(ssize(lcp)) {
+        tie(le, ri) = get_range(lcp);
         vector<vector<int>> adj;
-        tie(root, adj) = min_cartesian_tree(info.lcp, le, ri);
+        tie(root, adj) = min_cartesian_tree(lcp, le, ri);
         if (root == -1) return;
         queue<int> q({root});
         while (!q.empty()) {
@@ -33,43 +28,43 @@ template <typename T> struct enhanced_sa {
             int prev = le[u] + 1;
             for (int v : adj[u]) {
                 for (int i = prev; i <= le[v]; i++)
-                    assert(child[u].emplace(s[info.sa[i] + info.lcp[u]], ssize(s) + i).second);
-                assert(child[u].emplace(s[info.sa[v] + info.lcp[u]], v).second);
+                    assert(child[u].emplace(s[sa[i] + lcp[u]], ssize(s) + i).second);
+                assert(child[u].emplace(s[sa[v] + lcp[u]], v).second);
                 prev = ri[v] + 1;
                 q.push(v);
             }
             for (int i = prev; i <= ri[u]; i++)
-                assert(child[u].emplace(s[info.sa[i] + info.lcp[u]], ssize(s) + i).second);
+                assert(child[u].emplace(s[sa[i] + lcp[u]], ssize(s) + i).second);
         }
     }
     /**
      * performs trie-style downwards tree walk
      * @param t needle
      * @returns range [le, ri) such that:
-     * - for all i in [le, ri): t == s.substr(info.sa[i], ssize(t))
+     * - for all i in [le, ri): t == s.substr(sa[i], ssize(t))
      * - `ri - le` is the # of matches of t in s.
      * @time O(|t| * log(|alphabet|)); |alphabet| = 26 if only lowercase letters
      * @space O(1)
      */
     pair<int, int> find(const T& t) const {
         if (root == -1) {
-            assert(ssize(info.sa) <= 1);
+            assert(ssize(sa) <= 1);
             return (ssize(t) == 1 && s == t) ? pair(0, 1) : pair(0, 0);
         }
-        assert(ssize(info.sa) >= 2);
+        assert(ssize(sa) >= 2);
         int u = root;
         for (int i = 0; i < ssize(t); i++) {
-            if (i == info.lcp[u]) {
+            if (i == lcp[u]) {
                 auto it = child[u].find(t[i]);
                 if (it == end(child[u])) return {0, 0};
                 u = it->second;
             }
             if (u >= ssize(s)) {
                 int idx = u - ssize(s);
-                auto it = mismatch(begin(t) + i, end(t), begin(s) + info.sa[idx] + i, end(s)).first;
+                auto it = mismatch(begin(t) + i, end(t), begin(s) + sa[idx] + i, end(s)).first;
                 return {idx, idx + (it == end(t))};
             }
-            if (s[info.sa[u] + i] != t[i]) return {0, 0};
+            if (s[sa[u] + i] != t[i]) return {0, 0};
         }
         return {le[u] + 1, ri[u] + 1};
     }
--- tests/library_checker_aizu_tests/strings/sa_query_single_matching.test.cpp
+++ tests/library_checker_aizu_tests/strings/sa_query_single_matching.test.cpp
@@ -6,7 +6,7 @@
 
 #include "../../../library/strings/suffix_array.hpp"
 #include "../../../library/strings/suffix_array_query.hpp"
-//#include "../../../library/strings/enhanced_suffix_array.hpp"
+#include "../../../library/strings/enhanced_suffix_array.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -14,11 +14,11 @@ int main() {
     cin >> s >> t;
     auto [sa, rank, lcp] = get_suffix_array(s, 128);
     sa_query sq(s, sa, rank, lcp);
-    //enhanced_sa esa(s, 128);
+    enhanced_sa esa(s, sa, rank, lcp);
     auto [le, ri] = sq.find(t);
-    //auto [le2, ri2] = esa.find(t);
-    //assert(ri - le == ri2 - le2);
-    //if (ri - le > 0) assert(le == le2);
+    auto [le2, ri2] = esa.find(t);
+    assert(ri - le == ri2 - le2);
+    if (ri - le > 0) assert(le == le2);
     vector<int> matches(begin(sa) + le, begin(sa) + ri);
     sort(begin(matches), end(matches));
     {
