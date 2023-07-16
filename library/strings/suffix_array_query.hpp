ef115b92eea6e2bd1b3cd0d6bcf8f12579e44438
Luke Videckis <lukevideckis@gmail.com>
2023-07-16 00:15:11 -0700

    adding progress

--- library/strings/suffix_array_query.hpp
+++ library/strings/suffix_array_query.hpp
@@ -1,25 +1,26 @@
 /** @file */
 #pragma once
-#include "suffix_array.hpp"
 #include "../data_structures/sparse_table.hpp"
 /**
  * Various queries you can do based on suffix array.
+ * @code{.cpp}
+ *     auto [sa, rank, lcp] = get_suffix_array(s, 128);
+ *     sa_query saq(s, sa, rank, lcp);
+ * @endcode
  */
 template <typename T> struct sa_query {
     T s;
-    suffix_array<T> info;
+    vector<int> sa, rank, lcp;
     RMQ<int> rmq_lcp, rmq_sa;
     /**
-     * @param a_s,max_val string/array with 0 <= a_s[i] < max_val
-     * @time O((n log n) + max_val)
-     * @space O(n log n) for RMQ's; O(max_val) for `freq` array used
-     * temporarily in suffix_array constructor
+     * @param a_s,a_sa,a_rank,a_lcp a string and its suffix,lcp arrays
+     * @time O(n log n)
+     * @space O(n log n) for RMQ's
      */
-    sa_query(const T& a_s, int max_val) :
-        s(a_s),
-        info(suffix_array(s, max_val)),
-        rmq_lcp(info.lcp, [](int i, int j) -> int {return min(i, j);}),
-        rmq_sa(info.sa, [](int i, int j) -> int {return min(i, j);}) {}
+    sa_query(const T& a_s, const vector<int>& a_sa, const vector<int>& a_rank, const vector<int>& a_lcp) :
+        s(a_s), sa(a_sa), rank(a_rank), lcp(a_lcp),
+        rmq_lcp(lcp, [](int i, int j) -> int {return min(i, j);}),
+        rmq_sa(sa, [](int i, int j) -> int {return min(i, j);}) {}
     /**
      * @param idx1,idx2 starting 0-based-indexes of suffixes
      * @returns max integer k such that s.substr(idx1, k) == s.substr(idx2, k)
@@ -28,7 +29,7 @@ template <typename T> struct sa_query {
      */
     inline int get_lcp(int idx1, int idx2) const {
         if (idx1 == idx2) return ssize(s) - idx1;
-        auto [le, ri] = minmax(info.rank[idx1], info.rank[idx2]);
+        auto [le, ri] = minmax(rank[idx1], rank[idx2]);
         return rmq_lcp.query(le, ri);
     }
     /**
@@ -38,13 +39,13 @@ template <typename T> struct sa_query {
      * @space O(1)
      */
     inline bool less(int idx1, int idx2) const {
-        return info.rank[idx1] < info.rank[idx2];
+        return rank[idx1] < rank[idx2];
     }
     /**
      * @see https://github.com/yosupo06/Algorithm/blob /master/src/string/suffixarray.hpp
      * @param t needle
      * @returns range [le, ri) such that:
-     * - for all i in [le, ri): t == s.substr(info.sa[i], ssize(t))
+     * - for all i in [le, ri): t == s.substr(sa[i], ssize(t))
      * - `ri - le` is the # of matches of t in s.
      * @time O(|t| * log(|s|))
      * @space O(1)
@@ -53,9 +54,9 @@ template <typename T> struct sa_query {
         auto cmp = [&](int i, int cmp_val) -> bool {
             return s.compare(i, ssize(t), t) < cmp_val;
         };
-        auto le = lower_bound(begin(info.sa), end(info.sa), 0, cmp);
-        auto ri = lower_bound(le, end(info.sa), 1, cmp);
-        return {le - begin(info.sa), ri - begin(info.sa)};
+        auto le = lower_bound(begin(sa), end(sa), 0, cmp);
+        auto ri = lower_bound(le, end(sa), 1, cmp);
+        return {le - begin(sa), ri - begin(sa)};
     }
     /**
      * @param t needle
--- tests/library_checker_aizu_tests/strings/sa_query_single_matching.test.cpp
+++ tests/library_checker_aizu_tests/strings/sa_query_single_matching.test.cpp
@@ -4,20 +4,22 @@
 #undef _GLIBCXX_DEBUG
 #include "../template.hpp"
 
+#include "../../../library/strings/suffix_array.hpp"
 #include "../../../library/strings/suffix_array_query.hpp"
-#include "../../../library/strings/enhanced_suffix_array.hpp"
+//#include "../../../library/strings/enhanced_suffix_array.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     string s, t;
     cin >> s >> t;
-    sa_query sq(s, 128);
-    enhanced_sa esa(s, 128);
+    auto [sa, rank, lcp] = get_suffix_array(s, 128);
+    sa_query sq(s, sa, rank, lcp);
+    //enhanced_sa esa(s, 128);
     auto [le, ri] = sq.find(t);
-    auto [le2, ri2] = esa.find(t);
-    assert(ri - le == ri2 - le2);
-    if (ri - le > 0) assert(le == le2);
-    vector<int> matches(begin(sq.info.sa) + le, begin(sq.info.sa) + ri);
+    //auto [le2, ri2] = esa.find(t);
+    //assert(ri - le == ri2 - le2);
+    //if (ri - le > 0) assert(le == le2);
+    vector<int> matches(begin(sa) + le, begin(sa) + ri);
     sort(begin(matches), end(matches));
     {
         int first_match = sq.find_first(t);
