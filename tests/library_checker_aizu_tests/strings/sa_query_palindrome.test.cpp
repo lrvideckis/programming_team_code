6682e78d8f0b9885cb29083e7c58237cbc0cf844
Luke Videckis <lukevideckis@gmail.com>
2023-07-18 14:34:12 -0700

    update another test

--- tests/library_checker_aizu_tests/strings/sa_query_palindrome.test.cpp
+++ tests/library_checker_aizu_tests/strings/sa_query_palindrome.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/enumerate_palindromes"
 #include "../template.hpp"
 
-#include "../../../library/strings/suffix_array_related/suffix_array_query.hpp"
+#include "../../../library/strings/suffix_array_related/suffix_array_query/get_lcp.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -12,7 +12,7 @@ int main() {
     sa_query sq(s, 256);
     for (int i = 0; i < n; i++) {
         for (int j = i; j < min(i + 2, n); j++)
-            cout << sq.get_lcp(j, (n - i - 1) + n + 1) * 2 - (i == j) << " ";
+            cout << get_lcp(sq, j, (n - i - 1) + n + 1) * 2 - (i == j) << " ";
     }
     cout << '\n';
     return 0;
