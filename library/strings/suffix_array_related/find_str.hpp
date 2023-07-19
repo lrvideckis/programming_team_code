2675ba2398a95f0c85e4e23ef8d49f1bc020c092
Luke Videckis <lukevideckis@gmail.com>
2023-07-19 12:28:58 -0700

    finished find str

--- library/strings/suffix_array_related/find_str.hpp
+++ library/strings/suffix_array_related/find_str.hpp
@@ -1,8 +1,21 @@
 /** @file */
 #pragma once
-#include "../suffix_array.hpp"
+#include "suffix_array.hpp"
 /**
  * @see https://github.com/yosupo06/Algorithm/blob /master/src/string/suffixarray.hpp
+ *
+ * @code{.cpp}
+ *     string s;
+ *     auto [sa, sa_inv] = get_sa(s, 256);
+ *     string t;
+ *     auto [le, ri] = find_str(s, sa, t);
+ *     // or
+ *     vector<int> arr;
+ *     auto [sa, sa_inv] = get_sa(arr, 100'005);
+ *     string t;
+ *     auto [le, ri] = find_str(arr, sa, t);
+ * @endcode
+ *
  * @param t needle
  * @returns range [le, ri) such that:
  * - for all i in [le, ri): t == s.substr(sa[i], ssize(t))
@@ -11,7 +24,7 @@
  * @time O(|t| * log(|s|))
  * @space O(1)
  */
-template <typename T> inline pair<int, int> find_str(const T& s, const vector<int>& sa, const T& t) {
+template <typename T> pair<int, int> find_str(const T& s, const vector<int>& sa, const T& t) {
     auto cmp = [&](int i, int cmp_val) -> bool {
         return s.compare(i, ssize(t), t) < cmp_val;
     };
