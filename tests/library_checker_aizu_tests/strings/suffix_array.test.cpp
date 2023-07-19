87c138006423c39d32323f5a6965907fef1b1414
Luke Videckis <lukevideckis@gmail.com>
2023-07-19 13:32:44 -0700

    tests now compile

--- tests/library_checker_aizu_tests/strings/suffix_array.test.cpp
+++ tests/library_checker_aizu_tests/strings/suffix_array.test.cpp
@@ -1,50 +1,48 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/suffixarray"
 #include "../template.hpp"
 
-#include "../../../library/strings/suffix_array_related/suffix_array_query.hpp"
-#include "../../../library/strings/suffix_array_related/lcp_interval_tree.hpp"
+#include "../../../library/strings/suffix_array_related/find_str.hpp"
+#include "../../../library/strings/suffix_array_related/find_substr.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     {
         string s;
         auto [sa, sa_inv] = get_sa(s, 256);
-        assert(get_lcp_array(s, sa, sa_inv).empty());
-        s = "a";
-        tie(sa, sa_inv) = get_sa(s, 256);
-        assert(get_lcp_array(s, sa, sa_inv).empty());
+        assert(get_lcp_array({sa, sa_inv}, s).empty());
+    }
+    {
+        string s = "a";
+        auto [sa, sa_inv] = get_sa(s, 256);
+        assert(get_lcp_array({sa, sa_inv}, s).empty());
+    }
+    {
+        vector<int> arr;
+        auto [sa, sa_inv] = get_sa(arr, 100'005);
+        vector<int> lcp = get_lcp_array({sa, sa_inv}, arr);
+        assert(sa.empty() && sa_inv.empty() && lcp.empty());
     }
     string s;
     cin >> s;
     int n = ssize(s);
-    sa_query sq(s, 256);
-    lcp_tree lcpt(s, 256);
-    assert(sq.sa == lcpt.sa);
-    assert(sq.sa_inv == lcpt.sa_inv);
-    assert(sq.lcp == lcpt.lcp);
+    auto [sa, sa_inv] = get_sa(s, 256);
+    vector<int> lcp = get_lcp_array({sa, sa_inv}, s);
     {
-        auto [le, ri] = sq.find_str("");
+        auto [le, ri] = find_str(s, sa, string(""));
         assert(le == 0 && ri == n);
-        assert(ssize(sq.sa) == n);
-        assert(ssize(sq.sa_inv) == n);
-        assert(ssize(sq.lcp) == n - 1);
+        assert(ssize(sa) == n);
+        assert(ssize(sa_inv) == n);
+        assert(ssize(lcp) == n - 1);
     }
     for (int i : {0, ssize(s), ssize(s) / 2}) {
-        auto [le, ri] = sq.find_substr(i, i);
+        auto [le, ri] = find_substr({sa, sa_inv}, lcp, i, i);
         assert(le == 0 && ri == n);
-    }
-    {
-        auto [le, ri] = lcpt.find_str("");
-        assert(le == 0 && ri == n);
-        assert(ssize(lcpt.sa) == n);
-        assert(ssize(lcpt.sa_inv) == n);
-        assert(ssize(lcpt.lcp) == n - 1);
     }
     for (int i = 0; i < n; i++) {
-        assert(sq.sa[sq.sa_inv[i]] == i);
-        assert(sq.sa_inv[sq.sa[i]] == i);
+        assert(sa[sa_inv[i]] == i);
+        assert(sa_inv[sa[i]] == i);
     }
-    for (auto val : sq.sa)
+    for (auto val : sa)
         cout << val << " ";
     cout << '\n';
 }
