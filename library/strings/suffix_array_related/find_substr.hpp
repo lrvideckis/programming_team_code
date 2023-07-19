be6cafec3ab02387a69797e6e1b056896701f841
Luke Videckis <lukevideckis@gmail.com>
2023-07-19 16:20:58 -0700

    fix another test

--- library/strings/suffix_array_related/find_substr.hpp
+++ library/strings/suffix_array_related/find_substr.hpp
@@ -1,17 +1,8 @@
 /** @file */
 #pragma once
-#include "get_lcp.hpp"
+#include "lcp_query.hpp"
 /**
  * @code{.cpp}
- *     string s;
- *     auto [sa, sa_inv] = get_sa(s, 256);
- *     vector<int> lcp = get_lcp_array({sa, sa_inv}, s);
- *     auto [le, ri] = find_substr({sa, sa_inv}, lcp, str_le, str_ri);
- *     // or
- *     vector<int> arr;
- *     auto [sa, sa_inv] = get_sa(arr, 100'005);
- *     vector<int> lcp = get_lcp_array({sa, sa_inv}, arr);
- *     auto [le, ri] = find_substr({sa, sa_inv}, lcp, str_le, str_ri);
  * @endcode
  *
  * @param str_le,str_ri defines a substring [str_le, str_ri) of s
@@ -23,13 +14,13 @@
  * @time O(log(|s|))
  * @space O(1)
  */
-inline pair<int, int> find_substr(const suf& sf, const vector<int>& lcp, int str_le, int str_ri) {
-    assert(0 <= str_le && str_le <= str_ri && str_ri <= ssize(sf.sa));
-    if (str_le == ssize(sf.sa)) return {0, ssize(sf.sa)};
+template <typename T> inline pair<int, int> find_substr(const lcp_query<T>& lq, int str_le, int str_ri) {
+    assert(0 <= str_le && str_le <= str_ri && str_ri <= ssize(lq.sf.sa));
+    if (str_le == ssize(lq.sf.sa)) return {0, ssize(lq.sf.sa)};
     auto cmp = [&](int i, bool flip) -> bool {
-        return flip ^ (get_lcp(sf.sa_inv, lcp, i, str_le) < str_ri - str_le);
+        return flip ^ (lq.get_lcp(i, str_le) < str_ri - str_le);
     };
-    auto le = lower_bound(begin(sf.sa), begin(sf.sa) + sf.sa_inv[str_le], 0, cmp);
-    auto ri = lower_bound(begin(sf.sa) + sf.sa_inv[str_le] + 1, end(sf.sa), 1, cmp);
-    return {le - begin(sf.sa), ri - begin(sf.sa)};
+    auto le = lower_bound(begin(lq.sf.sa), begin(lq.sf.sa) + lq.sf.sa_inv[str_le], 0, cmp);
+    auto ri = lower_bound(begin(lq.sf.sa) + lq.sf.sa_inv[str_le] + 1, end(lq.sf.sa), 1, cmp);
+    return {le - begin(lq.sf.sa), ri - begin(lq.sf.sa)};
 }
--- tests/library_checker_aizu_tests/handmade_tests/sa_find_subarray.test.cpp
+++ tests/library_checker_aizu_tests/handmade_tests/sa_find_subarray.test.cpp
@@ -2,7 +2,7 @@
 #include "../template.hpp"
 #include "../../../library/misc/random.hpp"
 
-#include "../../../library/strings/suffix_array_related/suffix_array_query.hpp"
+#include "../../../library/strings/suffix_array_related/find_substr.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -11,18 +11,18 @@ int main() {
             string s(n, 'a');
             int mx_char = get_rand<int>(0, 5);
             generate(begin(s), end(s), [&]() {return char('a' + get_rand<int>(0, mx_char));});
-            sa_query sq(s, 256);
+            lcp_query lq(s, 256);
             for (int str_le = 0; str_le <= n; str_le++) {
                 for (int str_ri = str_le; str_ri <= n; str_ri++) {
-                    auto [le, ri] = sq.find_substr(str_le, str_ri);
+                    auto [le, ri] = find_substr(lq, str_le, str_ri);
                     if (str_le == str_ri)
                         assert(le == 0 && ri == n);
                     if (str_le < n)
-                        assert(0 <= le && le <= sq.sa_inv[str_le] && sq.sa_inv[str_le] < ri && ri <= n);
+                        assert(0 <= le && le <= lq.sf.sa_inv[str_le] && lq.sf.sa_inv[str_le] < ri && ri <= n);
                     for (int i = le; i < ri; i++)
-                        assert(s.substr(sq.sa[i], str_ri - str_le) == s.substr(str_le, str_ri - str_le));
-                    assert(le == 0 || s.substr(sq.sa[le - 1], str_ri - str_le) != s.substr(str_le, str_ri - str_le));
-                    assert(ri == n || s.substr(sq.sa[ri], str_ri - str_le) != s.substr(str_le, str_ri - str_le));
+                        assert(s.substr(lq.sf.sa[i], str_ri - str_le) == s.substr(str_le, str_ri - str_le));
+                    assert(le == 0 || s.substr(lq.sf.sa[le - 1], str_ri - str_le) != s.substr(str_le, str_ri - str_le));
+                    assert(ri == n || s.substr(lq.sf.sa[ri], str_ri - str_le) != s.substr(str_le, str_ri - str_le));
                 }
             }
         }
