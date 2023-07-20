64ff5f697f9abfab94bb6639f800c941422018a4
Luke Videckis <lukevideckis@gmail.com>
2023-07-20 11:42:23 -0700

    fix docs

--- library/graphs/lca_rmq/next_on_path.hpp
+++ library/graphs/lca_rmq/next_on_path.hpp
@@ -15,7 +15,7 @@ inline bool in_subtree(const LCA& lca, int u, int v) {
  * @see https://codeforces.com/blog/entry/71567?#comment-559285
  * @code{.cpp}
  *     int u, v; //to loop over all nodes (except v) on path from u to v:
- *     for (int i = u; i != v; i = lca.next_on_path(i, v)) {}
+ *     for (int i = u; i != v; i = next_on_path(lca, i, v)) {}
  * @endcode
  * @param lca lca via rmq
  * @param u,v endpoint nodes of path
