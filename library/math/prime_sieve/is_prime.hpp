b0efcc16069da1f810973711086f378559ad54b5
Luke Videckis <lukevideckis@gmail.com>
2023-07-20 00:39:00 -0700

    split up prime sieve

---
+++ library/math/prime_sieve/is_prime.hpp
@@ -0,0 +1,13 @@
+/** @file */
+#pragma once
+#include "calc_sieve.hpp"
+/**
+ * @param val an integer
+ * @returns 1 iff val is prime
+ * @time O(1)
+ * @space O(1)
+ */
+inline bool is_prime(int val) {
+    assert(val < N && sieve[1] == 1);
+    return val >= 2 && sieve[val] == val;
+}
