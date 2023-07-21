02ded1f15180ac2f7f70b665b2f66f06452b5a7d
Luke Videckis <lukevideckis@gmail.com>
2023-07-21 12:31:01 -0700

    golf/nits

--- library/strings/manacher/manacher.hpp
+++ library/strings/manacher/manacher.hpp
@@ -1,18 +1,16 @@
 /** @file */
 #pragma once
 // https://codeforces.com/blog/entry/12143#comment-324162
-vector<int> manacher(const string& s) {
-    const int n = (int)s.length();
-    if (n == 0)
-        return vector<int>();
-    vector<int> res(2*n - 1);
+template <class T> vector<int> manacher(const T& s) {
+    int n = ssize(s);
+    vector<int> res(max(0, 2*n - 1));
     int p = 0, pR = -1;
-    for (int pos = 0; pos < 2*n - 1; ++pos) {
+    for (int pos = 0; pos < 2*n - 1; pos++) {
         int R = pos <= 2*pR ? min(p - res[2*p - pos], pR) : pos/2;
         int L = pos - R;
         while (L > 0 && R < n-1 && s[L-1] == s[R+1]) {
-            --L;
-            ++R;
+            L--;
+            R++;
         }
         res[pos] = L;
         if (R > pR) {
