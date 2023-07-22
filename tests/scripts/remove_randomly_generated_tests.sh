f19d13cb938ca65d54b51836bf23c372f2fb008c
Luke Videckis <lukevideckis@gmail.com>
2023-07-22 14:20:31 -0700

    committing file

--- tests/scripts/remove_randomly_generated_tests.sh
+++ tests/scripts/remove_randomly_generated_tests.sh
@@ -4,11 +4,13 @@ set -euo pipefail
 # ** glob now searches any number of levels
 shopt -s globstar
 
-grep -V
-
 grep --recursive --files-with-matches \
 	-e "library/contest/random\.hpp" \
 	-e "https\://judge\.yosupo\.jp/problem/" library_checker_aizu_tests/ |
-	grep --invert-match --fixed-strings --file=- ../.verify-helper/timestamps.remote.json
+	grep --invert-match --fixed-strings --file=- ../.verify-helper/timestamps.remote.json >tmp_file
 
-#mv tmp_file ../.verify-helper/timestamps.remote.json
+mv tmp_file ../.verify-helper/timestamps.remote.json
+
+git add ../.verify-helper/timestamps.remote.json
+git commit --message "removing randomly generated tests to rerun them"
+git push
