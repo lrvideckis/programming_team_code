4af61242b7d82f5e98704266347c61b31b24e078
Luke Videckis <lukevideckis@gmail.com>
2023-07-22 14:05:18 -0700

    to see output

--- tests/scripts/remove_randomly_generated_tests.sh
+++ tests/scripts/remove_randomly_generated_tests.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 shopt -s globstar
 
 grep --recursive --files-with-matches library/contest/random\.hpp library_checker_aizu_tests/ |
-	grep --invert-match --fixed-strings --file=- ../.verify-helper/timestamps.remote.json > tmp_file
+	grep --invert-match --fixed-strings --file=- ../.verify-helper/timestamps.remote.json
 
-mv tmp_file ../.verify-helper/timestamps.remote.json
+#mv tmp_file ../.verify-helper/timestamps.remote.json
