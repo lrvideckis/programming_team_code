e7e214086aebb032e0cce819b28672ba66ea0f76
Luke Videckis <lukevideckis@gmail.com>
2023-07-22 14:35:12 -0700

    trying this

--- tests/scripts/remove_randomly_generated_tests.sh
+++ tests/scripts/remove_randomly_generated_tests.sh
@@ -10,3 +10,7 @@ grep --recursive --files-with-matches \
 	grep --invert-match --fixed-strings --file=- ../.verify-helper/timestamps.remote.json >tmp_file
 
 mv tmp_file ../.verify-helper/timestamps.remote.json
+
+# remove trailing new line. If you don't do this, the `oj-verify run` won't
+# read this file correctly, and rerun all tests.
+truncate --size=-1 ../.verify-helper/timestamps.remote.json
