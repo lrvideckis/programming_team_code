4d46f46cf7f1db2218d4bb1cb4faf527428f74b2
Luke Videckis <lukevideckis@gmail.com>
2023-07-22 14:07:57 -0700

    check grep version on ubuntu

--- tests/scripts/remove_randomly_generated_tests.sh
+++ tests/scripts/remove_randomly_generated_tests.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 # ** glob now searches any number of levels
 shopt -s globstar
 
+grep -V
+
 grep --recursive --files-with-matches library/contest/random\.hpp library_checker_aizu_tests/ |
 	grep --invert-match --fixed-strings --file=- ../.verify-helper/timestamps.remote.json
 
