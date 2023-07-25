5812ca79a901abb771e65ccd5b1ebf278e9eb79a
Luke Videckis <lukevideckis@gmail.com>
2023-07-25 19:07:28 -0400

    Update manacher.hpp

--- library/strings/manacher/manacher.hpp
+++ library/strings/manacher/manacher.hpp
@@ -6,25 +6,11 @@
 template <class T> vector<int> manacher(const T& s) {
     int n = ssize(s);
     vector<int> man(max(0, 2*n - 1));
-    for (int i = 0, p = 0, pR = 0/*-1*/; i < 2*n - 1; i++) {
-        if(pR>=0) assert(p-man[p] == pR);
-        int R = i <= 2*pR ? min(p - man[2*p - i], pR) : i/2;
-        int L = i - R;
-        assert((i <= 2*pR) == ((i+1)/2 <= pR));
-        assert((i <= 2*pR) == (i <= 2*(p-man[p])));
-        if(i > 2*pR) assert(L == (i+1)/2);
-        else assert(L == i - p + max(man[2*p-i],man[p]));
-        while (L > 0 && R < n-1 && s[L-1] == s[R+1]) {
-            L--;
-            R++;
-            assert(R>pR);
-        }
-        man[i] = L;
-        if (R > pR) {
-            pR = R;
-            p = i;
-        }
-        if(i==0) assert(p==0 && pR==0);
+    auto ri = [&](int i) -> int {return i - man[i];};
+    for (int i = 0, p = 0; i < 2*n - 1; i++) {
+        man[i] = i <= 2*ri(p) ? i - p + max(man[2*p-i],man[p])) : (i+1)/2;
+        while (man[i] > 0 && ri(i) < n-1 && s[man[i]-1] == s[ri(i)+1])
+            man[i]--, p=i;
     }
     return man;
 }
