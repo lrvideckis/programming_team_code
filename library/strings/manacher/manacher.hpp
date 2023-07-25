a362269c8f82bde85faad90e978e6da4de2ef0f7
Luke Videckis <lukevideckis@gmail.com>
2023-07-25 01:55:05 -0400

    Update manacher.hpp

--- library/strings/manacher/manacher.hpp
+++ library/strings/manacher/manacher.hpp
@@ -6,12 +6,11 @@
 template <class T> vector<int> manacher(const T& s) {
     int n = ssize(s);
     vector<int> man(max(0, 2*n - 1));
-    for (int i = 0, p = 0, pR = -1; i < 2*n - 1; i++) {
+    for (int i = 0, p = 0, pR = 0/*-1*/; i < 2*n - 1; i++) {
         if(pR>=0) assert(p-man[p] == pR);
         int R = i <= 2*pR ? min(p - man[2*p - i], pR) : i/2;
         int L = i - R;
         if(i > 2*pR) assert(L == (i+1)/2);
-        else assert(L == max(man[2*p - i], man[p]));
         while (L > 0 && R < n-1 && s[L-1] == s[R+1]) {
             L--;
             R++;
