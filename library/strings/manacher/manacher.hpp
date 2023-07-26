26dcea9288e00786890620bf5eb368fc63d32d64
Luke Videckis <lukevideckis@gmail.com>
2023-07-26 09:19:26 -0400

    Update manacher.hpp

--- library/strings/manacher/manacher.hpp
+++ library/strings/manacher/manacher.hpp
@@ -8,7 +8,7 @@ template <class T> vector<int> manacher(const T& s) {
     vector<int> man(max(0, 2*n - 1));
     for (int i = 0, p = 0; i < 2*n - 1; i++) {
         int ri = i <= 2*(p-man[p]) ? p-max(man[2*p-i],man[p]) : i/2;
-        man[i] = ri - i;
+        man[i] = i - ri;
         while (man[i] > 0 && ri < n-1 && s[man[i]-1] == s[ri+1])
             man[i]--, ri++, p=i;
     }
