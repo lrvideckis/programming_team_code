5e06eef8518663283d4185f810b2c85c9559bc07
Luke Videckis <lukevideckis@gmail.com>
2023-07-27 09:56:52 -0400

    Update longest_palindromic_substr.hpp

--- library/strings/manacher/longest_palindromic_substr.hpp
+++ library/strings/manacher/longest_palindromic_substr.hpp
@@ -21,13 +21,13 @@ template <class T> struct longest_pal_query {
         iota(begin(init), end(init), 0);
         rmq = RMQ<int>(init, [&](int i1, int i2) -> int {return len(i1) < len(i2) ? i2 : i1;});
     }
-inline int len(int i) {return i - 2*man[i] + 1;}
+inline int len(int i) const {return i - 2*man[i] + 1;}
     // returns {start index, length}
     inline pair<int, int> get_longest(int le, int ri) const {
         assert(0 <= le && le < ri && ri <= n);
-        int pal_len = *prev(lower_bound(begin(idx), begin(idx) + (ri-le), 0, [&](int mid, int) -> bool {
+        int pal_len = int(lower_bound(begin(idx), begin(idx) + (ri-le), 0, [&](int mid, int) -> bool {
             return len(rmq.query(2*le + mid-1, 2*ri - mid)) >= mid;
-        }));
+        }) - begin(idx));
         int best_center = rmq.query(2*le + pal_len-1, 2*ri - pal_len);
         return {(best_center + 1 - pal_len)/2, pal_len};
     }
