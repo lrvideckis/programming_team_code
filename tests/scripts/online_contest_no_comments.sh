eae9cc87ad925afe7372a4e58bcdfcdbffe5f34e
Luke Videckis <lukevideckis@gmail.com>
2023-07-28 11:23:38 -0700

    first attempt

--- tests/scripts/online_contest_no_comments.sh
+++ tests/scripts/online_contest_no_comments.sh
@@ -4,9 +4,15 @@ set -euo pipefail
 # ** glob now searches any number of levels
 shopt -s globstar
 
+git submodule init
+git submodule update
+
 for header in ../library/**/*.hpp; do
-	cpp -fpreprocessed -P "$header" temp_file
-	mv temp_file "$header"
+	new_path=${header//library/chicken}
+	mkdir --parents "$(dirname "$new_path")"
+	cpp -P "$header" "$new_path"
 done
+mv ../library/ ../library_old/
+mv ../chicken/ ../library/
 # the cpp preprocessor sometimes leaves blank empty lines
 sed --in-place '/^[[:space:]]*$/d' ../library/**/*.hpp
