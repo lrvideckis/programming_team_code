eca87f666684e39e545bab6c08f40a8fe32056b9
Luke Videckis <lukevideckis@gmail.com>
2023-07-27 22:00:25 -0700

    finish golfing

--- library/strings/manacher/count_palindromes_query.hpp
+++ library/strings/manacher/count_palindromes_query.hpp
@@ -18,6 +18,11 @@ template <class T> struct count_pal_query {
     int n;
     vector<int> man;
     wavelet_tree wt_le, wt_ri;
+    /**
+     * @param s string/vector
+     * @time O(n log n)
+     * @space O(n log n) for wavelet trees
+     */
     count_pal_query(const T& s) : n(ssize(s)), man(manacher(s)), wt_le(man, 0, n), wt_ri(init()) {}
     wavelet_tree init() {
         vector<int> right(ssize(man));
@@ -25,29 +30,26 @@ template <class T> struct count_pal_query {
             right[i] = i - man[i] + 1;
         return {right, 1, n + 1};
     }
-    //number of non-empty palindromes
+    /**
+     * @param le,ri defines substring [le, ri) of s
+     * @returns number of palindromic substrings of s.substr(le, ri - le)
+     * @time O(log n)
+     * @space O(1)
+     */
     inline long long count_pals(int le, int ri) const {
         assert(0 <= le && le < ri && ri <= n);
-        int man_le = 2 * le, man_ri = 2 * ri - 1;
-        assert((man_ri - man_le) % 2 == 1);
-        int man_mi = (man_le + man_ri) / 2;
-        //first half:
-        //
-        //we want sum over range [man_le, man_mi) of values:
-        //    i/2 - max(man[i],le) + 1
+        int man_le = 2 * le, man_ri = 2 * ri - 1, man_mi = (man_le + man_ri) / 2;
+        //sum over range [man_le, man_mi) of:
+        //    i / 2 - max(man[i], le) + 1
         long long cnt = -1LL * wt_le.rect_count(man_le, man_mi, 0, le) * le;
         cnt -= wt_le.rect_sum(man_le, man_mi, le, n);
-        if ((man_mi - man_le) % 2 == 1) cnt += (man_mi - 1) / 2 + 1LL * (man_mi - 1 - man_le) / 2 * (man_le / 2 + (man_mi - 2) / 2);
-        else cnt += 1LL * (man_mi - man_le) / 2 * (man_le / 2 + (man_mi - 1) / 2);
+        cnt += sum_consecutive(le, man_mi / 2) + sum_consecutive(le, (man_mi + 1) / 2);
         cnt += man_mi - man_le;
-        //second half:
-        //
-        //we want sum over range [man_mi, man_ri) of values:
-        //min(right[i], ri) - (i+1)/2
+        //sum over range [man_mi, man_ri) of:
+        //    min(right[i], ri) - (i + 1) / 2
         cnt += 1LL * wt_ri.rect_count(man_mi, man_ri, ri, n + 1) * ri;
         cnt += wt_ri.rect_sum(man_mi, man_ri, 1, ri);
-        if ((man_ri - man_mi) % 2 == 0) cnt -= 1LL * (man_ri - man_mi) / 2 * (man_ri / 2 + (man_mi + 1) / 2);
-        else cnt -= 1LL * (man_ri - man_mi - 1) / 2 * (man_ri / 2 + (man_mi + 2) / 2) + (man_mi + 1) / 2;
+        cnt -= sum_consecutive((man_mi + 1) / 2, ri) + sum_consecutive(man_mi / 2 + 1, ri);
         return cnt;
     }
 };
