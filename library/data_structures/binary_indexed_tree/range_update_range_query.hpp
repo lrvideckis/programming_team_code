acbe8c58d13fe62ca9122995a215918a0f9a9bbf
Luke Videckis <lukevideckis@gmail.com>
2023-07-30 12:18:38 -0600

    consistent naming

--- library/data_structures/binary_indexed_tree/range_update_range_query.hpp
+++ library/data_structures/binary_indexed_tree/range_update_range_query.hpp
@@ -8,13 +8,12 @@ template <class T> struct bit_rurq {
     int n;
     BIT<T> bit1, bit2;
     bit_rurq(int a_n) : n(a_n), bit1(n), bit2(n) {}
-    /*
-    bit_inv(const vector<T>& a) : n(ssize(a)) {
+    bit_rurq(const vector<T>& a) : n(ssize(a)) {
         vector<T> init(n);
         adjacent_difference(begin(a), end(a), begin(init));
-        bit = BIT<T>(init);
+        bit1 = BIT<T>(init);
+
     }
-    */
     inline void update(int le, int ri, const T& d) {
         assert(0 <= le && le <= ri && ri <= n);
         bit1.update(le, d);
