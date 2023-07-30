c5d06e734e3109a917d80060202cbae1c064b3ff
Luke Videckis <lukevideckis@gmail.com>
2023-07-30 11:32:19 -0600

    improve bit test

--- tests/library_checker_aizu_tests/data_structures/bit.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/bit.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
 #include "../template.hpp"
 
-#include "../../../library/data_structures/binary_indexed_tree/binary_indexed_tree.hpp"
+#include "../../../library/data_structures/binary_indexed_tree/range_update_point_query.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -11,6 +11,14 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> arr[i];
     BIT<long long> bit(arr);
+    vector<long long> suf_sum(n);
+    partial_sum(rbegin(arr), rend(arr), rbegin(suf_sum));
+    bit_inv<long long> bit_i(suf_sum);
+    for (int i = 0; i < n; i++) {
+        assert(arr[i] == bit.sum(i, i + 1));
+        assert(bit_i.get_index(i) == bit.sum(i, n));
+        assert(bit_i.get_index(i) == suf_sum[i]);
+    }
     while (q--) {
         int type;
         cin >> type;
@@ -18,10 +26,17 @@ int main() {
             int p, x;
             cin >> p >> x;
             bit.update(p, x);
+            bit_i.update(0, p + 1, x);
         } else {
             int l, r;
             cin >> l >> r;
-            cout << bit.sum(l, r) << '\n';
+            long long res = bit.sum(l, r);
+            {
+                long long bit_i_result = bit_i.get_index(l);
+                if (r < n) bit_i_result -= bit_i.get_index(r);
+                assert(res == bit_i_result);
+            }
+            cout << res << '\n';
         }
     }
     return 0;
