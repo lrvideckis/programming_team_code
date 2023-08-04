30aae47db409b8f556793b96101858f090b7e058
Luke Videckis <lukevideckis@gmail.com>
2023-08-03 23:31:45 -0600

    fix suffix array test

--- library/strings/suffix_array_related/find_substrs_concatenated.hpp
+++ library/strings/suffix_array_related/find_substrs_concatenated.hpp
@@ -1,21 +1,21 @@
 /** @file */
 #pragma once
-#include "find_substr.hpp"
 #include "substr_cmp.hpp"
 template <class T> inline pair<int, int> find_substrs_concated(const T& s, const lcp_query<T>& lq,
         const vector<pair<int, int>>& substrs) {
     const auto& sa = lq.sf.sa;
-    int n = ssize(sa);
-    //assert(0 <= s_le1 && s_le1 <= s_ri1 && s_ri1 <= n);
-    //assert(0 <= s_le2 && s_le2 <= s_ri2 && s_ri2 <= n);
-    auto [le, ri] = find_substr(lq, s_le1, s_ri1);
-    int len1 = s_ri1 - s_le1;
-    int len2 = s_ri2 - s_le2;
-    auto cmp = [&](int i, int cmp_val) -> bool {
-        i += len1;
-        return substr_cmp(s, lq, i, min(i + len2, n), s_le2, s_ri2) < cmp_val;
-    };
-    le = int(lower_bound(begin(sa) + le, begin(sa) + ri, 0, cmp) - begin(sa));
-    ri = int(lower_bound(begin(sa) + le, begin(sa) + ri, 1, cmp) - begin(sa));
+    int n = ssize(sa), le = 0, ri = n, sum_len = 0;
+    for (auto [s_le, s_ri] : substrs) {
+        assert(0 <= s_le && s_le <= s_ri && s_ri <= n);
+        //if (s_le == n) continue;//TODO: will substr_cmp handle this?
+        //TODO: test performance of lambda-initialization in loop
+        auto cmp = [&](int i, int cmp_val) -> bool {
+            i += sum_len;
+            return substr_cmp(s, lq, i, min(i + s_ri - s_le, n), s_le, s_ri) < cmp_val;
+        };
+        le = int(lower_bound(begin(sa) + le, begin(sa) + ri, 0, cmp) - begin(sa));
+        ri = int(lower_bound(begin(sa) + le, begin(sa) + ri, 1, cmp) - begin(sa));
+        sum_len += s_ri - s_le;
+    }
     return {le, ri};
 }
