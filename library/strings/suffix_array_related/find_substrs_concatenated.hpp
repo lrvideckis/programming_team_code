4a127d4b36a5795591dc119d99e003b6b28a1f90
Luke Videckis <lukevideckis@gmail.com>
2023-08-04 00:09:08 -0600

    finish docs

--- library/strings/suffix_array_related/find_substrs_concatenated.hpp
+++ library/strings/suffix_array_related/find_substrs_concatenated.hpp
@@ -1,6 +1,20 @@
 /** @file */
 #pragma once
 #include "substr_cmp.hpp"
+/**
+ * @param s string/array
+ * @param lq lcp query struct
+ * @param substrs array of substrings [substrs[i].first, substrs[i].second)
+ * @returns range [le, ri) such that:
+ *     - for all i in [le, ri): t == s.substr(sa[i], ssize(t))
+ *     - `ri - le` is the # of matches of t in s.
+ *     where t = s.substr(substrs[0].first, substrs[0].second - substrs[0].first) +
+ *               s.substr(substrs[1].first, substrs[1].second - substrs[1].first) +
+ *               ...
+ *               s.substr(substrs.back().first, substrs.back().second - substrs.back().first)
+ * @time O(ssize(substrs) * log(|s|))
+ * @space O(1)
+ */
 template <class T> inline pair<int, int> find_substrs_concated(const T& s, const lcp_query<T>& lq,
         const vector<pair<int, int>>& substrs) {
     using dt = array<int, 3>;
