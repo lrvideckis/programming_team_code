d85af9ce4a2034db02b800a7ee0e5929a074cfbd
Luke Videckis <lukevideckis@gmail.com>
2023-08-03 22:43:12 -0600

    brute force test for substr compare

--- tests/library_checker_aizu_tests/handmade_tests/sa_find_subarray.test.cpp
+++ tests/library_checker_aizu_tests/handmade_tests/sa_find_subarray.test.cpp
@@ -3,11 +3,12 @@
 #include "../../../library/contest/random.hpp"
 
 #include "../../../library/strings/suffix_array_related/find_substr.hpp"
+#include "../../../library/strings/suffix_array_related/substr_cmp.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
-    for (int n = 0; n <= 40; n++) {
-        for (int tests = 30; tests--;) {
+    for (int n = 0; n <= 30; n++) {
+        for (int tests = 10; tests--;) {
             string s(n, 'a');
             int mx_char = get_rand<int>(0, 5);
             generate(begin(s), end(s), [&]() {return char('a' + get_rand<int>(0, mx_char));});
@@ -23,6 +24,20 @@ int main() {
                         assert(s.substr(lq.sf.sa[i], str_ri - str_le) == s.substr(str_le, str_ri - str_le));
                     assert(le == 0 || s.substr(lq.sf.sa[le - 1], str_ri - str_le) != s.substr(str_le, str_ri - str_le));
                     assert(ri == n || s.substr(lq.sf.sa[ri], str_ri - str_le) != s.substr(str_le, str_ri - str_le));
+                }
+            }
+            for(int i = 0; i <= n; i++) {
+                for(int j = i; j <= n; j++) {
+                    for(int k = 0; k <= n; k++) {
+                        for(int l = k; l <= n; l++) {
+                            int cmp_val = substr_cmp(s, lq, i, j, k, l);
+                            string sub1 = s.substr(i, j - i);
+                            string sub2 = s.substr(k, l - k);
+                            if(cmp_val < 0) assert(sub1 < sub2);
+                            if(cmp_val == 0) assert(sub1 == sub2);
+                            if(cmp_val > 0) assert(sub1 > sub2);
+                        }
+                    }
                 }
             }
         }
