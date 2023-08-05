35ffce072d2beada0e8f2aacfb2e97e4558a13e4
Luke Videckis <lukevideckis@gmail.com>
2023-08-05 04:13:47 -0600

    not useful

--- library/strings/suffix_array_related/find/find_string.hpp
+++ library/strings/suffix_array_related/find/find_string.hpp
@@ -4,19 +4,6 @@
 #include "match.hpp"
 /**
  * @see https://github.com/yosupo06/Algorithm/blob /master/src/string/suffixarray.hpp
- *
- * @code{.cpp}
- *     string s;
- *     auto [sa, sa_inv] = get_sa(s, 256);
- *     string t;
- *     match m = find_str(s, sa, t);
- *     // or
- *     vector<int> arr;
- *     auto [sa, sa_inv] = get_sa(arr, 100'005);
- *     vector<int> t;
- *     match m = find_str(arr, sa, t);
- * @endcode
- *
  * @param s,sa string/array and its suffix array
  * @param t query string
  * @returns see match
