1019462edf42b94a73b126e95579e3b5a5bd0688
Luke Videckis <lukevideckis@gmail.com>
2023-08-05 03:52:39 -0600

    address final todo

--- library/strings/suffix_array_related/find/find_substrings_concatenated.hpp
+++ library/strings/suffix_array_related/find/find_substrings_concatenated.hpp
@@ -1,6 +1,6 @@
 /** @file */
 #pragma once
-#include "../compare/compare_substrings.hpp"
+#include "../lcp_query.hpp"
 #include "match.hpp"
 /**
  * @param lq lcp query struct
@@ -14,12 +14,11 @@ template <class T> inline match find_substrs_concated(const lcp_query<T>& lq,
     const auto& sa = lq.sa;
     int n = ssize(sa), sa_le = 0, sa_ri = n, str_le = 0, str_ri = 0, sum_len = 0;
     auto cmp = [&](int i, const dt & x) -> bool {
-        int j = i + sum_len;
-        //TODO lq.get_lcp is called twice: once here, once in substr_cmp
-        int len = min(lq.get_lcp(j, x[0]), x[1] - x[0]);
-        if (len + sum_len > str_ri - str_le)
-            str_le = i, str_ri = j + len;
-        return substr_cmp(lq, j, min(j + x[1] - x[0], n), x[0], x[1]) < x[2];
+        int j = i + sum_len, len = x[1] - x[0], lcp = min(lq.get_lcp(j, x[0]), len);
+        if (lcp + sum_len > str_ri - str_le)
+            str_le = i, str_ri = j + lcp;
+        if (lcp < min(n - j, len)) return lq.sa_inv[j] - lq.sa_inv[x[0]] < x[2];
+        return x[2] ^ (n - j < len);
     };
     for (auto [le, ri] : substrs) {
         assert(0 <= le && le <= ri && ri <= n);
