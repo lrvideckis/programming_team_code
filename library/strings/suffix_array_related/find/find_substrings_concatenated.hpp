1d8b124b3f07405a00c95fa107b26897c98d5efe
Luke Videckis <lukevideckis@gmail.com>
2023-08-06 15:38:23 -0600

    remove var

--- library/strings/suffix_array_related/find/find_substrings_concatenated.hpp
+++ library/strings/suffix_array_related/find/find_substrings_concatenated.hpp
@@ -19,16 +19,16 @@ template <class T> inline match find_substrs_concated(const lcp_query<T>& lq,
     const auto& sa = lq.sa;
     int n = ssize(sa), sa_le = 0, sa_ri = n, str_le = 0, str_ri = 0, sum_len = 0;
     auto cmp = [&](int i, const dt & x) -> bool {
-        int j = i + sum_len, len = x[1] - x[0], lcp = min(lq.get_lcp(j, x[0]), len);
+        int j = i + sum_len, lcp = min(lq.get_lcp(j, x[0]), x[1]);
         if (lcp + sum_len > str_ri - str_le)
             str_le = i, str_ri = j + lcp;
-        if (lcp < min(n - j, len)) return lq.sa_inv[j] - lq.sa_inv[x[0]] < x[2];
-        return x[2] ^ (n - j < len);
+        if (lcp < min(n - j, x[1])) return lq.sa_inv[j] - lq.sa_inv[x[0]] < x[2];
+        return x[2] ^ (n - j < x[1]);
     };
     for (auto [le, ri] : substrs) {
         assert(0 <= le && le <= ri && ri <= n);
-        sa_le = int(lower_bound(begin(sa) + sa_le, begin(sa) + sa_ri, dt{le, ri, 0}, cmp) - begin(sa));
-        sa_ri = int(lower_bound(begin(sa) + sa_le, begin(sa) + sa_ri, dt{le, ri, 1}, cmp) - begin(sa));
+        sa_le = int(lower_bound(begin(sa) + sa_le, begin(sa) + sa_ri, dt{le, ri - le, 0}, cmp) - begin(sa));
+        sa_ri = int(lower_bound(begin(sa) + sa_le, begin(sa) + sa_ri, dt{le, ri - le, 1}, cmp) - begin(sa));
         sum_len += ri - le;
     }
     return {sa_le, sa_ri, str_le, str_ri};
