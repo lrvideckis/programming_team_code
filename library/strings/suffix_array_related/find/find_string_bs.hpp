886a3cd6c397e721277b29df246dca5f0710093b
Luke Videckis <lukevideckis@gmail.com>
2023-08-07 22:35:10 -0600

    golf

--- library/strings/suffix_array_related/find/find_string_bs.hpp
+++ library/strings/suffix_array_related/find/find_string_bs.hpp
@@ -14,8 +14,7 @@ template <class T> inline match find_str(const T& s, const vector<int>& sa, cons
     int str_le = 0, str_ri = 0;
     auto cmp = [&](int i, int cmp_val) -> bool {
         auto [it_s, it_t] = mismatch(begin(s) + i, end(s), begin(t), end(t));
-        if (it_s - begin(s) - i > str_ri - str_le)
-            str_le = i, str_ri = int(it_s - begin(s));
+        if (it_s - begin(s) - i > str_ri - str_le) str_le = i, str_ri = int(it_s - begin(s));
         if (it_s != end(s) && it_t != end(t)) return (*it_s) - (*it_t) < cmp_val;
         return cmp_val ^ (ssize(s) - i < ssize(t));
     };
