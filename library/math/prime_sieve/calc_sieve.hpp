65bc5b7bd3c93e6bdc6d3f914e1a14a002beb809
Luke Videckis <lukevideckis@gmail.com>
2023-08-08 08:15:18 -0600

    consistency

--- library/math/prime_sieve/calc_sieve.hpp
+++ library/math/prime_sieve/calc_sieve.hpp
@@ -1,15 +1,15 @@
 /** @file */
 #pragma once
-const int n = 1001;
-int sieve[n];
+const int mx_n = 1001;
+int sieve[mx_n];
 /**
- * @time O(n * log(log n))
- * @space O(n) for `sieve` vector
+ * @time O(mx_n * log(log mx_n))
+ * @space O(mx_n) for `sieve` vector
  */
 void calc_sieve() {
     iota(begin(sieve), end(sieve), 0);
-    for (int i = 2; i * i < n; i++)
+    for (int i = 2; i * i < mx_n; i++)
         if (sieve[i] == i)
-            for (int j = i * i; j < n; j += i)
+            for (int j = i * i; j < mx_n; j += i)
                 sieve[j] = min(sieve[j], i);
 }
