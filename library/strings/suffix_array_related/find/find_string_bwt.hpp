f46a60a80ad01f21b34ede85dde558775e3ef471
Luke Videckis <lukevideckis@gmail.com>
2023-08-08 08:09:18 -0600

    update more

--- library/strings/suffix_array_related/find/find_string_bwt.hpp
+++ library/strings/suffix_array_related/find/find_string_bwt.hpp
@@ -1,15 +1,15 @@
 /** @file */
 #pragma once
 #include "../suffix_array.hpp"
-const int K = 75, MN = '0'; // lower case letters and digits
+const int mn = '0', len = 75; // lower case letters and digits
 /**
  * Burrows Wheeler transform
  */
 struct find_bwt {
     int n;
     char last;
-    vector<array<int, K>> occ;
-    array < int, K + 1 > cnt;
+    vector<array<int, len>> occ;
+    array < int, len + 1 > cnt;
     /**
      * @code{.cpp}
      *     string s;
@@ -17,17 +17,17 @@ struct find_bwt {
      *     find_bwt fb(s, sa);
      * @endcode
      * @param s,sa a string and its suffix array
-     * @time O(n * K)
-     * @space O(n * K) for `occ` vector; it's possible to improve this
-     *     to O(n * K / 64) https://codeforces.com/contest/963/submission/217802614
+     * @time O(n * len)
+     * @space O(n * len) for `occ` vector; it's possible to improve this
+     *     to O(n * len / 64) https://codeforces.com/contest/963/submission/217802614
      */
-    find_bwt(const string& s, const vector<int>& sa) : n(ssize(s)), last(s.empty() ? -1 : s.back() - MN), occ(n + 1) {
+    find_bwt(const string& s, const vector<int>& sa) : n(ssize(s)), last(s.empty() ? -1 : s.back() - mn), occ(n + 1) {
         fill(begin(cnt), end(cnt), 0);
         for (int i = 0; i < n; i++) {
-            cnt[s[i] + 1 - MN]++;
+            cnt[s[i] + 1 - mn]++;
             occ[i + 1] = occ[i];
             if (sa[i] == 0) continue;
-            occ[i + 1][s[sa[i] - 1] - MN]++;
+            occ[i + 1][s[sa[i] - 1] - mn]++;
         }
         partial_sum(begin(cnt), end(cnt), begin(cnt));
     }
@@ -42,7 +42,7 @@ struct find_bwt {
     pair<int, int> find_str(const string& t) const {
         int le = 0, ri = n;
         for (int i = ssize(t) - 1, x = 0; le < ri && i >= 0; i--) {
-            char c = t[i] - MN;
+            char c = t[i] - mn;
             le = cnt[c] + occ[le][c] + (c == last && x);
             ri = cnt[c] + occ[ri][c] + (c == last);
             x = 1;
--- library/strings/trie.hpp
+++ library/strings/trie.hpp
@@ -3,13 +3,14 @@
 /**
  * @see https://cp-algorithms.com/string /aho_corasick.html#construction-of-the-trie
  */
-const int K = 26, MN = 'A';
+const int mn = 'A', len = 26;
 struct trie {
     struct node {
-        int next[K], cnt_words = 0, par = -1;
+        array<int, len> next;
+        int cnt_words = 0, par = -1;
         char ch;
         node(int a_par = -1, char a_ch = '#') : par(a_par), ch(a_ch) {
-            fill(next, next + K, -1);
+            fill(begin(next), end(next), -1);
         }
     };
     vector<node> t;
@@ -17,7 +18,7 @@ struct trie {
     void insert(const string& s) {
         int v = 0;
         for (auto ch : s) {
-            int let = ch - MN;
+            int let = ch - mn;
             if (t[v].next[let] == -1) {
                 t[v].next[let] = ssize(t);
                 t.emplace_back(v, ch);
@@ -29,7 +30,7 @@ struct trie {
     int find(const string& s) const {
         int v = 0;
         for (auto ch : s) {
-            int let = ch - MN;
+            int let = ch - mn;
             if (t[v].next[let] == -1) return 0;
             v = t[v].next[let];
         }
