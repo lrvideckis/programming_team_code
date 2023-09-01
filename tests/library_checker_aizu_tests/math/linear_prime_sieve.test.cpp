7a4a53215929d771fe68029aed88ac5ab6947fbc
Luke Videckis <lukevideckis@gmail.com>
2023-09-01 12:50:17 -0600

    revert back

--- tests/library_checker_aizu_tests/math/linear_prime_sieve.test.cpp
+++ tests/library_checker_aizu_tests/math/linear_prime_sieve.test.cpp
@@ -1,15 +1,33 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/all/NTL_1_C"
 #include "../template.hpp"
 
-#include "../../../library/math/prime_sieve/is_prime.hpp"
-#include "../../../library/math/prime_sieve/get_prime_factors.hpp"
+#include "../../../library/math/prime_sieve/calc_linear_sieve.hpp"
 
 #include "../../../kactl/content/number-theory/MillerRabin.h"
 
+inline bool is_prime(int val) {
+    assert(val < mx_n && sieve[1] == 1);
+    return val >= 2 && sieve[val] == val;
+}
+
+vector<int> get_prime_factors(int val) {
+    assert(val < mx_n && sieve[1] == 1);
+    vector<int> factors;
+    while (val > 1) {
+        int p = sieve[val];
+        factors.push_back(p);
+        val /= p;
+    }
+    return factors;
+}
+
 int main() {
-    calc_sieve();
+    vector<int> primes = calc_sieve();
     for (int i = 1; i < mx_n; i++)
         assert(isPrime(i) == is_prime(i));
+    for (int p : primes)
+        assert(isPrime(p));
+    assert(ssize(primes) == 168);
     int n;
     cin >> n;
     map<int, int> prime_to_max_exponent;
