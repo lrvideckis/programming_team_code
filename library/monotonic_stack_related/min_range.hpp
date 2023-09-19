4ad369317e86600a296330715c7efddf85ab94f8
cameron custer <custer.cameron@gmail.com>
2023-09-19 13:32:08 -0400

    using the monotonic stack on arrays of non-integral values is nice, as long as the operator < is supported, is d, it should work

--- library/monotonic_stack_related/min_range.hpp
+++ library/monotonic_stack_related/min_range.hpp
@@ -15,7 +15,7 @@
  * @time O(n)
  * @space two O(n) vectors are allocated and returned
  */
-array<vector<int>, 2> min_range(const vector<int>& arr) {
+template<class T> array<vector<int>, 2> min_range(const vector<T>& arr) {
     vector le = monotonic_stack(arr, less_equal());
     vector ri = monotonic_stack({rbegin(arr), rend(arr)}, less());
     reverse(begin(ri), end(ri));
