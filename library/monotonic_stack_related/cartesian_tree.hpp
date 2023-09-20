22660cb56bba55421d3ed4840a2fa2b5793cded2
Luke Videckis <lukevideckis@gmail.com>
2023-09-20 14:53:08 -0700

    finished cartesian tree

--- library/monotonic_stack_related/cartesian_tree.hpp
+++ library/monotonic_stack_related/cartesian_tree.hpp
@@ -1,66 +1,44 @@
 /** @file */
 #pragma once
-#include "min_range.hpp"
+#include "monotonic_range.hpp"
 /**
- * min cartesian tree
+ * either min or max cartesian tree
  */
-struct min_tree {
+struct cart_tree {
     /**
-     * arr = {2, 1, 3, 1, 0, 2, 2, 1, 0, 2}
-     *       (------------x---------------)
-     *       (---x------) | (------x)   (x)
-     *       (x) | (x)    | (x---) |     |
-     *        |  |  |     |  |     |     |
-     * index: 0  1  2  3  4  5  6  7  8  9
-     * root = 4; note root = -1 iff arr is empty
-     * u | adj[u]
-     * 4 | 1, 7, 9
-     * 1 | 0, 2
-     * 7 | 5
-     * indexes 3, 6, 8 are not nodes, as they are not the left-most min of their subarrays
-     * to_min[3] = 1
-     * to_min[6] = 5
-     * to_min[8] = 4
-     * else to_min[i] = i
      * @{
      */
     int root;
     vector<vector<int>> adj;
-    vector<int> le, ri, to_min;
+    vector<int> le, ri, to_extrema;
     /** @} */
 };
 /**
  * @code{.cpp}
- *     auto [root, adj, le, ri, to_min] = min_cartesian_tree(arr);
- *     queue<int> q;
- *     if (root != -1) q.push(root);
- *     while (!q.empty()) {
- *         int u = q.front();
- *         q.pop();
- *         //arr[u] is the leftmost min of exclusive-exclusive range (le[u], ri[u])
- *         for (int v : adj[u])
- *             q.push(v);
- *     }
+ *     //min cartesian tree
+ *     auto [root, adj, le, ri, to_extrema] = get_cart_tree(arr, less());
+ *     //max cartesian tree
+ *     auto [root, adj, le, ri, to_extrema] = get_cart_tree(arr, greater());
  * @endcode
  * @param arr array of integers (there can be duplicates)
- * @returns see min_tree
+ * @returns see cart_tree
  * @time O(n)
- * @space this function allocates/returns min_tree struct which is O(n)
+ * @space this function allocates/returns cart_tree struct which is O(n)
  */
-template <class T> min_tree min_cartesian_tree(const vector<T>& arr) {
+template <class T, class F = function<bool(T, T)>> cart_tree get_cart_tree(const vector<T>& arr, F less) {
     int n = ssize(arr);
-    auto [le, ri] = min_range(arr);
-    vector<int> to_min(n);
-    for (int i = 0; i < n; i++)
-        to_min[i] = (0 <= le[i] && arr[le[i]] == arr[i]) ? to_min[le[i]] : i;
+    auto [le, ri] = mono_range(arr, less);
+    vector<int> to_extrema(n);
+    for (int i = n - 1; i >= 0; i--)
+        to_extrema[i] = (ri[i] < n && arr[i] == arr[ri[i]]) ? to_extrema[ri[i]] : i;
     vector<vector<int>> adj(n);
     int root = -1;
     for (int i = 0; i < n; i++) {
         if (le[i] == -1 && ri[i] == n) root = i;
-        else if (to_min[i] == i) {
-            bool le_par = (0 <= le[i] && (ri[i] == n || arr[le[i]] > arr[ri[i]]));
-            adj[to_min[le_par ? le[i] : ri[i]]].push_back(i);
+        else if (to_extrema[i] == i) {
+            bool le_par = (le[i] != -1 && (ri[i] == n || less(arr[ri[i]], arr[le[i]])));
+            adj[to_extrema[le_par ? le[i] : ri[i]]].push_back(i);
         }
     }
-    return {root, adj, le, ri, to_min};
+    return {root, adj, le, ri, to_extrema};
 }
---
+++ tests/library_checker_aizu_tests/monotonic_stack_related/cartesian_tree.test.cpp
@@ -0,0 +1,37 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/cartesian_tree"
+#include "../template.hpp"
+
+#include "../../../library/monotonic_stack_related/cartesian_tree.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    auto [root, adj, le, ri, to_extrema] = get_cart_tree(arr, less());
+
+    vector<int> arr_neg(n);
+    transform(begin(arr), end(arr), begin(arr_neg), [](int x) {return -x;});
+    auto [root_neg, adj_neg, le_neg, ri_neg, to_extrema_neg] = get_cart_tree(arr_neg, greater());
+    assert(root == root_neg);
+    assert(adj == adj_neg);
+    assert(le == le_neg);
+    assert(ri == ri_neg);
+    assert(to_extrema == to_extrema_neg);
+
+    vector<int> par(n, -1);
+    par[root] = root;
+    for (int i = 0; i < n; i++) {
+        assert(to_extrema[i] == i);//because distinct numbers
+        assert(-1 <= le[i] && le[i] < i && i < ri[i] && ri[i] <= n);
+        assert(le[i] == -1 || arr[le[i]] < arr[i]);
+        assert(ri[i] == n || arr[i] > arr[ri[i]]);//because distinct numbers, if dups, then arr[i] >= arr[ri[i]]
+        for (int j : adj[i]) {
+            assert(le[i] <= le[j] && ri[j] <= ri[i]);
+            par[j] = i;
+        }
+    }
+    for (int i = 0; i < n; i++) cout << par[i] << " ";
+    return 0;
+}
