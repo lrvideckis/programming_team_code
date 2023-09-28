012a1bb821216a7074d92a673dfb4501d3f447db
Luke Videckis <lrvideckis@gmail.com>
2023-09-28 12:51:28 -0700

    Bwt nit (#504)
    * I like this return type better
    
    * fix other test
    
    * fix cppcheck
    
    * [auto-verifier] verify commit 1732d51a386aa681de74de1ad1aef1fd2b7f7858
    
    * nits
    
    * split apart test to run BWT, LCP tree in debug mode
    
    * [auto-verifier] verify commit 23bff7d7aad8ef1dce0db6a45cbcd48c85817da7
    
    * split apart test to run LCP tree, BWT in debug mode
    
    * [auto-verifier] verify commit da1e8d81618f1701911dce90cf264927ba9bf9b3
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>
    Co-authored-by: GitHub <noreply@github.com>

--- tests/library_checker_aizu_tests/strings/multi_matching.test.cpp
+++ tests/library_checker_aizu_tests/strings/multi_matching.test.cpp
@@ -1,10 +1,5 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_14_D"
-//since this causes an O(n) partition check for each call to `lower_bound`,
-//causing TLE.
-#undef _GLIBCXX_DEBUG
 #include "../template.hpp"
-
-#include "../../../library/strings/suffix_array_related/find/find_string_bs.hpp"
 #include "../../../library/strings/suffix_array_related/find/find_string_bwt.hpp"
 #define mn mn_other
 #define len len_other
@@ -13,29 +8,19 @@
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     {
-        lcp_tree lt(string(""));
-        auto [sa_le, sa_ri, str_le, str_ri] = find_str(string(""), lt.sa, string(""));
-        assert(sa_le == 0 && sa_ri == 0 && str_le == 0 && str_ri == 0);
-    }
-    {
         find_bwt fb(string(""), vector<int>());
-        vector<array<int, 2>> ret = fb.find_str(string(""));
-        assert(ssize(ret) == 1);
-        assert(ret[0][0] == 0 && ret[0][1] == 0);
+        auto [le, ri] = fb.find_str(string(""));
+        assert(ssize(le) == 1 && ssize(ri) == 1);
+        assert(le[0] == 0 && ri[0] == 0);
     }
     string s;
     cin >> s;
     lcp_tree lt(s);
     find_bwt fb(s, lt.sa);
     {
-        auto [sa_le, sa_ri, str_le, str_ri] = find_str(s, lt.sa, string(""));
-        assert(sa_le == 0 && sa_ri == ssize(s));
-        assert(str_ri - str_le == 0);
-    }
-    {
-        auto ret = fb.find_str("");
-        assert(ssize(ret) == 1);
-        assert(ret[0][0] == 0 && ret[0][1] == ssize(s));
+        auto [le, ri] = fb.find_str("");
+        assert(ssize(le) == 1 && ssize(ri) == 1);
+        assert(le[0] == 0 && ri[0] == ssize(s));
     }
     {
         auto [le, ri] = find_str(s, lt, string(""));
@@ -46,24 +31,17 @@ int main() {
     while (q--) {
         string t;
         cin >> t;
-        auto [sa_le, sa_ri, str_le, str_ri] = find_str(s, lt.sa, t);
+        auto [lcpt_le, lcpt_ri] = find_str(s, lt, t);
         {
-            auto ret = fb.find_str("a" + t);
-            assert(ssize(ret) == 2 + ssize(t) && ret.back()[0] == 0 && ret.back()[1] == ssize(s));
-            auto [sa_le2, sa_ri2] = ret[1];
-            assert(sa_ri2 - sa_le2 == sa_ri - sa_le);
-            if (sa_le < sa_ri)
-                assert(sa_le == sa_le2 && sa_ri == sa_ri2);
+            auto [bwt_le, bwt_ri] = fb.find_str("a" + t);
+            assert(ssize(bwt_le) == 2 + ssize(t) && ssize(bwt_ri) == 2 + ssize(t) && bwt_le.back() == 0 && bwt_ri.back() == ssize(s));
+            for (int i = ssize(bwt_le) - 2; i >= 0; i--)
+                assert(bwt_ri[i] - bwt_le[i] <= bwt_ri[i + 1] - bwt_le[i + 1]);
+            assert(bwt_ri[1] - bwt_le[1] == lcpt_ri - lcpt_le);
+            if (lcpt_le < lcpt_ri)
+                assert(lcpt_le == bwt_le[1]);
         }
-        int str_len = str_ri - str_le;
-        assert(str_len <= ssize(t));
-        assert(s.substr(str_le, str_len) == t.substr(0, str_len));
-        assert(str_len == ssize(t) || str_ri == ssize(s) || t[str_len] != s[str_ri]);
-        assert((sa_le < sa_ri) == (str_len == ssize(t)));
-        auto [le, ri] = find_str(s, lt, t);
-        assert(sa_ri - sa_le == ri - le);
-        if (sa_ri - sa_le > 0) assert(sa_le == le);
-        cout << (!!(sa_ri - sa_le > 0)) << '\n';
+        cout << (!!(lcpt_ri - lcpt_le > 0)) << '\n';
     }
     return 0;
 }
---
+++ tests/library_checker_aizu_tests/strings/single_matching_bs.test.cpp
@@ -0,0 +1,48 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_14_B"
+//since this causes an O(n) partition check for each call to `lower_bound`,
+//causing TLE.
+#undef _GLIBCXX_DEBUG
+#include "../template.hpp"
+#include "../../../library/contest/random.hpp"
+
+#include "../../../library/strings/suffix_array_related/find/find_string_bs.hpp"
+#include "../../../library/strings/suffix_array_related/find/find_substrings_concatenated.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    string s, t;
+    cin >> s >> t;
+    auto [sa, sa_inv] = get_sa(s, 256);
+    auto [sa_le, sa_ri, str_le, str_ri] = find_str(s, sa, t);
+    int str_len = str_ri - str_le;
+    assert(s.substr(str_le, str_len) == t.substr(0, str_len));
+    assert(str_len <= ssize(t));
+    assert(str_len == ssize(t) || str_ri == ssize(s) || t[str_len] != s[str_ri]);
+    assert((sa_le < sa_ri) == (str_len == ssize(t)));
+    vector<int> matches(begin(sa) + sa_le, begin(sa) + sa_ri);
+    sort(begin(matches), end(matches));
+    {
+        //test find_substrs_concated
+        string both = s + '$' + t;
+        int t_start = ssize(s) + 1;
+        lcp_query lq_both(both, 256);
+        vector<int> splits = {0, ssize(t)};
+        for (int num_splits = get_rand(0, 4); num_splits--;)
+            splits.push_back(get_rand(0, ssize(t)));
+        sort(begin(splits), end(splits));
+        vector<array<int, 2>> subs;
+        for (int i = 1; i < ssize(splits); i++)
+            subs.push_back({splits[i - 1] + t_start, splits[i] + t_start});
+        assert(!subs.empty());
+        auto [sa_le2, sa_ri2, str_le2, str_ri2] = find_substrs_concated(lq_both, subs);
+        assert(both.substr(str_le2, str_ri2 - str_le2) == t);
+        assert(sa_ri2 - sa_le2 == 1 + sa_ri - sa_le);
+        vector<int> matches_other(begin(lq_both.sa) + sa_le2, begin(lq_both.sa) + sa_ri2);
+        matches_other.erase(remove_if(begin(matches_other), end(matches_other), [&](int val) {return val >= ssize(s) + 1;}), end(matches_other));
+        sort(begin(matches_other), end(matches_other));
+        assert(matches == matches_other);
+    }
+    for (auto match : matches)
+        cout << match << '\n';
+    return 0;
+}
