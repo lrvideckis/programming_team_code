dffb9ddd4464b86a38c22d5bf7619ba9a67cf56d
Luke Videckis <lukevideckis@gmail.com>
2023-09-29 14:42:21 -0700

    fix tests

--- tests/library_checker_aizu_tests/loops/submasks.test.cpp
+++ tests/library_checker_aizu_tests/loops/submasks.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/all/ITP2_11_C"
 #include "../template.hpp"
 
-#include "../../../library/iterate_subsets/iterate_submasks.hpp"
+#include "../../../library/loops/submasks.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -14,13 +14,13 @@ int main() {
         mask |= 1 << bit;
     }
     cout << 0 << ':' << '\n';
-    vector<int> submasks;
-    auto func = [&](int submask) {
-        submasks.push_back(submask);
+    vector<int> sb_msks;
+    auto f = [&](int submask) {
+        sb_msks.push_back(submask);
     };
-    iterate_submasks(mask, func);
-    sort(begin(submasks), end(submasks));
-    for (auto submask : submasks) {
+    submasks(mask, f);
+    sort(begin(sb_msks), end(sb_msks));
+    for (auto submask : sb_msks) {
         cout << submask << ':';
         for (int bit = 0; bit < n; bit++) {
             if ((submask >> bit) & 1)
