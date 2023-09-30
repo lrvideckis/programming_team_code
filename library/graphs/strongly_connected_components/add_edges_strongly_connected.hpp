d49387351345f1b36a304e1b10db7dde31aa060a
Luke Videckis <lukevideckis@gmail.com>
2023-09-30 16:39:21 -0700

    more golf

--- library/graphs/strongly_connected_components/add_edges_strongly_connected.hpp
+++ library/graphs/strongly_connected_components/add_edges_strongly_connected.hpp
@@ -24,13 +24,12 @@ vector<array<int, 2>> extra_edges(const vector<vector<int>>& adj, int num_sccs, 
     int n = ssize(adj);
     vector<vector<int>> scc_adj(num_sccs);
     vector<bool> zero_in(num_sccs, 1);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         for (int v : adj[i]) {
             if (scc_id[i] == scc_id[v]) continue;
             scc_adj[scc_id[i]].push_back(scc_id[v]);
             zero_in[scc_id[v]] = 0;
         }
-    }
     vector<bool> vis(num_sccs);
     auto dfs = [&](auto&& self, int u) -> int {
         if (scc_adj[u].empty()) return u;
@@ -53,16 +52,14 @@ vector<array<int, 2>> extra_edges(const vector<vector<int>>& adj, int num_sccs, 
         }
     for (int i = 1; i < ssize(edges); i++) swap(edges[i][0], edges[i - 1][0]);
     for (int i = 0; i < num_sccs; i++)
-        if (scc_adj[i].empty() && !vis[i]) {
+        if (scc_adj[i].empty() && !vis[i])
             if (!in_unused.empty()) {
                 edges.push_back({i, in_unused.back()});
                 in_unused.pop_back();
-            } else
-                edges.push_back({i, num_sccs - 1});
-        }
+            } else edges.push_back({i, num_sccs - 1});
     for (int u : in_unused) edges.push_back({0, u});
-    vector<int> to_a_node(num_sccs);
-    for (int i = 0; i < n; i++) to_a_node[scc_id[i]] = i;
-    for (auto& [u, v] : edges) u = to_a_node[u], v = to_a_node[v];
+    vector<int> to_node(num_sccs);
+    for (int i = 0; i < n; i++) to_node[scc_id[i]] = i;
+    for (auto& [u, v] : edges) u = to_node[u], v = to_node[v];
     return edges;
 }
