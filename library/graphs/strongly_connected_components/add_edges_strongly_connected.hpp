71b0285f96e4035c541327275baff66ba5686ba8
Luke Videckis <lukevideckis@gmail.com>
2023-09-30 15:51:47 -0700

    nits

--- library/graphs/strongly_connected_components/add_edges_strongly_connected.hpp
+++ library/graphs/strongly_connected_components/add_edges_strongly_connected.hpp
@@ -22,19 +22,18 @@ vector<pair<int, int>> extra_edges(const vector<vector<int>>& adj, int num_sccs,
     if (num_sccs == 1) return {};
     int n = ssize(adj);
     vector<vector<int>> scc_adj(num_sccs);
-    vector<int> in_deg(num_sccs);
+    vector<bool> zero_in(num_sccs, 1);
     for (int i = 0; i < n; i++) {
         for (int v : adj[i]) {
             if (scc_id[i] == scc_id[v]) continue;
             scc_adj[scc_id[i]].push_back(scc_id[v]);
-            in_deg[scc_id[v]]++;
+            zero_in[scc_id[v]] = 0;
         }
     }
-    vector<pair<int, int>> res_edges;
     vector<bool> vis(num_sccs);
     auto dfs = [&](auto&& self, int u) -> int {
         if (scc_adj[u].empty()) return u;
-        for (int v : scc_adj[u]) {
+        for (int v : scc_adj[u])
             if (!vis[v]) {
                 vis[v] = 1;
                 int zero_out_node = self(self, v);
@@ -42,13 +41,12 @@ vector<pair<int, int>> extra_edges(const vector<vector<int>>& adj, int num_sccs,
                 //WA if this isn't here, reason: complicated
                 if (zero_out_node != -1) return zero_out_node;
             }
-        }
         return -1;
     };
     vector<pair<int, int>> blocking_matching;
     vector<bool> node_used(num_sccs);
     for (int i = 0; i < num_sccs; i++) {
-        if (!in_deg[i]) {
+        if (zero_in[i]) {
             if (!vis[i]) {
                 vis[i] = 1;
                 int zero_out_node = dfs(dfs, i);
@@ -60,12 +58,13 @@ vector<pair<int, int>> extra_edges(const vector<vector<int>>& adj, int num_sccs,
             }
         }
     }
+    vector<pair<int, int>> res_edges;
     for (int i = 1; i < ssize(blocking_matching); i++)
         res_edges.emplace_back(blocking_matching[i].second, blocking_matching[i - 1].first);
     res_edges.emplace_back(blocking_matching[0].second, blocking_matching.back().first);
     queue<int> in_unused, out_unused;
     for (int i = 0; i < num_sccs; i++) {
-        if (!in_deg[i] && !node_used[i]) in_unused.push(i);
+        if (zero_in[i] && !node_used[i]) in_unused.push(i);
         if (scc_adj[i].empty() && !node_used[i]) out_unused.push(i);
     }
     while (!in_unused.empty() && !out_unused.empty()) {
