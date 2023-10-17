91bd142bbf6349459c31d93616038d31d7f56d68
Luke Videckis <lukevideckis@gmail.com>
2023-10-17 08:18:16 -0700

    different style which I like more now

--- library/strings/longest_common_subsequence/lcs_queries.hpp
+++ library/strings/longest_common_subsequence/lcs_queries.hpp
@@ -1,24 +1,23 @@
 /** @file */
 #pragma once
-#include "../../data_structures/binary_indexed_tree/rupq.hpp"
+#include "../../data_structures/binary_indexed_tree/bit.hpp"
 #include "lcs_dp.hpp"
-vector<int> lcs_queries(const vector<vector<int>>& h, const vector<array<int, 3>>& queries) {
-    auto n = ssize(h) - 1, m = ssize(h[0]) - 1, q = ssize(queries);
-    vector qs(n + 1, vector(m + 1, vector<array<int, 2>>()));
+template <class T> vector<int> lcs_queries(const T& s, const T& t, const vector<array<int, 3>>& queries) {
+    auto n = ssize(s), m = ssize(t), q = ssize(queries);
+    vector qs(n, vector(m, vector<array<int, 2>>()));
     for (int i = 0; i < q; i++) {
-        auto [a, b, c] = queries[i];
-        assert(0 <= a && a <= n);
-        assert(0 <= b && b <= c && c <= m);
-        qs[a][c].push_back({b, i});
+        auto [s_ri, t_le, t_ri] = queries[i];
+        assert(0 <= s_ri && s_ri <= n);
+        assert(0 <= t_le && t_le <= t_ri && t_ri <= m);
+        //if (s_ri == 0 || t_le == m) continue;
+        qs[s_ri - 1][t_le].push_back({t_ri, i});
     }
-    vector<int> res(q);
-    for (int i = 0; i <= n; i++) {
-        bit_rupq<int> bit{int(m)};
-        for (int j = 0; j <= m; j++) {
-            bit.update(h[i][j], j, 1);
-            for (auto [le, idx] : qs[i][j])
-                res[idx] = (le == m ? 0 : bit.get_index(le));
-        }
+    lcs_dp lcs(t);
+    for(int i = 0; i < n; i++) {
+        lcs.push_onto_s(s[i]);
+        // queries of given [t_le, t_ri): find number of indexes i such that t_le <= i < t_ri && lcs.dp[i] < t_le
+        BIT<int> bit{int(m)};
+
     }
     return res;
 }
--- tests/library_checker_aizu_tests/strings/lcs_queries.test.cpp
+++ tests/library_checker_aizu_tests/strings/lcs_queries.test.cpp
@@ -1,7 +1,9 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/prefix_substring_lcs"
 #include "../template.hpp"
 
-#include "../../../library/strings/longest_common_subsequence/lcs_queries.hpp"
+//#include "../../../library/strings/longest_common_subsequence/lcs_queries.hpp"
+#include "../../../library/strings/longest_common_subsequence/lcs_dp.hpp"
+#include "../../../library/data_structures/wavelet_merge/wavelet_tree.hpp"
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -9,13 +11,24 @@ int main() {
     string s, t;
     cin >> q >> s >> t;
     vector<vector<int>> h = lcs_dp(s, t);
+    vector<wavelet_tree> wts;
+    {
+        lcs_dp
+        for(int i = 0; i < ssize(h); i++)
+            wts.emplace_back(h[i], -1, ssize(t));
+    }
     vector<array<int, 3>> queries(q);
+    vector<int> res_from_wavelet(q);
     for (int i = 0; i < q; i++) {
         int a, b, c;
         cin >> a >> b >> c;
         queries[i] = {a, b, c};
+
+        cout << (a == 0 ? 0 : wts[a-1].rect_count(b, c, -1, b)) << '\n';
+        //res_from_wavelet[i] = wts[a].rect_count(b + 1, c + 1, 0, b + 1);
     }
-    vector<int> res = lcs_queries(h, queries);
-    for (int val : res) cout << val << '\n';
+    //vector<int> res = lcs_queries(h, queries);
+    //assert(res == res_from_wavelet);
+    //for (int val : res) cout << val << '\n';
     return 0;
 }
