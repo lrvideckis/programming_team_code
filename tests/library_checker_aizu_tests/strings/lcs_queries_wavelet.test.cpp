078a27e3320c233c72fe32b9c71a0c3e71bdcf86
Luke Videckis <lukevideckis@gmail.com>
2023-10-17 09:17:23 -0700

    online test for lcs queries

---
+++ tests/library_checker_aizu_tests/strings/lcs_queries_wavelet.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/prefix_substring_lcs"
+#include "../template.hpp"
+
+#include "../../../library/strings/longest_common_subsequence/lcs_dp.hpp"
+#include "../../../library/data_structures/wavelet_merge/wavelet_tree.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int q;
+    string s, t;
+    cin >> q >> s >> t;
+    vector<wavelet_tree> wts;
+    {
+        lcs_dp lcs(t);
+        wts.emplace_back(lcs.dp, -1, ssize(t));
+        for(char c : s) {
+            lcs.push_onto_s(c);
+            wts.emplace_back(lcs.dp, -1, ssize(t));
+        }
+    }
+    for (int i = 0; i < q; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << wts[a].rect_count(b, c, -1, b) << '\n';
+    }
+    return 0;
+}
