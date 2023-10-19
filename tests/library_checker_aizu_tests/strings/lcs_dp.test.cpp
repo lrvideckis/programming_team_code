c0f1e4b0ee215bb3007d4896c21e684a0b810cdd
Luke Videckis <lrvideckis@gmail.com>
2023-10-19 16:32:10 -0700

    Add misc test (#518)
    * a nice little test
    
    * [auto-verifier] verify commit 3cfebeb100539a6aa3a19a1f4afa5b97fffa2a0d
    
    * format
    
    * fix compile warning
    
    * [auto-verifier] verify commit 7b4be8096796ec1d233d81bf201fb9806665ce55
    
    * format
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>
    Co-authored-by: GitHub <noreply@github.com>

---
+++ tests/library_checker_aizu_tests/strings/lcs_dp.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_C"
+#include "../template.hpp"
+
+#include "../../../library/strings/longest_common_subsequence/lcs_dp.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        string s, t;
+        cin >> s >> t;
+        array<int, 2> res;
+        for (int j = 0; j < 2; j++) {
+            lcs_dp lcs(t);
+            for (char c : s) lcs.push_onto_s(c);
+            res[j] = int(count(begin(lcs.dp), end(lcs.dp), -1));
+            swap(s, t);
+        }
+        assert(res[0] == res[1]);
+        cout << res[0] << '\n';
+    }
+    return 0;
+}
