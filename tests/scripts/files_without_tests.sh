38370f3fa19747b3d52d0ffe0178101b7fe82dda
Luke Videckis <lukevideckis@gmail.com>
2023-10-25 14:29:41 -0700

    to test CI

--- tests/scripts/files_without_tests.sh
+++ tests/scripts/files_without_tests.sh
@@ -8,15 +8,15 @@ git submodule init
 git submodule update
 
 echo ".hpp files included by no .test.cpp file:"
-! comm -23 --check-order <(
+comm -23 --check-order <(
 	basename --multiple ../library/**/*.hpp |
 		sort |
 		uniq
 ) <(
 	g++ -MM library_checker_aizu_tests/**/*.test.cpp |
 		xargs basename --multiple |
-		grep "\.hpp$" |
 		sort |
 		uniq
 ) |
-	grep .
+	grep . &&
+    exit 1
