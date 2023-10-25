dc0f8856ef730c6ce3488af0701aedd94b7d1bc1
Luke Videckis <lrvideckis@gmail.com>
2023-10-25 09:39:40 -0700

    add interesting discussion

--- library/contest/hash.sh
+++ library/contest/hash.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#Hashes a file, ignoring all:
+# Hashes a file, ignoring all:
 #	- whitespace
 #	- comments
 #	- includes
 #	- pragmas
-#Use to verify that code was correctly typed.
+# see https://github.com/kth-competitive-programming/kactl/issues/72
 
-#usage:
+# usage:
 #	chmod +x hash.sh
 #	cat a.cpp | ./hash.sh
-#or just copy this command:
+# or just copy this command:
 #	cat a.cpp | sed -r '/(include|pragma)/d' | cpp -dD -fpreprocessed -P | tr -d '[:space:]' | md5sum
 sed -r '/(include|pragma)/d' | cpp -dD -fpreprocessed -P | tr -d '[:space:]' | md5sum
