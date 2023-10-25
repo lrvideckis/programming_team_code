5a967d3481ae999b60b607e529b2b1a543a460ed
Luke Videckis <lrvideckis@gmail.com>
2023-10-25 00:16:26 -0700

    Better ladder test (#526)
    * rooted and forest tests
    
    * remove todo comments
    
    * ran astyle
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>

--- tests/library_checker_aizu_tests/graphs/ladder_decomposition.test.cpp
+++ tests/library_checker_aizu_tests/graphs/ladder_decomposition.test.cpp
@@ -3,8 +3,6 @@
 
 #include "../../../library/graphs/ladder_decomposition.hpp"
 
-//TODO test forest
-//TODO test directed tree (edges point to children)
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     int n, q;
@@ -17,6 +15,13 @@ int main() {
         adj[v].push_back(u);
     }
     ladder ld(adj);
+    vector<vector<int>> adj_rooted(n + n);
+    for (int i = 0; i < n; i++)
+        if (ld.p[i] != i) {
+            adj_rooted[ld.p[i]].push_back(i);
+            adj_rooted[ld.p[i] + n].push_back(i + n);
+        }
+    ladder ld_rooted(adj_rooted);
     while (q--) {
         int u, v, k;
         cin >> u >> v >> k;
@@ -26,11 +31,15 @@ int main() {
         if (k > u_lca + v_lca) cout << -1 << '\n';
         else if (k <= u_lca) {
             int res = ld.kth_par(u, k);
-            assert(res == jmp(ld.b_tbl, u, k));//TODO maybe assert with side effects
+            assert(res == jmp(ld.b_tbl, u, k));
+            assert(res == ld_rooted.kth_par(u, k));
+            assert(res == ld_rooted.kth_par(u + n, k) - n);
             cout << res << '\n';
         } else {
             int res = ld.kth_par(v, u_lca + v_lca - k);
             assert(res == jmp(ld.b_tbl, v, u_lca + v_lca - k));
+            assert(res == ld_rooted.kth_par(v, u_lca + v_lca - k));
+            assert(res == ld_rooted.kth_par(v + n, u_lca + v_lca - k) - n);
             cout << res << '\n';
         }
     }
