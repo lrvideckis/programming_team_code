a2ddc7dbd869c0f6ed5376281f7247e378c9a4c4
Luke Videckis <lukevideckis@gmail.com>
2023-10-25 20:02:49 -0700

    fix script

--- tests/scripts/files_without_tests.sh
+++ tests/scripts/files_without_tests.sh
@@ -6,16 +6,14 @@ git submodule init
 git submodule update
 
 echo ".hpp files included by no .test.cpp file:"
-comm -23 --check-order <(
+! comm -23 --check-order <(
 	basename --multiple ../library/**/*.hpp |
 		sort |
 		uniq
 ) <(
 	g++ -MM library_checker_aizu_tests/**/*.test.cpp |
 		xargs basename --multiple |
-		grep "\.hpp$" |
 		sort |
 		uniq
 ) |
-	grep . &&
-    exit 1
+	grep .
