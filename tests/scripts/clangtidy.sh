14ea90674cbbfeb2e3e6e3197d3a1900549f5b66
Luke Videckis <lukevideckis@gmail.com>
2023-10-26 08:25:17 -0700

    trying this

--- tests/scripts/clangtidy.sh
+++ tests/scripts/clangtidy.sh
@@ -5,8 +5,6 @@ shopt -s globstar
 git submodule init
 git submodule update
 
-clang-tidy --config-file=.config/.clang-tidy --dump-config
-
-#find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
-	#parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=c++17 ||
-	#exit 1
+find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
+	parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=c++17 ||
+	exit 1
