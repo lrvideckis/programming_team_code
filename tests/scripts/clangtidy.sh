0937c3f52aff7fff63fe10a6842783b62caa96b1
Luke Videckis <lukevideckis@gmail.com>
2023-10-26 08:18:07 -0700

    seeing config on remote

--- tests/scripts/clangtidy.sh
+++ tests/scripts/clangtidy.sh
@@ -5,6 +5,8 @@ shopt -s globstar
 git submodule init
 git submodule update
 
-find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
-	parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=c++17 ||
-	exit 1
+clang-tidy --config-file=.config/.clang-tidy --dump-config
+
+#find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
+	#parallel clang-tidy --config-file=.config/.clang-tidy {} -- -std=c++17 ||
+	#exit 1
