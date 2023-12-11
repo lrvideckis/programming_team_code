779330d761bd137756100936e5d649dfd9a82f2e
Luke Videckis <lukevideckis@gmail.com>
2023-12-11 06:46:34 -0800

    Squashed commit of the following:
    commit 453a47189105e4a3a7d8aa652ca6e2254e41abf8
    Author: GitHub <noreply@github.com>
    Date:   Mon Dec 11 14:42:45 2023 +0000
    
        [auto-verifier] verify commit 41b0082a87d22d09d74137ff0c4092ea2d0d5768
    
    commit 41b0082a87d22d09d74137ff0c4092ea2d0d5768
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Mon Dec 11 06:40:35 2023 -0800
    
        should fix compile warning
    
    commit 59ad118e4564c5df8ae587af2c09dec4c7dd844a
    Author: GitHub <noreply@github.com>
    Date:   Mon Dec 11 14:29:30 2023 +0000
    
        [auto-verifier] verify commit 0b66252eb772cf3670903e3d45b89b4697b47bb1
    
    commit 0b66252eb772cf3670903e3d45b89b4697b47bb1
    Merge: 4341600 eca18d5
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Mon Dec 11 06:27:35 2023 -0800
    
        Merge branch 'add_tests' of github.com:lrvideckis/programming_team_code into add_tests
    
    commit 4341600e41e797d4c00ab13ff6fb2e338e2a6be7
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Mon Dec 11 06:27:22 2023 -0800
    
        fix overflow in test
    
    commit eca18d52ac24caa84ce4101738233b852eabbf72
    Author: GitHub <noreply@github.com>
    Date:   Mon Dec 11 14:25:34 2023 +0000
    
        [auto-verifier] verify commit 88915f02639b0e021910ed89b822963beeda6273
    
    commit 88915f02639b0e021910ed89b822963beeda6273
    Merge: 4e0a358 1ab1bd4
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Mon Dec 11 06:23:19 2023 -0800
    
        Merge branch 'add_tests' of github.com:lrvideckis/programming_team_code into add_tests
    
    commit 4e0a358f1acbb127e2470ee8ea5275483ed210fc
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Mon Dec 11 06:23:04 2023 -0800
    
        fix TLE
    
    commit 1ab1bd4137cbae265579e7a701a755ef25a6f954
    Author: GitHub <noreply@github.com>
    Date:   Mon Dec 11 14:04:46 2023 +0000
    
        [auto-verifier] verify commit 91522e02de06a434ccf70b322980baf0bb54b4fc
    
    commit 91522e02de06a434ccf70b322980baf0bb54b4fc
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Mon Dec 11 05:55:07 2023 -0800
    
        add some tests

---
+++ tests/library_checker_aizu_tests/strings/sa_cmp.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/all/ITP2_3_D"
+#include "../template.hpp"
+
+#include "../../../library/strings/suffix_array_related/compare/compare_substrings.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        arr.push_back(val);
+    }
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int val;
+        cin >> val;
+        arr.push_back(val);
+    }
+    lcp_query lq(arr, 1001);
+    cout << (substr_cmp(lq, 0, n, n, n + m) < 0) << '\n';
+    return 0;
+}
