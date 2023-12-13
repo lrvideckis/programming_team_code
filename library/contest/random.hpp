870c064493e7e4ba6c80b02c3c5ff47438fc6645
Luke Videckis <lukevideckis@gmail.com>
2023-12-13 05:40:39 -0800

    unhackable rand on codeforces
    commit e2cd98afa30b62046b5a6a31d53932811b0761c6
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Wed Dec 13 05:34:30 2023 -0800
    
        fix dox
    
    commit 8d69ece6ab779fbec99db61f287960810ffac49f
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Wed Dec 13 05:32:49 2023 -0800
    
        actually lets not change the name
    
    commit 516a8a73f8e2ca36f431f8cd538ca4d14188e1b1
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Wed Dec 13 05:32:19 2023 -0800
    
        yet another example
    
    commit 27cdd5d747facb7f1a02d889075081c78ee2a58a
    Author: GitHub <noreply@github.com>
    Date:   Wed Dec 13 13:29:21 2023 +0000
    
        [auto-verifier] verify commit 071b75e9541f793f9e37d9136e3f83f46553a2d2
    
    commit 071b75e9541f793f9e37d9136e3f83f46553a2d2
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Wed Dec 13 05:19:17 2023 -0800
    
        add another example
    
    commit a55451738246507581c7c66272311e9c73cdaf15
    Author: GitHub <noreply@github.com>
    Date:   Wed Dec 13 13:03:42 2023 +0000
    
        [auto-verifier] verify commit 4236281b3bb8d957f202d00979facd65ca052f9a
    
    commit 4236281b3bb8d957f202d00979facd65ca052f9a
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Wed Dec 13 04:53:35 2023 -0800
    
        unhackable rand on codeforces

--- library/contest/random.hpp
+++ library/contest/random.hpp
@@ -1,22 +1,30 @@
 /** @file */
 #pragma once
-random_device rd;
-/**
- * @returns pseudo-random number generator
- * @code{.cpp}
- *     vector<int> a;
- *     shuffle(begin(a), end(a), rng);
- * @endcode
- *
- * use mt19937_64 rng(rd()) for 64-bit random-number generation
- */
-mt19937 rng(rd());
+mt19937 rng((unsigned int)chrono::steady_clock::now().time_since_epoch().count());
 /**
  * @see https://codeforces.com/blog/entry/61675
+ * @code{.cpp}
+ *     unsigned int x = rng();
  *
- * Intended types: int, unsigned, long long
+ *     // 64-bit rng
+ *     mt19937_64 rng((unsigned int)chrono::steady_clock::now().time_since_epoch().count());
+ *     unsigned long long x = rng();
+ *
+ *     // fixed seed for debugging
+ *     mt19937 rng;
+ *     unsigned int x = rng();
+ *
+ *     vector<int> a(n);
+ *     shuffle(begin(a), end(a), rng);
+ *
+ *     vector<unsigned int> a(n);
+ *     generate(begin(a), end(a), rng);
+ *
+ *     int x = get_rand<int>(0, 1);
+ *     long long x = get_rand<long long>(1, 1e18);
+ * @endcode
  * @param le,ri defines range [le, ri]
- * @returns random number in range
+ * @returns random number in range, chosen with probability 1/(ri - le + 1)
  */
 template <class T> inline T get_rand(T le, T ri) {
     assert(le <= ri);
