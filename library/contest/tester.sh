4d45c19aba14a2202b2085d1a21a6aa38135732b
cameron custer <custer.cameron@gmail.com>
2023-12-20 12:48:46 -0500

    upgrade the testing script to use names with less collisions on linux systems and better output for # of tests run

---
+++ library/contest/tester.sh
@@ -0,0 +1,14 @@
+# see: https://github.com/Errichto/youtube/blob/master/testing/s.sh
+# usage:
+#	bash tester.sh
+
+g++ -std=c++17 a.cpp -o a.out
+g++ -std=c++17 b.cpp -o b.out
+g++ -std=c++17 gen.cpp -o gen.out
+
+for((i = 1; ; ++i))
+do
+	echo $i
+	./gen.out > input
+	diff -w <(./a.out < input) <(./b.out < input) || break
+done
