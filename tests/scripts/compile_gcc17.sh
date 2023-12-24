cab17bd4d6e68a71b4dfdb88428cc1393da72a61
Luke Videckis <lukevideckis@gmail.com>
2023-12-23 22:54:59 -0600

    revert

--- tests/scripts/compile_gcc17.sh
+++ tests/scripts/compile_gcc17.sh
@@ -6,4 +6,4 @@ git submodule init
 git submodule update
 
 find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
-	parallel g++ {} "$(tr '\n' ' ' <.config/.gcc_compile_flags)" -std=c++17 -Wconversion
+	parallel g++ {} "$(tr '\n' ' ' <.config/.gcc_compile_flags)" -std=c++17
