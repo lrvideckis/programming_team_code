c46d76dde83fef227bc3c8a82ebcf4395da010a5
Luke Videckis <lrvideckis@gmail.com>
2023-12-24 02:27:30 -0600

    Xenia and tree (#544)
    * add contour sum CD test
    
    * nit
    
    * fix cpp check
    
    * fix compile warning
    
    * [auto-verifier] verify commit 64a16d06ab1f56caabc176cfec74005c030eed66
    
    * add doxygen
    
    * fix cpp chekc
    
    * fix hackpack
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>
    Co-authored-by: GitHub <noreply@github.com>

---
+++ library/graphs/edge_centroid_decomp/contour_sum.hpp
@@ -0,0 +1,86 @@
+/** @file */
+#pragma once
+#include "../../data_structures/bit.hpp"
+#include "edge_cd.hpp"
+/**
+ * @see https://judge.yosupo.jp/problem /vertex_add_range_contour_sum_on_tree
+ *
+ * Note for edge centroid decomp you need to special case single-edge paths.
+ * I do this by BIT over pre order BFS traversal
+ */
+template <class T> struct contour_sum {
+    vector<vector<int>> adj;
+    int n;
+    vector<T> a;
+    vector<vector<array<int, 3>>> info;
+    vector<array<BIT<T>, 2>> bits;
+    vector<int> in, in_ch, par;
+    BIT<T> order;
+    /**
+     * @param a_adj unrooted, undirected tree
+     * @param a_a a_a[u] = initial value for node u
+     * @time O(n log1.5 n)
+     * @space O(n log1.5 n) for `info` and `bits`
+     */
+    contour_sum(const vector<vector<int>>& a_adj, const vector<T>& a_a) : adj(a_adj), n(ssize(a_a)), a(a_a), info(n), in(n), in_ch(n, -1), par(n) {
+        edge_cd(adj, [&](const vector<vector<int>>& adj_cd, int cent, int split) -> void {
+            vector<vector<T>> sum_val(2, vector<T>(1));
+            auto dfs = [&](auto&& self, int u, int p, int d, int side) -> void {
+                info[u].push_back({ssize(bits), d, side});
+                if (ssize(sum_val[side]) == d) sum_val[side].push_back(0);
+                sum_val[side][d] += a[u];
+                for (int v : adj_cd[u])
+                    if (v != p) self(self, v, u, 1 + d, side);
+            };
+            for (int i = 0; i < ssize(adj_cd[cent]); i++)
+                dfs(dfs, adj_cd[cent][i], cent, 1, i < split);
+            bits.push_back({BIT<T>(sum_val[0]), BIT<T>(sum_val[1])});
+        });
+        queue<array<int, 2>> q({{0, -1}});
+        vector<T> init;
+        while (!empty(q)) {
+            auto [u, p] = q.front();
+            q.pop();
+            par[u] = p;
+            if (p != -1 && in_ch[p] == -1) in_ch[p] = ssize(init);
+            in[u] = ssize(init);
+            init.push_back(a[u]);
+            for (int v : adj[u])
+                if (v != p) q.push({v, u});
+        }
+        order = {init};
+    }
+    /**
+     * @param u node
+     * @param delta value to add to node u's value
+     * @time O(log1.5(n) * log2(n))
+     * @space O(1)
+     */
+    void update(int u, T delta) {
+        a[u] += delta;
+        order.update(in[u], delta);
+        for (auto [decomp, d, side] : info[u])
+            bits[decomp][side].update(d, delta);
+    }
+    /**
+     * @param u node
+     * @param le,ri defines range [le, ri)
+     * @returns sum of node v's value over all v such that le <= dist_edges(u, v) < ri
+     * @time O(log1.5(n) * log2(n))
+     * @space O(1)
+     */
+    T query(int u, int le, int ri) {
+        T sum = 0;
+        if (le == 0) sum += a[u];
+        if (le <= 1 && 1 < ri) {
+            if (par[u] != -1) sum += a[par[u]];
+            if (in_ch[u] != -1) sum += order.sum(in_ch[u], in_ch[u] + ssize(adj[u]) - (par[u] != -1));
+        }
+        for (auto [decomp, d, side] : info[u]) {
+            auto& bit = bits[decomp][!side];
+            int my_l = clamp<int>(le - d, 1, ssize(bit.s)), my_r = clamp<int>(ri - d, 1, ssize(bit.s));
+            sum += bit.sum(my_l, my_r);
+        }
+        return sum;
+    }
+};
---
+++ tests/library_checker_aizu_tests/graphs/contour_sum_edge_cd.test.cpp
@@ -0,0 +1,57 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/vertex_add_range_contour_sum_on_tree"
+#include "../template.hpp"
+
+#include "../../../library/graphs/edge_centroid_decomp/contour_sum.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, q;
+    cin >> n >> q;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    vector<vector<int>> adj(n);
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    {
+        edge_cd(adj, [&](const vector<vector<int>>& adj_cd, int cent, int split) -> void {
+            auto dfs = [&](auto&& self, int u, int p) -> int {
+                int siz = 1;
+                for (int v : adj_cd[u])
+                    if (v != p) siz += self(self, v, u);
+                return siz;
+            };
+            int sz_all = dfs(dfs, cent, -1);
+            array<int, 2> cnts = {0, 0};
+            for (int i = 0; i < ssize(adj_cd[cent]); i++) {
+                int sz_subtree = dfs(dfs, adj_cd[cent][i], cent);
+                assert(2 * sz_subtree <= sz_all);
+                cnts[i < split] += sz_subtree;
+            }
+            assert(cnts[0] + cnts[1] + 1 == sz_all);
+            for (int i = 0; i < 2; i++)
+                assert(cnts[i] <= 2 * cnts[!i]);
+        });
+    }
+    contour_sum<long long> cs(adj, a);
+    while (q--) {
+        int type;
+        cin >> type;
+        if (type == 0) {
+            int u;
+            long long delta;
+            cin >> u >> delta;
+            cs.update(u, delta);
+        } else {
+            assert(type == 1);
+            int u, le, ri;
+            cin >> u >> le >> ri;
+            cout << cs.query(u, le, ri) << '\n';
+        }
+    }
+    return 0;
+}
