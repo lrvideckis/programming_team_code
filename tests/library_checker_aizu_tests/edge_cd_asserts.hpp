ebb29353a33f51d4e6efa6e9c1904e9906eb3ca3
Luke Videckis <lukevideckis@gmail.com>
2023-12-26 16:46:40 -0600

    easier to read
    commit c7c66bd12905c9408dd0975dab005ac7d189266a
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Tue Dec 26 16:45:16 2023 -0600
    
        rename it here
    
    commit 80177a76338d384f095dae429a9174bdf2ac292c
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Tue Dec 26 16:42:47 2023 -0600
    
        easier to read

--- tests/library_checker_aizu_tests/edge_cd_asserts.hpp
+++ tests/library_checker_aizu_tests/edge_cd_asserts.hpp
@@ -1,18 +1,18 @@
 #pragma once
-void edge_cd_asserts(const vector<vector<int>>& cd_adj, int cent, int split) {
-    assert(ssize(cd_adj) > 2);
-    assert(0 < split && split < ssize(cd_adj[cent]));
+void edge_cd_asserts(const vector<vector<int>>& adj, int cent, int split) {
+    assert(ssize(adj) > 2);
+    assert(0 < split && split < ssize(adj[cent]));
     auto dfs = [&](auto&& self, int u, int p) -> int {
         int siz = 1;
-        for (int v : cd_adj[u])
+        for (int v : adj[u])
             if (v != p) siz += self(self, v, u);
         return siz;
     };
     int sz_all = dfs(dfs, cent, -1);
     assert(sz_all >= 3);
     array<int, 2> cnts = {0, 0};
-    for (int i = 0; i < ssize(cd_adj[cent]); i++) {
-        int sz_subtree = dfs(dfs, cd_adj[cent][i], cent);
+    for (int i = 0; i < ssize(adj[cent]); i++) {
+        int sz_subtree = dfs(dfs, adj[cent][i], cent);
         assert(2 * sz_subtree <= sz_all);
         cnts[i < split] += sz_subtree;
     }
