d41e191bf728882043ab7dbd7fc1987e526dade5
Luke Videckis <lukevideckis@gmail.com>
2023-12-25 18:27:23 -0600

    add new lib checker test
    commit d3da2d69a088c75c20879cbc436cea0c0a5d34f0
    Author: GitHub <noreply@github.com>
    Date:   Tue Dec 26 00:25:45 2023 +0000
    
        [auto-verifier] verify commit f4f38676c6bcb6f0e3113dae6fa11e3f1197f12c
    
    commit f4f38676c6bcb6f0e3113dae6fa11e3f1197f12c
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Mon Dec 25 18:23:35 2023 -0600
    
        add new lib checker test

---
+++ tests/library_checker_aizu_tests/data_structures/distinct_query.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_count_distinct"
+#include "../template.hpp"
+
+#include "../../../library/data_structures/seg_tree_uncommon/distinct_query.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    distinct_query dq(a);
+    while (q--) {
+        int le, ri;
+        cin >> le >> ri;
+        cout << dq.query(le, ri) << '\n';
+    }
+    return 0;
+}
