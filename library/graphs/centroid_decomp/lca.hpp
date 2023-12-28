ec9ba12ce778d461874f988252fea3f5d5d4c035
Luke Videckis <lrvideckis@gmail.com>
2023-12-28 12:47:03 -0600

    Lca nits (#548)
    * golf
    
    * fix unused var warning
    
    * add another test
    
    * [auto-verifier] verify commit d38d86e73e51e07b2ab419bd32faf270419b142c
    
    * doxygen
    
    * remove old comment
    
    ---------
    
    Co-authored-by: Luke Videckis <lukevideckis@gmail.com>
    Co-authored-by: GitHub <noreply@github.com>

--- library/graphs/centroid_decomp/lca.hpp
+++ library/graphs/centroid_decomp/lca.hpp
@@ -1,26 +1,26 @@
 /** @file */
 #pragma once
-#include "edge_cd.hpp"
+#include "centroid_decomp.hpp"
 /**
  * @see https://codeforces.com/blog/entry/81661#comment-686024
  */
-struct edgecd_lca {
+struct cd_lca {
     vector<int> d;
     vector<vector<int>> mn_d, to_cent;
     int cmp(int u, int v) {return d[u] < d[v] ? u : v;}
     /**
      * @param adj unrooted, undirected tree
-     * @time O(n log1.5 n)
-     * @space O(n log1.5 n) for `mn_d` and `to_cent` vectors
+     * @time O(n log n)
+     * @space O(n log n) for `mn_d` and `to_cent` vectors
      */
-    edgecd_lca(const vector<vector<int>>& adj) : d(ssize(adj)), mn_d(ssize(adj)), to_cent(ssize(adj)) {
+    cd_lca(const vector<vector<int>>& adj) : d(ssize(adj)), mn_d(ssize(adj)), to_cent(ssize(adj)) {
         auto dfs_d = [&](auto&& self, int u, int p) -> void {
             for (int v : adj[u])
                 if (v != p)
                     d[v] = 1 + d[u], self(self, v, u);
         };
         dfs_d(dfs_d, 0, -1);
-        edge_cd(adj, [&](const vector<vector<int>>& cd_adj, int cent, int) -> void {
+        centroid(adj, [&](const vector<vector<int>>& cd_adj, int cent) -> void {
             auto dfs = [&](auto&& self, int u, int p) -> void {
                 mn_d[u].push_back(p == -1 ? u : cmp(mn_d[p].back(), u));
                 to_cent[u].push_back(cent);
@@ -31,13 +31,12 @@ struct edgecd_lca {
         });
     }
     /**
-     * @param u,v 2 nodes
+     * @param u,v nodes
      * @returns lca of u, v; where the root is 0
-     * @time O(log1.5 n)
+     * @time O(log(n))
      * @space O(1)
      */
     int get_lca(int u, int v) {
-        if (u == v) return u;
         for (int i = min(ssize(mn_d[u]), ssize(mn_d[v])) - 1;; i--)
             if (to_cent[u][i] == to_cent[v][i])
                 return cmp(mn_d[u][i], mn_d[v][i]);
