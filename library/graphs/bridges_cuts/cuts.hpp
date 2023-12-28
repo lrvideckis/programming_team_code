b38fa0fb2174db268a3f1211404dba4e46a638d7
Luke Videckis <lukevideckis@gmail.com>
2023-12-27 20:25:32 -0600

    document+assert that self edges are not allowed

--- library/graphs/bridges_cuts/cuts.hpp
+++ library/graphs/bridges_cuts/cuts.hpp
@@ -22,7 +22,7 @@ struct cut_info {
  *     }
  *     auto [num_bccs, is_cut, bcc_id] = cuts(adj, m);
  * @endcode
- * @param adj undirected graph; possibly with multiple edges
+ * @param adj undirected graph; possibly with multiple edges; self edges not allowed
  * @param m number of edges
  * @returns info about cut nodes.
  * @time O(n + m)
@@ -33,13 +33,14 @@ cut_info cuts(const vector<vector<array<int, 2>>>& adj, int m) {
     vector<int> tin(n), bcc_id(m), st;
     vector<bool> is_cut(n);
     st.reserve(m);
-    auto dfs = [&](auto&& self, int u, int p_id) -> int {
+    auto dfs = [&](auto&& self, int u, int p) -> int {
         int low = tin[u] = timer++, deg = 0;
         for (auto [v, e_id] : adj[u]) {
-            if (e_id == p_id) continue;
+            assert(u != v);
+            if (v == p) continue;
             if (!tin[v]) {
                 st.push_back(e_id);
-                int low_ch = self(self, v, e_id);
+                int low_ch = self(self, v, u);
                 if (low_ch >= tin[u]) {
                     is_cut[u] = 1;
                     while (1) {
@@ -57,7 +58,7 @@ cut_info cuts(const vector<vector<array<int, 2>>>& adj, int m) {
                 low = min(low, tin[v]);
             }
         }
-        if (p_id == -1) is_cut[u] = (deg > 1);
+        if (p == -1) is_cut[u] = (deg > 1);
         return low;
     };
     for (int i = 0; i < n; i++)
