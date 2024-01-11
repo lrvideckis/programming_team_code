573e450599b903295bff92c58fad41e3dec3fc4e
Luke Videckis <lukevideckis@gmail.com>
2024-01-11 12:16:40 -0600

    pull out common code into helper struct
    commit ed0b6b578c5e743e1e05352cc0c2bd351b3e147e
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Thu Jan 11 12:15:42 2024 -0600
    
        doxygen
    
    commit 0dfeffbfa95f6b94a3c03d7c400b649efcf4b4ef
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Thu Jan 11 12:06:36 2024 -0600
    
        fix cpp check
    
    commit 6f0d66654dc9db6111370a92c8ac7ce0805e5b58
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Thu Jan 11 12:04:46 2024 -0600
    
        pull out common code into helper struct

--- library/graphs/edge_centroid_decomp/contour_range_query.hpp
+++ library/graphs/edge_centroid_decomp/contour_range_query.hpp
@@ -1,23 +1,23 @@
 /** @file */
 #pragma once
 #include "../../data_structures/bit.hpp"
+#include "sum_adjacent.hpp"
 #include "edge_cd.hpp"
 /**
  * @see https://judge.yosupo.jp/problem /vertex_add_range_contour_sum_on_tree
  */
 template <class T> struct contour_range_query {
     int n;
-    vector<T> a, sum_ch;
-    vector<int> par;
+    sum_adj<T> sum_a;
     vector<vector<array<int, 3>>> info;
     vector<array<BIT<T>, 2>> bits;
     /**
      * @param adj unrooted, undirected tree
-     * @param a_a a_a[u] = initial value for node u
+     * @param a a[u] = initial value for node u
      * @time O(n log1.5 n)
      * @space O(n log1.5 n) for `info` and `bits`
      */
-    contour_range_query(const vector<vector<int>>& adj, const vector<T>& a_a) : n(ssize(a_a)), a(a_a), sum_ch(n), par(n, -1), info(n) {
+    contour_range_query(const vector<vector<int>>& adj, const vector<T>& a) : n(ssize(a)), sum_a(adj, a), info(n) {
         edge_cd(adj, [&](const vector<vector<int>>& cd_adj, int cent, int split) {
             vector<vector<T>> sum_val(2, vector<T>(1));
             auto dfs = [&](auto&& self, int u, int p, int d, int side) -> void {
@@ -31,12 +31,6 @@ template <class T> struct contour_range_query {
                 dfs(dfs, cd_adj[cent][i], cent, 1, i < split);
             bits.push_back({BIT<T>(sum_val[0]), BIT<T>(sum_val[1])});
         });
-        auto dfs = [&](auto&& self, int u) -> void {
-            for (int v : adj[u])
-                if (v != par[u])
-                    par[v] = u, sum_ch[u] += a[v], self(self, v);
-        };
-        dfs(dfs, 0);
     }
     /**
      * @param u node
@@ -45,8 +39,7 @@ template <class T> struct contour_range_query {
      * @space O(1)
      */
     void update(int u, T delta) {
-        a[u] += delta;
-        if (par[u] != -1) sum_ch[par[u]] += delta;
+        sum_a.update(u, delta);
         for (auto [decomp, d, side] : info[u])
             bits[decomp][side].update(d, delta);
     }
@@ -60,8 +53,8 @@ template <class T> struct contour_range_query {
     T query(int u, int le, int ri) {
         assert(0 <= le && le <= ri && ri <= n);
         T sum = 0;
-        if (le <= 0 && 0 < ri) sum += a[u];
-        if (le <= 1 && 1 < ri) sum += sum_ch[u] + (par[u] != -1 ? a[par[u]] : 0);
+        if (le <= 0 && 0 < ri) sum += sum_a.sum[u];
+        if (le <= 1 && 1 < ri) sum += sum_a.query(u);
         for (auto [decomp, d, side] : info[u]) {
             auto& bit = bits[decomp][!side];
             int my_l = clamp<int>(le - d, 1, ssize(bit.s));
--- library/graphs/edge_centroid_decomp/contour_range_update.hpp
+++ library/graphs/edge_centroid_decomp/contour_range_update.hpp
@@ -1,14 +1,15 @@
 /** @file */
 #pragma once
 #include "../../data_structures/bit_uncommon/rupq.hpp"
+#include "sum_adjacent.hpp"
 #include "edge_cd.hpp"
 /**
  * @see https://judge.yosupo.jp/problem /vertex_get_range_contour_add_on_tree
  */
 template <class T> struct contour_range_update {
     int n;
-    vector<T> a, sum_par, sum_ch;
-    vector<int> par;
+    vector<T> a;
+    sum_adj<T> sum_a;
     vector<vector<array<int, 3>>> info;
     vector<array<bit_rupq<T>, 2>> bits;
     /**
@@ -17,7 +18,7 @@ template <class T> struct contour_range_update {
      * @time O(n log1.5 n)
      * @space O(n log1.5 n) for `info` and `bits`
      */
-    contour_range_update(const vector<vector<int>>& adj, const vector<T>& a_a) : n(ssize(a_a)), a(a_a), sum_par(n), sum_ch(n), par(n, -1), info(n) {
+    contour_range_update(const vector<vector<int>>& adj, const vector<T>& a_a) : n(ssize(a_a)), a(a_a), sum_a(adj, vector<T>(n)), info(n) {
         edge_cd(adj, [&](const vector<vector<int>>& cd_adj, int cent, int split) {
             array<int, 2> mx_d = {0, 0};
             auto dfs = [&](auto&& self, int u, int p, int d, int side) -> void {
@@ -30,12 +31,6 @@ template <class T> struct contour_range_update {
                 dfs(dfs, cd_adj[cent][i], cent, 1, i < split);
             bits.push_back({bit_rupq<T>(mx_d[0] + 1), bit_rupq<T>(mx_d[1] + 1)});
         });
-        auto dfs = [&](auto&& self, int u) -> void {
-            for (int v : adj[u])
-                if (v != par[u])
-                    par[v] = u, self(self, v);
-        };
-        dfs(dfs, 0);
     }
     /**
      * @param u,le,ri,delta add delta to all nodes v such that le <= dist_edges(u, v) < ri
@@ -45,10 +40,7 @@ template <class T> struct contour_range_update {
     void update(int u, int le, int ri, T delta) {
         assert(0 <= le && le <= ri && ri <= n);
         if (le <= 0 && 0 < ri) a[u] += delta;
-        if (le <= 1 && 1 < ri) {
-            sum_par[u] += delta;
-            if (par[u] != -1) sum_ch[par[u]] += delta;
-        }
+        if (le <= 1 && 1 < ri) sum_a.update(u, delta);
         for (auto [decomp_id, d, side] : info[u]) {
             auto& bit = bits[decomp_id][!side];
             int my_l = clamp<int>(le - d, 1, bit.n);
@@ -63,7 +55,7 @@ template <class T> struct contour_range_update {
      * @space O(1)
      */
     T query(int u) {
-        T sum = a[u] + sum_ch[u] + (par[u] != -1 ? sum_par[par[u]] : 0);
+        T sum = a[u] + sum_a.query(u);
         for (auto [decomp_id, d, side] : info[u])
             sum += bits[decomp_id][side].get_index(d);
         return sum;
