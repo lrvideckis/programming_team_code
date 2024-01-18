856436bc987b70eb5e61825a9acc3caed5a86875
Luke Videckis <lukevideckis@gmail.com>
2024-01-18 13:29:38 -0600

    add asserts to perm tree test
    commit 373aca1bbf9482e10f6bde2f5c80994fe81ef2d9
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Thu Jan 18 13:28:23 2024 -0600
    
        more asserts!!
    
    commit 1da48059d35f06cf804cad700da0b13fa6887d0d
    Author: GitHub <noreply@github.com>
    Date:   Thu Jan 18 19:28:07 2024 +0000
    
        [auto-verifier] verify commit 050e0129df388c194d9bdf2632591b70fb08496b
    
    commit 050e0129df388c194d9bdf2632591b70fb08496b
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Thu Jan 18 13:24:54 2024 -0600
    
        add asserts to perm tree test

--- tests/library_checker_aizu_tests/data_structures/permutation_tree.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/permutation_tree.test.cpp
@@ -1,7 +1,12 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/common_interval_decomposition_tree"
 #include "../template.hpp"
 
+#include "../../../library/data_structures/rmq.hpp"
 #include "../../../library/data_structures/uncommon/permutation_tree.hpp"
+
+bool my_touches(int u, int v, const vector<int>& mn_val, const vector<int>& len) {
+    return mn_val[u] == mn_val[v] + len[v] || mn_val[v] == mn_val[u] + len[u];
+};
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -10,16 +15,48 @@ int main() {
     vector<int> a(n);
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    perm_tree pt(a);
-    cout << ssize(pt.adj) << '\n';
+    RMQ rmq_min(a, [](int x, int y) {return min(x, y);});
+    RMQ rmq_max(a, [](int x, int y) {return max(x, y);});
+    auto [is_join, mn_idx, mn_val, len, root, adj] = perm_tree(a);
+    assert(mn_idx[root] == 0 && mn_val[root] == 0 && len[root] == n);
+    cout << ssize(adj) << '\n';
     int curr_time = 0;
-    vector<int> node_to_time(ssize(pt.adj), -1);
+    vector<int> node_to_time(ssize(adj), -1);
     auto dfs = [&](auto&& self, int u, int p) -> void {
+        int mn_val_rmq = rmq_min.query(mn_idx[u], mn_idx[u] + len[u]);
+        assert(mn_val_rmq == mn_val[u]);
+        int mx_val_rmq = rmq_max.query(mn_idx[u], mn_idx[u] + len[u]);
+        assert(mx_val_rmq - mn_val_rmq + 1 == len[u]);
+        if (empty(adj[u])) {
+            assert(len[u] == 1);
+            assert(u == mn_idx[u]);
+            assert(a[u] == mn_val[u]);
+            assert(is_join[u]);
+        } else {
+            assert(len[u] > 1);
+            assert(ssize(adj[u]) >= 2);
+            assert(mn_idx[u] == mn_idx[adj[u][0]]);
+            assert(mn_idx[u] + len[u] == mn_idx[adj[u].back()] + len[adj[u].back()]);
+            for (int i = 1; i < ssize(adj[u]); i++) {
+                int prev = adj[u][i - 1], curr = adj[u][i];
+                assert(mn_idx[prev] + len[prev] == mn_idx[curr]);
+                assert(is_join[u] == my_touches(prev, curr, mn_val, len));
+            }
+            int mn_val_naive = n, mx_val_naive = -1, sum_len_naive = 0;
+            for (int v : adj[u]) {
+                mn_val_naive = min(mn_val_naive, mn_val[v]);
+                mx_val_naive = max(mx_val_naive, mn_val[v] + len[v]);
+                sum_len_naive += len[v];
+            }
+            assert(mn_val_naive == mn_val[u]);
+            assert(mx_val_naive == mn_val[u] + len[u]);
+            assert(sum_len_naive == len[u]);
+        }
         node_to_time[u] = curr_time++;
-        cout << (p == -1 ? p : node_to_time[p]) << " " << pt.mn_idx[u] << " " << pt.mn_idx[u] + pt.len[u] - 1 << " " << (pt.is_join[u] ? "linear" : "prime") << '\n';
-        for (int v : pt.adj[u])
+        cout << (p == -1 ? p : node_to_time[p]) << " " << mn_idx[u] << " " << mn_idx[u] + len[u] - 1 << " " << (is_join[u] ? "linear" : "prime") << '\n';
+        for (int v : adj[u])
             self(self, v, u);
     };
-    dfs(dfs, pt.root, -1);
+    dfs(dfs, root, -1);
     return 0;
 }
