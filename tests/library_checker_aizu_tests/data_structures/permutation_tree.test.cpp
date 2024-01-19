8868aac2c4d0a04457b348274fd4d9a1ec93bb45
Luke Videckis <lukevideckis@gmail.com>
2024-01-19 13:15:18 -0600

    test perm tree on all permutations <= 8
    commit 3cb0fcbc3f1b5319914f1b192e6f55401ba92cf0
    Author: GitHub <noreply@github.com>
    Date:   Fri Jan 19 19:15:35 2024 +0000
    
        [auto-verifier] verify commit 91f96ea1f10f45c9966d127d2f0428b51f593dc3
    
    commit 91f96ea1f10f45c9966d127d2f0428b51f593dc3
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Fri Jan 19 13:13:58 2024 -0600
    
        reduce again
    
    commit 0445f26f6c1de668dc3a01522a556e026ea13fee
    Merge: 56b2b81 8259c8a
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Fri Jan 19 13:12:24 2024 -0600
    
        Merge branch 'perm_tree_small_tests' of github.com:lrvideckis/programming_team_code into perm_tree_small_tests
    
    commit 8259c8ace0671ba42dc56247ead0d608b8a8b061
    Author: GitHub <noreply@github.com>
    Date:   Fri Jan 19 19:12:25 2024 +0000
    
        [auto-verifier] verify commit e2aaf4e339aec700540289632e2e819d370b50e4
    
    commit 56b2b81c74452a2290da55a859ad1e9824ee7782
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Fri Jan 19 13:12:13 2024 -0600
    
        reduce
    
    commit e2aaf4e339aec700540289632e2e819d370b50e4
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Fri Jan 19 13:08:42 2024 -0600
    
        test perm tree on all permutations < 10

--- tests/library_checker_aizu_tests/data_structures/permutation_tree.test.cpp
+++ tests/library_checker_aizu_tests/data_structures/permutation_tree.test.cpp
@@ -1,7 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/common_interval_decomposition_tree"
 #include "../template.hpp"
+#include "../perm_tree_asserts.hpp"
 
-#include "../../../library/data_structures/rmq.hpp"
 #include "../../../library/data_structures/uncommon/permutation_tree.hpp"
 
 int main() {
@@ -11,56 +11,18 @@ int main() {
     vector<int> a(n);
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    RMQ rmq_min(a, [](int x, int y) {return min(x, y);});
-    RMQ rmq_max(a, [](int x, int y) {return max(x, y);});
     perm_tree pt(a);
+    perm_tree_asserts(a, pt);
     auto is_join = pt.is_join;
     auto mn_idx = pt.mn_idx;
     auto mn_val = pt.mn_val;
     auto len = pt.len;
     auto root = pt.root;
     auto adj = pt.adj;
-    assert(mn_idx[root] == 0 && mn_val[root] == 0 && len[root] == n);
     cout << ssize(adj) << '\n';
     int curr_time = 0;
     vector<int> node_to_time(ssize(adj), -1);
     auto dfs = [&](auto&& self, int u, int p) -> void {
-        {
-            int mn_val_rmq = rmq_min.query(mn_idx[u], mn_idx[u] + len[u]);
-            assert(mn_val_rmq == mn_val[u]);
-            int mx_val_rmq = rmq_max.query(mn_idx[u], mn_idx[u] + len[u]);
-            assert(mx_val_rmq - mn_val_rmq + 1 == len[u]);
-        }
-        if (empty(adj[u])) {
-            assert(len[u] == 1);
-            assert(u == mn_idx[u]);
-            assert(a[u] == mn_val[u]);
-            assert(is_join[u]);
-        } else {
-            if (is_join[u]) assert(len[u] >= 2);
-            else assert(len[u] >= 3);
-            assert(ssize(adj[u]) >= 2);
-            assert(mn_idx[u] == mn_idx[adj[u][0]]);
-            assert(mn_idx[u] + len[u] == mn_idx[adj[u].back()] + len[adj[u].back()]);
-            assert(is_join[u] == (mn_val[u] == mn_val[adj[u][0]] || mn_val[u] == mn_val[adj[u].back()]));
-            assert(is_join[u] == (mn_val[u] + len[u] == mn_val[adj[u][0]] + len[adj[u][0]] ||
-                                  mn_val[u] + len[u] == mn_val[adj[u].back()] + len[adj[u].back()]));
-            for (int i = 1; i < ssize(adj[u]); i++) {
-                int prev = adj[u][i - 1], curr = adj[u][i];
-                assert(mn_idx[prev] + len[prev] == mn_idx[curr]);
-                bool curr_touches = pt.touches(prev, curr);
-                assert(is_join[u] == curr_touches);
-            }
-            int mn_val_naive = n, mx_val_naive = -1, sum_len_naive = 0;
-            for (int v : adj[u]) {
-                mn_val_naive = min(mn_val_naive, mn_val[v]);
-                mx_val_naive = max(mx_val_naive, mn_val[v] + len[v]);
-                sum_len_naive += len[v];
-            }
-            assert(mn_val_naive == mn_val[u]);
-            assert(mx_val_naive == mn_val[u] + len[u]);
-            assert(sum_len_naive == len[u]);
-        }
         node_to_time[u] = curr_time++;
         cout << (p == -1 ? p : node_to_time[p]) << " " << mn_idx[u] << " " << mn_idx[u] + len[u] - 1 << " " << (is_join[u] ? "linear" : "prime") << '\n';
         for (int v : adj[u])
---
+++ tests/library_checker_aizu_tests/handmade_tests/permutation_tree_small.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+#include "../template.hpp"
+#include "../perm_tree_asserts.hpp"
+
+#include "../../../library/data_structures/uncommon/permutation_tree.hpp"
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    for (int n = 1; n <= 8; n++) {
+        vector<int> a(n);
+        iota(begin(a), end(a), 0);
+        do {
+            perm_tree pt(a);
+            perm_tree_asserts(a, pt);
+        } while (next_permutation(begin(a), end(a)));
+    }
+    cout << "Hello World\n";
+    return 0;
+}
