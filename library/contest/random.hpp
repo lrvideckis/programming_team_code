313f5b5548ce540af21e0c69dc029ccfa9716a31
Luke Videckis <lrvideckis@gmail.com>
2024-01-23 20:37:22 -0600

    clean up comment

--- library/contest/random.hpp
+++ library/contest/random.hpp
@@ -4,17 +4,15 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 /**
  * @see https://codeforces.com/blog/entry/61675
  * @code{.cpp}
-       unsigned int x = rng();
-       // 64-bit rng
        mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
        unsigned long long x = rng();
        // fixed seed for debugging
        mt19937 rng;
        unsigned int x = rng();
+       //
        vector<int> a(n);
        shuffle(begin(a), end(a), rng);
-       vector<unsigned int> a(n);
-       generate(begin(a), end(a), rng);
+       //
        int x = get_rand<int>(0, 1);
        long long x = get_rand<long long>(1, 1e18);
  * @endcode
