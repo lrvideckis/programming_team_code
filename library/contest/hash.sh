0cf172270b49c777030d93d556a4316ede0787de
Luke Videckis <lukevideckis@gmail.com>
2024-02-13 14:45:41 -0600

    consistency with KACTL's hash

--- library/contest/hash.sh
+++ library/contest/hash.sh
@@ -2,5 +2,5 @@
 # Do type asserts
 
 # cat a.cpp | bash hash.sh
-# cat a.cpp | tr -d '[:space:]' | md5sum
-tr -d '[:space:]' | md5sum
+# cat a.cpp | tr -d '[:space:]' | md5sum | cut -c-6
+tr -d '[:space:]' | md5sum | cut -c-6
