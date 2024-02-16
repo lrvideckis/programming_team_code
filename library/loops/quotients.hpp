90814bba96dd6548ff19e5c9054bf0635595509c
Luke Videckis <lukevideckis@gmail.com>
2024-02-15 21:16:36 -0600

    now comment formatting won't be messed up

--- library/loops/quotients.hpp
+++ library/loops/quotients.hpp
@@ -2,7 +2,13 @@
 #pragma once
 /**
  * @see https://github.com/tfg50/Competitive-Programming/ blob/master/Biblioteca/Math/DivisionTrick.cpp
- * division trick
+ *
+ * i-th iteration gives an interval [le_i, ri_i], we have:
+ *     - for all i in [le_i, ri_i]: n / i has the same value
+ *     - le_0 = 1
+ *     - ri_last = n
+ *     - ri_i + 1 = le_(i+1)
+ *
  * @param n,f number and helper function
  * @time O(sqrt(n))
  * @space O(1)
@@ -10,13 +16,6 @@
 template <class F> void quotients(long long n, F f) {
 	for (long long le = 1, ri; le <= n; le = ri + 1) {
 		ri = n / (n / le);
-		/**
-		 * i-th iteration gives an interval [le_i, ri_i], we have:
-		 *     - for all i in [le_i, ri_i]: n / i has the same value
-		 *     - le_0 = 1
-		 *     - ri_last = n
-		 *     - ri_i + 1 = le_(i+1)
-		 */
 		f(le, ri);
 	}
 }
