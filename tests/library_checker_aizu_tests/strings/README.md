778478342f0e7570376eedde216c88fdff722783
Luke Videckis <lukevideckis@gmail.com>
2024-02-16 14:06:38 -0600

    removing again

--- tests/library_checker_aizu_tests/strings/README.md
+++
@@ -1,1 +0,0 @@
-Most string algs are intended to work for both `std::string` and `std::vector`. To ensure they compile with `std::vector`'s, add them to [this test](../handmade_tests/string_with_vector.test.cpp).
--- tests/scripts/compile_gcc17.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# ** glob now searches any number of levels
-shopt -s globstar
-
-git submodule init
-git submodule update
-
-find library_checker_aizu_tests/ -type f -name "*.test.cpp" |
-	parallel g++ {} "$(tr '\n' ' ' <.config/.gcc_compile_flags)" -std=c++17
