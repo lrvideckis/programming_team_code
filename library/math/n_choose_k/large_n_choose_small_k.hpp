36765e4a6ed5257cbd70c1aa8c373dca2522ce12
Luke Videckis <lukevideckis@gmail.com>
2024-02-19 21:33:40 -0600

    less mods, better style
    commit 282fb0a186747ba8193da4c6dc0ac468cf0aa161
    Merge: 1f65abf 24f8ac5
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Mon Feb 19 21:33:22 2024 -0600
    
        Merge branch 'dev' into less_mod_operations
    
    commit 1f65abff720be1034c977fb56715f1d4f69128cf
    Author: Luke Videckis <lukevideckis@gmail.com>
    Date:   Mon Feb 19 21:32:16 2024 -0600
    
        less mods, better style

--- library/math/n_choose_k/large_n_choose_small_k.hpp
+++ library/math/n_choose_k/large_n_choose_small_k.hpp
@@ -2,16 +2,16 @@
 #pragma once
 #include "factorial.hpp"
 /**
- * @param n arbitrarily large integer; n - k < mod < n is ok
+ * @param n arbitrarily large integer
  * @param k integer with k < mod
  * @returns number of ways to choose k objects out of n
  * @time O(k)
  * @space O(1) ammortized
  */
 inline int64_t c_small_k(int64_t n, int k) {
-	if (k < 0 || n < k) return 0;
+	if (k < 0 || n < k || (n %= mod) < k) return 0;
 	grow(k);
 	auto res = inv_fact[k];
-	for (int64_t i = n; i > n - k && res; i--) res = i % mod * res % mod;
+	for (int i = n; i > n - k; i--) res = res * i % mod;
 	return res;
 }
